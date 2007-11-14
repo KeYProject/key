e934138a0b570a1c6f012cbfd0de22c247ba7933
Jochen Seidel <jseidel@ira.uka.de>
2007-11-14 22:03:13 +0100

    some more work. strange things happen.

--- system/de/uka/ilkd/key/rule/metaconstruct/MethodCallToUpdate.java
+++ system/de/uka/ilkd/key/rule/metaconstruct/MethodCallToUpdate.java
@@ -2,8 +2,10 @@ package de.uka.ilkd.key.rule.metaconstruct;
 
 import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.logic.Name;
+import de.uka.ilkd.key.logic.TermFactory;
 import de.uka.ilkd.key.logic.op.AbstractMetaOperator;
 import de.uka.ilkd.key.logic.op.EntryOfSchemaVariableAndInstantiationEntry;
+import de.uka.ilkd.key.logic.op.IProgramVariable;
 import de.uka.ilkd.key.logic.op.IteratorOfEntryOfSchemaVariableAndInstantiationEntry;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.rule.inst.ProgramInstantiation;
@@ -23,18 +25,23 @@ import de.uka.ilkd.key.rule.inst.ProgramInstantiation;
  */
 
 public class MethodCallToUpdate extends AbstractMetaOperator {
-
+    
+    /* printf-style "debugging"... */
+    static void debug( String s ) {
+        System.out.println(s);          
+    }
+    
     public MethodCallToUpdate() {
         super(new Name("#methodCallToUpdate"), 2);
     }
     
     public Term calculate( Term term, SVInstantiations svInst, Services services ) {
         
-        System.out.println("MethodCallToUpdate.calculate() called.");
-        System.out.println("term.sub(0) is of type " + term.sub(0).getClass().getName() + " " +
-                        "and contains " + term.sub(0).toString() );
-        System.out.println("term.sub(1) is of type " + term.sub(1).getClass().getName() + " " +
-                        "and contains " + term.sub(1).toString() );
+        debug("MethodCallToUpdate.calculate() called.");
+        debug("term.sub(0) is of type " + term.sub(0).getClass().getName() + " " +
+              "and contains " + term.sub(0).toString() );
+        debug("term.sub(1) is of type " + term.sub(1).getClass().getName() + " " +
+              "and contains " + term.sub(1).toString() );
         //System.out.println("term.sub(2) is of type " + term.sub(2).getClass().getName() );
 
         IteratorOfEntryOfSchemaVariableAndInstantiationEntry it
@@ -47,18 +54,23 @@ public class MethodCallToUpdate extends AbstractMetaOperator {
                 ProgramElement pe = pi.getProgramElement();
                 
                 if( pe instanceof MethodBodyStatement ) {
+                    assert( spmbs == null ); // there shouldn't be more than one instanciated method body statement
                     spmbs = (MethodBodyStatement)pe;
                 }
             }
         }
         
-        assert( spmbs != null );
-        // fish for the first method body statement
+        assert( spmbs != null ); // .. and at least one
+        
+        debug(spmbs.toString());
+        
+        IProgramVariable method_result = spmbs.getResultVariable();
         
         
-        System.out.println(spmbs.toString());
+        //Term location = TermFactory.DEFAULT.createVariableTerm( spmbs.getResultVariable(). );
+        //Term location_value;
         
-        
+        //return TermFactory.DEFAULT.createUpdateTerm(location, location_value, term.sub(1));
         
         return term.sub(1);
     }
