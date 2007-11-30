d922cd4be8591614bb55e456551ba3952f1ac65a
Richard Bubel <bubel@Ardberg.local>
2007-11-30 20:11:30 +0100

    * fixed sound play in bytecode and webstart version * updated README file with supported set of SMT solvers

--- system/de/uka/ilkd/key/gui/notification/actions/GeneralInformationSoundPlay.java
+++ system/de/uka/ilkd/key/gui/notification/actions/GeneralInformationSoundPlay.java
@@ -3,7 +3,6 @@
  */
 package de.uka.ilkd.key.gui.notification.actions;
 
-import java.io.File;
 import java.net.URL;
 
 import de.uka.ilkd.key.gui.notification.NotificationTask;
@@ -24,7 +23,7 @@ public class GeneralInformationSoundPlay extends PlaySound {
             KeYResourceManager.getManager().getResourceFile
         (NotificationTask.class, "sounds/information.wav");
         if (internalSoundFile != null) {
-            setSoundFile(new File(internalSoundFile.getFile()));
+            setSoundURL(internalSoundFile);
         }        
     }
 
