9bef4c0123fe79b5bc4651f498bbb5b88afa2b0d
Christian Engel <engelc@ira.uka.de>
2008-06-20 13:48:55 +0200

    merged with master

--- system/resources/de/uka/ilkd/key/java/JavaRedux/javax/realtime/RealtimeSystem.java
+++ system/resources/de/uka/ilkd/key/java/JavaRedux/javax/realtime/RealtimeSystem.java
@@ -2,16 +2,16 @@ package javax.realtime;
 
 public class RealtimeSystem{
 
-    //@ public static invariant OOME!=null && \inImmortalMemory(OOME);
+    //@ public static invariant \inImmortalMemory(OOME);
 
-    //@ public static invariant IAE!=null && \inImmortalMemory(IAE);
+    // public static invariant IAE!=null && \inImmortalMemory(IAE);
 
-    //@ public static invariant TBE!=null && \inImmortalMemory(TBE);
+    //@ public static invariant \inImmortalMemory(TBE);
 
     public static final OutOfMemoryError OOME = new OutOfMemoryError();
 
-    public static final IllegalAssignmentError IAE = 
-	new IllegalAssignmentError();
+    //    public static final IllegalAssignmentError IAE = 
+    //	new IllegalAssignmentError();
 
     public static final ThrowBoundaryError TBE = 
 	new ThrowBoundaryError();
@@ -20,9 +20,9 @@ public class RealtimeSystem{
 	return OOME;
     }
 
-    public static IllegalAssignmentError iae(){
-	return IAE;
-    }
+    //    public static IllegalAssignmentError iae(){
+    //	return IAE;
+    //    }
 
     public static IllegalAssignmentError tbe(){
 	return TBE;
