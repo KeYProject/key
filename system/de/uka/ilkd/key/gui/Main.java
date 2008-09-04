65a71d18e4a543ec9f4e0479a3e5d83b9bd09e9e
Richard Bubel <bubel@chalmers.se>
2008-09-04 10:46:48 +0200

    Fixed typo

--- system/de/uka/ilkd/key/gui/Main.java
+++ system/de/uka/ilkd/key/gui/Main.java
@@ -69,7 +69,7 @@ public class Main extends JFrame implements IMain {
 
     private static final String VERSION = 
 	KeYResourceManager.getManager().getVersion() + 
-	"-Technological Preview (internal: "+INTERNAL_VERSION+")";
+	"-Technology Preview (internal: "+INTERNAL_VERSION+")";
 
     private static final String COPYRIGHT="(C) Copyright 2001-2008 "
         +"Universit\u00e4t Karlsruhe, Universit\u00e4t Koblenz-Landau, "
