946f0a8ed6a90bd657e140d0af4bf1d8e08b19f6
Richard Bubel <bubel@chalmers.se>
2008-09-25 17:36:18 +0200

    Fixed bug with test of empty intersection sorts causing unsound MV unification

--- system/de/uka/ilkd/key/logic/sort/IntersectionSort.java
+++ system/de/uka/ilkd/key/logic/sort/IntersectionSort.java
@@ -382,7 +382,8 @@ public class IntersectionSort extends AbstractSort {
        emptyDomain = false;
                      
        for (int i = 0, sz = memberCount(); i<sz; i++) {
-           final Sort s = getComponent(i);         
+           final Sort s = getComponent(i);    
+           
            if (s instanceof PrimitiveSort) {
                nonReferenceType = true;
            } else if (s instanceof IntersectionSort) {               
@@ -398,17 +399,14 @@ public class IntersectionSort extends AbstractSort {
                if (s_intersect.hasEmptyDomain()) {
                    emptyDomainComputed = true;
                    emptyDomain = true;
-                   break;
-               } else {
-                   if (nonReferenceType) {
-                       emptyDomainComputed = true;
-                       emptyDomain = true;
-                       break;
-                   }
+                   return emptyDomain;
                }               
            }                  
        }
-       emptyDomainComputed = true;       
+       emptyDomainComputed = true;
+       if (nonReferenceType) {
+           emptyDomain = true;
+       }
        return emptyDomain;
     }
     
