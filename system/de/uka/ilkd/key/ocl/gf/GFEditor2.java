3a823f0b40314dd22fbf4cbebc79dd64efc70699
Richard Bubel <bubel@chalmers.se>
2009-03-17 15:52:09 +0100

    updated headers

--- system/de/uka/ilkd/key/ocl/gf/GFEditor2.java
+++ system/de/uka/ilkd/key/ocl/gf/GFEditor2.java
@@ -1,3 +1,10 @@
+// This file is part of KeY - Integrated Deductive Software Design
+// Copyright (C) 2001-2009 Universitaet Karlsruhe, Germany
+//                         Universitaet Koblenz-Landau, Germany
+//                         Chalmers University of Technology, Sweden
+//
+// The KeY system is protected by the GNU General Public License. 
+// See LICENSE.TXT for details.
 //Copyright (c) Janna Khegai 2004, Kristofer Johanisson 2004, 
 //              Hans-Joachim Daniels 2005
 //
--- system/de/uka/ilkd/key/visualization/VisualizationModel.java
+++ system/de/uka/ilkd/key/visualization/VisualizationModel.java
@@ -1,3 +1,10 @@
+// This file is part of KeY - Integrated Deductive Software Design
+// Copyright (C) 2001-2009 Universitaet Karlsruhe, Germany
+//                         Universitaet Koblenz-Landau, Germany
+//                         Chalmers University of Technology, Sweden
+//
+// The KeY system is protected by the GNU General Public License. 
+// See LICENSE.TXT for details.
 package de.uka.ilkd.key.visualization;
 
 import java.util.Arrays;
