60a3c22168407aaa11756d3afaef52bbde400e4f
Richard Bubel <bubel@chalmers.se>
2009-04-16 23:39:33 +0200

    Fix bug that caused nullpointer exceptions when proving using the FOL strategy.

--- system/de/uka/ilkd/key/logic/BoundedNumericalQuantifierTerm.java
+++ system/de/uka/ilkd/key/logic/BoundedNumericalQuantifierTerm.java
@@ -3,26 +3,23 @@ package de.uka.ilkd.key.logic;
 import de.uka.ilkd.key.logic.op.ArrayOfQuantifiableVariable;
 import de.uka.ilkd.key.logic.op.BoundedNumericalQuantifier;
 
+/**
+ * Represents a term with a numerical quantifier as top level operator.
+ * 
+ * This kind of terms bind variables in their third sub term, while the first
+ * two ones express range restrictions.
+ */
 class BoundedNumericalQuantifierTerm extends OpTerm.ArbitraryOpTerm {
 
-    private ArrayOfQuantifiableVariable varsBoundHere;
+    private final ArrayOfQuantifiableVariable varsBoundHere;
 
-    public BoundedNumericalQuantifierTerm(BoundedNumericalQuantifier op, Term[] subTerm, 
+    BoundedNumericalQuantifierTerm(BoundedNumericalQuantifier op, Term[] subTerm, 
             ArrayOfQuantifiableVariable varsBoundHere) {
         super(op, subTerm);
 	this.varsBoundHere = varsBoundHere;
     }
 
     public ArrayOfQuantifiableVariable varsBoundHere(int n) {
-	return n==2? varsBoundHere : EMPTY_VAR_LIST;
+	return n == 2 ? varsBoundHere : EMPTY_VAR_LIST;
     }
-
-    public boolean equals(Object o){
-	return equalsModRenaming(o);
-    }
-    
-    public int hashCode(){
-	return sub(0).hashCode()*3 + sub(1).hashCode()*7;
-    }
-
 }
