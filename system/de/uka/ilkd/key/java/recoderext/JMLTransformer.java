9932445c086d16ee843a55a0cc8f24b1e68749ca
Richard Bubel <bubel@chalmers.se>
2010-07-28 16:30:39 +0200

    [Cleanup] Remove system.out.println

--- system/de/uka/ilkd/key/java/recoderext/JMLTransformer.java
+++ system/de/uka/ilkd/key/java/recoderext/JMLTransformer.java
@@ -546,7 +546,6 @@ public class JMLTransformer extends RecoderModelTransformer {
              tdc.walk(unit);
              HashSet<TypeDeclaration> typeDeclarations = tdc.result();               
              for(TypeDeclaration td : typeDeclarations) { 
-                 System.out.println("Focus on:" + td.getFullName());
                  //collect pre-existing operations
                  List<? extends Constructor> constructorList 
                      = td.getConstructors();
