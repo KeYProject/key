92d3a80339f7596fcd3fd7a63ca86a2f67ef1215
Richard Bubel <bubel@chalmers.se>
2010-08-09 17:33:16 +0200

    [Strings] Completed some contracts

--- examples/strings/Sanitization/src/Sanitize.java
+++ examples/strings/Sanitization/src/Sanitize.java
@@ -3,10 +3,10 @@ public class Sanitize {
     // private static String[] exceptions = {"em", "strong", "h1"}; 
 
     /*@ public normal_behavior 
-      @ ensures \result.indexOf('<') == -1;
+      @ ensures \result.indexOf('<') == -1 && \result.indexOf('>') == -1;
       @*/
     public static String simpleSanitize(String s) {
-	return s.replace('<', '[');//.replace('>',']');
+	return s.replace('<', '[').replace('>',']');
     }
 
 
