f63114f8e5491f59d840bba778f72a63d0b83b77
Daniel Bruns <bruns@kit.edu>
2011-04-20 17:18:55 +0200

    working on red-black tree example; adding operations to sequence ADT

---
+++ system/de/uka/ilkd/key/java/expression/operator/SeqIndexOf.java
@@ -0,0 +1,44 @@
+// This file is part of KeY - Integrated Deductive Software Design
+// Copyright (C) 2001-2011 Universitaet Karlsruhe, Germany
+//                         Universitaet Koblenz-Landau, Germany
+//                         Chalmers University of Technology, Sweden
+//
+// The KeY system is protected by the GNU General Public License. 
+// See LICENSE.TXT for details.
+//
+//
+
+
+package de.uka.ilkd.key.java.expression.operator;
+
+import de.uka.ilkd.key.java.PrettyPrinter;
+import de.uka.ilkd.key.java.visitor.Visitor;
+import de.uka.ilkd.key.util.ExtList;
+
+public class SeqIndexOf extends BinaryOperator {
+
+    public SeqIndexOf(ExtList children) {
+        super(children);
+    }
+
+
+    public int getPrecedence() {
+        return 0;
+    }
+
+
+    public int getNotation() {
+        return PREFIX;
+    }
+
+
+    public void visit(Visitor v) {
+	v.performActionOnSeqIndexOf(this);
+    }
+
+    
+    public void prettyPrint(PrettyPrinter p) throws java.io.IOException {
+        p.printIndexOf(this);
+    }
+
+}
