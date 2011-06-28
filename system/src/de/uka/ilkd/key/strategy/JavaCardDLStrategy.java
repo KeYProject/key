698bb7fbc93551b263594072ebb55064b6cc6a53
Richard Bubel <bubel@chalmers.se>
2011-06-28 04:00:20 +0200

    increased proof step limit for some benchmarks

--- system/src/de/uka/ilkd/key/strategy/JavaCardDLStrategy.java
+++ system/src/de/uka/ilkd/key/strategy/JavaCardDLStrategy.java
@@ -141,7 +141,7 @@ public final class JavaCardDLStrategy extends AbstractFeatureStrategy {
         }
         
         final Feature oneStepSimplificationF 
-        	= oneStepSimplificationFeature(longConst(-9000));
+        	= oneStepSimplificationFeature(longConst(-11000));
       //  final Feature smtF = smtFeature(inftyConst());
 
         return SumFeature.createSum ( new Feature [] {
@@ -185,12 +185,7 @@ public final class JavaCardDLStrategy extends AbstractFeatureStrategy {
         return ConditionalFeature.createConditional(filter, cost);        
     }
     
-    private Feature pullOutConditionalsFeature(Feature cost) {
-	SetRuleFilter filter = new SetRuleFilter();
-	filter.addRuleToSet(PullOutConditionalsRule.INSTANCE);
-        return ConditionalFeature.createConditional(filter, cost);        
-    }     
-    
+   
     //private Feature smtFeature(Feature cost) {
 	//ClassRuleFilter filter = new ClassRuleFilter(SMTRule.class);
         //return ConditionalFeature.createConditional(filter, cost);        
