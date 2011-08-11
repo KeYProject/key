d5e7cc8a2c869fad4b2b555428ac39bd65bc8751
Daniel Bruns <bruns@kit.edu>
2011-08-11 13:11:03 +0200

    [RBT] changed to BSD license (due to VACID publication policy)

--- examples/heap/vacid0_05_RedBlackTrees/src/Harness.java
+++ examples/heap/vacid0_05_RedBlackTrees/src/Harness.java
@@ -1,12 +1,6 @@
-// This file is part of KeY - Integrated Deductive Software Design
-// Copyright (C) 2001-2011 Universitaet Karlsruhe, Germany
-//                         Universitaet Koblenz-Landau, Germany
-//                         Chalmers University of Technology, Sweden
-//
-// The KeY system is protected by the GNU General Public License. 
-// See LICENSE.TXT for details.
-//
-//
+// Copyright (C) 2011 Daniel Bruns
+// Published under Modified BSD License
+// See LICENSE for details.
 
 
 package vacid0.redblacktree;
