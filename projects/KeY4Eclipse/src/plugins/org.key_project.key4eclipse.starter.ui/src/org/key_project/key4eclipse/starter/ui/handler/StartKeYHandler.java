bfbf0a80806937ca2e2a9caf8f5f226dd92fe857
Martin Hentschel <mahentschel@gmx.de>
2012-01-26 11:09:44 +0100

    Next try for synchronous message execution in Swing and SWT on Mac OS.

--- projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.starter.ui/src/org/key_project/key4eclipse/starter/ui/handler/StartKeYHandler.java
+++ projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.starter.ui/src/org/key_project/key4eclipse/starter/ui/handler/StartKeYHandler.java
@@ -1,20 +1,7 @@
 package org.key_project.key4eclipse.starter.ui.handler;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.awt.SWT_AWT;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Shell;
 import org.key_project.key4eclipse.util.key.KeYUtil;
-
-import de.uka.ilkd.key.util.removegenerics.Main;
 
 /**
  * Handler that starts the KeY UI via {@link KeYUtil#openMainWindow()}.
@@ -25,33 +12,9 @@ public class StartKeYHandler extends AbstractSaveExecutionHandler {
      */
     @Override
     protected Object doExecute(ExecutionEvent event) throws Exception {
-        Shell shell = new Shell();
-        shell.setText("SWT with Swing");
-        shell.setLayout(new FillLayout());
-        shell.setSize(200, 200);
-        Composite swtAwtComponent = new Composite(shell, SWT.EMBEDDED);
-        java.awt.Frame frame = SWT_AWT.new_Frame( swtAwtComponent );
-        frame.setLayout(new BorderLayout());
-        javax.swing.JPanel panel = new javax.swing.JPanel( );
-        panel.setLayout(new BorderLayout());
-        JButton button = new JButton("JButton");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    KeYUtil.openMainWindow();
-                }
-                catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-            }
-        });
-        panel.add(button, BorderLayout.CENTER);
-        frame.add(panel, BorderLayout.CENTER);
-        frame.setVisible(true);
-        shell.setVisible(true);
-        
-//        KeYUtil.openMainWindowAsync();
+        System.out.println("### START KEY ###");
+        KeYUtil.openMainWindow();
+        System.out.println("### END KEY ###");
         return null;
     }
 }
--- projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.util/src/org/key_project/key4eclipse/util/java/SwingUtil.java
+++ projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.util/src/org/key_project/key4eclipse/util/java/SwingUtil.java
@@ -18,6 +18,16 @@ public final class SwingUtil {
     }
     
     /**
+     * Executes the {@link Runnable} in the {@link Thread} of Swing asynchronous.
+     * @param run The {@link Runnable} to execute.
+     * @throws InterruptedException Occurred Exception.
+     * @throws InvocationTargetException Occurred Exception.
+     */
+    public static void invokeLater(Runnable run) throws InterruptedException, InvocationTargetException {
+        SwingUtilities.invokeLater(run);
+    }
+
+    /**
      * <p>
      * Executes the {@link Runnable} in the {@link Thread} of Swing synchronous.
      * </p>
@@ -25,6 +35,13 @@ public final class SwingUtil {
      * <b>Attention:</b> This method does not work correctly from
      * {@link Display}s Thread on Mac OS. Also {@link Object#wait()}
      * and {@link Object#notify()} don't work in this scenario.
+     * </p>
+     * <p>
+     * More informations about SWT and Swing integration have a look at:
+     * <ul>
+     *    <li><a href="http://www.eclipsezone.com/eclipse/forums/t45697.html">http://www.eclipsezone.com/eclipse/forums/t45697.html</li>
+     *    <li><a href="http://www.eclipse.org/articles/article.php?file=Article-Swing-SWT-Integration/index.html">http://www.eclipse.org/articles/article.php?file=Article-Swing-SWT-Integration/index.html</li>
+     * </ul> 
      * </p>
      * @param run The {@link Runnable} to execute.
      * @throws InterruptedException Occurred Exception.
@@ -36,25 +53,35 @@ public final class SwingUtil {
                 run.run();
             }
             else {
-                SwingUtilities.invokeAndWait(run);
+                RunnableLock lock = new RunnableLock(run);
+                SwingUtilities.invokeLater(lock);
+                while (!lock.done()) {
+                    if (!Display.getDefault().readAndDispatch()) {
+                        Display.getDefault().sleep();
+                    }
+                }
             }
         }
     }
     
-    /**
-     * Executes the {@link Runnable} in the {@link Thread} of Swing asynchronous.
-     * @param run The {@link Runnable} to execute.
-     * @throws InterruptedException Occurred Exception.
-     * @throws InvocationTargetException Occurred Exception.
-     */
-    public static void invokeLater(Runnable run) throws InterruptedException, InvocationTargetException {
-        if (run != null) {
-            if (SwingUtilities.isEventDispatchThread()) {
+    private static class RunnableLock implements Runnable {
+        private Runnable run;
+        
+        private RunnableLock(Runnable run) {
+            super();
+            this.run = run;
+        }
+
+        public boolean done() {
+            return run == null;
+        }
+
+        @Override
+        public void run() {
+            if (run != null) {
                 run.run();
             }
-            else {
-                SwingUtilities.invokeLater(run);
-            }
+            run = null;
         }
     }
 }
