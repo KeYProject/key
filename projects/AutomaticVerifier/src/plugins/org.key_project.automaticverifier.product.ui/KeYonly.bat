dc808e41c5a1525dff56fe2e39baf1ad3e1de2b0
Martin Hentschel <mahentschel@gmx.de>
2012-04-25 16:48:07 +0200

    MonKeY: Renamed "Automatic Verifier" into "MonKeY" because the old name suggests that the verification is automatic which is wrong because the tool simplifies just the required interactions in KeY to proof an overall project.

--- projects/AutomaticVerifier/src/plugins/org.key_project.automaticverifier.product.ui/KeYonly.bat
+++
@@ -1,1 +0,0 @@
-AutomaticVerifier KeYonly
---
+++ projects/MonKeY/src/plugins/org.key_project.monkey.product.ui/src/org/key_project/monkey/product/ui/application/MonKeYWorkbenchAdvisor.java
@@ -0,0 +1,44 @@
+package org.key_project.monkey.product.ui.application;
+
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+import org.key_project.monkey.product.ui.perspective.MonKeYPerspective;
+
+/**
+ * <p>
+ * Special implementation of {@link WorkbenchAdvisor} for the product "MonKeY".
+ * </p>
+ * <p>
+ * For more information about RCP based products have a look at:
+ * <a href="http://javawiki.sowas.com/doku.php?id=eclipse-rcp:source-example-application">http://javawiki.sowas.com/doku.php?id=eclipse-rcp:source-example-application</a>
+ * </p> 
+ * @author Martin Hentschel
+ */
+public class MonKeYWorkbenchAdvisor extends WorkbenchAdvisor {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getInitialWindowPerspectiveId() {
+        return MonKeYPerspective.ID;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(IWorkbenchConfigurer configurer) {
+        super.initialize(configurer);
+        configurer.setSaveAndRestore(true); // Enable state storage via IMemento
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new MonKeYWorkbenchWindowAdvisor(configurer);
+    }
+}
