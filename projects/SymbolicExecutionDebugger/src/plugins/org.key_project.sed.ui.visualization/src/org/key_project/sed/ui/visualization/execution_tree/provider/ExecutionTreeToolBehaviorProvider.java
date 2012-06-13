dcc3994cb61755ea5f02c2addb8ef13b4483603e
Martin Hentschel <mahentschel@gmx.de>
2012-06-13 17:51:16 +0200

    SED: Implemented read-only context actions for symbolic execution tree diagrams to execute the eclipse debug commands.

--- projects/SymbolicExecutionDebugger/src/plugins/org.key_project.sed.ui.visualization/src/org/key_project/sed/ui/visualization/execution_tree/provider/ExecutionTreeToolBehaviorProvider.java
+++ projects/SymbolicExecutionDebugger/src/plugins/org.key_project.sed.ui.visualization/src/org/key_project/sed/ui/visualization/execution_tree/provider/ExecutionTreeToolBehaviorProvider.java
@@ -5,10 +5,21 @@ import java.util.List;
 
 import org.eclipse.graphiti.dt.IDiagramTypeProvider;
 import org.eclipse.graphiti.features.context.IPictogramElementContext;
+import org.eclipse.graphiti.features.context.impl.CustomContext;
+import org.eclipse.graphiti.features.custom.ICustomFeature;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.palette.IPaletteCompartmentEntry;
+import org.eclipse.graphiti.tb.ContextButtonEntry;
 import org.eclipse.graphiti.tb.DefaultToolBehaviorProvider;
+import org.eclipse.graphiti.tb.IContextButtonEntry;
 import org.eclipse.graphiti.tb.IContextButtonPadData;
 import org.eclipse.graphiti.tb.IToolBehaviorProvider;
+import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeResumeFeature;
+import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeStepIntoFeature;
+import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeStepOverFeature;
+import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeStepReturnFeature;
+import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeSuspendFeature;
+import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeTerminateFeature;
 import org.key_project.util.java.CollectionUtil;
 
 /**
@@ -47,8 +58,34 @@ public class ExecutionTreeToolBehaviorProvider extends DefaultToolBehaviorProvid
          return super.getContextButtonPad(context);
       }
       else {
-         return null;
+         IContextButtonPadData data = super.getContextButtonPad(context);
+         data.getGenericContextButtons().clear();
+         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeStepReturnFeature(getFeatureProvider()), context, "Step Return", null, IExecutionTreeImageConstants.IMG_STEP_RETURN));
+         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeStepOverFeature(getFeatureProvider()), context, "Step Over", null, IExecutionTreeImageConstants.IMG_STEP_OVER));
+         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeStepIntoFeature(getFeatureProvider()), context, "Step Into", null, IExecutionTreeImageConstants.IMG_STEP_INTO));
+
+         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeTerminateFeature(getFeatureProvider()), context, "Terminate", null, IExecutionTreeImageConstants.IMG_TERMINATE));
+         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeSuspendFeature(getFeatureProvider()), context, "Suspend", null, IExecutionTreeImageConstants.IMG_SUSPEND));
+         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeResumeFeature(getFeatureProvider()), context, "Resume", null, IExecutionTreeImageConstants.IMG_RESUME));
+         return data;
       }
+   }
+   
+   /**
+    * Creates an {@link IContextButtonEntry} which executes an {@link ICustomFeature}.
+    * @param feature The {@link ICustomFeature} to execute.
+    * @param context The parent {@link IPictogramElementContext}.
+    * @param text The text.
+    * @param description The description.
+    * @param iconId The icon id.
+    * @return The created {@link IContextButtonEntry}.
+    */
+   protected IContextButtonEntry createCustomEntry(ICustomFeature feature, IPictogramElementContext context, String text, String description, String iconId) {
+      IContextButtonEntry entry = new ContextButtonEntry(feature, new CustomContext(new PictogramElement[] {context.getPictogramElement()}));
+      entry.setDescription(description);
+      entry.setText(text);
+      entry.setIconId(iconId);
+      return entry;
    }
 
    /**
