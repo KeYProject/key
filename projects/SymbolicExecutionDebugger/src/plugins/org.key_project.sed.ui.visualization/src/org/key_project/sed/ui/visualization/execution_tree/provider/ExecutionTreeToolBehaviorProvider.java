b292ea34efc2cbd830452e3b1543493f8dfb5f11
Martin Hentschel <mahentschel@gmx.de>
2012-06-14 09:43:04 +0200

    SED: Implemented custom context menu of symbolic execution tree diagrams.

--- projects/SymbolicExecutionDebugger/src/plugins/org.key_project.sed.ui.visualization/src/org/key_project/sed/ui/visualization/execution_tree/provider/ExecutionTreeToolBehaviorProvider.java
+++ projects/SymbolicExecutionDebugger/src/plugins/org.key_project.sed.ui.visualization/src/org/key_project/sed/ui/visualization/execution_tree/provider/ExecutionTreeToolBehaviorProvider.java
@@ -4,15 +4,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.graphiti.dt.IDiagramTypeProvider;
+import org.eclipse.graphiti.features.context.ICustomContext;
 import org.eclipse.graphiti.features.context.IPictogramElementContext;
 import org.eclipse.graphiti.features.context.impl.CustomContext;
 import org.eclipse.graphiti.features.custom.ICustomFeature;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.palette.IPaletteCompartmentEntry;
 import org.eclipse.graphiti.tb.ContextButtonEntry;
+import org.eclipse.graphiti.tb.ContextMenuEntry;
 import org.eclipse.graphiti.tb.DefaultToolBehaviorProvider;
 import org.eclipse.graphiti.tb.IContextButtonEntry;
 import org.eclipse.graphiti.tb.IContextButtonPadData;
+import org.eclipse.graphiti.tb.IContextMenuEntry;
 import org.eclipse.graphiti.tb.IToolBehaviorProvider;
 import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeResumeFeature;
 import org.key_project.sed.ui.visualization.execution_tree.feature.DebugNodeStepIntoFeature;
@@ -60,17 +63,17 @@ public class ExecutionTreeToolBehaviorProvider extends DefaultToolBehaviorProvid
       else {
          IContextButtonPadData data = super.getContextButtonPad(context);
          data.getGenericContextButtons().clear();
-         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeStepReturnFeature(getFeatureProvider()), context, "Step Return", null, IExecutionTreeImageConstants.IMG_STEP_RETURN));
-         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeStepOverFeature(getFeatureProvider()), context, "Step Over", null, IExecutionTreeImageConstants.IMG_STEP_OVER));
-         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeStepIntoFeature(getFeatureProvider()), context, "Step Into", null, IExecutionTreeImageConstants.IMG_STEP_INTO));
+         data.getGenericContextButtons().add(createCustomContextButtonEntry(new DebugNodeStepReturnFeature(getFeatureProvider()), context, "Step Return", null, IExecutionTreeImageConstants.IMG_STEP_RETURN));
+         data.getGenericContextButtons().add(createCustomContextButtonEntry(new DebugNodeStepOverFeature(getFeatureProvider()), context, "Step Over", null, IExecutionTreeImageConstants.IMG_STEP_OVER));
+         data.getGenericContextButtons().add(createCustomContextButtonEntry(new DebugNodeStepIntoFeature(getFeatureProvider()), context, "Step Into", null, IExecutionTreeImageConstants.IMG_STEP_INTO));
 
-         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeTerminateFeature(getFeatureProvider()), context, "Terminate", null, IExecutionTreeImageConstants.IMG_TERMINATE));
-         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeSuspendFeature(getFeatureProvider()), context, "Suspend", null, IExecutionTreeImageConstants.IMG_SUSPEND));
-         data.getGenericContextButtons().add(createCustomEntry(new DebugNodeResumeFeature(getFeatureProvider()), context, "Resume", null, IExecutionTreeImageConstants.IMG_RESUME));
+         data.getGenericContextButtons().add(createCustomContextButtonEntry(new DebugNodeTerminateFeature(getFeatureProvider()), context, "Terminate", null, IExecutionTreeImageConstants.IMG_TERMINATE));
+         data.getGenericContextButtons().add(createCustomContextButtonEntry(new DebugNodeSuspendFeature(getFeatureProvider()), context, "Suspend", null, IExecutionTreeImageConstants.IMG_SUSPEND));
+         data.getGenericContextButtons().add(createCustomContextButtonEntry(new DebugNodeResumeFeature(getFeatureProvider()), context, "Resume", null, IExecutionTreeImageConstants.IMG_RESUME));
          return data;
       }
    }
-   
+
    /**
     * Creates an {@link IContextButtonEntry} which executes an {@link ICustomFeature}.
     * @param feature The {@link ICustomFeature} to execute.
@@ -80,8 +83,49 @@ public class ExecutionTreeToolBehaviorProvider extends DefaultToolBehaviorProvid
     * @param iconId The icon id.
     * @return The created {@link IContextButtonEntry}.
     */
-   protected IContextButtonEntry createCustomEntry(ICustomFeature feature, IPictogramElementContext context, String text, String description, String iconId) {
+   protected IContextButtonEntry createCustomContextButtonEntry(ICustomFeature feature, IPictogramElementContext context, String text, String description, String iconId) {
       IContextButtonEntry entry = new ContextButtonEntry(feature, new CustomContext(new PictogramElement[] {context.getPictogramElement()}));
+      entry.setDescription(description);
+      entry.setText(text);
+      entry.setIconId(iconId);
+      return entry;
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public IContextMenuEntry[] getContextMenu(ICustomContext context) {
+      IContextMenuEntry[] menuEntries = super.getContextMenu(context);
+      if (isReadOnly()) {
+         List<IContextMenuEntry> result = new LinkedList<IContextMenuEntry>();
+         CollectionUtil.addAll(result, menuEntries);
+
+         result.add(createCustomContextMenuEntry(new DebugNodeResumeFeature(getFeatureProvider()), context, "Resume", null, IExecutionTreeImageConstants.IMG_RESUME));
+         result.add(createCustomContextMenuEntry(new DebugNodeSuspendFeature(getFeatureProvider()), context, "Suspend", null, IExecutionTreeImageConstants.IMG_SUSPEND));
+         result.add(createCustomContextMenuEntry(new DebugNodeTerminateFeature(getFeatureProvider()), context, "Terminate", null, IExecutionTreeImageConstants.IMG_TERMINATE));
+
+         result.add(createCustomContextMenuEntry(new DebugNodeStepOverFeature(getFeatureProvider()), context, "Step Over", null, IExecutionTreeImageConstants.IMG_STEP_OVER));
+         result.add(createCustomContextMenuEntry(new DebugNodeStepIntoFeature(getFeatureProvider()), context, "Step Into", null, IExecutionTreeImageConstants.IMG_STEP_INTO));
+         result.add(createCustomContextMenuEntry(new DebugNodeStepReturnFeature(getFeatureProvider()), context, "Step Return", null, IExecutionTreeImageConstants.IMG_STEP_RETURN));
+         return result.toArray(new IContextMenuEntry[result.size()]);
+      }
+      else {
+         return menuEntries;
+      }
+   }
+
+   /**
+    * Creates an {@link IContextMenuEntry} which executes an {@link ICustomFeature}.
+    * @param feature The {@link ICustomFeature} to execute.
+    * @param context The {@link ICustomContext} to execute.
+    * @param text The text.
+    * @param description The description.
+    * @param iconId The icon id.
+    * @return The created {@link IContextMenuEntry}.
+    */
+   protected IContextMenuEntry createCustomContextMenuEntry(ICustomFeature feature, ICustomContext context, String text, String description, String iconId) {
+      IContextMenuEntry entry = new ContextMenuEntry(feature, context);
       entry.setDescription(description);
       entry.setText(text);
       entry.setIconId(iconId);
