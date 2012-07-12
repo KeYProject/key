631c5de28a002bc6cf3220f19cf691497f067cbe
Daniel Bruns <bruns@kit.edu>
2012-07-12 16:43:14 +0200

    metaconstruct for enhanced for loops over arrays

--- system/src/de/uka/ilkd/key/rule/metaconstruct/EnhancedForElimination.java
+++ system/src/de/uka/ilkd/key/rule/metaconstruct/EnhancedForElimination.java
@@ -15,12 +15,13 @@ import de.uka.ilkd.key.java.*;
 import de.uka.ilkd.key.java.abstraction.KeYJavaType;
 import de.uka.ilkd.key.java.declaration.LocalVariableDeclaration;
 import de.uka.ilkd.key.java.declaration.VariableSpecification;
+import de.uka.ilkd.key.java.expression.Assignment;
 import de.uka.ilkd.key.java.expression.ParenthesizedExpression;
-import de.uka.ilkd.key.java.reference.MethodName;
-import de.uka.ilkd.key.java.reference.MethodReference;
-import de.uka.ilkd.key.java.reference.TypeRef;
-import de.uka.ilkd.key.java.statement.EnhancedFor;
-import de.uka.ilkd.key.java.statement.While;
+import de.uka.ilkd.key.java.expression.PassiveExpression;
+import de.uka.ilkd.key.java.expression.literal.IntLiteral;
+import de.uka.ilkd.key.java.expression.operator.*;
+import de.uka.ilkd.key.java.reference.*;
+import de.uka.ilkd.key.java.statement.*;
 import de.uka.ilkd.key.logic.ProgramElementName;
 import de.uka.ilkd.key.logic.VariableNamer;
 import de.uka.ilkd.key.logic.op.LocationVariable;
@@ -36,15 +37,19 @@ import de.uka.ilkd.key.util.ExtList;
  * This class is used to transform an enh. for loop over an iterable object into
  * a while loop + surrounding statements.
  * 
- * The "array"-case can be handled by a taclet.
- * 
  * @author mulbrich
  * 
  */
 
 public class EnhancedForElimination extends ProgramTransformer {
 
+    private static final String ITERABLE = "java.lang.Iterable";
+    private static final String ITERATOR_METH = "iterator";
+    private static final String HAS_NEXT = "hasNext";
+    private static final String NEXT = "next";
+    private static final String ITERATOR = "java.util.Iterator";
     private Services services;
+    private JavaInfo ji;
 
     public EnhancedForElimination(EnhancedFor forStatement) {
         super("enhancedfor-elim", forStatement);
@@ -62,12 +67,6 @@ public class EnhancedForElimination extends ProgramTransformer {
      * <li>updates remains empty
      * <li>body: stm
      * </ul>
-     * 
-     * <p>
-     * Loops over arrays are treated by a taclet without use of this class.
-     * 
-     * <p>
-     * Loops over Iterable-objects are treated by this meta-construct.
      * 
      * <p>
      * The rules which use this meta construct must ensure that exp is of type
@@ -93,8 +92,90 @@ public class EnhancedForElimination extends ProgramTransformer {
         LocalVariableDeclaration lvd = enhancedFor.getVariableDeclaration();
         Expression expression = enhancedFor.getGuardExpression();
         Statement body = enhancedFor.getBody();
+        
+        ji = services.getJavaInfo();
+        ExecutionContext ec = null; // TODO: how to get this? do we need it?
+        boolean iterable = ji.isSubtype(expression.getKeYJavaType(services, ec),ji.getTypeByName(ITERABLE));
+//        boolean array = ji.isSubtype(expression.getKeYJavaType(services, ec),ji.getTypeByName("java.lang.Object[]"));
+//        
+//        assert iterable | array : "Enhanced for-loops may only range over arrays or instances of Iterable.";
 
-        return makeIterableForLoop(lvd, expression, body);
+        ProgramElement result = iterable? makeIterableForLoop(lvd, expression, body) : makeArrayForLoop(lvd, expression, body);
+        return result;
+    }
+
+    /** Transform an enhanced for-loop over an array to a regular for-loop. */
+    private ProgramElement makeArrayForLoop(LocalVariableDeclaration lvd,
+            Expression expression, Statement body) {
+        VariableNamer varNamer = services.getVariableNamer();
+        ProgramElementName itName = varNamer.getTemporaryNameProposal("i");
+        KeYJavaType intType = ji.getPrimitiveKeYJavaType("int");
+        ProgramVariable itVar = new LocationVariable(itName, intType);
+        
+
+        ILoopInit inits = makeForInit(intType, itVar);
+        IGuard guard = makeGuard(expression, itVar);
+        IForUpdates updates = makeUpdates(itVar);
+        
+        // there may be only one variable iterated over (see Language Specification Sect. 14.14.2)
+        ProgramVariable lvdVar = (ProgramVariable)lvd.getVariables().get(0).getProgramVariable();
+        Statement declArrayElemVar = makeElemDecl(lvdVar);
+        
+        For forLoop = makeLoop(body, itVar, inits, guard, updates, expression,lvdVar);
+        
+        // put everything together
+        Statement[] complete = {declArrayElemVar,forLoop};
+        return new StatementBlock(complete);
+    }
+
+    /** Declare the iterated element. */
+    private Statement makeElemDecl(ProgramVariable lvdVar) {
+        KeYJavaType lvdType = lvdVar.getKeYJavaType();
+        TypeRef lvdTyperef = new TypeRef(lvdType);
+        VariableSpecification lvdSpec = new VariableSpecification(lvdVar, lvdType);
+        Statement declArrayElemVar = new LocalVariableDeclaration(lvdTyperef,lvdSpec);
+        return declArrayElemVar;
+    }
+
+    /** Loop statement including assignment to the iterated element. */
+    private For makeLoop(Statement body, ProgramVariable itVar,
+            ILoopInit inits, IGuard guard, IForUpdates updates,
+            Expression array, ProgramVariable lvdVar) {
+        Expression[] arrayAccess = {itVar};
+        Expression nextElement = new ArrayReference(new PassiveExpression(array), arrayAccess);
+        VariableReference lhs = new VariableReference(lvdVar);
+        Statement getNextElement = new CopyAssignment(lhs, nextElement);
+        Statement[] newBlock = {getNextElement,body};
+        body = new StatementBlock(newBlock);      
+        For forLoop = new For(inits, guard, updates, body);
+        return forLoop;
+    }
+
+    /** Updates to loop index variable (i++). */
+    private IForUpdates makeUpdates(ProgramVariable itVar) {
+        Expression[] update = {new PostIncrement(itVar)};
+        IForUpdates updates = new ForUpdates(new ImmutableArray<Expression>(update));
+        return updates;
+    }
+
+    /** For-loop guard (i < a.length). */
+    private IGuard makeGuard(Expression expression, ProgramVariable itVar) {
+        Expression lengthExpr = new ArrayLengthReference(new PassiveExpression(expression));
+        IGuard guard = new Guard(new LessThan(itVar,lengthExpr));
+        return guard;
+    }
+
+    /** Declare index variable and assign zero. */
+    private ILoopInit makeForInit(KeYJavaType intType, ProgramVariable itVar) {
+        VariableSpecification spec =
+                new VariableSpecification(itVar, intType);
+        TypeRef intTyperef = new TypeRef(intType);
+        LocalVariableDeclaration init =
+                new LocalVariableDeclaration(intTyperef, spec);
+        Assignment setToZero = new CopyAssignment(itVar, new IntLiteral(0));
+        LoopInitializer[] linit = {init,setToZero};
+        ILoopInit inits = new LoopInit(linit);
+        return inits;
     }
 
     /*
@@ -142,8 +223,8 @@ public class EnhancedForElimination extends ProgramTransformer {
         //
         // it.next()
         KeYJavaType iteratorType =
-                services.getJavaInfo().getTypeByName("java.util.Iterator");
-        ProgramElementName nextMeth = new ProgramElementName("next");
+                services.getJavaInfo().getTypeByName(ITERATOR);
+        ProgramElementName nextMeth = new ProgramElementName(NEXT);
         ProgramVariable itVar = new LocationVariable(itName, iteratorType);
         MethodReference methodCall =
                 new MethodReference(new ImmutableArray<Expression>(), nextMeth, itVar);
@@ -168,8 +249,8 @@ public class EnhancedForElimination extends ProgramTransformer {
      */
     private Expression makeGuard(ProgramElementName itName) {
         KeYJavaType iteratorType =
-                services.getJavaInfo().getTypeByName("java.util.Iterator");
-        ProgramElementName hasNextMeth = new ProgramElementName("hasNext");
+                services.getJavaInfo().getTypeByName(ITERATOR);
+        ProgramElementName hasNextMeth = new ProgramElementName(HAS_NEXT);
         ProgramVariable itVar = new LocationVariable(itName, iteratorType);
         MethodReference methodCall =
                 new MethodReference(new ImmutableArray<Expression>(), hasNextMeth, itVar);
@@ -187,12 +268,12 @@ public class EnhancedForElimination extends ProgramTransformer {
         // Iterator it;
 
         KeYJavaType iteratorType =
-                services.getJavaInfo().getTypeByName("java.util.Iterator");
+                services.getJavaInfo().getTypeByName(ITERATOR);
         ProgramVariable itVar = new LocationVariable(itName, iteratorType);
 
         //
         // expression.iterator();
-        MethodName iteratorMeth = new ProgramElementName("iterator");
+        MethodName iteratorMeth = new ProgramElementName(ITERATOR_METH);
         Expression methodcall =
                 new MethodReference(new ImmutableArray<Expression>(), iteratorMeth,
                         new ParenthesizedExpression(expression));
