007c7b4d267be20474d8b6991588d46410c0843b
Daniel Bruns <bruns@kit.edu>
2012-09-20 18:08:03 +0200

    [VerifyThis] problem3 in one Java class

--- examples/heap/fm2012/problem3/Tree.java
+++ examples/heap/fm2012/problem3/Tree.java
@@ -1,5 +1,4 @@
-
-  class Tree {
+class Tree {
     /*@ nullable @*/ Tree left; 
     /*@ nullable @*/ Tree right;
     int data;
@@ -8,10 +7,10 @@
       @                  (left==null? \seq_empty: left.seq), 
       @                  \seq_concat(\seq_singleton(data),
       @                  (right==null? \seq_empty: right.seq)));
-      @ invariant seq.length == 1+ (left==null?0:left.seq.length)+(right==null?0:right.seq.length);
-      @*/
-
-     //@ invariant (\forall int i; 0 < i && i < seq.length; (int)seq[i-1] <= (int)seq[i]);
+      @ invariant seq.length == 1+ (left==null?0:left.seq.length)+
+      @                           (right==null?0:right.seq.length);
+      @ invariant (\forall int i; 0 < i && i < seq.length; 
+      @               (int)seq[i-1] <= (int)seq[i]);
      //@ accessible \inv: footprint \measured_by seq.length;
 
      /*@ ghost \locset footprint;
@@ -23,8 +22,43 @@
        @ invariant \disjoint(this.*, left.footprint) || left==null;
        @ invariant \disjoint(this.*, right.footprint) ||right==null;
        @ invariant left != right || left == null;
-       @ invariant \disjoint(left.footprint,right.footprint)||(left==null&&right==null);
+       @ invariant \disjoint(left.footprint,right.footprint)
+       @           || (left==null&&right==null);
        @*/
-  }
+ 
+    /*@ normal_behavior
+      @ requires \invariant_for(t);
+      @ requires t.seq.length >= 2;
+      @ ensures \invariant_for(\result);
+      @ ensures \result.seq == t.seq[1..t.seq.length-1];
+      @*/
+    static Tree search_tree_delete_min (Tree t) {
+       Tree tt, pp, p;
+       p = t.left;
+       if (p == null) {
+           t = t.right;
+       } else {
+           pp = t; tt = p.left;
+           /*@ maintaining (\invariant_for(tt)||tt==null) && \invariant_for(pp)
+             @                 && \invariant_for(p) && \invariant_for(t);
+             @ maintaining pp.seq == t.seq[0..pp.seq.length-1] 
+             @                 && pp.seq.length <= t.seq.length;
+             @ maintaining p.seq == pp.seq[0..p.seq.length-1] 
+             @                 && p.seq.length+1==pp.seq.length;
+             @ maintaining tt == p.left;
+             @ maintaining p == pp.left;
+             @ maintaining tt==null ==> p.data == t.seq[0];
+             @ maintaining t != null && pp!=null && p != null;
+             @ decreasing tt==null? 0: tt.seq.length;
+             @ assignable \less_than_nothing;
+             @*/
+           while (tt != null) {
+               pp = p; p = tt; tt = p.left;
+           }
+           pp.left= p.right;
+           //@ set t.seq = \seq_sub(t.seq,1,t.seq.length-1);
+       }
+       return t;
+    }
 
-
+}
