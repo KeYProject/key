b302adddd8d0405707206c27695d77a78b69cf01
Mattias Ulbrich <ulbrich@kit.edu>
2012-09-28 15:56:20 +0200

    removed debug output in macro

--- system/src/de/uka/ilkd/key/gui/macros/TryCloseMacro.java
+++ system/src/de/uka/ilkd/key/gui/macros/TryCloseMacro.java
@@ -54,15 +54,12 @@ public class TryCloseMacro implements ProofMacro {
         // ... we only had a clone, so we need to replace the original settings.
         strategySettings.setActiveStrategyProperties(properties);
 
-        System.err.println("Set retreat mode to " + StrategyProperties.RETREAT_MODE_RETREAT);
-
         mediator.startAutoMode();
 
         mediator.addAutoModeListener(new AutoModeListener() {
 
             @Override 
             public void autoModeStopped(ProofEvent e) {
-                System.err.println("Reset retreat mode to " + oldRetreatMode);
                 properties.put(StrategyProperties.RETREAT_MODE_OPTIONS_KEY, oldRetreatMode);
                 strategySettings.setActiveStrategyProperties(properties);
                 mediator.removeAutoModeListener(this);
