daec2a1a7d999e70ce818f505b72f88a9ba3e61f
Christoph Scheben <scheben@ira.uka.de>
2012-11-09 20:31:44 +0100

    po-generation refactoring -- part 3

---
+++ system/src/de/uka/ilkd/key/proof/init/po/snippet/InfFlowContractAppSnippet.java
@@ -0,0 +1,37 @@
+package de.uka.ilkd.key.proof.init.po.snippet;
+
+import de.uka.ilkd.key.collection.ImmutableList;
+import de.uka.ilkd.key.collection.ImmutableSLList;
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.Visitor;
+import de.uka.ilkd.key.proof.init.ProofObligationVars;
+
+
+/**
+ *
+ *
+ * @author christoph
+ */
+class InfFlowContractAppSnippet extends ReplaceAnRegisterMethod
+        implements InfFlowFactoryMethod {
+
+    @Override
+    public Term produce(BasicSnippetData d,
+                        ProofObligationVars poVars1,
+                        ProofObligationVars poVars2)
+            throws UnsupportedOperationException {
+        BasicPOSnippetFactory f1 =
+                POSinppetFactory.getBasicFactory(d, poVars1);
+        BasicPOSnippetFactory f2 =
+                POSinppetFactory.getBasicFactory(d, poVars2);
+
+        Term preCond1 = f1.create(BasicPOSnippetFactory.Snippet.CONTRACT_PRE);
+        Term preCond2 = f2.create(BasicPOSnippetFactory.Snippet.CONTRACT_PRE);
+
+        InfFlowPOSnippetFactory iff =
+                POSinppetFactory.getInfFlowFactory(d, poVars1, poVars2);
+        Term inOutRelations =
+                iff.create(InfFlowPOSnippetFactory.Snippet.INF_FLOW_POST);
+        return d.tb.imp(d.tb.and(preCond1, preCond2), inOutRelations);
+    }
+}
