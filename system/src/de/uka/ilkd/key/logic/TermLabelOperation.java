7cb3fab3cf4280747a5c13ce7d5e095b873a4a2f
Richard Bubel <bubel@cs.tu-darmstadt.de>
2012-12-12 18:20:01 +0100

    started iwth term label operation hierarchy

---
+++ system/src/de/uka/ilkd/key/logic/TermLabelOperation.java
@@ -0,0 +1,79 @@
+package de.uka.ilkd.key.logic;
+
+import de.uka.ilkd.key.collection.ImmutableArray;
+
+/**
+ * TermLabelOperations are used in goal templates of taclets to describe unions, set difference etc. operations
+ * on labels.
+ * 
+ */
+public class TermLabelOperation implements ITermLabel, Named {
+
+    private final Name name;
+    private final ImmutableArray<ITermLabel> children;
+    
+    protected TermLabelOperation(String name, ITermLabel[] children) {
+        this.name = new Name(name);
+        this.children = new ImmutableArray<ITermLabel>(children);
+    }
+
+    public int getChildCount() {
+        return children.size();
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        for (ITermLabel child : children) {
+            hash += 17*child.hashCode();
+        }
+        return 23*name.hashCode() + hash;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof TermLabelOperation)) {
+            return false;
+        }
+        final TermLabelOperation cmp = (TermLabelOperation) o;
+        if (cmp.getChildCount() != getChildCount() || 
+                !cmp.name().equals(name())) {
+            return false;
+        }
+        for (int i = 0; i<getChildCount(); i++) {
+            if (!children.get(i).equals(cmp.children.get(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public ImmutableArray<ITermLabel> getChildren() {
+        return children;
+    }
+    
+    public ITermLabel getChild(int i) {
+        return children.get(i);
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(name);
+        result.append("(");
+        for (ITermLabel l : children) {
+            result.append(l);
+            result.append(", ");
+        }
+        if (getChildCount() > 0) {
+            result.deleteCharAt(result.length() - 1);
+        }
+        result.append(")");
+        return result.toString();
+    }
+
+    @Override
+    public Name name() {
+        return name;
+    }
+    
+}
