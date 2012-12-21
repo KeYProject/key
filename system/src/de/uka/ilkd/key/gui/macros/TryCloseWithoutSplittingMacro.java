ff22002069f851e9d91da1e9bcc85665ac92db08
Christoph Scheben <scheben@ira.uka.de>
2012-12-21 14:01:33 +0100

    adopted changes in TryToCloseMacro to TryToCloseWithoutSplittingMacro

--- system/src/de/uka/ilkd/key/gui/macros/TryCloseWithoutSplittingMacro.java
+++ system/src/de/uka/ilkd/key/gui/macros/TryCloseWithoutSplittingMacro.java
@@ -1,35 +1,91 @@
+// This file is part of KeY - Integrated Deductive Software Design
+// Copyright (C) 2001-2011 Universitaet Karlsruhe, Germany
+//                         Universitaet Koblenz-Landau, Germany
+//                         Chalmers University of Technology, Sweden
+//
+// The KeY system is protected by the GNU General Public License.
+// See LICENSE.TXT for details.
+//
+//
+
 package de.uka.ilkd.key.gui.macros;
 
+import de.uka.ilkd.key.collection.ImmutableList;
 import de.uka.ilkd.key.gui.AutoModeListener;
 import de.uka.ilkd.key.gui.KeYMediator;
 import de.uka.ilkd.key.gui.configuration.StrategySettings;
 import de.uka.ilkd.key.logic.PosInOccurrence;
+import de.uka.ilkd.key.proof.Goal;
+import de.uka.ilkd.key.proof.Node;
 import de.uka.ilkd.key.proof.Proof;
 import de.uka.ilkd.key.proof.ProofEvent;
 import de.uka.ilkd.key.strategy.StrategyProperties;
 
 /**
+ * The Class TryCloseMacro tries to close goals. Goals are either closed or left
+ * untouched.
  *
- * @author christoph
+ * This uses the code provided by Michael Kirsten in
+ * {@link InteractiveProver$AutoWorker}.
+ *
+ * The number of autosteps may be temporarily altered for this macro.
+ *
+ * @author mattias ulbrich
  */
 public class TryCloseWithoutSplittingMacro implements ProofMacro {
 
-    @Override 
-    public String getName() {
-        return "Close provable goals without splits";
+    /**
+     * The max number of steps to be applied.
+     * A value of -1 means no changes.
+     */
+    private final int numberSteps;
+
+    /**
+     * Instantiates a new try close macro.
+     * No changes to the max number of steps.
+     */
+    public TryCloseWithoutSplittingMacro() {
+        this.numberSteps = -1;
     }
 
+    /**
+     * Instantiates a new try close macro.
+     *
+     * @param numberSteps
+     *            the max number of steps. -1 means no change.
+     */
+    public TryCloseWithoutSplittingMacro(int numberSteps) {
+        this.numberSteps = numberSteps;
+    }
+
+    /* (non-Javadoc)
+     * @see de.uka.ilkd.key.gui.macros.ProofMacro#getName()
+     */
+    @Override 
+    public String getName() {
+        return "Close provable goals below without splits";
+    }
+
+    /* (non-Javadoc)
+     * @see de.uka.ilkd.key.gui.macros.ProofMacro#getDescription()
+     */
     @Override 
     public String getDescription() {
         return "Closes closable goals, leave rest untouched (see settings "
                 + "AutoPrune); does not use splitting rules";
     }
 
+    /* (non-Javadoc)
+     * @see de.uka.ilkd.key.gui.macros.ProofMacro#canApplyTo(de.uka.ilkd.key.gui.KeYMediator, de.uka.ilkd.key.logic.PosInOccurrence)
+     */
     @Override 
     public boolean canApplyTo(KeYMediator mediator, PosInOccurrence posInOcc) {
         return true;
     }
 
+    /* (non-Javadoc)
+     * @see de.uka.ilkd.key.gui.macros.ProofMacro#applyTo(de.uka.ilkd.key.gui.KeYMediator, de.uka.ilkd.key.logic.PosInOccurrence)
+     */
     @Override 
     public void applyTo(final KeYMediator mediator, PosInOccurrence posInOcc) {
 
@@ -44,24 +100,56 @@ public class TryCloseWithoutSplittingMacro implements ProofMacro {
                 properties.getProperty(StrategyProperties.RETREAT_MODE_OPTIONS_KEY);
         final String oldSplittingMode = 
                 properties.getProperty(StrategyProperties.SPLITTING_OPTIONS_KEY);
+        final String oldDepMode =
+                properties.getProperty(StrategyProperties.DEP_OPTIONS_KEY);
 
+        final int oldNumberSteps =
+                mediator.getMaxAutomaticSteps();
+
+        //
+        // activate retreat mode
         properties.put(StrategyProperties.RETREAT_MODE_OPTIONS_KEY, 
                 StrategyProperties.RETREAT_MODE_RETREAT);
+
+        // deactivate splitting
         properties.put(StrategyProperties.SPLITTING_OPTIONS_KEY, 
                 StrategyProperties.SPLITTING_OFF);
+
+        // deactivate dependency contracts
+        properties.put(StrategyProperties.DEP_OPTIONS_KEY,
+                StrategyProperties.DEP_OFF);
 
         // ... we only had a clone, so we need to replace the original settings.
         strategySettings.setActiveStrategyProperties(properties);
 
-        mediator.startAutoMode();
+        //
+        // set the max number of steps if given
+        if(numberSteps > 0) {
+            mediator.setMaxAutomaticSteps(numberSteps);
+        }
+
+        //
+        // start actual autoprove
+        Node invokedNode = mediator.getSelectedNode();
+        ImmutableList<Goal> enabledGoals = proof.getSubtreeEnabledGoals(invokedNode);
+        mediator.startAutoMode(enabledGoals);
 
         mediator.addAutoModeListener(new AutoModeListener() {
 
             @Override 
             public void autoModeStopped(ProofEvent e) {
+                // set retreat mode back to old value
                 properties.put(StrategyProperties.RETREAT_MODE_OPTIONS_KEY, oldRetreatMode);
+                // set splitting back to old setting
                 properties.put(StrategyProperties.SPLITTING_OPTIONS_KEY, oldSplittingMode);
+                // set dependency contract option back to old setting
+                properties.put(StrategyProperties.DEP_OPTIONS_KEY, oldDepMode);
                 strategySettings.setActiveStrategyProperties(properties);
+
+                // reset the number of steps to old value
+                mediator.setMaxAutomaticSteps(oldNumberSteps);
+
+                // listener has done its job. remove it.
                 mediator.removeAutoModeListener(this);
             }
 
