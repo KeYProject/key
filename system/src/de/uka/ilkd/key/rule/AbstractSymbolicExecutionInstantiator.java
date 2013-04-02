8d3dcddaa737ad7b1727be6f7d84e7601ce393b0
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2013-04-02 14:22:09 +0200

    KeY and SED: Fixed AssertionError in LabeledTermImpl#containsLabel(ITermLabel)

--- system/src/de/uka/ilkd/key/rule/AbstractSymbolicExecutionInstantiator.java
+++ system/src/de/uka/ilkd/key/rule/AbstractSymbolicExecutionInstantiator.java
@@ -37,7 +37,7 @@ public abstract class AbstractSymbolicExecutionInstantiator implements ITermLabe
          if (applicationTerm != null) {
             applicationTerm = TermBuilder.DF.goBelowUpdates(applicationTerm);
             ITermLabel termLabel = getTermLabel(applicationTerm);
-            if (applicationTerm.containsLabel(termLabel)) {
+            if (termLabel != null && applicationTerm.containsLabel(termLabel)) {
                instantiatedLabels.add(termLabel);
             }
          }
