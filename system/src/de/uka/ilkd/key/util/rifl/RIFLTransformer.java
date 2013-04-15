2521b123f587dd5ff265cc20bc012d8fb26a2523
Daniel Bruns <bruns@kit.edu>
2013-04-15 15:45:55 +0200

    [RIFL] write to Java files

--- system/src/de/uka/ilkd/key/util/rifl/RIFLTransformer.java
+++ system/src/de/uka/ilkd/key/util/rifl/RIFLTransformer.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
@@ -15,97 +16,18 @@ import javax.xml.parsers.*;
 import org.xml.sax.*;
 import org.xml.sax.helpers.*;
 
+import recoder.DefaultServiceConfiguration;
 import recoder.ParserException;
-import recoder.java.Comment;
 import recoder.java.CompilationUnit;
 import recoder.java.JavaProgramFactory;
-import recoder.list.generic.ASTArrayList;
-import recoder.list.generic.ASTList;
 
-import de.uka.ilkd.key.java.JavaReader;
-import de.uka.ilkd.key.java.recoderext.ProofJavaProgramFactory;
 import de.uka.ilkd.key.util.KeYExceptionHandler;
 
 
 public class RIFLTransformer {
 
-    private static final JavaProgramFactory jpf = ProofJavaProgramFactory.getInstance();
-    private final XMLReader xmlReader;
-    private List<CompilationUnit> javaCUs = new ArrayList<CompilationUnit>();
-
-    private RIFLTransformer() throws ParserConfigurationException, SAXException {   
-        SAXParserFactory spf = SAXParserFactory.newInstance();
-        spf.setNamespaceAware(true);
-        SAXParser saxParser = spf.newSAXParser();
-        xmlReader = saxParser.getXMLReader();
-        xmlReader.setContentHandler(new RIFLHandler());
-        xmlReader.setErrorHandler(new RIFLHandler.ErrorHandler());
-    }
-
-    private void readRIFL (String fileName) throws IOException, SAXException {
-        // TODO: validate input file
-        xmlReader.parse(convertToFileURL(fileName));
-        System.out.println("parsing finished");
-    }
-    
-    public static void transform (String riflFilename, String javaSource, String savePath, KeYExceptionHandler kexh) {
-        // step 1: parse RIFL file
-        RIFLTransformer rt = null;
-        try {
-            rt = new RIFLTransformer();
-        } catch (ParserConfigurationException e) {
-            kexh.reportException(e);
-        } catch (SAXException e) {
-            kexh.reportException(e);
-        }
-        try {
-            rt.readRIFL(riflFilename);
-        } catch (IOException e) {
-            kexh.reportException(e);
-        } catch (SAXException e) {
-            kexh.reportException(e);
-        }
-        
-        // step 2: parse Java source
-        try {
-            rt.readJava(javaSource);
-        } catch (IOException e) {
-            kexh.reportException(e);
-        } catch (ParserException e) {
-            kexh.reportException(e);
-        }
-    }
-
-    private void readJava(String source) throws IOException, ParserException {
-        // TODO: collect all Java files from directory
-        final Collection<String> javaFiles = new ArrayList<String>();
-        javaFiles.add(source);
-        
-        // parse
-        for (String javaFile: javaFiles) {
-            final CompilationUnit cu;
-            Reader fr = new BufferedReader(new FileReader(javaFile));
-            cu = jpf.parseCompilationUnit(fr);
-            
-            // testing stuff
-            SpecificationInjector si = new SpecificationInjector();
-            cu.accept(si);
-            
-            javaCUs.add(cu);
-        }
-    }
-    
-    private void writeJava (String target) {
-        
-    }
-
-    /** Entry point for the stand-alone RIFL to JML* tool.
-     */
-    public static void main(String[] args) {
-        final String riflFilename = "/tmp/test.xml";
-        final String javaFilename = "/tmp/Test.java";
-        RIFLTransformer.transform(riflFilename, javaFilename, null, SimpleRIFLExceptionHandler.INSTANCE);
-    }
+    private static final JavaProgramFactory jpf =
+            de.uka.ilkd.key.java.recoderext.ProofJavaProgramFactory.getInstance();
     
     private static String convertToFileURL(String filename) {
         String path = new File(filename).getAbsolutePath();
@@ -117,6 +39,100 @@ public class RIFLTransformer {
             path = "/" + path;
         }
         return "file:" + path;
+    }
+    
+    
+    /** Entry point for the stand-alone RIFL to JML* tool.
+     */
+    public static void main(String[] args) {
+        final String riflFilename = "/tmp/test.xml";
+        final String javaFilename = "/tmp/Test.java";
+        final String targetFilename = "/tmp/TestNew.java";
+        RIFLTransformer.transform(riflFilename, javaFilename, targetFilename, SimpleRIFLExceptionHandler.INSTANCE);
+    }
+
+    public static void transform (String riflFilename, String javaSource, String savePath, KeYExceptionHandler kexh) {
+        RIFLTransformer rt = null;
+        try {
+            rt = new RIFLTransformer();
+            rt.transform(riflFilename, javaSource, savePath);
+        } catch (ParserConfigurationException e) {
+            kexh.reportException(e);
+        } catch (SAXException e) {
+            kexh.reportException(e);
+        } catch (ParserException e) {
+            kexh.reportException(e);
+        } catch (IOException e) {
+            kexh.reportException(e);
+            
+        }
+    }
+
+    private final XMLReader xmlReader;
+    private List<CompilationUnit> javaCUs = new ArrayList<CompilationUnit>();
+
+    
+    private RIFLTransformer() throws ParserConfigurationException, SAXException {   
+        SAXParserFactory spf = SAXParserFactory.newInstance();
+        spf.setNamespaceAware(true);
+        SAXParser saxParser = spf.newSAXParser();
+        xmlReader = saxParser.getXMLReader();
+        xmlReader.setContentHandler(new RIFLHandler());
+        xmlReader.setErrorHandler(new RIFLHandler.ErrorHandler());
+        jpf.initialize(new DefaultServiceConfiguration());
+        assert jpf.getServiceConfiguration() != null;
+    }
+    
+    private void readJava(String source) throws IOException, ParserException {
+        // TODO: collect all Java files from directory
+        final Collection<String> javaFiles = new ArrayList<String>();
+        javaFiles.add(source);
+        
+        // parse
+        for (String javaFile: javaFiles) {
+            final CompilationUnit cu;
+            Reader fr = new BufferedReader(new FileReader(javaFile));
+            cu = jpf.parseCompilationUnit(fr);
+            javaCUs.add(cu);
+        }
+    }
+    
+    private void readRIFL (String fileName) throws IOException, SAXException {
+        // TODO: validate input file
+        xmlReader.parse(convertToFileURL(fileName));
+    }
+
+    private void transform(String riflFilename, String javaSource, String savePath) throws IOException, SAXException, ParserException {
+
+        // step 1: parse RIFL file
+        readRIFL(riflFilename);
+
+        // step 2: parse Java source
+        readJava(javaSource);
+
+        // step 3: inject specifications
+        for (CompilationUnit cu: javaCUs) {
+            SpecificationInjector si = new SpecificationInjector();
+            cu.accept(si);
+        }
+
+        // step 4: write modified Java files
+        writeJavaFile(savePath,javaCUs.get(0));
+    }
+    
+    /** Writes a single Java file. */
+    private void writeJavaFile (String target, CompilationUnit cu) throws IOException {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(target);
+            final String source = cu.toSource();
+            System.out.println(source);
+            writer.append(source);
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            writer.close();
+        }
     }
 
 }
--- system/src/de/uka/ilkd/key/util/rifl/SpecificationInjector.java
+++ system/src/de/uka/ilkd/key/util/rifl/SpecificationInjector.java
@@ -1,5 +1,8 @@
 package de.uka.ilkd.key.util.rifl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import recoder.java.*;
 import recoder.java.declaration.*;
 import recoder.list.generic.ASTArrayList;
@@ -11,6 +14,10 @@ public class SpecificationInjector extends SourceVisitor {
         // TODO Auto-generated constructor stub
     }
     
+    //////////////////////////////////////////////////////////////
+    // visitor methods
+    //////////////////////////////////////////////////////////////
+    
     @Override
     public void visitCompilationUnit (CompilationUnit cu) {
         accessChildren(cu);
@@ -19,7 +26,6 @@ public class SpecificationInjector extends SourceVisitor {
     @Override
     public void visitClassDeclaration (ClassDeclaration cd) {
         accessChildren(cd);
-        addComment(cd,"/* test */");
     }
 
     @Override
@@ -29,8 +35,14 @@ public class SpecificationInjector extends SourceVisitor {
 
     @Override
     public void visitMethodDeclaration (MethodDeclaration md) {
+        addComment(md,"/* test */");
         // do not go down anymore
     }
+    
+    
+    //////////////////////////////////////////////////////////////
+    // private methods
+    //////////////////////////////////////////////////////////////
     
     private void accessChildren(JavaNonTerminalProgramElement pe) {
         for (int i= 0; i < pe.getChildCount(); i++)
@@ -38,11 +50,66 @@ public class SpecificationInjector extends SourceVisitor {
     }
 
     private static void addComment(JavaProgramElement se, String comment) {
+        // TODO: place comments more appropriately
         final ASTArrayList<Comment> commentList = new ASTArrayList<Comment>();
         ASTList<Comment> oldComments = se.getComments();
         if (oldComments != null) commentList.addAll(oldComments);
         commentList.add(new Comment(comment));
         se.setComments(commentList);
+    }
+    
+    private static class JMLWriter {
+        
+        private static final String RESULT = "\\result";
+        private static final String RESPECTS = "respects";
+        private static final String JML_END = "@*/";
+        private static final String JML_START = "/*@ ";
+        
+        private JavaProgramElement se;
+        private List<String> respects = new ArrayList<String>();
+        
+        JMLWriter (JavaProgramElement se) {
+            this.se = se;
+        }
+        
+        void clear () {
+            respects = new ArrayList<String>();
+        }
+        
+        void addToRespects(de.uka.ilkd.key.util.rifl.SpecificationEntity.Field f){
+            respects.add(f.toString());
+        }
+        
+        void addResultToRespects(){
+            respects.add(RESULT);
+        }
+        
+        String getSpecification() {
+            // start JML
+            StringBuffer sb = new StringBuffer("// JML* comment created by KeY RIFL Transformer.\n");
+            sb.append(JML_START);
+
+            // respects clause
+            if (!respects.isEmpty()) {
+                sb.append(RESPECTS);
+                for (String elem: respects) {
+                    sb.append(" ");
+                    sb.append(elem);
+                    sb.append(",");
+                }
+                sb.deleteCharAt(sb.length()-1);
+                sb.append(";\n");
+            }
+            
+            // close JML
+            indent(sb);
+            sb.append(JML_END);
+            return sb.toString();
+        }
+        
+        private void indent (StringBuffer sb) {
+            sb.append("  ");
+        }
     }
     
 }
