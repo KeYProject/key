6a9cd7df9770e4c117c3bc3e0fe93145a6349b64
Daniel Bruns <bruns@kit.edu>
2013-04-26 16:46:34 +0200

    move more classes to package proof.io

--- system/src/de/uka/ilkd/key/proof/io/ProblemLoaderException.java
+++ system/src/de/uka/ilkd/key/proof/io/ProblemLoaderException.java
@@ -11,7 +11,7 @@
 // Public License. See LICENSE.TXT for details.
 //
 
-package de.uka.ilkd.key.proof;
+package de.uka.ilkd.key.proof.io;
 
 
 public class ProblemLoaderException extends Exception {
