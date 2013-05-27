67f3699390439caba3fc8ee799602c5e5808f2d9
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2013-05-27 14:16:28 +0200

    KeY4Eclipse: Refactored code to avoid code duplications and reused utility methods.

--- projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.resources.ui/src/org/key_project/key4eclipse/resources/ui/handlers/ConvertJavaToKeYProjectHandler.java
+++ projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.resources.ui/src/org/key_project/key4eclipse/resources/ui/handlers/ConvertJavaToKeYProjectHandler.java
@@ -1,7 +1,6 @@
 package org.key_project.key4eclipse.resources.ui.handlers;
 
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.internal.resources.Project;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.jdt.internal.core.JavaProject;
@@ -10,6 +9,7 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import org.key_project.key4eclipse.common.ui.handler.AbstractSaveExecutionHandler;
 import org.key_project.key4eclipse.resources.nature.KeYProjectNature;
 import org.key_project.util.eclipse.swt.SWTUtil;
+import org.key_project.util.java.ArrayUtil;
 
 @SuppressWarnings("restriction")
 public class ConvertJavaToKeYProjectHandler extends AbstractSaveExecutionHandler {
@@ -23,27 +23,15 @@ public class ConvertJavaToKeYProjectHandler extends AbstractSaveExecutionHandler
       ISelection selection = HandlerUtil.getCurrentSelection(event);
       Object[] elements = SWTUtil.toArray(selection);
       for(Object obj : elements){
-         if(obj instanceof JavaProject){
-            JavaProject javaProject = (JavaProject) obj;
-            IProject project = javaProject.getProject();
-            IProjectDescription description = project.getDescription();
-            String[] natures = description.getNatureIds();
-            String[] newNatures = new String[natures.length + 1];
-            System.arraycopy(natures,0,newNatures,0,natures.length);
-            newNatures[natures.length] = KeYProjectNature.NATURE_ID;
-            description.setNatureIds(newNatures);
-            project.setDescription(description,null);            
+         if (obj instanceof JavaProject){
+            obj = ((JavaProject) obj).getProject();
          }
-         else if(obj instanceof Project){
+         if (obj instanceof IProject){
             IProject project = (IProject) obj;
             IProjectDescription description = project.getDescription();
-            String[] natures = description.getNatureIds();
-            String[] newNatures = new String[natures.length + 1];
-            System.arraycopy(natures,0,newNatures,0,natures.length);
-            newNatures[natures.length] = KeYProjectNature.NATURE_ID;
+            String[] newNatures = ArrayUtil.add(description.getNatureIds(), KeYProjectNature.NATURE_ID);
             description.setNatureIds(newNatures);
-            project.setDescription(description,null);      
-            
+            project.setDescription(description, null);            
          }
       }
       return null;
