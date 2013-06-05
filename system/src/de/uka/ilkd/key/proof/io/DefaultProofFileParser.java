5841b6ac362a93f604371f046f18f09912530e9b
Daniel Bruns <bruns@kit.edu>
2013-06-05 14:36:48 +0200

    [code quality] add serial

--- system/src/de/uka/ilkd/key/proof/io/DefaultProofFileParser.java
+++ system/src/de/uka/ilkd/key/proof/io/DefaultProofFileParser.java
@@ -558,6 +558,9 @@ public class DefaultProofFileParser implements IProofFileParser {
    }
 
     private static class SkipSMTRuleException extends Exception {
+
+        private static final long serialVersionUID = -2932282883810135168L;
+
     }
 
 }
