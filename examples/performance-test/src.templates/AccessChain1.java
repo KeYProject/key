d0d593bf799155fc99b09a40a1db7ed1f9b50b89
Christoph Scheben <scheben@ira.uka.de>
2013-07-11 16:08:43 +0200

    new performance test added

--- examples/performance-test/src.templates/AccessChain1.java
+++ examples/performance-test/src.templates/AccessChain1.java
@@ -7,7 +7,42 @@ class AccessChain1 {
   //@ requires a != null;
   //@ requires a.x > 0;
   //@ ensures a.x > 0;
-  void foo () {
+  void foo_1() {
+    a.x++; bar();
+  }
+
+
+  //@ requires a != null;
+  //@ requires a.x > 0;
+  //@ ensures a.x > 0;
+  void foo_2() {
+    a.x++; bar();
+    a.x++; bar();
+  }
+
+
+  //@ requires a != null;
+  //@ requires a.x > 0;
+  //@ ensures a.x > 0;
+  void foo_4() {
+    a.x++; bar();
+    a.x++; bar();
+    a.x++; bar();
+    a.x++; bar();
+  }
+
+
+  //@ requires a != null;
+  //@ requires a.x > 0;
+  //@ ensures a.x > 0;
+  void foo_8() {
+    a.x++; bar();
+    a.x++; bar();
+    a.x++; bar();
+    a.x++; bar();
+    a.x++; bar();
+    a.x++; bar();
+    a.x++; bar();
     a.x++; bar();
   }
 
---
+++ examples/performance-test/src.templates/AccessChain2.java
@@ -0,0 +1,54 @@
+class AccessChain2 {
+  AccessChain2 a;
+  int x;
+  int y;
+
+
+  //@ requires a != null && a.a != null;
+  //@ requires a.a.x > 0;
+  //@ ensures a.a.x > 0;
+  void foo_1 () {
+    a.a.x++; bar();
+  }
+
+
+  //@ requires a != null && a.a != null && a.a.a != null && a.a.a.a != null;
+  //@ requires a.a.x > 0;
+  //@ ensures a.a.x > 0;
+  void foo_2 () {
+    a.a.x++; bar();
+    a.a.x++; bar();
+  }
+
+
+  //@ requires a != null && a.a != null && a.a.a != null && a.a.a.a != null;
+  //@ requires a.a.x > 0;
+  //@ ensures a.a.x > 0;
+  void foo_4 () {
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+  }
+
+
+  //@ requires a != null && a.a != null && a.a.a != null && a.a.a.a != null;
+  //@ requires a.a.x > 0;
+  //@ ensures a.a.x > 0;
+  void foo_8 () {
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+    a.a.x++; bar();
+  }
+
+
+  //@ ensures true;
+  //@ assignable y;
+  void bar () {};
+
+}
