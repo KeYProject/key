5a58c6158804645c2b460d7fbe584a9e9c024a82
Richard Bubel <bubel@cs.tu-darmstadt.de>
2013-07-14 09:40:31 +0200

    [Fix] Remove usage of Java 7 switch in TermBuilder

--- system/src/de/uka/ilkd/key/logic/TermBuilder.java
+++ system/src/de/uka/ilkd/key/logic/TermBuilder.java
@@ -1076,17 +1076,17 @@ public class TermBuilder {
         int digit;
         for(int i = j, sz = numberString.length(); i<sz; i++){
             
-            switch(numberString.substring(i,i+1)) {
-                case "0" : digit = 0; break;
-                case "1" : digit = 1; break;
-                case "2" : digit = 2; break;
-                case "3" : digit = 3; break;
-                case "4" : digit = 4; break;
-                case "5" : digit = 5; break;
-                case "6" : digit = 6; break;
-                case "7" : digit = 7; break;
-                case "8" : digit = 8; break;
-                case "9" : digit = 9; break;
+            switch(numberString.charAt(i)) {
+                case '0' : digit = 0; break;
+                case '1' : digit = 1; break;
+                case '2' : digit = 2; break;
+                case '3' : digit = 3; break;
+                case '4' : digit = 4; break;
+                case '5' : digit = 5; break;
+                case '6' : digit = 6; break;
+                case '7' : digit = 7; break;
+                case '8' : digit = 8; break;
+                case '9' : digit = 9; break;
                 default:
                     throw new NumberFormatException(numberString + " is not a number.");
             }
