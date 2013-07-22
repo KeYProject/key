12b3f3faf60052de6e899ab846ef44dab37f0ad3
Christoph Scheben <scheben@ira.uka.de>
2013-07-22 12:50:43 +0200

    fixed update method of AuxiliaryTermLabelInstantiator

--- system/src/de/uka/ilkd/key/rule/AuxiliaryTermLabelInstantiator.java
+++ system/src/de/uka/ilkd/key/rule/AuxiliaryTermLabelInstantiator.java
@@ -10,43 +10,78 @@
 // The KeY system is protected by the GNU General 
 // Public License. See LICENSE.TXT for details.
 //
-
 package de.uka.ilkd.key.rule;
 
+import de.uka.ilkd.key.collection.ImmutableArray;
 import de.uka.ilkd.key.logic.AuxiliaryTermLabel;
 import de.uka.ilkd.key.logic.ITermLabel;
+import de.uka.ilkd.key.logic.JavaBlock;
+import de.uka.ilkd.key.logic.PosInOccurrence;
 import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.op.Operator;
+import de.uka.ilkd.key.logic.op.QuantifiableVariable;
+import de.uka.ilkd.key.proof.Goal;
+import java.util.LinkedList;
+import java.util.List;
+
 
 /**
  * The {@link ITermLabelWorker} used during prove to define how a
  * {@link AuxiliaryTermLabel} is maintained.
+ * <p/>
  * @author Christoph Scheben
  */
-public final class AuxiliaryTermLabelInstantiator extends AbstractSymbolicExecutionInstantiator {
-   /**
-    * The only instance of this class.
-    */
-   public static final AuxiliaryTermLabelInstantiator INSTANCE = new AuxiliaryTermLabelInstantiator();
+public final class AuxiliaryTermLabelInstantiator implements ITermLabelWorker {
 
-   /**
-    * Constructor to forbid multiple instances.
-    */
-   private AuxiliaryTermLabelInstantiator() {
-   }
+    /**
+     * The only instance of this class.
+     */
+    public static final AuxiliaryTermLabelInstantiator INSTANCE =
+            new AuxiliaryTermLabelInstantiator();
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   protected ITermLabel getTermLabel(Term applicationTerm) {
-      return AuxiliaryTermLabel.INSTANCE;
-   }
+    /**
+     * Constructor to forbid multiple instances.
+     */
+    private AuxiliaryTermLabelInstantiator() {
+    }
 
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public String getName() {
-      return AuxiliaryTermLabel.NAME.toString();
-   }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getName() {
+        return AuxiliaryTermLabel.NAME.toString();
+    }
+
+
+    @Override
+    public List<ITermLabel> instantiateLabels(Term tacletTerm,
+                                              PosInOccurrence applicationPosInOccurrence,
+                                              Term applicationTerm,
+                                              Rule rule,
+                                              Goal goal,
+                                              Operator newTermOp,
+                                              ImmutableArray<Term> newTermSubs,
+                                              ImmutableArray<QuantifiableVariable> newTermBoundVars,
+                                              JavaBlock newTermJavaBlock) {
+        // nothing to do
+        return new LinkedList<ITermLabel>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<ITermLabel> updateLabels(Term tacletTerm,
+                                         PosInOccurrence applicationPosInOccurrence,
+                                         Term termToUpdate,
+                                         Rule rule,
+                                         Goal goal) {
+        // keep labels
+        List<ITermLabel> updatedLabels = new LinkedList<ITermLabel>();
+        if (termToUpdate.containsLabel(AuxiliaryTermLabel.INSTANCE)) {
+            updatedLabels.add(AuxiliaryTermLabel.INSTANCE);
+        }
+        return updatedLabels;
+    }
 }
