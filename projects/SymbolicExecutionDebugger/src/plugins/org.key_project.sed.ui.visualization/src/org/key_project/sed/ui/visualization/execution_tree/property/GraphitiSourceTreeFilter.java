9297cb41cc3dcde1da6f3943bdbd9cd4ed208306
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2013-07-26 11:05:40 +0200

    SED: Fixed focus bug in source tab caused by new focus management in Eclipse 4.2

--- projects/SymbolicExecutionDebugger/src/plugins/org.key_project.sed.ui.visualization/src/org/key_project/sed/ui/visualization/execution_tree/property/GraphitiSourceTreeFilter.java
+++ projects/SymbolicExecutionDebugger/src/plugins/org.key_project.sed.ui.visualization/src/org/key_project/sed/ui/visualization/execution_tree/property/GraphitiSourceTreeFilter.java
@@ -13,10 +13,12 @@
 
 package org.key_project.sed.ui.visualization.execution_tree.property;
 
+import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.ui.platform.AbstractPropertySectionFilter;
 import org.eclipse.jface.viewers.IFilter;
 import org.eclipse.ui.IWorkbenchPart;
+import org.key_project.sed.ui.visualization.view.ExecutionTreeView;
 import org.key_project.util.eclipse.WorkbenchUtil;
 
 /**
@@ -32,6 +34,9 @@ public class GraphitiSourceTreeFilter extends AbstractPropertySectionFilter {
    protected boolean accept(PictogramElement pe) {
       IWorkbenchPart part = WorkbenchUtil.getActivePart();
       if (part != null) {
+         if (IDebugUIConstants.ID_DEBUG_VIEW.equals(part.getSite().getId())) {
+            part = WorkbenchUtil.findView(ExecutionTreeView.VIEW_ID);
+         }
          GraphitiSourcePropertySection section = new GraphitiSourcePropertySection();
          section.setInput(part, null);
          return section.getStackFrame(pe) != null;
