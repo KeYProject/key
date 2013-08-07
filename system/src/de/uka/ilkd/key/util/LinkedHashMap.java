db6076be6e743199e8d2b54a513ca0ea5ae49ac1
Daniel Bruns <bruns@kit.edu>
2013-08-07 08:45:09 +0200

    [util] LinkedHashMap implements Iterable<Pair>

--- system/src/de/uka/ilkd/key/util/LinkedHashMap.java
+++ system/src/de/uka/ilkd/key/util/LinkedHashMap.java
@@ -1,13 +1,13 @@
-// This file is part of KeY - Integrated Deductive Software Design 
+// This file is part of KeY - Integrated Deductive Software Design
 //
-// Copyright (C) 2001-2011 Universitaet Karlsruhe (TH), Germany 
+// Copyright (C) 2001-2011 Universitaet Karlsruhe (TH), Germany
 //                         Universitaet Koblenz-Landau, Germany
 //                         Chalmers University of Technology, Sweden
-// Copyright (C) 2011-2013 Karlsruhe Institute of Technology, Germany 
+// Copyright (C) 2011-2013 Karlsruhe Institute of Technology, Germany
 //                         Technical University Darmstadt, Germany
 //                         Chalmers University of Technology, Sweden
 //
-// The KeY system is protected by the GNU General 
+// The KeY system is protected by the GNU General
 // Public License. See LICENSE.TXT for details.
 //
 
@@ -16,37 +16,35 @@ package de.uka.ilkd.key.util;
 import java.util.Iterator;
 
 public class LinkedHashMap<K, V>
-	extends java.util.LinkedHashMap<K, V> {
+	extends java.util.LinkedHashMap<K, V>
+    implements Iterable<Pair<K,V>> {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -6216276580053259582L;
-    
-    
+
+    private static final long serialVersionUID = 4295774122581786871L;
+
     public LinkedHashMap() {
 	super();
     }
-    
-    
+
+
     public LinkedHashMap(K key, V value) {
 	super();
 	put(key, value);
     }
-    
-    
+
+
     public LinkedHashMap(K[] keys, V[] values) {
 	super();
 	putAll(keys, values);
     }
-    
-    
+
+
     public LinkedHashMap(Iterable<K> keys, Iterable<V> values) {
 	super();
 	putAll(keys, values);
     }
-    
-    
+
+
     public void putAll(K[] keys, V[]vals) {
 	for (int i = 0; i < keys.length; i++) {
 	    if (i < vals.length) {
@@ -57,7 +55,7 @@ public class LinkedHashMap<K, V>
 	}
     }
 
-    
+
     public void putAll(Iterable<K> keys, Iterable<V> vals) {
 	Iterator<V> itVals = vals.iterator();
 	for (K key : keys) {
@@ -68,5 +66,41 @@ public class LinkedHashMap<K, V>
 	    }
 	}
     }
-    
+
+    public Iterator<Pair<K,V>> iterator() {
+        return new PairIterator<K,V>(this);
+    }
+
+    private static class PairIterator<K,V> implements Iterator<Pair<K,V>> {
+
+        private final Iterator<K> keyIt;
+        private final LinkedHashMap<K, V> map;
+        private K last = null;
+
+        public PairIterator (final LinkedHashMap<K, V> map) {
+            this.map = map;
+            keyIt = map.keySet().iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return keyIt.hasNext();
+        }
+
+        @Override
+        public Pair<K, V> next() {
+            last = keyIt.next();
+            return new Pair<K, V>(last, map.get(last));
+        }
+
+        @Override
+        public void remove() {
+            if (last != null) {
+                map.remove(last);
+                last = null;
+            }
+        }
+
+    }
+
 }
