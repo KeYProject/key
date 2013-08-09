7ef78af3aa10ac38d774db5e39a50f423cd2cd23
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2013-08-09 14:53:15 +0200

    KeY and SED: Fixed exception in configuration computation in case of if in updates.

--- system/src/de/uka/ilkd/key/symbolic_execution/SymbolicConfigurationExtractor.java
+++ system/src/de/uka/ilkd/key/symbolic_execution/SymbolicConfigurationExtractor.java
@@ -828,7 +828,9 @@ public class SymbolicConfigurationExtractor {
             // Static fields have a null term as select argument.
          }
          else {
-            throw new ProofInputException("Unsupported operator in select argument of heap update \"" + selectArgument.op() + "\".");
+            for (int i = 0; i < selectArgument.arity(); i++) {
+               collectLocationsFromHeapUpdate(selectArgument.sub(i), locationsToFill, updateCreatedObjectsToFill, updateValueObjectsToFill);
+            }
          }
          // Add select value term to result
          ProgramVariable var = SymbolicExecutionUtil.getProgramVariable(getServices(), heapLDT, term.sub(2));
@@ -871,7 +873,9 @@ public class SymbolicConfigurationExtractor {
          collectLocationsFromHeapUpdate(term.sub(0), locationsToFill, updateCreatedObjectsToFill, updateValueObjectsToFill);
       }
       else {
-         throw new ProofInputException("Unsupported operator in heap update \"" + term.op() + "\".");
+         for (int i = 0; i < term.arity(); i++) {
+            collectLocationsFromHeapUpdate(term.sub(i), locationsToFill, updateCreatedObjectsToFill, updateValueObjectsToFill);
+         }
       }
    }
 
