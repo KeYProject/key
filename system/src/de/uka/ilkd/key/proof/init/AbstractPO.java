ba38122b6a8c6ba54917edf9ab61aa85018f6a1b
Christoph Scheben <scheben@ira.uka.de>
2013-08-20 12:03:25 +0200

    comments

--- system/src/de/uka/ilkd/key/proof/init/AbstractPO.java
+++ system/src/de/uka/ilkd/key/proof/init/AbstractPO.java
@@ -289,7 +289,7 @@ public abstract class AbstractPO implements IPersistablePO {
                                 ? initConfig.getSettings()
                                 : new ProofSettings(ProofSettings.DEFAULT_SETTINGS));
 
-        // Make sure that required label works are present
+        // Make sure that required label workers are present
         ImmutableList<ITermLabelWorker> labelInstantiators = proof.getSettings().getLabelSettings().getLabelInstantiators();
         if (!labelInstantiators.contains(SelectSkolemConstantTermLabelInstantiator.INSTANCE)) {
            labelInstantiators = labelInstantiators.append(SelectSkolemConstantTermLabelInstantiator.INSTANCE);
