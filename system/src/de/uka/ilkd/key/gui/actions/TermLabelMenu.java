3cb55ab4f01ccfce3bd6ef6b8d78ec9cbd699046
Kai Wallisch <kai.wallisch@ira.uka.de>
2013-10-12 16:45:01 +0200

    CheckBox for every ITermLabel.

--- system/src/de/uka/ilkd/key/gui/actions/TermLabelMenu.java
+++ system/src/de/uka/ilkd/key/gui/actions/TermLabelMenu.java
@@ -4,10 +4,13 @@ import de.uka.ilkd.key.collection.ImmutableList;
 import de.uka.ilkd.key.gui.KeYMediator;
 import de.uka.ilkd.key.gui.KeYSelectionEvent;
 import de.uka.ilkd.key.gui.KeYSelectionListener;
+import de.uka.ilkd.key.gui.MainWindow;
 import de.uka.ilkd.key.logic.Name;
+import java.awt.event.ActionEvent;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
 
 /**
@@ -37,7 +40,17 @@ public class TermLabelMenu extends JMenu {
                 }
                 Collections.sort(stringNames);
                 for (String name : stringNames) {
-                    add(name);
+                    MainWindowAction mainWindowAction = new MainWindowAction(MainWindow.getInstance()) {
+                        @Override
+                        public void actionPerformed(ActionEvent ae) {
+                            mainWindow.makePrettyView();
+                        }
+                    };
+                    mainWindowAction.setName(name);
+                    JCheckBoxMenuItem checkBox = new JCheckBoxMenuItem(mainWindowAction);
+                    checkBox.setName("ITermLabel_" + name);
+                    checkBox.setState(true);
+                    add(checkBox);
                 }
             }
         });
