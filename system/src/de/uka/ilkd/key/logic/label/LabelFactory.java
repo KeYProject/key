8a2df4d06bf17ceda9109825359eda93bc7528e8
Christoph Scheben <scheben@ira.uka.de>
2013-10-14 16:06:32 +0200

    added method to get created term labels to the LabelFactory

--- system/src/de/uka/ilkd/key/logic/label/LabelFactory.java
+++ system/src/de/uka/ilkd/key/logic/label/LabelFactory.java
@@ -1,52 +1,68 @@
-// This file is part of KeY - Integrated Deductive Software Design 
+// This file is part of KeY - Integrated Deductive Software Design
 //
-// Copyright (C) 2001-2011 Universitaet Karlsruhe (TH), Germany 
+// Copyright (C) 2001-2011 Universitaet Karlsruhe (TH), Germany
 //                         Universitaet Koblenz-Landau, Germany
 //                         Chalmers University of Technology, Sweden
-// Copyright (C) 2011-2013 Karlsruhe Institute of Technology, Germany 
+// Copyright (C) 2011-2013 Karlsruhe Institute of Technology, Germany
 //                         Technical University Darmstadt, Germany
 //                         Chalmers University of Technology, Sweden
 //
-// The KeY system is protected by the GNU General 
+// The KeY system is protected by the GNU General
 // Public License. See LICENSE.TXT for details.
 //
-
 package de.uka.ilkd.key.logic.label;
 
 import de.uka.ilkd.key.logic.ITermLabel;
 import de.uka.ilkd.key.logic.UnknownLabelException;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 
 /**
  * Factory class for term labels
- * 
- * Attention: TermLabels used as patterns or construction labels are provided by the
- * label factory in package rule.label
+ * <p/>
+ * Attention: TermLabels used as patterns or construction labels are provided by
+ * the label factory in package rule.label
  */
 public class LabelFactory {
-   public static ITermLabel createLabel(String name, List<String> parameters) throws UnknownLabelException {
-      if (SymbolicExecutionTermLabel.NAME.toString().equals(name)) {
-         if (parameters != null && parameters.size() == 1) {
-            return new SymbolicExecutionTermLabel(Integer.valueOf(parameters.get(0)));
-         }
-         else {
-            throw new IllegalArgumentException("Label " + SymbolicExecutionTermLabel.NAME + " requires exactly one Integer-Parameter with its ID.");
-         }
-      }
-      else if (LoopBodyTermLabel.NAME.toString().equals(name)) {
-         return LoopBodyTermLabel.INSTANCE;
-      }
-      else if (LoopInvariantNormalBehaviorTermLabel.NAME.toString().equals(name)) {
-         return LoopInvariantNormalBehaviorTermLabel.INSTANCE;
-      }
-      else if (SelectSkolemConstantTermLabel.NAME.toString().equals(name)) {
-          return SelectSkolemConstantTermLabel.INSTANCE;
-      }
-      else if (AnonHeapTermLabel.NAME.toString().equals(name)) {
-          return AnonHeapTermLabel.INSTANCE;
-      }
-      else {
-         throw new UnknownLabelException(name);
-      }
-   }
+    private static Set<ITermLabel> createdLabels = new TreeSet<ITermLabel>();
+
+
+    public static Set<ITermLabel> getCreatedLabels() {
+        return createdLabels;
+    }
+
+
+    public static ITermLabel createLabel(String name,
+                                         List<String> parameters) throws UnknownLabelException {
+        ITermLabel result = null;
+        if (SymbolicExecutionTermLabel.NAME.toString().equals(name)) {
+            if (parameters != null && parameters.size() == 1) {
+                result = new SymbolicExecutionTermLabel(Integer.valueOf(parameters.get(0)));
+            } else {
+                throw new IllegalArgumentException("Label " +
+                                                   SymbolicExecutionTermLabel.NAME +
+                                                   " requires exactly one Integer-Parameter with its ID.");
+            }
+        } else if (LoopBodyTermLabel.NAME.toString().equals(name)) {
+            result = LoopBodyTermLabel.INSTANCE;
+        } else if (LoopInvariantNormalBehaviorTermLabel.NAME.toString().equals(name)) {
+            result = LoopInvariantNormalBehaviorTermLabel.INSTANCE;
+        } else if (SelectSkolemConstantTermLabel.NAME.toString().equals(name)) {
+            result = SelectSkolemConstantTermLabel.INSTANCE;
+        } else if (AnonHeapTermLabel.NAME.toString().equals(name)) {
+            result = AnonHeapTermLabel.INSTANCE;
+        } else {
+            throw new UnknownLabelException(name);
+        }
+
+        if (result == null) {
+            throw new NullPointerException("Instantiation of term label '" +
+                                           name + "' resulted in null value.");
+        }
+
+        createdLabels.add(result);
+        return result;
+    }
 }
