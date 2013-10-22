8ae44a7415f0e352013b73124219e0b8cf7e6267
Kai Wallisch <kai.wallisch@ira.uka.de>
2013-10-22 16:28:05 +0200

    Added JML Specification for maps.

---
+++ examples/heap/Map/Map2.java
@@ -0,0 +1,79 @@
+package CaseStudyUseMaps;
+
+public interface Map2 {
+    
+    //@ public ghost instance \locset footprint;
+    //@ public ghost instance \free map;
+    
+    //@ public instance invariant \subset(\singleton(this.map), footprint);
+    //@ public instance invariant \subset(\singleton(this.footprint), footprint);
+    
+    /* @ public instance invariant (\forall Object o; 
+     @ \dl_mapGet(map, o) instanceof Object || \dl_mapGet(map, o) == null );
+     @*/
+    
+    /* @ public instance invariant (\forall Object o; 
+     @ (\dl_inDomain(map, o) <==> (\dl_mapGet(map, o) != null )));
+     @*/
+    
+    //@ public accessible \inv: footprint;
+    
+    // -----
+    // Method signatures + specs
+    // -----
+    
+    /*@ public normal_behaviour
+    @ accessible footprint;
+    @ ensures \result == (\dl_inDomain(map, o)?\dl_mapGet(map, o):null);
+    @*/
+    public /*@pure nullable@*/ Object get(Object o);
+
+    /*@ public normal_behaviour
+     @ accessible footprint;
+     @   ensures \result == (map == \dl_mapEmpty());
+     @*/
+    public /*@pure@*/ boolean isEmpty();
+    
+    /*@ public normal_behaviour
+     @ accessible footprint;
+     @   ensures \result == (\sum Object key; \dl_mapGet(map,key) != null; 1);
+     @*/
+    public /*@pure@*/ int size();
+
+    /*@ public normal_behaviour
+     @ accessible footprint;
+     @ ensures \result == \dl_inDomain(map,key);
+     @*/
+    public /*@pure@*/ boolean containsKey(Object key);
+
+    /*@ public normal_behaviour
+    @ accessible footprint;
+    @ ensures \result == (\exists Object key; \dl_inDomain(map,key); \dl_mapGet(map,key) == value);
+    @*/
+    public /*@pure@*/ boolean containsValue(Object value);
+    
+    /*@ public normal_behaviour
+     @ assignable footprint;
+     @ ensures \dl_mapGet(map,key) == value && \result == \old(\dl_mapGet(map,key)) &&
+     @ ( \forall Object o; (\fresh(o) ==> \dl_mapGet(map,o) == null) && 
+     @ (!\fresh(o) && o != key ==> \dl_mapGet(map,o) == \old(\dl_mapGet(map,o)))
+     @ );
+     @*/
+    public /*@nullable@*/ Object put(Object key, Object value);
+
+    /*@ public normal_behaviour
+     @ assignable footprint;
+     @   ensures \dl_mapGet(map,key) == null && \result == \old(\dl_mapGet(map,key)) &&
+     @ ( \forall Object o; (\fresh(o) ==> \dl_mapGet(map,o) == null) && 
+     @ (!\fresh(o) && o != key ==> \dl_mapGet(map,o) == \old(\dl_mapGet(map,o)))
+     @ );
+     @*/
+    public /*@nullable@*/ Object remove(Object key);
+
+    /*@ public normal_behaviour
+     @ assignable footprint;
+     @ ensures map == \dl_mapEmpty();
+     @*/
+    public void clear();
+    
+}
---
+++ examples/heap/Map/MapImplementation.java
@@ -0,0 +1,144 @@
+package CaseStudyUseMaps;
+
+final class MapImplementation implements Map2 {
+
+    private Object keys[];
+    private Object values[];
+
+    /*@
+     @ private invariant (\forall int i1; 0 <= i1 && i1 <= keys.length;
+     @                   (\forall int i2; 0 <= i2 && i2 <= keys.length && i1 != i2; keys[i1] != keys[i2]));
+     @ private invariant keys != null;
+     @ private invariant values != null;
+     @ private invariant footprint == \set_union(\set_union(this.*,keys[*]),values[*]);
+     @ private invariant (\forall int i; 0 <= i && i < keys.length; keys[i] != null);
+     @ private invariant (\forall int i; 0 <= i && i < keys.length; values[i] != null);
+     @ private invariant \typeof(keys) == \type(Object[]);
+     @ private invariant \typeof(values) == \type(Object[]);
+     @ private invariant keys.length == values.length;
+     @ private invariant (\forall int i; i < 0 || i >= keys.length || \dl_mapGet(map, keys[i]) == values[i]);
+     @ private invariant (\forall Object o;
+     @ (\exists int i; i >= 0 && i < keys.length && keys[i] == o) <==> \dl_inDomain(map, o));
+     @ private invariant (keys.length == 0) <==> (map == \dl_mapEmpty());
+     @*/
+
+    /*@ normal_behaviour
+     @   ensures map == \dl_mapEmpty() && \fresh(footprint);
+     @*/
+    public /*@pure@*/ MapImplementation() {
+        keys = new Object[0];
+        values = new Object[0];
+        //@ set map = \dl_mapEmpty();
+        //@ set footprint = \set_union(\set_union(\all_fields(keys), \all_fields(values)), \all_fields(this));
+    }
+
+    public int size() {
+        return keys.length;
+    }
+
+    public Object get(Object o) {
+        /*@ loop_invariant 0 <= i && i <= keys.length;
+         @  loop_invariant (\forall int x; 0 <= x && x < i; o != keys[x]);
+         @ decreases keys.length - i;
+         @ assignable \strictly_nothing;
+         @*/
+        for (int i = 0; i < keys.length; i++) {
+            if (o == keys[i]) {
+                return values[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return keys.length == 0;
+    }
+
+    public Object put(Object k, Object v) {
+        Object old = get(k);
+        if (!containsKey(k)) {
+            Object keysNew[] = new Object[keys.length + 1];
+            Object valuesNew[] = new Object[keys.length + 1];
+            for (int i = 0; i < keys.length; i++) {
+                keysNew[i] = keys[i];
+                valuesNew[i] = values[i];
+            }
+            keysNew[keys.length] = k;
+            valuesNew[keys.length] = v;
+
+            keys = keysNew;
+            values = valuesNew;
+        } else {
+            for (int i = 0; i < keys.length; i++) {
+                if (keys[i] == k) {
+                    values[i] = v;
+                }
+            }
+        }
+        return old;
+    }
+
+    public boolean containsKey(Object o) {
+        return get(o) != null;
+    }
+
+    public boolean containsValue(Object o) {
+        /*@ loop_invariant 0 <= i && i <= keys.length
+          @    && (\forall int x; x < 0 || x >= i || o != values[x]);
+          @ decreases keys.length - i;
+          @ assignable \strictly_nothing;
+          @*/
+        for (int i = 0; i < keys.length; i++) {
+            if (o == values[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Object remove(Object o) {
+
+        if (!containsKey(o)) {
+            return null;
+        }
+
+        int index = -1;
+
+        // Again, same loop_invariant as for get()
+        /*@ loop_invariant 0 <= i && i <= keys.length
+         @    && (\forall int x; 0 <= x && x < i; !(o == keys[x]));
+         @*/
+        for (int i = 0; i < keys.length; i++) {
+            if (o == keys[i]) {
+                index = i;
+            }
+        }
+
+        Object ret = values[index];
+        Object keysNew[] = new Object[keys.length - 1];
+        Object valuesNew[] = new Object[keys.length - 1];
+
+        for (int i = 0; i < keys.length; i++) {
+            if (i < index) {
+                keysNew[i] = keys[i];
+                valuesNew[i] = values[i];
+            }
+            if (i > index) {
+                keysNew[i] = keys[i - 1];
+                valuesNew[i] = values[i - 1];
+            }
+        }
+
+        keys = keysNew;
+        values = valuesNew;
+        return ret;
+    }
+
+    public void clear() {
+        keys = new Object[0];
+        values = new Object[0];
+        //@ set map = \dl_mapEmpty();
+        //@ set footprint = \set_union(\set_union(\all_fields(keys), \all_fields(values)), \all_fields(this));
+    }
+
+}
