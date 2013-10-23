2713e4301dec98654fdc7b2c13880be13a6bfca4
Daniel Bruns <bruns@kit.edu>
2013-10-23 13:42:25 +0200

    [bugfix 1372] do not join contracts when contract rule is selected manually

--- system/src/de/uka/ilkd/key/rule/ContractRuleApp.java
+++ system/src/de/uka/ilkd/key/rule/ContractRuleApp.java
@@ -69,6 +69,7 @@ public class ContractRuleApp extends AbstractContractRuleApp {
         return pio != null;      
     }
     
+    @Override
     public ContractRuleApp tryToInstantiate(Goal goal) {
     	if (complete()) {
     		return this;
@@ -78,13 +79,31 @@ public class ContractRuleApp extends AbstractContractRuleApp {
     	        .getApplicableContracts(
     	                UseOperationContractRule.computeInstantiation(
     	                        posInOccurrence().subTerm(), services),
-    	                services);
-        Modality m = (Modality)programTerm().op();
-        boolean transaction = (m == Modality.DIA_TRANSACTION || m == Modality.BOX_TRANSACTION); 
-        heapContext = HeapContext.getModHeaps(goal.proof().getServices(), transaction);
-    	return setContract(services.getSpecificationRepository()
-    	                .combineOperationContracts(
-    	                		contracts));
+    	                        services);
+    	if (contracts.size() !=1) return this; // incomplete app;
+    	Modality m = (Modality)programTerm().op();
+    	boolean transaction = (m == Modality.DIA_TRANSACTION || m == Modality.BOX_TRANSACTION); 
+    	heapContext = HeapContext.getModHeaps(goal.proof().getServices(), transaction);
+    	return setContract(contracts.iterator().next());
+    }
+
+    @Override
+    public ContractRuleApp forceInstantiate(Goal goal) {
+    	if (complete()) {
+    		return this;
+    	}
+    	Services services = goal.proof().getServices();
+    	ImmutableSet<FunctionalOperationContract> contracts = UseOperationContractRule
+    	.getApplicableContracts(
+    			UseOperationContractRule.computeInstantiation(
+    					posInOccurrence().subTerm(), services),
+    					services);
+    	Modality m = (Modality)programTerm().op();
+    	boolean transaction = (m == Modality.DIA_TRANSACTION || m == Modality.BOX_TRANSACTION); 
+    	heapContext = HeapContext.getModHeaps(goal.proof().getServices(), transaction);
+    	final FunctionalOperationContract combinedContract = services.getSpecificationRepository()
+    	.combineOperationContracts(contracts);
+    	return setContract(combinedContract);
     }
 
     @Override
