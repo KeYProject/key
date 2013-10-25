0170f767037ba0ff4e4eb9fdcd294e5109314dd5
Kai Wallisch <kai.wallisch@ira.uka.de>
2013-10-25 18:21:08 +0200

    Simplified invariant of put() method.

--- examples/heap/Map/MapImplementation.java
+++ examples/heap/Map/MapImplementation.java
@@ -91,19 +91,21 @@ final class MapImplementation implements Map2 {
 
         /*@ loop_invariant 0 <= i && i <= keys.length;
          @ loop_invariant (\forall int x; 0 <= x && x < i; 
-         @ (key == keys[x] ==> ( index == x && value == values[x])));
+         @ (key == keys[x] ==> ( index == x )));
          @ decreases keys.length - i;
-         @ assignable \set_union(values[*], ret);
+         @ assignable \strictly_nothing;
          @*/
         for (i = 0; i < keys.length; i++) {
             if (key == keys[i]) {
                 index = i;
-                ret = values[i];
-                values[i] = value;
             }
         }
         
-        if (index == -1) {
+        if (index != -1) {
+            ret = values[i];
+            values[i] = value;
+        }
+        else {
 
             Object keysNew[] = new Object[keys.length + 1];
             Object valuesNew[] = new Object[keys.length + 1];
