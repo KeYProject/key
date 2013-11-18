dfac8e3f277e7d690df412b78ebd30acf3b81d13
Christoph Scheben <scheben@ira.uka.de>
2013-11-18 18:32:41 +0100

    added description to IFLoopExamples

--- examples/information_flow/collection/src/loop/IFLoopExamples.java
+++ examples/information_flow/collection/src/loop/IFLoopExamples.java
@@ -1,5 +1,13 @@
 package loop;
 
+/**
+ * Information flow examples.
+ *
+ * A collection of several examples showing the usage of information flow
+ * loop invariants.
+ *
+ * @author Christoph Scheben
+ */
 public class IFLoopExamples {
 	int low;
 
