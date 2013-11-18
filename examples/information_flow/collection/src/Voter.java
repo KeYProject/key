b069a42c6bf12e0aca4f312ce36608984112bbcb
Christoph Scheben <scheben@ira.uka.de>
2013-11-18 13:06:40 +0100

    explanation of the voter example added

--- examples/information_flow/collection/src/Voter.java
+++ examples/information_flow/collection/src/Voter.java
@@ -1,13 +1,17 @@
-
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * Information flow example.
  *
- * @author christoph
+ * The example is a toy implementation of a voting process. The vote of every
+ * voter is read and send over a not further modelled network. If the read
+ * vote is not valid, then 0 is send instead to indicate abstention. The votes
+ * itself and whether a vote is was valid is secrete. At the end the
+ * participation is output.
+ *
+ * Without the optimisations described in the FM-Paper the verification of
+ * the method secure_voting() with the help of self-composition is very
+ * expensive or even infeasible.
+ *
+ * @author Christoph Scheben
  */
 public class Voter {
     public static int low_outputStream;
