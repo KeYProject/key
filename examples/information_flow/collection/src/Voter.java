198ce832a4b1795a286f9a6bc4564f2ded40edd2
Christoph Scheben <scheben@ira.uka.de>
2013-11-18 13:23:35 +0100

    explanation of the voter example added

--- examples/information_flow/collection/src/Voter.java
+++ examples/information_flow/collection/src/Voter.java
@@ -4,7 +4,7 @@
  * The example is a toy implementation of a voting process. The vote of every
  * voter is read and send over a not further modelled network. If the read
  * vote is not valid, then 0 is send instead to indicate abstention. The votes
- * itself and whether a vote is was valid is secrete. At the end the
+ * itself and whether a vote is valid is secrete. At the end the
  * participation is output.
  *
  * Without the optimisations described in the FM-Paper the verification of
