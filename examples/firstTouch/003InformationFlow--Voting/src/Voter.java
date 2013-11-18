e94eb7e0167de33d74697659cf967937416c8443
Christoph Scheben <scheben@ira.uka.de>
2013-11-18 18:46:18 +0100

    further first touch examples added

---
+++ examples/firstTouch/003InformationFlow--Voting/src/Voter.java
@@ -0,0 +1,134 @@
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author christoph
+ */
+public class Voter {
+    public static int low_outputStream;
+    public static boolean low_outputStreamAvailable;
+    private static int high_inputStream;
+
+    public static final int low_NUM_OF_VOTERS = 763;
+    public static int low_numOfVotes;
+    public boolean low_sendSuccessful;
+    
+    private boolean high_voteValid;
+    
+    /*@ normal_behavior
+        separates low_outputStream,
+                  low_outputStreamAvailable,
+                  low_NUM_OF_VOTERS,
+                  low_numOfVotes,
+                  low_sendSuccessful;    @*/
+    void secure_voting() {
+        /*@ loop_invariant 0 <= i && i <= low_NUM_OF_VOTERS;
+            loop_invariant \invariant_for(this);
+            separates low_outputStream,
+                      low_outputStreamAvailable,
+                      low_NUM_OF_VOTERS,
+                      low_numOfVotes,
+                      low_sendSuccessful,
+                      i;
+            decreases low_NUM_OF_VOTERS - i;
+            assignable \everything;
+            @*/
+        for (int i = 0; i < low_NUM_OF_VOTERS; i++) {
+            int high_vote = inputVote();
+            /*@ normal_behavior
+                separates low_outputStream,
+                          low_outputStreamAvailable,
+                          low_NUM_OF_VOTERS,
+                          low_numOfVotes,
+                          low_sendSuccessful;    @*/
+            {   if (isValid(high_vote)) {
+                    high_voteValid = true;
+                    low_sendSuccessful = sendVote(high_vote);
+                } else {
+                    high_voteValid = false;
+                    low_sendSuccessful = sendVote(0);
+                }
+            }
+            /*@ normal_behavior
+                separates low_outputStream,
+                          low_outputStreamAvailable,
+                          low_NUM_OF_VOTERS,
+                          low_numOfVotes,
+                          low_sendSuccessful;    @*/
+            { low_numOfVotes = (low_sendSuccessful ? low_numOfVotes + 1 : low_numOfVotes); }
+        }
+        publishVoterParticipation();
+    }
+    
+    /*@ normal_behavior
+        separates low_outputStream,
+                  low_outputStreamAvailable,
+                  low_NUM_OF_VOTERS,
+                  low_numOfVotes,
+                  low_sendSuccessful;    @*/
+    int inputVote() {
+        return high_inputStream;
+    }
+    
+    /*@ normal_behavior
+        separates low_outputStream,
+                  low_outputStreamAvailable,
+                  low_NUM_OF_VOTERS,
+                  low_numOfVotes,
+                  low_sendSuccessful,
+                  \result;               @*/
+    boolean sendVote(int x) {
+        if (low_outputStreamAvailable) {
+            // encrypt and send over some channel (not further modeled here)
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    /*@ normal_behavior
+        separates low_outputStream,
+                  low_outputStreamAvailable,
+                  low_NUM_OF_VOTERS,
+                  low_numOfVotes,
+                  low_sendSuccessful;    @*/
+    boolean isValid(int high_vote) {
+        // vote has to be in range 1..255
+        return 0 < high_vote && high_vote <= 255;
+    }
+    
+    /*@ normal_behavior
+        separates low_outputStream,
+                  low_outputStreamAvailable,
+                  low_NUM_OF_VOTERS,
+                  low_numOfVotes,
+                  low_sendSuccessful;    @*/
+    void publishVoterParticipation() {
+        low_outputStream = low_numOfVotes * 100 / low_NUM_OF_VOTERS;
+    }
+    
+    
+    /*@ normal_behavior
+        separates low_outputStream,
+                  low_outputStreamAvailable,
+                  low_NUM_OF_VOTERS,
+                  low_numOfVotes,
+                  low_sendSuccessful;    @*/
+    void insecure_voting() {
+        int high_vote = inputVote();
+        // vote has to be in range 1..255
+        if (0 < high_vote && high_vote <= 255) {
+            low_sendSuccessful = sendVote(high_vote);
+            low_numOfVotes++;
+        } else {
+            high_vote = 0;
+            low_sendSuccessful = sendVote(high_vote);
+        }
+        publishVoterParticipation();
+    }
+}
