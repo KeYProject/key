852b9e40b7c6d4d7b38e88918ac49fa1372425a5
Wojciech Mostowski <w.mostowski@utwente.nl>
2013-12-11 10:03:26 +0100

    [ModelMethods] Final touches for pushing onto master (there will still be more work later).

--- examples/heap/model_methods/src/CellTest.java
+++ examples/heap/model_methods/src/CellTest.java
@@ -1,18 +1,18 @@
 class CellTest {
 
-    /*@ requires c.<inv>; ensures c.post_set(x) && c.<inv>; @*/
+    /*@ requires \invariant_for(c); ensures c.post_set(x) && \invariant_for(c); @*/
     void callSet(Cell c, int x) {
         c.set(x);
     }
 
-    /*@ requires r.<inv>; ensures r.get() == 5; @*/
+    /*@ requires \invariant_for(r); ensures r.get() == 5; @*/
     void test(Recell r) {
         r.set(5);
         callSet(r, 4);
         r.undo();
     }
 
-    /*@ requires c.<inv>; ensures c.get() == 4; @*/
+    /*@ requires \invariant_for(c); ensures c.get() == 4; @*/
     void test2(Cell c) {
         c.set(5);
         callSet(c, 4);
--- examples/heap/model_methods/src/Coll.java
+++ examples/heap/model_methods/src/Coll.java
@@ -15,7 +15,7 @@ class Indirect {
         c.add(v);
     }
 
-    //@ requires c1.<inv> && c2.<inv>;
+    //@ requires \invariant_for(c1) && \invariant_for(c2);
     //@ ensures true;
     void test(Coll1 c1, Coll2 c2) {
         callAdd(c1, 42);
