3c25213ef7aa1f7a4f416a7ed38453680e4d6908
Kai Wallisch <kai.wallisch@ira.uka.de>
2014-01-27 11:24:46 +0100

    Hide all term labels --> Display term labels in formulas

--- system/src/de/uka/ilkd/key/gui/actions/KeYMenuCheckBox.java
+++ system/src/de/uka/ilkd/key/gui/actions/KeYMenuCheckBox.java
@@ -13,7 +13,16 @@ public abstract class KeYMenuCheckBox extends JCheckBoxMenuItem {
 
     protected final MainWindowAction mainWindowAction;
 
+    /*
+     * If this constructor is used, default selected state for the CheckBox
+     * is false.
+     */
     KeYMenuCheckBox(MainWindow mainWindow, String label) {
+        this(mainWindow, label, false);
+    }
+
+    KeYMenuCheckBox(MainWindow mainWindow, String label, boolean selectedState) {
+        super("", selectedState);
         final KeYMenuCheckBox checkBox = this;
         mainWindowAction = new MainWindowAction(mainWindow) {
             @Override
