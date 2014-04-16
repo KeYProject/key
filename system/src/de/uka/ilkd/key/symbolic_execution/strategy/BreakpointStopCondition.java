a71c4d964cf6f465a1177bfd678b02285ff4b985
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-04-16 13:21:31 +0200

    KeY and SED: Same number of steps is now executed on each branch when a breakpoint is hit.

--- system/src/de/uka/ilkd/key/symbolic_execution/strategy/BreakpointStopCondition.java
+++ system/src/de/uka/ilkd/key/symbolic_execution/strategy/BreakpointStopCondition.java
@@ -13,13 +13,6 @@
 
 package de.uka.ilkd.key.symbolic_execution.strategy;
 
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-
-import de.uka.ilkd.key.gui.ApplyStrategy.IStopCondition;
-import de.uka.ilkd.key.gui.ApplyStrategy.SingleRuleApplicationInfo;
 import de.uka.ilkd.key.java.SourceElement;
 import de.uka.ilkd.key.proof.Goal;
 import de.uka.ilkd.key.proof.IGoalChooser;
@@ -28,31 +21,17 @@ import de.uka.ilkd.key.proof.NodeInfo;
 import de.uka.ilkd.key.proof.Proof;
 import de.uka.ilkd.key.proof.init.ProofInputException;
 import de.uka.ilkd.key.rule.RuleApp;
-import de.uka.ilkd.key.symbolic_execution.util.SymbolicExecutionUtil;
 
-public abstract class BreakpointStopCondition implements IStopCondition {
+public abstract class BreakpointStopCondition extends ExecutedSymbolicExecutionTreeNodesStopCondition {
    /**
     * The flag if the Breakpoint is enabled.
     */
    private boolean enabled;
    
    /**
-    * The proof this stop condition is associated with
+    * The proof this stop condition is associated with.
     */
-   private Proof proof;
-   
-   /**
-    * Saves all {@link Goal}s that were responsible for a breakpoint to trigger.
-    */
-   private LinkedHashSet<Goal> breakpointGoals = new LinkedHashSet<Goal>();
-   
-   /**
-    * Stores for each {@link Node} which is a symbolic execution tree node the computed result
-    * of {@link #isGoalAllowed(int, long, Proof, IGoalChooser, long, int, Goal)} to make
-    * sure that it is only computed once and that the number of executed set statements is
-    * not increased multiple times for the same {@link Node}.
-    */
-   private Map<Node, Boolean> goalAllowedResultPerSetNode = new LinkedHashMap<Node, Boolean>();
+   private final Proof proof;
 
    /**
     * Creates a new {@link BreakpointStopCondition}.
@@ -60,100 +39,46 @@ public abstract class BreakpointStopCondition implements IStopCondition {
     * @param enabled flag if the Breakpoint is enabled
     */
    public BreakpointStopCondition(Proof proof, boolean enabled) {
-      this.enabled=enabled;
-      this.setProof(proof);
+      super(Integer.MAX_VALUE);
+      this.proof = proof;
+      this.enabled = enabled;
    }
-
+   
    /**
     * {@inheritDoc}
     */
    @Override
-   public boolean isGoalAllowed(int maxApplications, 
-                                long timeout, 
-                                Proof proof, 
-                                IGoalChooser goalChooser, 
-                                long startTime, 
-                                int countApplied, 
-                                Goal goal) { 
-         if(goal!=null){
-            Node node = goal.node();
-            RuleApp ruleApp = goal.getRuleAppManager().peekNext();
-            SourceElement activeStatement = NodeInfo.computeActiveStatement(ruleApp);
-               if (SymbolicExecutionUtil.isSymbolicExecutionTreeNode(node, ruleApp)) {
-                  // Check if the result for the current node was already computed.
-                  Boolean value = getGoalAllowedResultPerSetNode().get(node);
-                  if (value == null) {
-                     // Check if limit of set nodes of the current goal is exceeded
-                     if (!getBreakpointGoals().contains(goal)) {
-                           try{
-                              if(isEnabled()&&isBreakpointHit(activeStatement, ruleApp, proof, node)){
-                                 getBreakpointGoals().add(goal);
-                                 getGoalAllowedResultPerSetNode().put(node, Boolean.FALSE);
-                                 }
-                           }catch(ProofInputException e){
-                              //TODO
-                           }
-                        return true; 
-                     }
-                  }
-               }
-         }
-      return true;
-   }
-
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public boolean shouldStop(int maxApplications, 
+   public int getMaximalWork(int maxApplications, 
                              long timeout, 
                              Proof proof, 
-                             IGoalChooser goalChooser, 
-                             long startTime, 
-                             int countApplied, 
-                             SingleRuleApplicationInfo singleRuleApplicationInfo) {
-      // Check if a rule was applied
-      if (singleRuleApplicationInfo != null) {
-         // Get the node on which a rule was applied.
-         Goal goal = singleRuleApplicationInfo.getGoal();
-         Node node = goal.node();
-         assert node.childrenCount() == 0; // Make sure that this is the current goal node
-         Node updatedNode = node.parent();
-         // Check if multiple branches where created.
-         if (updatedNode.childrenCount() >= 2) {
-            if (getBreakpointGoals().contains(goal)) {
-               // Reuse number of set nodes for new created goals
-               Iterator<Node> childIter = updatedNode.childrenIterator();
-               while (childIter.hasNext()) {
-                  Node next = childIter.next();
-                  Goal nextGoal = next.proof().getGoal(next);
-                  // Check if the current goal is a new one
-                  if (nextGoal != goal) {
-                     // New goal found, use the number of set nodes for it.
-                     getBreakpointGoals().add(nextGoal);
-                  }
-               }
-            }
-         }
-         RuleApp ruleApp = goal.getRuleAppManager().peekNext();
-         if(ruleApp != null){
-            if (SymbolicExecutionUtil.isSymbolicExecutionTreeNode(node, ruleApp)) {
-               // Check if the result for the current node was already computed.
-               Boolean value = getGoalAllowedResultPerSetNode().get(node);
-               if (value == null) {
-                  if (getBreakpointGoals().contains(goal)) {
-                     getGoalAllowedResultPerSetNode().put(node, Boolean.TRUE);
-                     return true; // Limit of set nodes of this goal exceeded
-                  }
-               }
-               else {
-                  // Reuse already computed result.
-                  return value.booleanValue();
-               }
-            }
+                             IGoalChooser goalChooser) {
+      setMaximalNumberOfSetNodesToExecutePerGoal(Integer.MAX_VALUE);
+      return super.getMaximalWork(maxApplications, timeout, proof, goalChooser);
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   protected void handleNodeLimitNotExceeded(int maxApplications, 
+                                             long timeout, 
+                                             Proof proof, 
+                                             IGoalChooser goalChooser, 
+                                             long startTime, 
+                                             int countApplied, 
+                                             Goal goal,
+                                             Node node,
+                                             RuleApp ruleApp,
+                                             Integer executedNumberOfSetNodes) {
+      super.handleNodeLimitNotExceeded(maxApplications, timeout, proof, goalChooser, startTime, countApplied, goal, node, ruleApp, executedNumberOfSetNodes);
+      try {
+         SourceElement activeStatement = NodeInfo.computeActiveStatement(ruleApp);
+         if (isEnabled() && isBreakpointHit(activeStatement, ruleApp, proof, node)) {
+            setMaximalNumberOfSetNodesToExecutePerGoal(executedNumberOfSetNodes.intValue());
          }
       }
-      return false;
+      catch (ProofInputException e) {
+      }
    }
    
    /**
@@ -193,81 +118,5 @@ public abstract class BreakpointStopCondition implements IStopCondition {
     */
    public Proof getProof() {
       return proof;
-   }
-
-   /**
-    * @param proof the proof to set
-    */
-   public void setProof(Proof proof) {
-      this.proof = proof;
-   }
-   
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public String getGoalNotAllowedMessage(int maxApplications, 
-                                          long timeout, 
-                                          Proof proof, 
-                                          IGoalChooser goalChooser, 
-                                          long startTime, 
-                                          int countApplied, 
-                                          Goal goal) {
-      return "Breakpoint hit!";
-   }
-   
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public int getMaximalWork(int maxApplications, 
-                             long timeout, 
-                             Proof proof, 
-                             IGoalChooser goalChooser) {
-      getBreakpointGoals().clear(); // Reset number of already detected symbolic execution tree nodes for all goals.
-      getGoalAllowedResultPerSetNode().clear(); // Remove no longer needed references.
-      return 0;
-   }
-   
-   /**
-    * {@inheritDoc}
-    */
-   @Override
-   public String getStopMessage(int maxApplications, 
-                                long timeout, 
-                                Proof proof, 
-                                IGoalChooser goalChooser, 
-                                long startTime, 
-                                int countApplied, 
-                                SingleRuleApplicationInfo singleRuleApplicationInfo) {
-      return "Breakpoint hit!";
-   }
-
-   /**
-    * @return the goalAllowedResultPerSetNode
-    */
-   public Map<Node, Boolean> getGoalAllowedResultPerSetNode() {
-      return goalAllowedResultPerSetNode;
-   }
-
-   /**
-    * @param goalAllowedResultPerSetNode the goalAllowedResultPerSetNode to set
-    */
-   public void setGoalAllowedResultPerSetNode(Map<Node, Boolean> goalAllowedResultPerSetNode) {
-      this.goalAllowedResultPerSetNode = goalAllowedResultPerSetNode;
-   }
-
-   /**
-    * @return the breakpointGoals
-    */
-   public LinkedHashSet<Goal> getBreakpointGoals() {
-      return breakpointGoals;
-   }
-
-   /**
-    * @param breakpointGoals the breakpointGoals to set
-    */
-   public void setBreakpointGoals(LinkedHashSet<Goal> breakpointGoals) {
-      this.breakpointGoals = breakpointGoals;
    }
 }
