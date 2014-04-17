12b1008cb1ff445e8074f663e784dd3ecd2b90ce
gladisch <gladisch@linux-pr67.site>
2014-04-17 20:40:53 +0200

    Filtering of infeasible paths. SMT result report. Improved user feedback.

--- system/src/de/uka/ilkd/key/gui/smt/TGInfoDialog.java
+++ system/src/de/uka/ilkd/key/gui/smt/TGInfoDialog.java
@@ -2,6 +2,7 @@ package de.uka.ilkd.key.gui.smt;
 
 import java.awt.BorderLayout;
 import java.util.Collection;
+import java.util.Vector;
 
 import javax.swing.JDialog;
 import javax.swing.JScrollPane;
@@ -10,9 +11,11 @@ import javax.swing.text.DefaultCaret;
 
 import de.uka.ilkd.key.smt.SMTProblem;
 import de.uka.ilkd.key.smt.SMTSolver;
+import de.uka.ilkd.key.smt.SMTSolverResult;
 import de.uka.ilkd.key.smt.SolverLauncher;
 import de.uka.ilkd.key.smt.SolverLauncherListener;
 import de.uka.ilkd.key.smt.SolverType;
+import de.uka.ilkd.key.smt.model.Model;
 import de.uka.ilkd.key.testgen.TestCaseGenerator;
 
 public class TGInfoDialog extends JDialog implements SolverLauncherListener{
@@ -58,8 +61,59 @@ public class TGInfoDialog extends JDialog implements SolverLauncherListener{
 		writeln("Finished solving SMT problems: "+problemSolvers.size());
 		TestCaseGenerator tg = new TestCaseGenerator();
 		tg.setLogger(this);
-		tg.generateJUnitTestSuite(problemSolvers);
+		problemSolvers = filterSolverResultsAndShowSolverStatistics(problemSolvers);
+		if(problemSolvers.size()>0){
+			tg.generateJUnitTestSuite(problemSolvers);
+		}else{
+			writeln("No test data was generated.");
+		}
 		
+	}
+	
+	public Collection<SMTSolver> filterSolverResultsAndShowSolverStatistics(Collection<SMTSolver> problemSolvers){
+		int unknown=0;
+		int infeasiblePaths=0;
+		int solvedPaths=0;
+		int problem=0;
+		Vector<SMTSolver> output = new Vector<SMTSolver>();
+		int i = 0;
+		for(SMTSolver solver : problemSolvers){
+			try{
+				SMTSolverResult.ThreeValuedTruth res=solver.getFinalResult().isValid();
+				if(res==SMTSolverResult.ThreeValuedTruth.UNKNOWN){
+					unknown++;
+				}else if(res==SMTSolverResult.ThreeValuedTruth.FALSIFIABLE){
+					solvedPaths++;
+					if(solver.getQuery()!=null){
+						Model m = solver.getQuery().getModel();
+						if(TestCaseGenerator.modelIsOK(m)){
+							output.add(solver);
+						}else{
+							problem++;
+						}
+					}else{
+						problem++;
+					}
+				}else if(res==SMTSolverResult.ThreeValuedTruth.VALID){
+					infeasiblePaths++;
+				}
+			}catch(Exception ex){
+				writeln(ex.getMessage());
+			}
+		}
+		writeln("--- SMT Solver Results ---\n"+
+				" solved pathconditions:"+solvedPaths+"\n"+
+				" invalid pre-/pathconditions:"+infeasiblePaths+"\n"+
+				" unknown:"+unknown	);
+		if(problem>0){
+			writeln(" problems             :"+problem);
+		}
+		if(unknown>0){
+			writeln(" Adjust the SMT solver settings (e.g. timeout) in Options->SMT Solvers and restart key.\n Make sure you use Z3 version 4.3.1.");
+		}
+		writeln("----------------------");
+		
+		return output;
 	}
 
 	@Override
