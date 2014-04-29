b020badf010f83cdb69a4e01c60ded711859ea92
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-04-29 15:06:14 +0200

    KeY: Updated license headers

--- system/src/de/uka/ilkd/key/java/expression/literal/RealLiteral.java
+++ system/src/de/uka/ilkd/key/java/expression/literal/RealLiteral.java
@@ -3,15 +3,13 @@
 // Copyright (C) 2001-2011 Universitaet Karlsruhe (TH), Germany
 //                         Universitaet Koblenz-Landau, Germany
 //                         Chalmers University of Technology, Sweden
-// Copyright (C) 2011-2013 Karlsruhe Institute of Technology, Germany
+// Copyright (C) 2011-2014 Karlsruhe Institute of Technology, Germany
 //                         Technical University Darmstadt, Germany
 //                         Chalmers University of Technology, Sweden
 //
 // The KeY system is protected by the GNU General
 // Public License. See LICENSE.TXT for details.
 //
-
-
 
 package de.uka.ilkd.key.java.expression.literal;
 
