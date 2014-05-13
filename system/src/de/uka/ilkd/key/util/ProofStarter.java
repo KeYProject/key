2b3bd54a3c14e8f0771ae70d117eb297822d9327
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-05-13 11:34:44 +0200

    KeY: Avoided use of ProofStarter in BatchMode      => OneStepSimplifier still interferes with strategy settings used by ProofStarter; TestProofStarter shows the buggy behavior => Please FIX!

--- system/src/de/uka/ilkd/key/util/ProofStarter.java
+++ system/src/de/uka/ilkd/key/util/ProofStarter.java
@@ -206,18 +206,17 @@ public class ProofStarter {
     */
     public ApplyStrategyInfo start(ImmutableList<Goal> goals) {
         try {
-           proof.setRuleAppIndexToAutoMode();
+           final Profile profile = proof.env().getInitConfig().getProfile();
+           final StrategyFactory factory = profile.getDefaultStrategyFactory();
+           if (strategyProperties == null) {
+              strategyProperties = factory.getSettingsDefinition().getDefaultPropertiesFactory().createDefaultStrategyProperties();
+           }
            
            if (proof.getProofIndependentSettings().getGeneralSettings().oneStepSimplification()) {
               OneStepSimplifier simplifier = MiscTools.findOneStepSimplifier(proof);
               if (simplifier != null) {
                  simplifier.refresh(proof);
               }
-           }
-           final Profile profile = proof.env().getInitConfig().getProfile();
-           StrategyFactory factory = profile.getDefaultStrategyFactory();
-           if (strategyProperties == null) {
-              strategyProperties = factory.getSettingsDefinition().getDefaultPropertiesFactory().createDefaultStrategyProperties();
            }
            proof.setActiveStrategy(factory.create(proof, strategyProperties));
 
@@ -234,6 +233,7 @@ public class ProofStarter {
 
            boolean stopMode = strategyProperties.getProperty(StrategyProperties.STOPMODE_OPTIONS_KEY).equals(StrategyProperties.STOPMODE_NONCLOSE);
            ApplyStrategy.ApplyStrategyInfo result;
+           proof.setRuleAppIndexToAutoMode();
            result = prover.start(proof, goals, maxSteps, timeout, stopMode);
            
            if (result.isError()) {
@@ -256,6 +256,13 @@ public class ProofStarter {
         }
     }
 
+    public void init(Proof proof) {
+       this.proof = proof;
+       this.setMaxRuleApplications(proof.getSettings().getStrategySettings().getMaxSteps());
+       this.setTimeout(proof.getSettings().getStrategySettings().getTimeout());
+       this.setStrategy(proof.getSettings().getStrategySettings().getActiveStrategyProperties());
+    }
+    
     public void init(ProofAggregate proofAggregate) {
     	this.proof = proofAggregate.getFirstProof();
 
