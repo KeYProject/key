4b728ccf8c3c1a287be6216b3a6397fb04af2d70
Kai Wallisch <kai.wallisch@ira.uka.de>
2014-05-21 20:46:24 +0200

    Removed "extends CharScanner" from KeYLexerF.

--- system/src/de/uka/ilkd/key/parser/KeYLexerF.java
+++ system/src/de/uka/ilkd/key/parser/KeYLexerF.java
@@ -10,7 +10,6 @@
 // The KeY system is protected by the GNU General
 // Public License. See LICENSE.TXT for details.
 //
-
 package de.uka.ilkd.key.parser;
 
 import java.io.FileNotFoundException;
@@ -22,52 +21,51 @@ import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.ANTLRReaderStream;
 import org.antlr.runtime.ANTLRStringStream;
 
-import antlr.CharScanner;
 import antlr.Token;
 import antlr.TokenStreamException;
 
 import de.uka.ilkd.key.util.KeYExceptionHandler;
 
-public class KeYLexerF extends CharScanner {
+public class KeYLexerF {
 
     private KeYLexer keYLexer;
 
     public KeYLexerF(InputStream file, String filename, KeYExceptionHandler keh)
-	    throws FileNotFoundException {
-	try {
-	    final ANTLRInputStream stream = new ANTLRInputStream(file);
-	    stream.name = filename;
-	    this.keYLexer = new KeYLexer(stream, keh);
-	} catch (IOException e) {
-	    throw new FileNotFoundException(e.getMessage());
-	}
+            throws FileNotFoundException {
+        try {
+            final ANTLRInputStream stream = new ANTLRInputStream(file);
+            stream.name = filename;
+            this.keYLexer = new KeYLexer(stream, keh);
+        } catch (IOException e) {
+            throw new FileNotFoundException(e.getMessage());
+        }
     }
 
     public KeYLexerF(Reader file, String filename, KeYExceptionHandler keh)
-	    throws FileNotFoundException {
-	try {
-	    final ANTLRReaderStream stream = new ANTLRReaderStream(file);
-	    stream.name = filename;
-	    this.keYLexer = new KeYLexer(stream, keh);
-	} catch (IOException e) {
-	    throw new FileNotFoundException(e.getMessage());
-	}
+            throws FileNotFoundException {
+        try {
+            final ANTLRReaderStream stream = new ANTLRReaderStream(file);
+            stream.name = filename;
+            this.keYLexer = new KeYLexer(stream, keh);
+        } catch (IOException e) {
+            throw new FileNotFoundException(e.getMessage());
+        }
     }
 
     public KeYLexerF(String file, String filename, KeYExceptionHandler keh) {
-	final ANTLRStringStream stream = new ANTLRStringStream(file);
-	stream.name = filename;
-	this.keYLexer = new KeYLexer(stream, keh);
+        final ANTLRStringStream stream = new ANTLRStringStream(file);
+        stream.name = filename;
+        this.keYLexer = new KeYLexer(stream, keh);
     }
 
     public KeYLexer getKeYLexer() {
-	return this.keYLexer;
+        return this.keYLexer;
     }
 
-    @Override
     public Token nextToken() throws TokenStreamException {
-	org.antlr.runtime.Token token = this.keYLexer.nextToken();
+        org.antlr.runtime.Token token = this.keYLexer.nextToken();
 
-	return new Token(token.getType(), token.getText());
+        return new Token(token.getType(), token.getText());
     }
+
 }
