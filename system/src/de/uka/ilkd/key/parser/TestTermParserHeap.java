4187f6fc05b6a80d543dac806fde60a99a9ca15d
Kai Wallisch <kai.wallisch@ira.uka.de>
2014-05-28 23:44:18 +0200

    Added description for TestTermParserHeap.

--- system/src/de/uka/ilkd/key/parser/TestTermParserHeap.java
+++ system/src/de/uka/ilkd/key/parser/TestTermParserHeap.java
@@ -1,5 +1,10 @@
 package de.uka.ilkd.key.parser;
 
+/**
+ * Parser tests for heap terms.
+ *
+ * @author Kai Wallisch <kai.wallisch@ira.uka.de>
+ */
 public class TestTermParserHeap extends TestTermParser {
 
     public TestTermParserHeap() {
