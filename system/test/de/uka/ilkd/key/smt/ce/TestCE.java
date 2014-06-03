550629245d4b878616ef6ec6427f4411608cbddc
Mihai Herda <herda@kit.edu>
2014-06-03 17:02:10 +0200

    Implemented some tests for counterexample generator.

--- system/test/de/uka/ilkd/key/smt/ce/TestCE.java
+++ system/test/de/uka/ilkd/key/smt/ce/TestCE.java
@@ -53,6 +53,8 @@ public class TestCE extends TestCommons {
     @Override
     public SolverType getSolverType() {
        SolverType type = SolverType.Z3_CE_SOLVER;
+//       SolverType type = SolverType.Z3_SOLVER;
+       
        String solverPathProperty = System.getProperty(SYSTEM_PROPERTY_SOLVER_PATH);
        if (solverPathProperty != null && !solverPathProperty.isEmpty()) {
           type.setSolverCommand(solverPathProperty);
@@ -60,35 +62,83 @@ public class TestCE extends TestCommons {
        return type;
     }
     
-    public void testArrayClear(){
-    	UserInterface ui = new ConsoleUserInterface(new BatchMode("test", true), false, false);
-    	KeYMediator mediator = new KeYMediator(ui, false);
-    
-    	SemanticsBlastingMacro macro = new SemanticsBlastingMacro();
-    	TryCloseMacro close = new TryCloseMacro();
-    	FinishSymbolicExecutionMacro se = new FinishSymbolicExecutionMacro();
-    	
-    	HelperClassForTests helper = new HelperClassForTests();
-    	
-    	File file = new File(testFile+"Middle.java");
-    	ui.loadProblem(file);
-    	
-    	
-    	
-    	Proof proof = mediator.getSelectedProof();
-    	System.out.println(proof.root());
-    	//mediator.setProof(proof);
-    	
-    	try {
-    		se.applyTo(mediator, null, null);
-    		close.applyTo(mediator, null, null);
-	        macro.applyTo(mediator, null, null);
-        } catch (InterruptedException e) {	        
-	        e.printStackTrace();
-        }
-    	
-    	
-    	
-    	
+    public void testOverFlow1(){
+    	this.correctResult(testFile+"overflow1.key", true);
     }
+    
+    public void testOverFlow2(){
+    	this.correctResult(testFile+"overflow2.key", true);
+    }
+    
+    public void testTypes1(){
+    	this.correctResult(testFile+"types1.key", true);
+    }
+    
+    public void testTypes2(){
+    	this.correctResult(testFile+"types2.key", true);
+    }
+    
+    public void testTypes3(){
+    	this.correctResult(testFile+"types3.key", false);
+    }
+    
+    public void testTypes4(){
+    	this.correctResult(testFile+"types4.key", true);
+    }
+    
+    public void testTypes5(){
+    	this.correctResult(testFile+"types5.key", false);
+    }
+    
+    public void testTypes6(){
+    	this.correctResult(testFile+"types6.key", true);
+    }
+    
+    public void testTypes7(){
+    	this.correctResult(testFile+"types7.key", true);
+    }
+    
+    public void testTypes8(){
+    	this.correctResult(testFile+"types8.key", true);
+    }
+    
+    public void testTypes9(){
+    	this.correctResult(testFile+"types9.key", true);
+    }
+    
+    
+//    public void testArrayClear(){
+//    	UserInterface ui = new ConsoleUserInterface(new BatchMode("test", true), false, false);
+//    	KeYMediator mediator = ui.getMediator();
+//    
+//    	SemanticsBlastingMacro macro = new SemanticsBlastingMacro();
+//    	TryCloseMacro close = new TryCloseMacro();
+//    	FinishSymbolicExecutionMacro se = new FinishSymbolicExecutionMacro();
+//    	File file = new File(testFile+"Middle.java");
+//    	ui.loadProblem(file);
+//    	
+//    	Proof proof = null;
+//    	
+//    	
+//    	
+//    	do{
+//    		proof = mediator.getSelectedProof();
+//    	}while(proof == null);
+//    	
+//    	
+//    	System.out.println(proof.root());
+//    	//mediator.setProof(proof);
+//    	
+//    	try {
+//    		se.applyTo(mediator, null, null);
+//    		close.applyTo(mediator, null, null);
+//	        macro.applyTo(mediator, null, null);
+//        } catch (InterruptedException e) {	        
+//	        e.printStackTrace();
+//        }
+//    	
+//    	
+//    	
+//    	
+//    }
 }
