93239bcb48b8c06a4ed8d147cbf90cbb61bce422
Sven Braeutigam <Braeutigam.Sven@gmail.com>
2014-07-09 13:16:59 +0200

    TODOs done in FunctionalOperationContractCompletion.java. Still need to be checked and tested if implemented correctly.

--- projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/completion/FunctionalOperationContractCompletion.java
+++ projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/completion/FunctionalOperationContractCompletion.java
@@ -1,12 +1,18 @@
 package org.key_project.key4eclipse.common.ui.completion;
 
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.key_project.key4eclipse.common.ui.provider.ContractLabelProvider;
 import org.key_project.key4eclipse.common.ui.provider.ImmutableCollectionContentProvider;
 
 import de.uka.ilkd.key.collection.ImmutableSet;
+import de.uka.ilkd.key.gui.ContractConfigurator;
 import de.uka.ilkd.key.gui.InteractiveRuleApplicationCompletion;
+import de.uka.ilkd.key.gui.MainWindow;
 import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.proof.Goal;
 import de.uka.ilkd.key.rule.IBuiltInRuleApp;
@@ -15,67 +21,82 @@ import de.uka.ilkd.key.rule.UseOperationContractRule.Instantiation;
 import de.uka.ilkd.key.speclang.FunctionalOperationContract;
 
 /**
- * The {@link InteractiveRuleApplicationCompletion} to treat {@link UseOperationContractRule} in the Eclipse context.
+ * The {@link InteractiveRuleApplicationCompletion} to treat
+ * {@link UseOperationContractRule} in the Eclipse context.
+ * 
  * @author Martin Hentschel
  */
-public class FunctionalOperationContractCompletion extends AbstractInteractiveRuleApplicationCompletion {
+public class FunctionalOperationContractCompletion extends
+      AbstractInteractiveRuleApplicationCompletion {
    /**
     * {@inheritDoc}
     */
    @Override
    public boolean canComplete(IBuiltInRuleApp app) {
-      return de.uka.ilkd.key.gui.FunctionalOperationContractCompletion.checkCanComplete(app);
+      return de.uka.ilkd.key.gui.FunctionalOperationContractCompletion
+            .checkCanComplete(app);
    }
 
    /**
     * {@inheritDoc}
     */
    @Override
-   protected IInteractiveRuleApplicationCompletionPerform createPerform(IBuiltInRuleApp app, Goal goal, boolean forced) {
+   protected IInteractiveRuleApplicationCompletionPerform createPerform(
+         IBuiltInRuleApp app, Goal goal, boolean forced) {
       return new Perform(app, goal, forced);
    }
-   
+
    /**
     * The used {@link IInteractiveRuleApplicationCompletionPerform}.
+    * 
     * @author Martin Hentschel
     */
-   public static class Perform extends AbstractInteractiveRuleApplicationCompletionPerform {
+   public static class Perform extends
+         AbstractInteractiveRuleApplicationCompletionPerform {
       /**
        * The {@link Instantiation}.
        */
       private final Instantiation inst;
-      
+
       /**
        * The {@link FunctionalOperationContract}s
        */
       private final ImmutableSet<FunctionalOperationContract> contracts;
-      
+
       /**
        * The used {@link Services}.
        */
       private final Services services;
-      
+
       /**
        * The {@link TableViewer} which shows the contracts.
        */
       private TableViewer viewer;
-      
+
       /**
        * The {@link ContractLabelProvider} used in {@link #viewer}.
        */
       private ContractLabelProvider labelViewer;
-      
+
       /**
        * Constructor.
-       * @param app The DefaultBuiltInRuleApp to be completed.
-       * @param goal The Goal where the app will later be applied to.
-       * @param forced A boolean indicating if the rule should be applied without any additional interaction from the user provided that the application object can be made complete automatically.
+       * 
+       * @param app
+       *           The DefaultBuiltInRuleApp to be completed.
+       * @param goal
+       *           The Goal where the app will later be applied to.
+       * @param forced
+       *           A boolean indicating if the rule should be applied without
+       *           any additional interaction from the user provided that the
+       *           application object can be made complete automatically.
        */
       public Perform(IBuiltInRuleApp app, Goal goal, boolean forced) {
          super(app, goal, forced);
          services = goal.proof().getServices();
-         inst = UseOperationContractRule.computeInstantiation(app.posInOccurrence().subTerm(), getServices());
-         contracts = UseOperationContractRule.getApplicableContracts(inst, getServices());
+         inst = UseOperationContractRule.computeInstantiation(app
+               .posInOccurrence().subTerm(), getServices());
+         contracts = UseOperationContractRule.getApplicableContracts(inst,
+               getServices());
       }
 
       /**
@@ -100,11 +121,25 @@ public class FunctionalOperationContractCompletion extends AbstractInteractiveRu
       @Override
       public void createControl(Composite root) {
          viewer = new TableViewer(root);
-         viewer.setContentProvider(ImmutableCollectionContentProvider.getInstance());
+         viewer.setContentProvider(ImmutableCollectionContentProvider
+               .getInstance());
          labelViewer = new ContractLabelProvider(services);
          viewer.setLabelProvider(labelViewer);
          viewer.setInput(contracts);
-         // TODO: Ensure that "Finish" is only available when at least one contract is selected (use setErrorMessage)
+         // TODO: Ensure that "Finish" is only available when at least one
+         // contract is selected (use setErrorMessage)
+         setErrorMessage("Please select at least one contract.");
+         viewer.addSelectionChangedListener(new ISelectionChangedListener() {
+
+            @Override
+            public void selectionChanged(SelectionChangedEvent event) {
+               IStructuredSelection selection = (IStructuredSelection) viewer
+                     .getSelection();
+               if (!selection.isEmpty()) {
+                  setErrorMessage(null);
+               }
+            }
+         });
       }
 
       /**
@@ -112,7 +147,18 @@ public class FunctionalOperationContractCompletion extends AbstractInteractiveRu
        */
       @Override
       public IBuiltInRuleApp finish() {
-         return null; // TODO: Implement similar as de.uka.ilkd.key.collection.ImmutableSet.FunctionalOperationContractCompletion
+         FunctionalOperationContract[] contractsArr = contracts
+               .toArray(new FunctionalOperationContract[contracts.size()]);
+         ContractConfigurator cc = new ContractConfigurator(
+               MainWindow.getInstance(), services, contractsArr,
+               "Contracts for " + inst.pm.getName(), true);
+
+         if (cc.wasSuccessful()) {
+            return ((UseOperationContractRule) getApp().rule()).createApp(
+                  getApp().posInOccurrence()).setContract(cc.getContract());
+         }
+         return getApp(); // TODO: Implement similar as
+                      // de.uka.ilkd.key.collection.ImmutableSet.FunctionalOperationContractCompletion
       }
 
       /**
