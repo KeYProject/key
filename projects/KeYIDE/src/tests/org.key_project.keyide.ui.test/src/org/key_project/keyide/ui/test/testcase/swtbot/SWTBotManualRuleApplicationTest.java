912a3762fdc5578af30cf99ba7900c9a816a3075
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-08-14 11:46:03 +0200

    KeYIDE: Improved documentation and test usage.

--- projects/KeYIDE/src/tests/org.key_project.keyide.ui.test/src/org/key_project/keyide/ui/test/testcase/swtbot/SWTBotManualRuleApplicationTest.java
+++ projects/KeYIDE/src/tests/org.key_project.keyide.ui.test/src/org/key_project/keyide/ui/test/testcase/swtbot/SWTBotManualRuleApplicationTest.java
@@ -15,6 +15,7 @@ package org.key_project.keyide.ui.test.testcase.swtbot;
 
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEditor;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotStyledText;
@@ -37,10 +38,20 @@ import de.uka.ilkd.key.symbolic_execution.util.SymbolicExecutionUtil;
 import de.uka.ilkd.key.ui.CustomUserInterface;
 import de.uka.ilkd.key.util.MiscTools;
 
+/**
+ * Tests the interactive rule application in the {@link KeYEditor}.
+ * @author Martin Hentschel
+ */
 public class SWTBotManualRuleApplicationTest extends AbstractSWTBotKeYEditorTest {
+   /**
+    * Tests the application of a closing rule which closes the full proof.
+    * @throws Exception Occurred Exception.
+    */
    @Test
    public void testCloseFalse_ProofClosed() throws Exception {
       doStartProofTest("SWTBotManualRuleApplicationTest_testCloseFalse_ProofClosed", 
+                       "data/paycard",
+                       TestKeY4EclipseUtil.createOperationContractId("PayCard", "PayCard", "isValid()", "0", "normal_behavior"),
                        new IStopCondition() {
                           @Override
                           public boolean shouldStop(int maxApplications, long timeout, Proof proof, IGoalChooser goalChooser, long startTime, int countApplied, SingleRuleApplicationInfo singleRuleApplicationInfo) {
@@ -73,26 +84,60 @@ public class SWTBotManualRuleApplicationTest extends AbstractSWTBotKeYEditorTest
                        "false",
                        "closeFalse",
                        null,
+                       1,
                        true);
    }
    
+   /**
+    * Tests the application of the assignment rule which leafs the proof still open.
+    * @throws Exception Occurred Exception.
+    */
    @Test
    public void testAssignment_ProofStillOpen() throws Exception {
       doStartProofTest("SWTBotManualRuleApplicationTest_testAssignment_ProofStillOpen", 
+                       "data/paycard",
+                       TestKeY4EclipseUtil.createOperationContractId("PayCard", "PayCard", "isValid()", "0", "normal_behavior"),
                        null,
                        false,
                        "exc=null;",
                        "assignment",
                        null,
+                       1,
                        false);
    }
    
-   protected void doStartProofTest(String projectName, 
+   /**
+    * Performs the following test steps to test an interactive rule application:
+    * <ol>
+    *    <li>New project is created in workspace.</li>
+    *    <li>Source code to test is extracted from plug-in into the created project.</li>
+    *    <li>A proof is instantiated and opened in the {@link KeYEditor}.</li>
+    *    <li>The auto mode is started to reach a proof {@link Node} where the rule to test can be applied at.</li>
+    *    <li>The rule to test is applied interactively.</li>
+    *    <li>Arbitrary test steps implemented as {@link IAppliedRuleTest} are optionally executed.</li>
+    *    <li>The proof closed state is ensured.</li>
+     * </ol>
+    * @param projectName The unique project name of the project which will be created in the workspace.
+    * @param pathToSourceCodeInTestPlugin The path to the source code in the test plug-in which will be extracted into the created project.
+    * @param contractId The unique ID of the proof obligation (contract) for which a proof will be started.
+    * @param stopCondition An optional custom {@link IStopCondition} which stops the started auto mode at a node on which the rule to test can be applied.
+    * @param useOperationContracts {@code true} use operation contracts, {@code false} inline methods instead
+    * @param textToApplyRuleOn The text in the {@link KeYEditor} for which the context menu contains the rule to apply.
+    * @param ruleNameToApply The name of the rule to apply.
+    * @param appliedRuleTest Optionally, some additional test steps, e.g. to deal with an opened {@link Shell}.
+    * @param expectedNumOfChildrenAfterRuleApplication The number of child branches the rule creates or {@code 0} if no rule is applied.
+    * @param expectedProofClosed {@code true} {@link Proof} should be closed after rule application, {@code false} {@link Proof} will be still open.
+    * @throws Exception Occurred Exception.
+    */
+   protected void doStartProofTest(String projectName,
+                                   String pathToSourceCodeInTestPlugin,
+                                   String contractId,
                                    final IStopCondition stopCondition,
                                    final boolean useOperationContracts,
                                    final String textToApplyRuleOn,
                                    final String ruleNameToApply,
                                    final IAppliedRuleTest appliedRuleTest,
+                                   final int expectedNumOfChildrenAfterRuleApplication,
                                    final boolean expectedProofClosed) throws Exception {
       IKeYEditorTestSteps steps = new IKeYEditorTestSteps() {
          @Override
@@ -130,8 +175,10 @@ public class SWTBotManualRuleApplicationTest extends AbstractSWTBotKeYEditorTest
             }
             // Make sure that correct rule was applied
             assertEquals(expectedProofClosed, keyEditor.getCurrentProof().closed());
-            assertEquals(1, node.childrenCount());
-            assertEquals(ruleNameToApply, MiscTools.getRuleDisplayName(node));
+            assertEquals(expectedNumOfChildrenAfterRuleApplication, node.childrenCount());
+            if (expectedNumOfChildrenAfterRuleApplication >= 1) {
+               assertEquals(ruleNameToApply, MiscTools.getRuleDisplayName(node));
+            }
             assertEquals(expectedProofClosed, node.isClosed());
             // Make sure that start stop auto mode buttons are as expected
             assertEquals(!expectedProofClosed, bot.toolbarButtonWithTooltip("Start Auto Mode").isEnabled());
@@ -139,8 +186,8 @@ public class SWTBotManualRuleApplicationTest extends AbstractSWTBotKeYEditorTest
          }
       };
       doEditorTest(projectName, 
-                   "data/paycard", 
-                   TestKeY4EclipseUtil.createOperationContractId("PayCard", "PayCard", "isValid()", "0", "normal_behavior"),
+                   pathToSourceCodeInTestPlugin, 
+                   contractId,
                    5,
                    false, 
                    steps);
