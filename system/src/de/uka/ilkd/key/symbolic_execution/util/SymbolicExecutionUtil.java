2abbc13fa00ceedc2a1ef43d4e2335a3f26f59d9
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-09-05 15:12:18 +0200

    KeY and SED: Fixed exception during layout computation when accessing array.length without pretty printing.

--- system/src/de/uka/ilkd/key/symbolic_execution/util/SymbolicExecutionUtil.java
+++ system/src/de/uka/ilkd/key/symbolic_execution/util/SymbolicExecutionUtil.java
@@ -843,7 +843,7 @@ public final class SymbolicExecutionUtil {
          Term sub = arrayIndexTerm.sub(0);
          // Make sure that the defined index is an integer
          if (services.getTypeConverter().getIntegerLDT().getNumberSymbol() == sub.op()) {
-            return Integer.parseInt(ProofSaver.printAnything(sub, services));
+            return Integer.parseInt(SymbolicExecutionUtil.formatTerm(sub, services, false, true));
          }
          else {
             return -1;
