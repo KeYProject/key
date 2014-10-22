7f167210ef4da5150622f8bdfcbf69efde4a25f1
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-10-22 18:12:26 +0200

    KeY Resources: Implemented example wizard.

--- projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/wizard/KeYExampleNewWizard.java
+++ projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/wizard/KeYExampleNewWizard.java
@@ -49,7 +49,7 @@ import de.uka.ilkd.key.gui.ExampleChooser.ShortFile;
  * content provided by the KeY project.
  * @author Martin Hentschel
  */
-public class KeYExampleNewWizard extends AbstractNewJavaProjectWizard {
+public class KeYExampleNewWizard extends AbstractNewJavaExampleProjectWizard {
    /**
     * The used {@link KeYExampleWizardPage} in which the user selects one example.
     */
