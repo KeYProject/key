e119c15df5237219520d0388aafa46216db77fa7
Sven Bräutigam <Braeutigam.Sven@gmail.com>
2014-10-23 14:23:04 +0200

    Started working on LoopInvariantRuleCompletion.java

--- projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/completion/LoopInvariantRuleCompletion.java
+++ projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/completion/LoopInvariantRuleCompletion.java
@@ -103,7 +103,7 @@ public class LoopInvariantRuleCompletion extends AbstractInteractiveRuleApplicat
                                      null);
             try {
              inv = InvariantConfigurator.getInstance().getLoopInvariant(inv,
-                     services, false, loopApp.getHeapContext()); 
+                     services, false, loopApp.getHeapContext());  
             } catch (RuleAbortException e) {
             }
          } else {
