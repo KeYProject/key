b9546c4f263dd70880010ea3d2fefa635babc140
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-10-27 11:14:40 +0100

    KeY Resources: Improved example according to KeY book.

--- projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.resources.ui/src/org/key_project/key4eclipse/resources/ui/wizard/KeYResourceExampleNewWizard.java
+++ projects/KeY4Eclipse/src/plugins/org.key_project.key4eclipse.resources.ui/src/org/key_project/key4eclipse/resources/ui/wizard/KeYResourceExampleNewWizard.java
@@ -2,6 +2,7 @@ package org.key_project.key4eclipse.resources.ui.wizard;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.ui.wizards.NewJavaProjectWizardPageOne;
@@ -9,6 +10,7 @@ import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.Wizard;
 import org.key_project.key4eclipse.common.ui.util.LogUtil;
 import org.key_project.key4eclipse.resources.ui.Activator;
+import org.key_project.key4eclipse.resources.util.KeYResourcesUtil;
 import org.key_project.util.eclipse.BundleUtil;
 import org.key_project.util.eclipse.WorkbenchUtil;
 
@@ -88,8 +90,20 @@ public class KeYResourceExampleNewWizard extends KeYProjectWizard {
     */
    @SuppressWarnings("restriction")
    protected boolean createExampleContent(IContainer sourceDirectory) throws Exception {
+      // Add source code
       BundleUtil.extractFromBundleToWorkspace(Activator.PLUGIN_ID, "data/exampleProject/src", sourceDirectory);
-      IFile integerUtilFile = sourceDirectory.getFile(new Path("example/IntegerUtil.java"));
+      IFile integerUtilFile = sourceDirectory.getFile(new Path("IntegerUtil.java"));
+      IFile multipleRecursionFile = sourceDirectory.getFile(new Path("MultipleRecursion.java"));
+      // Add proofs
+      IFolder fileFolder = KeYResourcesUtil.getProofFolder(multipleRecursionFile);
+      IFile aProof = KeYResourcesUtil.getProofFile("MultipleRecursion[MultipleRecursion::b()].JML normal_behavior operation contract.0", fileFolder.getFullPath());
+      IFile bProof = KeYResourcesUtil.getProofFile("MultipleRecursion[MultipleRecursion::a()].JML normal_behavior operation contract.0", fileFolder.getFullPath());
+      if (!fileFolder.exists()) {
+         KeYResourcesUtil.createFolder(aProof);
+      }
+      BundleUtil.extractFromBundleToWorkspace(Activator.PLUGIN_ID, "data/exampleProject/proofs/a.proof", aProof);
+      BundleUtil.extractFromBundleToWorkspace(Activator.PLUGIN_ID, "data/exampleProject/proofs/b.proof", bProof);
+      // Open first example file
       if (integerUtilFile != null && integerUtilFile.exists()) {
          selectAndReveal(integerUtilFile);
          WorkbenchUtil.openEditor(integerUtilFile);
