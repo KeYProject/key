d9c4f9352ad2dd20dba32c68e7b7d8bff7eabd61
Moritz Lichter <lichtemo@googlemail.com>
2014-11-04 13:33:51 +0100

    Skeleton for accessing properties/preferences

--- projects/JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/JMLProfilePropertiesPage.java
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/JMLProfilePropertiesPage.java
@@ -1,48 +1,120 @@
 package org.key_project.jmlediting.ui;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.List;
-import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.IWorkbenchPropertyPage;
-import org.eclipse.ui.dialogs.PropertyPage;
 
-public class JMLProfilePropertiesPage extends PropertyPage implements
-      IWorkbenchPropertyPage {
+public class JMLProfilePropertiesPage extends PropertyAndPreferencePage {
 
-   private Button useDefaultButton;
    private List profilesList;
-   
+
+   public static final String JML_PROFILE_PREF_ID = "org.key_project.jmlediting.ui.preferences.profile";
+   public static final String JML_PROFILE_PROP_ID = "org.key_project.jmlediting.ui.propertypages.profile";
+
+   public static final QualifiedName PROFILE = new QualifiedName(
+         "org.key_project.jmleiditing.ui", "profile");
+   public static final String DEFAULT_JML_PROFILE = "default_jml_profile";
+
    public JMLProfilePropertiesPage() {
       // TODO Auto-generated constructor stub
+
    }
 
    @Override
-   protected Control createContents(Composite parent) {
+   protected Control createPreferenceContent(Composite parent) {
       final Composite myComposite = new Composite(parent, SWT.NONE);
       final GridLayout layout = new GridLayout();
-      layout.numColumns=1;
+      layout.numColumns = 1;
       myComposite.setLayout(layout);
-      
-      GridData data = new GridData();
-      data.grabExcessHorizontalSpace = true;
-      
-      this.useDefaultButton = new Button(myComposite, SWT.CHECK);
-      this.useDefaultButton.setText("Use project specific settings");
-      this.useDefaultButton.setData(data);
-      
+
+      GridData data;
+
       data = new GridData();
       data.grabExcessHorizontalSpace = true;
       data.grabExcessVerticalSpace = true;
-      
-      this.profilesList = new List(myComposite, SWT.V_SCROLL| SWT.SINGLE |SWT.BORDER);
+
+      this.profilesList = new List(myComposite, SWT.V_SCROLL | SWT.SINGLE
+            | SWT.BORDER);
       this.profilesList.setData(data);
+
+      this.initUI();
       
       return myComposite;
+   }
+
+   @Override
+   protected boolean hasProjectSpecificOptions(IProject project) {
+      try {
+         return project.getPersistentProperty(PROFILE) != null;
+      }
+      catch (CoreException e) {
+         return false;
+      }
+   }
+
+   @Override
+   protected String getPreferencePageID() {
+      return JML_PROFILE_PREF_ID;
+   }
+
+   @Override
+   protected String getPropertyPageID() {
+      return JML_PROFILE_PROP_ID;
+   }
+   
+   
+   private void initUI() {
+      if (this.isProjectPreferencePage()) {
+         // Project preferences
+         
+      } else {
+         // Gobal preferences
+      // global properties
+         IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
+         String defaultProfile = preferences.get(DEFAULT_JML_PROFILE, null);
+         
+      }
+   }
+   
+
+   @Override
+   public boolean performOk() {
+      if (this.isProjectPreferencePage()) {
+         // Project preferences
+         IProject project = this.getProject();
+         try {
+            if (this.useProjectSettings()) {
+               // Set property
+               // TODO
+            }
+            else {
+               // Remove property
+               project.setPersistentProperty(PROFILE, null);
+            }
+         }
+         catch (CoreException e) {
+            e.printStackTrace();
+            // TODO
+         }
+
+      }
+      else {
+         // global properties
+         IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
+         preferences.put(DEFAULT_JML_PROFILE, "TODO");
+      }
+
+      return super.performOk();
    }
 
 }
