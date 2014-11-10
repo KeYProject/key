21598a30193986487d40f0222d150b78809a15d8
David Giessing <david.giessing@live.de>
2014-11-10 09:26:58 +0100

    Added JML Content types in orig Config via getConfiguredContentTypes

--- projects/JMLEditing/src/plugins/org.key_project.javaeditor/src/org/key_project/javaeditor/extension/JMLJavaSourceViewerConfigurationExtension.java
+++ projects/JMLEditing/src/plugins/org.key_project.javaeditor/src/org/key_project/javaeditor/extension/JMLJavaSourceViewerConfigurationExtension.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.key_project.javaeditor.extension;
 
 import org.eclipse.jdt.ui.text.IColorManager;
@@ -17,8 +14,11 @@ import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.jface.text.hyperlink.IHyperlinkPresenter;
 import org.eclipse.jface.text.information.IInformationPresenter;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
+import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.quickassist.IQuickAssistAssistant;
 import org.eclipse.jface.text.reconciler.IReconciler;
+import org.eclipse.jface.text.reconciler.Reconciler;
+import org.eclipse.jface.text.rules.DefaultDamagerRepairer;
 import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.util.PropertyChangeEvent;
@@ -30,7 +30,26 @@ import org.eclipse.ui.texteditor.ITextEditor;
  */
 public class JMLJavaSourceViewerConfigurationExtension implements
       IJavaSourceViewerConfigurationExtension {
-
+   /**
+    * The {@link IColorManger} to use
+    */
+   private IColorManager colorManager;
+   
+   /**
+    * The {@link IPreferenceStore} to use
+    */
+   private IPreferenceStore preferenceStore;
+   
+   /**
+    * The {@link ITextEditor} to use
+    */
+   private ITextEditor editor;
+   
+   /**
+    * The partitioning to use
+    */
+   private String partitioning;
+   
    /* (non-Javadoc)
     * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#init(org.eclipse.jdt.ui.text.IColorManager, org.eclipse.jface.preference.IPreferenceStore, org.eclipse.ui.texteditor.ITextEditor, java.lang.String)
     */
@@ -38,8 +57,42 @@ public class JMLJavaSourceViewerConfigurationExtension implements
    public void init(IColorManager colorManager,
          IPreferenceStore preferenceStore, ITextEditor editor,
          String partitioning) {
-      // TODO Auto-generated method stub
+      this.colorManager = colorManager;
+      this.preferenceStore = preferenceStore;
+      this.editor = editor;
+      this.partitioning = partitioning;
+   }
 
+   /**
+    * returns the {@link IColorManager} to use
+    * @return the {@link IColorManager} to use
+    */
+   protected IColorManager getColorManager() {
+      return colorManager;
+   }
+
+   /**
+    * returns the {@link IPreferenceStore} to use
+    * @return the {@link IPreferenceStore} to use
+    */
+   protected IPreferenceStore getPreferenceStore() {
+      return preferenceStore;
+   }
+
+   /**
+    * returns the {@link ITextEditor} to use
+    * @return the {@link ITextEditor} to use
+    */
+   protected ITextEditor getEditor() {
+      return editor;
+   }
+
+   /**
+    * returns the partitioning to use
+    * @return the partitioning to use
+    */
+   protected String getPartitioning() {
+      return partitioning;
    }
 
    /**
@@ -58,24 +111,29 @@ public class JMLJavaSourceViewerConfigurationExtension implements
          IUndoManager currentResult) {
        return currentResult;
    }
-   /* (non-Javadoc)
-    * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#getReconciler(org.eclipse.jface.text.source.ISourceViewer, org.eclipse.jface.text.reconciler.IReconciler)
+   /**
+    * @{inheritDoc}
     */
    @Override
    public IReconciler getReconciler(ISourceViewer sourceViewer,
          IReconciler currentResult) {
-      // TODO Auto-generated method stub
-      return null;
+      // TODO ggf anpassen
+      return currentResult;
    }
 
-   /* (non-Javadoc)
-    * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#getPresentationReconciler(org.eclipse.jface.text.source.ISourceViewer, org.eclipse.jface.text.presentation.IPresentationReconciler)
+   /**
+    *@{inheritDoc}
     */
    @Override
    public IPresentationReconciler getPresentationReconciler(
          ISourceViewer sourceViewer, IPresentationReconciler currentResult) {
-      // TODO Auto-generated method stub
-      return null;
+    //TODO: create PresentationReconciler, install damagers and Repairers for JML ContentTypes
+      PresentationReconciler presReconciler = new PresentationReconciler(); //new Reconciler to Modify
+      /* NEED: access to configured ContentTypes of JavaSourceViewerConfiguration
+         to get Damager and Repairers of currentResult, to add them to reconciler
+         Problem: original Config is not accessible via the Extension.
+      */
+      return presReconciler;
    }
 
    /**
@@ -87,34 +145,31 @@ public class JMLJavaSourceViewerConfigurationExtension implements
       return currentResult;
    }
 
-   /* (non-Javadoc)
-    * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#getContentAssistant(org.eclipse.jface.text.source.ISourceViewer, org.eclipse.jface.text.contentassist.IContentAssistant)
+   /**
+    * @{inheritDoc}
     */
    @Override
    public IContentAssistant getContentAssistant(ISourceViewer sourceViewer,
          IContentAssistant currentResult) {
-      // TODO Auto-generated method stub
-      return null;
+      return currentResult;
    }
 
-   /* (non-Javadoc)
-    * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#getQuickAssistAssistant(org.eclipse.jface.text.source.ISourceViewer, org.eclipse.jface.text.quickassist.IQuickAssistAssistant)
+   /**
+    * @{inheritDoc}
     */
    @Override
    public IQuickAssistAssistant getQuickAssistAssistant(
          ISourceViewer sourceViewer, IQuickAssistAssistant currentResult) {
-      // TODO Auto-generated method stub
-      return null;
+      return currentResult;
    }
 
-   /* (non-Javadoc)
-    * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#getAutoIndentStrategy(org.eclipse.jface.text.source.ISourceViewer, java.lang.String, org.eclipse.jface.text.IAutoIndentStrategy)
+   /**
+    * @{inheritDoc}
     */
    @Override
    public IAutoIndentStrategy getAutoIndentStrategy(ISourceViewer sourceViewer,
          String contentType, IAutoIndentStrategy currentResult) {
-      // TODO Auto-generated method stub
-      return null;
+      return currentResult;
    }
 
    /**
@@ -132,7 +187,6 @@ public class JMLJavaSourceViewerConfigurationExtension implements
    @Override
    public String[] getDefaultPrefixes(ISourceViewer sourceViewer,
          String contentType, String[] currentResult) {
-      // TODO Auto-generated method stub
       return null;
    }
 
@@ -219,22 +273,28 @@ public class JMLJavaSourceViewerConfigurationExtension implements
    }
 
    /**
-    * @{inheritDoc}
+    * {@inheritDoc}
+    * @returns a modified List of configured ContentTypes, adds JMLSingleLine and JMLMultiLine content Types
     */
    @Override
-   public String[] getConfiguredContentTypes(ISourceViewer sourceViewer,
-         String[] currentResult) {
-      return currentResult;
+   public String[] getConfiguredContentTypes(ISourceViewer sourceViewer, String[] currentResult) {
+      //TODO: TEST
+         String[] modifiedcurrentResult= new String[currentResult.length+2];
+         for(int i=0;i<currentResult.length;i++){  //copy currentResult
+            modifiedcurrentResult[i]=currentResult[i];
+         }
+         modifiedcurrentResult[currentResult.length]="JMLSingleLine";   //add JMLSingleLine Type
+         modifiedcurrentResult[currentResult.length+1]="JMLMultiLine";  //add JMLMultiLine Type
+      return modifiedcurrentResult;
    }
 
-   /* (non-Javadoc)
-    * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#getConfiguredDocumentPartitioning(org.eclipse.jface.text.source.ISourceViewer, java.lang.String)
+   /**
+    * @{inheritDoc}
     */
    @Override
    public String getConfiguredDocumentPartitioning(ISourceViewer sourceViewer,
          String currentResult) {
-      // TODO Auto-generated method stub
-      return null;
+      return currentResult;
    }
 
    /**
@@ -282,15 +342,13 @@ public class JMLJavaSourceViewerConfigurationExtension implements
          IInformationPresenter currentResult) {
       return currentResult;
    }
-
-   /* (non-Javadoc)
-    * @see org.key_project.javaeditor.extension.IJavaSourceViewerConfigurationExtension#affectsTextPresentation(org.eclipse.jface.util.PropertyChangeEvent, boolean)
+   /**
+    * @{inheritDoc}
     */
    @Override
    public boolean affectsTextPresentation(PropertyChangeEvent event,
          boolean currentResult) {
-      // TODO Auto-generated method stub
-      return false;
+      return currentResult;
    }
 
 }
