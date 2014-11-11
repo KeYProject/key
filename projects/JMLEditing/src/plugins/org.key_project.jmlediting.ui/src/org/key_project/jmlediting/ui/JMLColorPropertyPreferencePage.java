6a7ed81c3d56fc54556d30483836e1363871ed08
Lisa Eisenhardt <lisa_eisenhardt@yahoo.de>
2014-11-11 06:48:22 +0100

    Added the JML color page to the menu of property, preference Working version of the JML Color page

--- projects/JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/JMLColorPropertyPreferencePage.java
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/JMLColorPropertyPreferencePage.java
@@ -2,38 +2,28 @@ package org.key_project.jmlediting.ui;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences.PreferenceChangeEvent;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jdt.internal.ui.preferences.PropertyAndPreferencePage;
-import org.eclipse.jface.preference.ColorFieldEditor;
-import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.ColorSelector;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.ColorDialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.List;
-import org.eclipse.swt.widgets.Shell;
-import org.key_project.jmlediting.core.IJMLProfile;
-import org.key_project.jmlediting.core.JMLProfileManagement;
-import org.key_project.jmlediting.ui.preferences.PreferenceConstants;
 
 
    /**
     * The {@link JMLProfilePropertiesPage} implements a properties and preferences
     * page to show in project settings or global preferences. The page allows the
-    * user to select a JML profile from available ones as project specific or
-    * global default.
+    * user to select the colours 
     * 
-    * @author Moritz Lichter
+    * @author Lisa Eisenhardt
     *
     */
    public class JMLColorPropertyPreferencePage extends PropertyAndPreferencePage
@@ -48,30 +38,48 @@ import org.key_project.jmlediting.ui.preferences.PreferenceConstants;
        * The ID of the page when acting as properties page.
        */
       public static final String JML_COLOUR_PROP_ID = "org.key_project.jmlediting.ui.propertypages.colour";
-     
+      
+      /**
+       * The name of the JML profile property of a project.
+       */
+      public static final QualifiedName COMMENT_COLOR = new QualifiedName(
+            "org.key_project.jmleiditing.ui", "CommentColor");
+      
+      /**
+       * The name of the global preference for the default JML profile.
+       */
+      public static final RGB DEFAULT_JML_COMMENT_COLOR = new RGB(0,255,0);
+      
+      /**
+       * The {@link IPreferenceChangeListener} which listens to changes of the
+       * profile property for properties. This is used to change the selection in
+       * the properies page when global settings are used and they change.
+       */     
       private IPreferenceChangeListener currentPreferenceListener;
       
-      private ColorFieldEditor CommentColor;
-      
-      public static final IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+      /**
+       * A {@link ColorSelector} which selects the Color for the JML-Comments
+       */
+      private ColorSelector CommentColor;
+
 
           
       /**
        * Creates a new {@link JMLProfilePropertiesPage}.
        */
       public JMLColorPropertyPreferencePage() {
-         this.currentPreferenceListener = new IPreferenceChangeListener(){
+
+         this.currentPreferenceListener = new IPreferenceChangeListener() {
 
             @Override
-            public void preferenceChange(final PreferenceChangeEvent event) {
-               updateSelection();
+            public void preferenceChange(PreferenceChangeEvent event) {
+               updateValues();
                
             }
-         }  ;
-
-
+          
+         };
       }
-      
+
       @Override
       public void setVisible(boolean visible) {
          // Register the preference listener if the dialog is visible
@@ -85,62 +93,68 @@ import org.key_project.jmlediting.ui.preferences.PreferenceConstants;
          }
          super.setVisible(visible);
       }
+      
+      
       @Override
       protected Control createPreferenceContent(Composite parent) {
          // Initialize the UI
-         // Create a list for the profile with a label
-         final Composite myComposite = new Composite(parent, 0);
 
-         //final GridLayout layout = new GridLayout();
-        // layout.numColumns = 1;
-         //myComposite.setLayout(layout);
-         
-         //GridData data;
+         final Composite myComposite = new Composite(parent, SWT.NONE);
 
-         //data = new GridData();
+         final GridLayout layout = new GridLayout();
+         layout.numColumns = 1;
+         myComposite.setLayout(layout);
          
-       
-         new ColorFieldEditor(PropertyNames.COMMENT_COLOR.getLocalName(), 
-               "Choose JML comment color:  ", parent);
-         
-         
-         
-         
-         this.initUI();
-         
-         
+         GridData data;
 
+         data = new GridData();
+         Label label = new Label(myComposite, SWT.NONE);
+         label.setText("Choose JML comment color:");
+         label.setLayoutData(data);
+ 
+         CommentColor = new ColorSelector(myComposite);
+
+
+         IEclipsePreferences preferences = InstanceScope.INSTANCE
+               .getNode(Activator.PLUGIN_ID);
+         if (preferences.get(COMMENT_COLOR.getLocalName(), null) == null){
+            initializeDefaults();
+         }
+         this.updateValues();
+         
          return myComposite;
-      }
-      
-      
-      
-      
-      /**
-       * Initlializes the content of the UI. 
-       * Sets the values of all fields to the default
-       */
-      private void initUI() {
-         // Get all profiles and set them to the list
          
-        store.setDefault(PropertyNames.COMMENT_COLOR.getLocalName(), PropertyNames.DEFAULT_JML_COMMENT_COLOR.toString());
-                
-
       }
+
+
+   /**
+    * Saves the default values of the fields 
+    */
+   private void initializeDefaults() {
+      //set default Values:
+      IEclipsePreferences preferences = InstanceScope.INSTANCE
+            .getNode(Activator.PLUGIN_ID);
+      preferences.put(COMMENT_COLOR.getLocalName(), DEFAULT_JML_COMMENT_COLOR.toString());
+
+      
+      }
+      
+
       /**
-       * Updates the selected profile in the list of profiles to match the profile
+       * Updates the values
        * in the properties or preferences (with respect whether the pane is used
        * for preferences or properties).
        */
-      private void updateSelection() {
+      private void updateValues() {
+         IEclipsePreferences preferences = InstanceScope.INSTANCE
+               .getNode(Activator.PLUGIN_ID);
+         
          String color = null;
          if (this.isProjectPreferencePage() || this.useProjectSettings()) {
             // Read local project properties if we are in a properties pane and
             // project specific settings are enabled
             try {
-               store.setValue(PropertyNames.COMMENT_COLOR.getLocalName(), 
-                     this.getProject().getPersistentProperty(PropertyNames.COMMENT_COLOR));
-               color="set";
+               color =  this.getProject().getPersistentProperty(COMMENT_COLOR);
             }
             catch (CoreException e) {
                color = null;
@@ -149,34 +163,31 @@ import org.key_project.jmlediting.ui.preferences.PreferenceConstants;
          // Read from global preferences if no project specific profile is set
          if (color == null) {
             // Global preferences
-            //IEclipsePreferences preferences = InstanceScope.INSTANCE
-                 // .getNode(Activator.PLUGIN_ID);
-            store.setToDefault(PropertyNames.COMMENT_COLOR.getLocalName());
-            /*color = preferences.get(
-                  PropertyNames.DEFAULT_JML_COMMENT_COLOR.toString(), null);*/
-         }
-
-         }
+            color = preferences.get(
+                  COMMENT_COLOR.getLocalName(), null);
+            }
+         
+        CommentColor.setColorValue(StringtoRGB(color));
+        }
 
 
       @Override
       protected boolean hasProjectSpecificOptions(final IProject project) {
          // We have project specific options if a property is set on the project
          try {
-            return project.getPersistentProperty(PropertyNames.PROFILE) != null;
+            return project.getPersistentProperty(COMMENT_COLOR) != null;
          }
          catch (CoreException e) {
             return false;
          }
       }
       
-      
       @Override
       protected void enableProjectSpecificSettings(
             boolean useProjectSpecificSettings) {
          if (!useProjectSpecificSettings) {
             // Reset selection to default if no project settings
-            this.updateSelection();
+            this.updateValues();
          }
         // this.setListEnabled(useProjectSpecificSettings);
          super.enableProjectSpecificSettings(useProjectSpecificSettings);
@@ -191,10 +202,37 @@ import org.key_project.jmlediting.ui.preferences.PreferenceConstants;
       protected String getPropertyPageID() {
          return JML_COLOUR_PROP_ID;
       }
-   
       
+      
+      /**
+       * Changes the String col in a RGB-representation of a color.
+       * The format of the string is the outcome of the method RGB.toString()
+       * @param col the string should have the format RGB {x, y, z}
+       * @return the RGB representation
+       */
+      
+      private RGB StringtoRGB (String col) {
+
+         String[] colors = col.substring(5, col.length() - 1 ).split(", ");
+         RGB color = new RGB(
+             Integer.parseInt(colors[0].trim()),
+             Integer.parseInt(colors[1].trim()),
+             Integer.parseInt(colors[2].trim())
+             );
+
+         return color;
+      }
+         
 
 
+      @Override
+      public void performDefaults(){
+         CommentColor.setColorValue(DEFAULT_JML_COMMENT_COLOR);
+         super.performDefaults();
+         
+      }
+      
+      
       @Override
       public boolean performCancel() {
          // Remove preferences listener
@@ -206,30 +244,67 @@ import org.key_project.jmlediting.ui.preferences.PreferenceConstants;
       }
 
       @Override
+      public void performApply() {
+         IEclipsePreferences preferences = InstanceScope.INSTANCE
+               .getNode(Activator.PLUGIN_ID);
+         
+         if (this.isProjectPreferencePage()) {
+            // Project preferences
+            
+            IProject project = this.getProject();
+            
+            try {
+               if (this.useProjectSettings()) {
+                  // Set property
+                  project.setPersistentProperty(COMMENT_COLOR,
+                        CommentColor.getColorValue().toString());
+
+               }
+               else {
+                  // Remove property
+                  project.setPersistentProperty(COMMENT_COLOR, null);
+               }
+            }
+            catch (CoreException e) {
+               
+            }
+         }
+         else {
+            // global properties
+            preferences
+               .put(COMMENT_COLOR.getLocalName(), CommentColor.getColorValue().toString());
+    
+         }
+         
+         super.performApply();
+      }
+      
+      
+      @Override
       public boolean performOk() {
          // Remove preference listener
+    
          IEclipsePreferences preferences = InstanceScope.INSTANCE
                .getNode(Activator.PLUGIN_ID);
          preferences
                .removePreferenceChangeListener(this.currentPreferenceListener);
 
-                  
-
-         
 
          if (this.isProjectPreferencePage()) {
             // Project preferences
             
             IProject project = this.getProject();
+            
             try {
                if (this.useProjectSettings()) {
                   // Set property
-                  project.setPersistentProperty(PropertyNames.COMMENT_COLOR, CommentColor.getColorSelector().getColorValue().toString());
+                  project.setPersistentProperty(COMMENT_COLOR,
+                        CommentColor.getColorValue().toString());
 
                }
                else {
                   // Remove property
-                  project.setPersistentProperty(PropertyNames.COMMENT_COLOR, null);
+                  project.setPersistentProperty(COMMENT_COLOR, null);
                }
             }
             catch (CoreException e) {
@@ -239,9 +314,13 @@ import org.key_project.jmlediting.ui.preferences.PreferenceConstants;
          else {
             // global properties
             preferences
-                  .put(PropertyNames.COMMENT_COLOR.getLocalName(), CommentColor.getColorSelector().getColorValue().toString());
+               .put(COMMENT_COLOR.getLocalName(), CommentColor.getColorValue().toString());
+    
          }
-
+         
+         
          return super.performOk();
       }
+
+
 }
