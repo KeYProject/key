41c559ddf000cf9802fd3a71a99d0446848991df
David Giessing <david.giessing@live.de>
2014-11-13 13:42:58 +0100

    Cleaned imports

--- projects/JMLEditing/src/plugins/org.key_project.jmlediting.core/src/org/key_project/jmlediting/core/editor/SingleTokenScanner.java
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.core/src/org/key_project/jmlediting/core/editor/SingleTokenScanner.java
@@ -1,10 +1,7 @@
 package org.key_project.jmlediting.core.editor;
 
-import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.jface.text.rules.BufferedRuleBasedScanner;
-import org.eclipse.jface.text.rules.IToken;
-import org.eclipse.jface.text.rules.ITokenScanner;
 import org.eclipse.jface.text.rules.Token;
 
 public class SingleTokenScanner extends BufferedRuleBasedScanner {
