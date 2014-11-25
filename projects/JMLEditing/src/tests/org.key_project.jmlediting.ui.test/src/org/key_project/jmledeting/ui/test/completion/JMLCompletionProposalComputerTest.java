1947d64354c29b11cdeff92b91cd8f4ffac814e9
Thomas Glaser <thomas.glaser@stud.tu-darmstadt.de>
2014-11-25 14:35:44 +0100

    commented Test

--- projects/JMLEditing/src/tests/org.key_project.jmlediting.ui.test/src/org/key_project/jmledeting/ui/test/completion/JMLCompletionProposalComputerTest.java
+++ projects/JMLEditing/src/tests/org.key_project.jmlediting.ui.test/src/org/key_project/jmledeting/ui/test/completion/JMLCompletionProposalComputerTest.java
@@ -41,23 +41,23 @@ public class JMLCompletionProposalComputerTest {
    public void testCompletion() {
       SWTBotEclipseEditor editor = bot.activeEditor().toTextEditor();
       
-      //test in JML Comment
+      //test in JML comment
       editor.navigateTo(6, 12);
-      List<String> list = editor.getAutoCompleteProposals("");
+      List<String> list = editor.getAutoCompleteProposals("");          //list all proposals
       assertTrue("Not the correct amount of Proposals", list.size() == 10);
-      list = editor.getAutoCompleteProposals("exeption");
+      list = editor.getAutoCompleteProposals("exeption");               //list proposals for "exceptional_behavior" and "exceptional_behaviour"
       assertTrue("Not the correct amount of Proposals", list.size() == 2);
-      list = editor.getAutoCompleteProposals("ensur");
+      list = editor.getAutoCompleteProposals("ensur");                  //list proposal for "ensures"
       assertTrue("Not the correct amount of Proposals", list.size() == 1);
       
-      //Test in Java Code
+      //Test in Java code
       editor.navigateTo(10, 9);
-      list = editor.getAutoCompleteProposals("ensu");
+      list = editor.getAutoCompleteProposals("ensu");                   //should not find any proposal for "ensu" in Java code
       assertFalse("Proposed JML in Java code", list.contains("ensures"));
       
-      //Test in Java Comment
+      //Test in Java comment
       editor.navigateTo(9, 18);
-      list = editor.getAutoCompleteProposals("ensu");
+      list = editor.getAutoCompleteProposals("ensu");                   //should not find any proposal for "ensu" in Java comment
       assertFalse("Proposed JML in Java commend", list.contains("ensures"));
       }
 }
