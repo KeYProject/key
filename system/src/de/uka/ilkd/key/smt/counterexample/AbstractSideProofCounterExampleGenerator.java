62f09045dac3b9b50ac5ba919004b6f79cbb7391
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2014-12-17 08:47:48 +0100

    KeY: Separated counter example functionality from UI.

---
+++ system/src/de/uka/ilkd/key/smt/counterexample/AbstractSideProofCounterExampleGenerator.java
@@ -0,0 +1,26 @@
+package de.uka.ilkd.key.smt.counterexample;
+
+import de.uka.ilkd.key.core.KeYMediator;
+import de.uka.ilkd.key.logic.Sequent;
+import de.uka.ilkd.key.proof.Proof;
+import de.uka.ilkd.key.proof.init.ProofInputException;
+import de.uka.ilkd.key.proof.mgt.ProofEnvironment;
+import de.uka.ilkd.key.symbolic_execution.util.SideProofUtil;
+import de.uka.ilkd.key.util.ProofStarter;
+
+/**
+ * Implementation of {@link AbstractCounterExampleGenerator} which instantiates
+ * the new {@link Proof} as side proof.
+ */
+public abstract class AbstractSideProofCounterExampleGenerator extends AbstractCounterExampleGenerator {
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   protected Proof createProof(KeYMediator mediator, Proof oldProof, Sequent oldSequent) throws ProofInputException {
+      Sequent newSequent = createNewSequent(oldSequent);
+      ProofEnvironment env = SideProofUtil.cloneProofEnvironmentWithOwnOneStepSimplifier(oldProof, false);
+      ProofStarter starter = SideProofUtil.createSideProof(env, newSequent);
+      return starter.getProof();
+   }
+}
