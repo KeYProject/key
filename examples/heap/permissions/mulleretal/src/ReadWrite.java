e7680dcf181e39ae92f0b76c9bb1ea92b111af65
Wojciech Mostowski <w.mostowski@utwente.nl>
2014-12-18 14:08:36 +0100

    [Permissions] fixes to Mullers example

--- examples/heap/permissions/mulleretal/src/ReadWrite.java
+++ examples/heap/permissions/mulleretal/src/ReadWrite.java
@@ -30,13 +30,13 @@ public class ReadWrite {
           requires !\dl_readPermission(\permission(this.rds));
           ensures !\dl_readPermission(\permission(this.rds));
           assignable<permissions> this.rds, this.lockedRds, this.val;
-          assignable<heap> this.val, this.lockedRds;
+          assignable<heap> this.val, this.rds, this.lockedRds;
           diverges true; @*/
     public /*@ helper @*/ void doWrite() {
         boolean done = false;
         /*@ loop_invariant !\dl_readPermission(\permission(this.rds));
             assignable<permissions> this.rds, this.lockedRds, this.val;
-            assignable<heap> this.lockedRds, this.val; @*/
+            assignable<heap> this.lockedRds, this.rds, this.val; @*/
         while(!done) {
             lock();
             if(rds == 0) {
@@ -52,7 +52,7 @@ public class ReadWrite {
           requires !\dl_readPermission(\permission(this.rds));
           ensures !\dl_readPermission(\permission(this.rds));
           assignable<permissions> this.rds, this.val, this.lockedRds;
-          assignable<heap> this.val, this.lockedRds; @*/
+          assignable<heap> this.rds, this.lockedRds; @*/
     public /*@ helper @*/ int doRead() {
         lock(); rds++; unlock(); // read lock
         int r = read();
@@ -63,14 +63,23 @@ public class ReadWrite {
     /*@ public normal_behavior
           requires \dl_currentThread() != this;
           requires !\dl_readPermission(\permission(this.rds));
+          ensures \old(\dl_readPermission(\permission(this.val))) ==> rds > 0;
           ensures \dl_writePermissionObject(this, \old(\permission(this.rds)));
           ensures \dl_writePermissionObject(this, \old(\permission(this.lockedRds)));
           ensures rds >= 0;
-          ensures rds == 0 ==> \dl_writePermissionObject(this, \old(\permission(this.val)));
-          ensures rds > 0 ==>  \dl_readPermissionObject(this, \old(\permission(this.val)));
-          ensures \permission(this.rds) == \dl_transferPermission(\dl_FALSE(), this, \dl_currentThread(), 0, \old(\permission(this.rds)));
-          ensures \permission(this.lockedRds) == \dl_transferPermission(\dl_FALSE(), this, \dl_currentThread(), 0, \old(\permission(this.lockedRds)));
-          ensures \permission(this.val) == \dl_transferPermission(\dl_FALSE(), this, \dl_currentThread(), 0, \old(\permission(this.val)));
+          ensures rds == 0 ==>
+            \dl_writePermissionObject(this, \old(\permission(this.val)));
+          ensures rds > 0 ==>
+            \dl_readPermissionObject(this, \old(\permission(this.val)));
+          ensures \permission(this.rds) ==
+            \dl_transferPermission(\dl_FALSE(), this, \dl_currentThread(), 0,
+              \old(\permission(this.rds)));
+          ensures \permission(this.lockedRds) ==
+            \dl_transferPermission(\dl_FALSE(), this, \dl_currentThread(), 0,
+              \old(\permission(this.lockedRds)));
+          ensures \permission(this.val) ==
+            \dl_transferPermission(\dl_FALSE(), this, \dl_currentThread(), 0,
+              \old(\permission(this.val)));
           ensures this.lockedRds == this.rds;
           assignable<permissions> this.rds, this.lockedRds, this.val;
           assignable<heap> this.lockedRds; @*/
@@ -81,14 +90,18 @@ public class ReadWrite {
           requires \dl_writePermission(\permission(this.rds));
           requires \dl_writePermission(\permission(this.lockedRds));
           requires rds >= 0;
-          ensures \permission(this.rds) == \dl_returnPermission(\dl_currentThread(), this, \old(\permission(this.rds)));
-          ensures \permission(this.lockedRds) == \dl_returnPermission(\dl_currentThread(), this, \old(\permission(this.lockedRds)));
+          ensures \permission(this.rds) == \dl_returnPermission(\dl_currentThread(), this,
+            \old(\permission(this.rds)));
+          ensures \permission(this.lockedRds) == \dl_returnPermission(\dl_currentThread(), this,
+            \old(\permission(this.lockedRds)));
           ensures \old(this.rds) <= \old(this.lockedRds) ==>
-              \permission(this.val) == \dl_returnPermission(\dl_currentThread(), this, \old(\permission(this.val)));
+            \permission(this.val) == \dl_returnPermission(\dl_currentThread(), this,
+              \old(\permission(this.val)));
           ensures \old(this.rds) > \old(this.lockedRds) ==>
-              \permission(this.val) == \dl_transferPermission(\dl_TRUE(), this, \dl_currentThread(), 0, \dl_returnPermission(\dl_currentThread(), this, \old(\permission(this.val))));
+            \permission(this.val) == \dl_transferPermission(\dl_TRUE(), this, \dl_currentThread(), 0,
+              \dl_returnPermission(\dl_currentThread(), this, \old(\permission(this.val))));
           assignable<permissions> this.rds, this.lockedRds, this.val;
-          assignable<heap> \strictly_nothing; @*/
+          assignable<heap> this.rds; @*/
     public native void unlock();
 
     private int rds;
