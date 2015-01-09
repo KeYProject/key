02cacfc884d2a5c2d1a5acccd2d8e4b336c0623e
Kai Wallisch <kai.wallisch@ira.uka.de>
2015-01-09 16:09:49 +0100

    Added tests for integer arrays in TestTermParserSorts.

--- system/test/de/uka/ilkd/key/parser/TestTermParserSorts.java
+++ system/test/de/uka/ilkd/key/parser/TestTermParserSorts.java
@@ -18,9 +18,19 @@ public class TestTermParserSorts extends AbstractTestTermParser {
     public void setUp() throws RecognitionException {
         parseDecls("\\programVariables {Seq s;}");
         parseDecls("\\programVariables {int i;}");
-        parseDecls("\\programVariables {testTermParserSorts.IntegerMethods a;}");
+        parseDecls("\\programVariables {"
+                + "testTermParserSorts.IntegerMethods a;"
+                + "byte[] ba;"
+                + "char[] ca;"
+                + "short[] sa;"
+                + "int[] ia;"
+                + "long[] la;"
+                + "}");
     }
 
+    /*
+     * Test whether pretty syntax of sequences is parsed correctly.
+     */
     public void testParseSequencePrettySyntax() throws Exception {
         /*
          * Test any::seqGet(s,i)
@@ -46,37 +56,73 @@ public class TestTermParserSorts extends AbstractTestTermParser {
         comparePrettySyntaxAgainstVerboseSyntax("s.length", "seqLen(s)");
     }
 
+    /*
+     * The KeY type int has several possible corresponding KeYJavaTypes.
+     * Those types are: char, byte, short, int, long
+     * 
+     * This test checks if the parser finds a suitable function,
+     * if a query with integer arguments is provided.
+     *
+     * Sometimes several functions are available that the parser may select.
+     * For example a query of the form "a.query(0)" with Java functions
+     * available that have the following signatures:
+     *
+     *      public int query(int i);
+     *      public int query(byte b);
+     *
+     * Such a case is not considered here.
+     */
     public void testParseIntegerArgs() throws Exception {
         String s = "testTermParserSorts.IntegerMethods::queryByte(heap,a,Z(0(#)))";
         Term t = parseTerm("a.queryByte(0)");
+        assertEquals(s, t.toString());
+
+        s = "testTermParserSorts.IntegerMethods::queryByteArray(heap,a,ba)";
+        t = parseTerm("a.queryByteArray(ba)");
         assertEquals(s, t.toString());
 
         s = "testTermParserSorts.IntegerMethods::queryChar(heap,a,Z(0(#)))";
         t = parseTerm("a.queryChar(0)");
         assertEquals(s, t.toString());
 
+        s = "testTermParserSorts.IntegerMethods::queryCharArray(heap,a,ca)";
+        t = parseTerm("a.queryCharArray(ca)");
+        assertEquals(s, t.toString());
+
         s = "testTermParserSorts.IntegerMethods::queryShort(heap,a,Z(0(#)))";
         t = parseTerm("a.queryShort(0)");
+        assertEquals(s, t.toString());
+
+        s = "testTermParserSorts.IntegerMethods::queryShortArray(heap,a,sa)";
+        t = parseTerm("a.queryShortArray(sa)");
         assertEquals(s, t.toString());
 
         s = "testTermParserSorts.IntegerMethods::queryInt(heap,a,Z(0(#)))";
         t = parseTerm("a.queryInt(0)");
         assertEquals(s, t.toString());
 
+        s = "testTermParserSorts.IntegerMethods::queryIntArray(heap,a,ia)";
+        t = parseTerm("a.queryIntArray(ia)");
+        assertEquals(s, t.toString());
+
         s = "testTermParserSorts.IntegerMethods::queryLong(heap,a,Z(0(#)))";
         t = parseTerm("a.queryLong(0)");
         assertEquals(s, t.toString());
 
-        s = "testTermParserSorts.IntegerMethods::queryMixed(heap,a,Z(0(#)),Z(0(#)),Z(0(#)),Z(0(#)),Z(0(#)))";
-        t = parseTerm("a.queryMixed(0,0,0,0,0)");
+        s = "testTermParserSorts.IntegerMethods::queryLongArray(heap,a,la)";
+        t = parseTerm("a.queryLongArray(la)");
         assertEquals(s, t.toString());
 
-        s = "testTermParserSorts.IntegerMethods::queryMixedStatic(heap,Z(0(#)),Z(0(#)),Z(0(#)),Z(0(#)),Z(0(#)))";
-        t = parseTerm("a.queryMixedStatic(0,0,0,0,0)");
+        s = "testTermParserSorts.IntegerMethods::queryMixed(heap,a,Z(0(#)),ca,Z(0(#)),ia,Z(0(#)))";
+        t = parseTerm("a.queryMixed(0,ca,0,ia,0)");
         assertEquals(s, t.toString());
 
-        s = "testTermParserSorts.IntegerMethods::queryMixedStatic(heap,Z(0(#)),Z(0(#)),Z(0(#)),Z(0(#)),Z(0(#)))";
-        t = parseTerm("testTermParserSorts.IntegerMethods.queryMixedStatic(0,0,0,0,0)");
+        s = "testTermParserSorts.IntegerMethods::queryMixedStatic(heap,Z(0(#)),ca,Z(0(#)),ia,Z(0(#)))";
+        t = parseTerm("a.queryMixedStatic(0,ca,0,ia,0)");
+        assertEquals(s, t.toString());
+
+        s = "testTermParserSorts.IntegerMethods::queryMixedStatic(heap,Z(0(#)),ca,Z(0(#)),ia,Z(0(#)))";
+        t = parseTerm("testTermParserSorts.IntegerMethods.queryMixedStatic(0,ca,0,ia,0)");
         assertEquals(s, t.toString());
     }
 
