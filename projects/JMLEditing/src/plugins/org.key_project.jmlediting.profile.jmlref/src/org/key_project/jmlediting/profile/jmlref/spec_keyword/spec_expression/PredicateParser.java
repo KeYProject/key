d1e8844b1bd92d2d6bce22ad0c659b36a2c531a1
Moritz Lichter <lichtemo@googlemail.com>
2015-01-13 14:44:11 +0100

    JMLEditing: Bugfix

--- projects/JMLEditing/src/plugins/org.key_project.jmlediting.profile.jmlref/src/org/key_project/jmlediting/profile/jmlref/spec_keyword/spec_expression/PredicateParser.java
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.profile.jmlref/src/org/key_project/jmlediting/profile/jmlref/spec_keyword/spec_expression/PredicateParser.java
@@ -1,22 +1,12 @@
 package org.key_project.jmlediting.profile.jmlref.spec_keyword.spec_expression;
 
-import org.key_project.jmlediting.core.dom.IASTNode;
-import org.key_project.jmlediting.core.parser.ParserException;
 import org.key_project.jmlediting.core.profile.IJMLProfile;
 
-public class PredicateParser extends ExpressionParser {
-
-   private SpecExpressionParser expressionParser;
+public class PredicateParser extends SpecExpressionParser {
 
    public PredicateParser(final IJMLProfile profile) {
       super(profile);
       // TODO Auto-generated constructor stub
-   }
-
-   @Override
-   public IASTNode parse(final String text, final int start, final int end)
-         throws ParserException {
-      return this.expressionParser.parse(text, start, end);
    }
 
 }
