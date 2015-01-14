180e958376cb6043c3456305fad1ae7c5c9e47aa
Dominic Scheurer <dom.scheurer@gmail.com>
2015-01-14 08:47:35 +0100

    Reverted erroneously merged change

--- system/src/de/uka/ilkd/key/logic/InnerVariableNamer.java
+++ system/src/de/uka/ilkd/key/logic/InnerVariableNamer.java
@@ -75,7 +75,8 @@ public class InnerVariableNamer extends VariableNamer {
                             getProgramFromPIO(posOfFind),
                             null);
             final NamespaceSet namespaces = services.getNamespaces();
-            while (!isUniqueInGlobals(newname.toString(), globals)) {
+            while (!isUniqueInGlobals(newname.toString(), globals) ||
+                  namespaces.lookupLogicSymbol(newname)!=null) {
 	        newcounter += 1;
 	        newname = createName(bai.basename, newcounter, nci);
 	    }
