ca2b303c5775e94795a56cf5ba444fa9136c5e89
Moritz Lichter <lichtemo@googlemail.com>
2015-01-25 12:09:10 +0100

    JMLEditing: Tests for quantifier

--- projects/JMLEditing/src/plugins/org.key_project.jmlediting.profile.jmlref/src/org/key_project/jmlediting/profile/jmlref/quantifier/SumQuantifierKeyword.java
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.profile.jmlref/src/org/key_project/jmlediting/profile/jmlref/quantifier/SumQuantifierKeyword.java
@@ -1,7 +1,16 @@
 package org.key_project.jmlediting.profile.jmlref.quantifier;
 
+/**
+ * Implementation of the sum quantifier.
+ * 
+ * @author Moritz Lichter
+ *
+ */
 public class SumQuantifierKeyword extends GeneralizedQuantifier {
 
+   /**
+    * Creates a new instance.
+    */
    public SumQuantifierKeyword() {
       super("\\sum");
    }
