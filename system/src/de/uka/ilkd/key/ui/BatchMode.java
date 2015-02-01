0156f9d8773ae5acdaa672967fe20a8b95be5948
Kai Wallisch <kai.wallisch@ira.uka.de>
2015-02-01 22:58:06 +0100

    Preparing merge with master for file BatchMode.java

--- system/src/de/uka/ilkd/key/ui/BatchMode.java
+++ system/src/de/uka/ilkd/key/ui/BatchMode.java
@@ -3,7 +3,7 @@
 // Copyright (C) 2001-2011 Universitaet Karlsruhe (TH), Germany
 //                         Universitaet Koblenz-Landau, Germany
 //                         Chalmers University of Technology, Sweden
-// Copyright (C) 2011-2013 Karlsruhe Institute of Technology, Germany
+// Copyright (C) 2011-2014 Karlsruhe Institute of Technology, Germany
 //                         Technical University Darmstadt, Germany
 //                         Chalmers University of Technology, Sweden
 //
@@ -13,7 +13,7 @@
 
 package de.uka.ilkd.key.ui;
 
-import de.uka.ilkd.key.gui.Main;
+import de.uka.ilkd.key.core.Main;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -36,14 +36,14 @@ public class BatchMode {
     }
 
    public void finishedBatchMode (Object result,
-            Proof proof) {
+                                  Proof proof) {
 
         if ( Main.getStatisticsFile() != null )
             printStatistics ( Main.getStatisticsFile(), result.toString(),
                               proof.statistics(), proof.closed() );
 
        if (result instanceof Throwable) {
-           throw new Error("Error in batchmode: " + (Throwable) result);
+           throw new Error("Error in batchmode.", (Throwable) result);
        }
 
         // Save the proof before exit.
@@ -70,18 +70,6 @@ public class BatchMode {
             saveProof (proof, baseName + ".auto.proof" );
         } catch (IOException e) {
             e.printStackTrace();
-            return;
-        }
-
-
-        if (proof.openGoals().size() == 0) {
-            // Says that all Proofs have succeeded
-            if (proof.getBasicTask() != null
-                    && proof.getBasicTask().getStatus().getProofClosedButLemmasLeft()) {
-                System.out.println("Proof is closed but depends on (unproved) lemmas.");
-            }
-        } else {
-            System.out.println("Terminated proof but there is at least one open Proof.");
         }
     }
 
@@ -97,6 +85,11 @@ public class BatchMode {
     private void printStatistics(String file, Object result,
                                  Proof.Statistics statistics,
                                  boolean proofClosed) {
+        
+        // get current memory consumption (after GC) in kB
+        Runtime.getRuntime().gc();
+        final long memory = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024;
+        
         try {
             final boolean fileExists = (new File(file)).exists();
             final FileWriter statisticsFW = new FileWriter ( file, true );
@@ -105,7 +98,7 @@ public class BatchMode {
             if (!fileExists) {
                 statPrinter.println("Name | Total rule apps | Nodes | " +
                         "Branches | Overall time | Automode time | " +
-                        "Closed | Time per step");
+                        "Closed | Time per step | Memory");
             }
 
             String name = fileName;
@@ -115,7 +108,7 @@ public class BatchMode {
 
             statPrinter.print ( name + " | " );
             if ("Error".equals ( result ) )
-                statPrinter.println ( "-1 | -1 | -1 | -1 | -1 | -1 | -1" );
+                statPrinter.println ( "-1 | -1 | -1 | -1 | -1 | -1 | -1 | -1" );
             else
                 statPrinter.println (statistics.totalRuleApps + " | " +
                                      statistics.nodes + " | " +
@@ -123,7 +116,8 @@ public class BatchMode {
                                      statistics.time + " | " +
                                      statistics.autoModeTime + " | " +
                                      (proofClosed ? 1 : 0) + " | " +
-                                     ((double)statistics.autoModeTime / (double)statistics.totalRuleApps));
+                                     statistics.timePerStep + " | " +
+                                     memory);
             statPrinter.close();
         } catch ( IOException e ) {
             e.printStackTrace();
