0b2fdcbe0f9451fcb9458ae1206568600a8c54af
David Giessing <david.giessing93@gmail.com>
2015-02-04 20:30:49 +0100

    JMLEditing: Refactored JavaElementIdentifier to keep the new Name Information too, as a result a rename had to be done

--- projects/JMLEditing/src/plugins/org.key_project.jmlediting.core/src/org/key_project/jmlediting/core/utilities/JavaRefactoringElementInformationContainer.java
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.core/src/org/key_project/jmlediting/core/utilities/JavaRefactoringElementInformationContainer.java
@@ -3,7 +3,14 @@ package org.key_project.jmlediting.core.utilities;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.dom.ITypeBinding;
 
-public class JavaElementIdentifier {
+/**
+ * Container Class that keeps all the useful Information for an Element that
+ * shall be refactored.
+ *
+ * @author David Giessing
+ *
+ */
+public class JavaRefactoringElementInformationContainer {
    /**
     * The name of the identified element.
     */
@@ -16,6 +23,17 @@ public class JavaElementIdentifier {
     * The declaringClass of the identified Element.
     */
    private final IType declaringClass;
+   /**
+    * The new Name after Refactoring for the Element.
+    */
+   private final String newName;
+
+   /**
+    * @return the newName
+    */
+   public String getNewName() {
+      return this.newName;
+   }
 
    /**
     * Creates a new JavaElementIdentifier that is used to uniquely identify a
@@ -28,11 +46,12 @@ public class JavaElementIdentifier {
     * @param declaringClass
     *           the declaring Class of the Element
     */
-   public JavaElementIdentifier(final String name, final ITypeBinding type,
-         final IType declaringClass) {
+   public JavaRefactoringElementInformationContainer(final String name, final ITypeBinding type,
+         final IType declaringClass, final String newName) {
       this.name = name;
       this.type = type;
       this.declaringClass = declaringClass;
+      this.newName = newName;
    }
 
    /**
