f425c4c1fb1f03bdc6a7d9f96a929094203ea7c3
Moritz Lichter <lichtemo@googlemail.com>
2015-02-24 11:43:44 +0100

    JMLEditing: Bigint und real

---
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.profile.jmlref/src/org/key_project/jmlediting/profile/jmlref/type/BigIntKeyword.java
@@ -0,0 +1,27 @@
+package org.key_project.jmlediting.profile.jmlref.type;
+
+import org.key_project.jmlediting.core.profile.syntax.AbstractEmptyKeyword;
+
+/**
+ * Implementation of the bigint keyword.
+ *
+ * @author Moritz Lichter
+ *
+ */
+public class BigIntKeyword extends AbstractEmptyKeyword implements ITypeKeyword {
+
+   /**
+    * New instance of bigint keyword.
+    */
+   public BigIntKeyword() {
+      super("\\bigint");
+   }
+
+   @Override
+   public String getDescription() {
+      return "The type \\bigint models arbitrary precision integers. "
+            + "However, note that arithmetic does not wrap around, this "
+            + "for all values i of type \\bigint, i < i+1.";
+   }
+
+}
