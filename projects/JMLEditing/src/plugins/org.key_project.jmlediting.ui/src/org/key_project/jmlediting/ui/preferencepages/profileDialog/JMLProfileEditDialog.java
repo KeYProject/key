1f04d82080e4f0c02cc8fda02441992a72c80d21
Thomas Glaser <thomas.glaser@stud.tu-darmstadt.de>
2015-03-08 19:02:34 +0100

    JMLEditing: Enable and Disable Keywords from Parent Profile and edit Name of Profile

--- projects/JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/preferencepages/profileDialog/JMLProfileEditDialog.java
+++ projects/JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/preferencepages/profileDialog/JMLProfileEditDialog.java
@@ -1,9 +1,12 @@
 package org.key_project.jmlediting.ui.preferencepages.profileDialog;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
@@ -12,6 +15,7 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
@@ -20,8 +24,11 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.swt.widgets.Text;
+import org.key_project.jmlediting.core.profile.IDerivedProfile;
 import org.key_project.jmlediting.core.profile.IEditableDerivedProfile;
 import org.key_project.jmlediting.core.profile.IJMLProfile;
+import org.key_project.jmlediting.core.profile.JMLProfileManagement;
 import org.key_project.jmlediting.core.profile.syntax.IKeyword;
 
 public class JMLProfileEditDialog extends AbstractJMLProfileDialog {
@@ -37,6 +44,9 @@ public class JMLProfileEditDialog extends AbstractJMLProfileDialog {
    private static final String BUTTON_TEXT_ENABLE = "Enable";
 
    private Button enableDisableButton;
+   private Text profileNameText;
+   private Text profileIdText;
+   private Combo derivedFromCombo;
 
    public JMLProfileEditDialog(final Shell parent, final IJMLProfile profile) {
       super(parent, profile);
@@ -48,20 +58,59 @@ public class JMLProfileEditDialog extends AbstractJMLProfileDialog {
       final Composite composite = (Composite) super.createDialogArea(parent);
 
       GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
-
       final Composite myComposite = new Composite(composite, SWT.NONE);
       myComposite.setLayoutData(data);
-      myComposite.setLayout(new GridLayout(2, false));
+      myComposite.setLayout(new GridLayout(3, false));
+
+      data = new GridData(SWT.FILL, SWT.TOP, false, true);
+      data.horizontalSpan = 1;
+      final Label profileNameLabel = new Label(myComposite, SWT.NONE);
+      profileNameLabel.setText("ProfileName: ");
+      profileNameLabel.setLayoutData(data);
+
+      data = new GridData(SWT.LEFT, SWT.TOP, true, true);
+      data.horizontalSpan = 2;
+      data.widthHint = 200;
+      this.profileNameText = new Text(myComposite, SWT.SINGLE | SWT.BORDER);
+      this.profileNameText.setLayoutData(data);
+
+      data = new GridData(SWT.FILL, SWT.TOP, false, true);
+      data.horizontalSpan = 1;
+      final Label profileIdLabel = new Label(myComposite, SWT.NONE);
+      profileIdLabel.setText("Profile ID: ");
+      profileIdLabel.setLayoutData(data);
+
+      data = new GridData(SWT.LEFT, SWT.TOP, true, true);
+      data.horizontalSpan = 2;
+      data.widthHint = 200;
+      this.profileIdText = new Text(myComposite, SWT.SINGLE | SWT.BORDER);
+      this.profileIdText.setLayoutData(data);
+      this.profileIdText.setEnabled(false);
+
+      data = new GridData(SWT.FILL, SWT.TOP, false, true);
+      data.horizontalSpan = 1;
+      final Label derivedFromLabel = new Label(myComposite, SWT.NONE);
+      derivedFromLabel.setText("Derived from: ");
+      derivedFromLabel.setLayoutData(data);
+
+      data = new GridData(SWT.LEFT, SWT.TOP, true, true);
+      data.horizontalSpan = 2;
+      data.widthHint = 200;
+      this.derivedFromCombo = new Combo(myComposite, SWT.BORDER | SWT.READ_ONLY);
+      this.derivedFromCombo.setLayoutData(data);
+      this.derivedFromCombo.setItems(this.getProfiles4Combo());
 
       data = new GridData(SWT.LEFT, SWT.TOP, false, false, 1, 1);
-      data.horizontalSpan = 2;
-      final Label label = new Label(myComposite, SWT.NONE);
-      label.setText("Keywords from parent profile: (Green: enabled; Red: disabled)");
-      label.setLayoutData(data);
+      data.horizontalSpan = 3;
+      data.verticalIndent = 20;
+      final Label parentTableLabel = new Label(myComposite, SWT.NONE);
+      parentTableLabel
+            .setText("Keywords from parent profile: (Green: enabled; Red: disabled)");
+      parentTableLabel.setLayoutData(data);
 
       data = new GridData(SWT.FILL, SWT.TOP, true, true);
       data.heightHint = 200;
-
+      data.horizontalSpan = 2;
       super.setKeywordTable(new Table(myComposite, SWT.H_SCROLL | SWT.V_SCROLL
             | SWT.BORDER | SWT.FULL_SELECTION));
       super.getKeywordTable().setLayoutData(data);
@@ -146,6 +195,11 @@ public class JMLProfileEditDialog extends AbstractJMLProfileDialog {
          return;
       }
 
+      this.profileNameText.setText(profile.getName());
+      this.profileIdText.setText(profile.getIdentifier());
+      this.derivedFromCombo.select(this
+            .getComboIndexForProfile(this.derivedProfile.getParentProfile()));
+
       super.getKeywordTable().removeAll();
       final List<IKeyword> keywordList = new ArrayList<IKeyword>(
             this.derivedProfile.getParentProfile().getSupportedKeywords());
@@ -179,5 +233,37 @@ public class JMLProfileEditDialog extends AbstractJMLProfileDialog {
    private IKeyword getSelectedKeyword() {
       final TableItem selectedItem = this.getSelectedTableItem();
       return (IKeyword) selectedItem.getData();
+   }
+
+   private int getComboIndexForProfile(final IJMLProfile profile) {
+      for (int i = 0; i < this.derivedFromCombo.getItemCount(); i++) {
+         if (this.derivedFromCombo.getItems()[i].equals(profile.getName())) {
+            return i;
+         }
+      }
+      return 0;
+   }
+
+   private String[] getProfiles4Combo() {
+      final Set<IJMLProfile> allProfiles = JMLProfileManagement.instance()
+            .getAvailableProfiles();
+
+      final StringBuilder resultBuilder = new StringBuilder();
+
+      final Iterator<IJMLProfile> iterator = allProfiles.iterator();
+      while (iterator.hasNext()) {
+         final IJMLProfile profile = iterator.next();
+         if (!(profile instanceof IDerivedProfile)) {
+            resultBuilder.append(profile.getName() + ";");
+         }
+      }
+
+      resultBuilder.deleteCharAt(resultBuilder.length() - 1);
+
+      final String[] result = resultBuilder.toString().split(";");
+
+      Arrays.sort(result);
+
+      return result;
    }
 }
