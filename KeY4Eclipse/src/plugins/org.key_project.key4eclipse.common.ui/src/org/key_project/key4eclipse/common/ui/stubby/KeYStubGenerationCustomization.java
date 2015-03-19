8c3fb1b35372b57fcd818409fbdfaa23a0b326a8
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2015-03-19 15:07:25 +0100

    KeY4Eclipse: Explanations are now shown during stub generation.

--- KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/stubby/KeYStubGenerationCustomization.java
+++ KeY4Eclipse/src/plugins/org.key_project.key4eclipse.common.ui/src/org/key_project/key4eclipse/common/ui/stubby/KeYStubGenerationCustomization.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.key_project.key4eclipse.common.ui.util.LogUtil;
 import org.key_project.key4eclipse.starter.core.property.KeYClassPathEntry;
 import org.key_project.key4eclipse.starter.core.property.KeYClassPathEntry.KeYClassPathEntryKind;
@@ -41,6 +45,11 @@ public class KeYStubGenerationCustomization extends AbstractStubGenerationCustom
    private Button bootClassPathButton;
 
    /**
+    * Shows an explanation oft the selected options.
+    */
+   private Label explanationLabel;
+   
+   /**
     * {@inheritDoc}
     */
    @Override
@@ -50,10 +59,34 @@ public class KeYStubGenerationCustomization extends AbstractStubGenerationCustom
       keyGroup.setLayout(new GridLayout(3, false));
       doNotUseButton = new Button(keyGroup, SWT.RADIO);
       doNotUseButton.setText("&Not considered");
+      doNotUseButton.setSelection(true);
+      doNotUseButton.addSelectionListener(new SelectionAdapter() {
+         @Override
+         public void widgetSelected(SelectionEvent e) {
+            updateExplanation();
+         }
+      });
       classPathButton = new Button(keyGroup, SWT.RADIO);
       classPathButton.setText("&Class Path");
+      classPathButton.addSelectionListener(new SelectionAdapter() {
+         @Override
+         public void widgetSelected(SelectionEvent e) {
+            updateExplanation();
+         }
+      });
       bootClassPathButton = new Button(keyGroup, SWT.RADIO);
       bootClassPathButton.setText("&Boot Class Path");
+      bootClassPathButton.addSelectionListener(new SelectionAdapter() {
+         @Override
+         public void widgetSelected(SelectionEvent e) {
+            updateExplanation();
+         }
+      });
+      explanationLabel = new Label(keyGroup, SWT.NONE);
+      GridData explanationData = new GridData(GridData.FILL_BOTH);
+      explanationData.horizontalSpan = 3;
+      explanationLabel.setLayoutData(explanationData);
+      updateExplanation();
       return keyGroup;
    }
 
@@ -76,6 +109,24 @@ public class KeYStubGenerationCustomization extends AbstractStubGenerationCustom
          doNotUseButton.setSelection(true);
          classPathButton.setSelection(false);         
          bootClassPathButton.setSelection(false);
+      }
+      updateExplanation();
+   }
+
+   /**
+    * Updates the shown explanation.
+    */
+   protected void updateExplanation() {
+      if (classPathButton.getSelection()) {
+         explanationLabel.setText("Stub folder is part of KeY's class path.\n\n" + 
+                                  "No stubs will be generated for types contained in KeY's boot class path.");
+      }
+      else if (bootClassPathButton.getSelection()) {
+         explanationLabel.setText("Stub folder is the boot class path of KeY.\n\n" + 
+                                  "An empty stub folder will be filled with types of KeY's default boot class path.");
+      }
+      else {
+         explanationLabel.setText("Stub folder is not used by KeY.\n\n ");
       }
    }
 
