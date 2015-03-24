518ab209d055b2a0db685f55c72f188760260051
Thomas Glaser <thomas.glaser@stud.tu-darmstadt.de>
2015-03-24 17:06:32 +0100

    JMLEditing: added ProfileDialogTest to Suite and corrected AutoCompletion Test

--- projects/JMLEditing/src/tests/org.key_project.jmlediting.ui.test/src/org/key_project/jmlediting/ui/test/suite/PreferenceSuite.java
+++ projects/JMLEditing/src/tests/org.key_project.jmlediting.ui.test/src/org/key_project/jmlediting/ui/test/suite/PreferenceSuite.java
@@ -3,12 +3,14 @@ package org.key_project.jmlediting.ui.test.suite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.key_project.jmlediting.ui.test.preferencepages.JMLColorPreferencePageTest;
+import org.key_project.jmlediting.ui.test.preferencepages.JMLProfileDialogTest;
 import org.key_project.jmlediting.ui.test.preferencepages.ProfilePropertiesTest;
 import org.key_project.jmlediting.ui.test.preferencepages.ProfilePropertiesTest2;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ ProfilePropertiesTest.class,
-      ProfilePropertiesTest2.class, JMLColorPreferencePageTest.class })
+      ProfilePropertiesTest2.class, JMLColorPreferencePageTest.class,
+      JMLProfileDialogTest.class })
 public class PreferenceSuite {
 
 }
