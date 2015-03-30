0ced99473b88e9f0b29873f1e8ce682bba0db4d3
Richard Bubel <bubel@cs.tu-darmstadt.de>
2015-03-30 10:00:33 +0200

    [Code Quality] Renamed variable to clarify expected unit

--- key/key.core/src/de/uka/ilkd/key/proof/ApplyStrategy.java
+++ key/key.core/src/de/uka/ilkd/key/proof/ApplyStrategy.java
@@ -299,18 +299,18 @@ public class ApplyStrategy {
 
         private final Throwable error;
 
-        private final long time;
+        private final long timeInNano;
         private final int appliedRuleAppsCount;
         private final int nrClosedGoals;
         private final Proof proof;
 
         public ApplyStrategyInfo(String message, Proof proof, Throwable error, Goal nonCloseableGoal,
-                long time, int appliedRuleAppsCount, int nrClosedGoals) {
+                long timeInNano, int appliedRuleAppsCount, int nrClosedGoals) {
             this.message = message;
             this.proof = proof;
             this.error   = error;
             this.nonCloseableGoal = nonCloseableGoal;
-            this.time    = time;
+            this.timeInNano    = timeInNano;
             this.appliedRuleAppsCount = appliedRuleAppsCount;
             this.nrClosedGoals = nrClosedGoals;
         }
@@ -332,7 +332,7 @@ public class ApplyStrategy {
         }
 
         public long getTime() {
-            return time;
+            return timeInNano;
         }
 
         public int getClosedGoals() {
@@ -356,7 +356,7 @@ public class ApplyStrategy {
                 sb.append("\n ").append(error.getMessage());
             }
             sb.append("\n Applied Rules: ").append(appliedRuleAppsCount);
-            sb.append("\n Time: ").append(time);
+            sb.append("\n Time: ").append(timeInNano);
             sb.append("\n Closed Goals: ").append(nrClosedGoals);
             return sb.toString();
         }
