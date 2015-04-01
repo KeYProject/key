1d2c2b06b84e8ab4351ad37d94c37584aa8da28e
Richard Bubel <bubel@cs.tu-darmstadt.de>
2015-04-02 00:51:08 +0200

    [Cleanup] Removed unused import

--- key/key.core/src/de/uka/ilkd/key/proof/MultiThreadedTacletIndex.java
+++ key/key.core/src/de/uka/ilkd/key/proof/MultiThreadedTacletIndex.java
@@ -7,7 +7,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 
