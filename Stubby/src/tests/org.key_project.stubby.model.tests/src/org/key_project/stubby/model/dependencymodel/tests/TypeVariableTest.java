d0941a2c5954d48101569eee6bdc9ebd74a1562d
Martin Hentschel <hentschel@cs.tu-darmstadt.de>
2015-04-01 14:14:50 +0200

    Stubby: Implemented missing tests

---
+++ Stubby/src/tests/org.key_project.stubby.model.tests/src/org/key_project/stubby/model/dependencymodel/tests/TypeVariableTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package org.key_project.stubby.model.dependencymodel.tests;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+import org.key_project.stubby.model.dependencymodel.DependencymodelFactory;
+import org.key_project.stubby.model.dependencymodel.TypeVariable;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Type Variable</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TypeVariableTest extends TestCase {
+
+   /**
+    * The fixture for this Type Variable test case.
+    * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+    * @generated
+    */
+   protected TypeVariable fixture = null;
+
+   /**
+    * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+    * @generated
+    */
+   public static void main(String[] args) {
+      TestRunner.run(TypeVariableTest.class);
+   }
+
+   /**
+    * Constructs a new Type Variable test case with the given name.
+    * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+    * @generated
+    */
+   public TypeVariableTest(String name) {
+      super(name);
+   }
+
+   /**
+    * Sets the fixture for this Type Variable test case.
+    * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+    * @generated
+    */
+   protected void setFixture(TypeVariable fixture) {
+      this.fixture = fixture;
+   }
+
+   /**
+    * Returns the fixture for this Type Variable test case.
+    * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+    * @generated
+    */
+   protected TypeVariable getFixture() {
+      return fixture;
+   }
+
+   /**
+    * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+    * @see junit.framework.TestCase#setUp()
+    * @generated
+    */
+   @Override
+   protected void setUp() throws Exception {
+      setFixture(DependencymodelFactory.eINSTANCE.createTypeVariable());
+   }
+
+   /**
+    * <!-- begin-user-doc -->
+    * <!-- end-user-doc -->
+    * @see junit.framework.TestCase#tearDown()
+    * @generated
+    */
+   @Override
+   protected void tearDown() throws Exception {
+      setFixture(null);
+   }
+
+} //TypeVariableTest
