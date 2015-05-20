b37d8575b51bbfff5c9b93f5c4fa93cd8dff67f4
Dominic Scheurer <dom.scheurer@gmail.com>
2015-05-20 13:50:12 +0200

    New intermediate apps for join (partner) nodes

---
+++ key/key.core/src/de/uka/ilkd/key/proof/io/intermediate/JoinAppIntermediate.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package de.uka.ilkd.key.proof.io.intermediate;
+
+import org.key_project.util.collection.ImmutableList;
+
+import de.uka.ilkd.key.logic.PosInTerm;
+import de.uka.ilkd.key.util.Pair;
+
+/**
+ * TODO: Document.
+ *
+ * @author Dominic Scheurer
+ *
+ */
+public class JoinAppIntermediate extends BuiltInAppIntermediate {
+
+    private int id = 0;
+    private String joinProc = null;
+    private int nrPartners = 0;
+    
+    /**
+     * TODO: Document.
+     *
+     * @param ruleName
+     * @param pos
+     * @param contract
+     * @param builtInIfInsts
+     * @param newNames
+     */
+    public JoinAppIntermediate(String ruleName,
+            Pair<Integer, PosInTerm> pos, String contract,
+            ImmutableList<Pair<Integer, PosInTerm>> builtInIfInsts,
+            String[] newNames, int id, String joinProc, int nrPartners) {
+        super(ruleName, pos, contract, builtInIfInsts, newNames);
+        this.id = id;
+        this.joinProc = joinProc;
+        this.nrPartners = nrPartners;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the joinProc
+     */
+    public String getJoinProc() {
+        return joinProc;
+    }
+
+    /**
+     * @return the nrPartners
+     */
+    public int getNrPartners() {
+        return nrPartners;
+    }
+
+}
---
+++ key/key.core/src/de/uka/ilkd/key/proof/io/intermediate/JoinPartnerAppIntermediate.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package de.uka.ilkd.key.proof.io.intermediate;
+
+import org.key_project.util.collection.ImmutableList;
+
+import de.uka.ilkd.key.logic.PosInTerm;
+import de.uka.ilkd.key.util.Pair;
+
+/**
+ * TODO: Document.
+ *
+ * @author Dominic Scheurer
+ */
+public class JoinPartnerAppIntermediate extends BuiltInAppIntermediate {
+
+    private int joinNodeId = 0;
+    
+    /**
+     * TODO: Document.
+     *
+     * @param ruleName
+     * @param pos
+     * @param contract
+     * @param builtInIfInsts
+     * @param newNames
+     */
+    public JoinPartnerAppIntermediate(String ruleName,
+            Pair<Integer, PosInTerm> pos, String contract,
+            ImmutableList<Pair<Integer, PosInTerm>> builtInIfInsts,
+            String[] newNames, int joinNodeId) {
+        super(ruleName, pos, contract, builtInIfInsts, newNames);
+        this.joinNodeId = joinNodeId;
+    }
+
+    /**
+     * @return the joinNodeId
+     */
+    public int getJoinNodeId() {
+        return joinNodeId;
+    }
+    
+}
