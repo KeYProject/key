93b1955b55576fd41e906c81368a890edd007d24
Timm Lippert <timm7@hotmail.de>
2015-06-11 16:12:49 +0200

    Outline: Pre version with commentlocator and outlineView/JMLOutlineElement.java for and working element in outline

---
+++ JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/outlineView/JMLASTCommentLocator.java
@@ -0,0 +1,14 @@
+package org.key_project.jmlediting.ui.outlineView;
+
+/**
+ * Locates JML Comments with JDT AST
+ * 
+ * @author Timm Lippert
+ *
+ */
+
+public class JMLASTCommentLocator {
+   //TODO: soll anahand von command ranges JML kommentare rausfilter und zurückgeben entwerder als AST noder oder neue klasse dafür definieren
+   
+
+}
---
+++ JMLEditing/src/plugins/org.key_project.jmlediting.ui/src/org/key_project/jmlediting/ui/outlineView/JMLOutlineElement.java
@@ -0,0 +1,167 @@
+package org.key_project.jmlediting.ui.outlineView;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.jdt.core.IAnnotation;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaModel;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IOpenable;
+import org.eclipse.jdt.core.ISourceRange;
+import org.eclipse.jdt.core.ISourceReference;
+import org.eclipse.jdt.core.JavaModelException;
+import org.key_project.jmlediting.core.dom.IASTNode;
+import org.key_project.jmlediting.core.parser.IJMLParser;
+
+public class JMLOutlineElement implements IJavaElement, ISourceReference {
+   private IJavaElement parent;
+   private final int type = 100;
+   private String elementName;
+   private IASTNode jmlNode;
+   private int endoffset = 0;
+   
+   public JMLOutlineElement(IJavaElement parent, IASTNode node) {
+      this.parent = parent;
+      this.jmlNode = node;
+      //TODO: ober nodes itereiren bis länge von
+   }
+
+   @SuppressWarnings("rawtypes")
+   @Override
+   public Object getAdapter(Class adapter) {
+      return Platform.getAdapterManager().getAdapter(this, adapter);
+   }
+
+   @Override
+   public boolean exists() {
+      return true;
+   }
+
+   @Override
+   public IJavaElement getAncestor(int ancestorType) {
+      return parent.getAncestor(ancestorType);
+   }
+
+   @Override
+   public String getAttachedJavadoc(IProgressMonitor monitor) throws JavaModelException {
+      return parent.getAttachedJavadoc(monitor);
+   }
+
+   @Override
+   public IResource getCorrespondingResource() throws JavaModelException {
+      return parent.getCorrespondingResource();
+   }
+
+   @Override
+   public String getElementName() {
+      return jmlNode.toString();
+   }
+
+   @Override
+   public int getElementType() {
+      return type;
+   }
+
+   @Override
+   public String getHandleIdentifier() {
+      // TODO Auto-generated method stub
+      return parent.getHandleIdentifier();
+   }
+
+   @Override
+   public IJavaModel getJavaModel() {
+      return parent.getJavaModel();
+   }
+
+   @Override
+   public IJavaProject getJavaProject() {
+      return parent.getJavaProject();
+   }
+
+   @Override
+   public IOpenable getOpenable() {
+      return parent.getOpenable();
+   }
+
+   @Override
+   public IJavaElement getParent() {
+      return parent.getParent();
+   }
+
+   @Override
+   public IPath getPath() {
+      return parent.getPath();
+   }
+
+   @Override
+   public IJavaElement getPrimaryElement() {
+      return parent.getPrimaryElement();
+   }
+
+   @Override
+   public IResource getResource() {
+      return parent.getResource();
+   }
+
+   @Override
+   public ISchedulingRule getSchedulingRule() {
+      return parent.getSchedulingRule();
+   }
+
+   @Override
+   public IResource getUnderlyingResource() throws JavaModelException {
+      return parent.getUnderlyingResource();
+   }
+
+   @Override
+   public boolean isReadOnly() {
+      return false;
+   }
+
+   @Override
+   public boolean isStructureKnown() throws JavaModelException {
+      return parent.isStructureKnown();
+   }
+
+   @Override
+   public String getSource() throws JavaModelException {
+      return "";
+   }
+
+   @Override
+   public ISourceRange getSourceRange() throws JavaModelException {
+      return new ISourceRange() {
+         
+         @Override
+         public int getOffset() {
+            return 1;//jmlNode.getStartOffset();
+         }
+         
+         @Override
+         public int getLength() {
+            return 1;//jmlNode.getChildren().get(0).getEndOffset();
+         }
+      };
+   }
+
+   @Override
+   public ISourceRange getNameRange() throws JavaModelException {
+      return new ISourceRange() {
+         
+         @Override
+         public int getOffset() {
+            return  jmlNode.getStartOffset();
+         }
+         
+         @Override
+         public int getLength() {
+            System.out.println();
+            return jmlNode.getEndOffset()-jmlNode.getStartOffset();
+         }
+      };
+   }
+
+}
