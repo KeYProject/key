26b48d79ea79bb91678a060942c2440abbb1a89d
Kai Wallisch <kai.wallisch@ira.uka.de>
2015-06-23 21:01:02 +0200

    [ProveRules] Added ant targets.

--- key/scripts/jenkins/runTests.sh
+++ key/scripts/jenkins/runTests.sh
@@ -14,14 +14,6 @@ ant -logger org.apache.tools.ant.NoBannerLogger test-deploy-all
 EXIT_UNIT_TESTS=$?
 
 #
-# rerun taclet proofs
-#
-# This script does not report an error if test cases fail.
-# TEST FAILURE are reported however. It may timeout or raise a system error, however
-timeout 3600 ./proveRules.pl -x ../key.core.test/testresults/proveRules.xml
-EXIT_PROVE_RULES=$?
-
-#
 # create statistics if successful
 #
 mkdir -p "$STATISTICS_DIR"
@@ -29,7 +21,6 @@ mkdir -p "$STATISTICS_DIR"
 if [ "$EXIT_UNIT_TESTS" -eq "0" -a "$EXIT_PROVE_RULES" -eq "0" ]
 then 
   cp ../key.core.test/testresults/runallproofs/runStatistics.csv "$STATISTICS_DIR/$BUILD_NUMBER.csv"
-#  perl ./tools/extractStatistics.pl --directory "../key.core.tests/testresults/" --project "statistics-$JOB_NAME" ../key.core.tests/testresults/runStatistics.csv
   exit 0
 else 
   exit 1
