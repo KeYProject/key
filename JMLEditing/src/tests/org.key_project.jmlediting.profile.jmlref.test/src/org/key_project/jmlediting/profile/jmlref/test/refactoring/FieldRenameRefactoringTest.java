46731252554018f6e3f6006d0fca7bbb9e15b067
Robert Heimbach <robert.heimbach@gmail.com>
2015-07-08 18:44:26 +0200

    First SWTBot test running for JMLRenameParticipant. Changed folder structure of tests.

--- JMLEditing/src/tests/org.key_project.jmlediting.profile.jmlref.test/src/org/key_project/jmlediting/profile/jmlref/test/refactoring/FieldRenameRefactoringTest.java
+++ JMLEditing/src/tests/org.key_project.jmlediting.profile.jmlref.test/src/org/key_project/jmlediting/profile/jmlref/test/refactoring/FieldRenameRefactoringTest.java
@@ -1,27 +1,28 @@
 package org.key_project.jmlediting.profile.jmlref.test.refactoring;
 
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotEclipseEditor;
 import org.eclipse.swtbot.swt.finder.SWTBot;
-import org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory;
 import org.eclipse.swtbot.swt.finder.waits.Conditions;
-import org.eclipse.swtbot.swt.finder.waits.ICondition;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
-import org.hamcrest.Matcher;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.key_project.jmlediting.core.profile.JMLPreferencesHelper;
 import org.key_project.jmlediting.profile.jmlref.test.Activator;
 import org.key_project.util.eclipse.BundleUtil;
+import org.key_project.util.eclipse.ResourceUtil;
 import org.key_project.util.jdt.JDTUtil;
 import org.key_project.util.test.util.TestUtilsUtil;
 
@@ -30,69 +31,92 @@ public class FieldRenameRefactoringTest<WaitForShell> {
     private static final String PROJECT_NAME = "JMLRefactoringRenameTest";
     private static final String PACKAGE_NAME = "test";
     private static final String CLASS_NAME = "TestClass1";
-    //private static final String CLASS_NAME_GOAL = "TestClass1Solution";
+    private static final String PATH_TO_TESTS = "data\\template\\refactoringRenameTest\\test1";
+    private static final String PATH_TO_ORACLE = "data\\template\\refactoringRenameTest\\test1\\oracle";
 
     private static final SWTWorkbenchBot bot = new SWTWorkbenchBot();
        
-    private static SWTBotEclipseEditor editor;
+    private static IFolder srcFolder;
+    private static IProject project;
 
     @BeforeClass
     public static void initProject() throws CoreException, InterruptedException {
         TestUtilsUtil.closeWelcomeView();
-                
-        final IJavaProject project = TestUtilsUtil.createJavaProject(PROJECT_NAME);
         
-        IFolder srcFolder = project.getProject().getFolder(JDTUtil.getSourceFolderName());
-        IFolder testFolder = TestUtilsUtil.createFolder(srcFolder, PACKAGE_NAME);
+        // Create Java Project and Source Folder
+        final IJavaProject javaProject = TestUtilsUtil.createJavaProject(PROJECT_NAME);
+        project = javaProject.getProject();
+        srcFolder = project.getFolder(JDTUtil.getSourceFolderName());
         
-        BundleUtil.extractFromBundleToWorkspace(Activator.PLUGIN_ID, "data\\template\\refactoringRenameTest", testFolder);
-        
-        TestUtilsUtil.waitForBuild();
-        
-        JMLPreferencesHelper.setProjectJMLProfile(project.getProject(), JMLPreferencesHelper.getDefaultJMLProfile());
-        
-        TestUtilsUtil.openEditor(testFolder.getFile(CLASS_NAME + JDTUtil.JAVA_FILE_EXTENSION_WITH_DOT));
-
-    }
- 
-    @Test
-    public void test1() throws InterruptedException {  
- 
-        // Go to the outline and select the field named "balance"
-        SWTBotTree tree = TestUtilsUtil.getOutlineView(bot).bot().tree(); 
-        SWTBotTreeItem outlineItem = tree.getTreeItem(CLASS_NAME); 
-        SWTBotTreeItem fieldToRename = outlineItem.getNode("balance : int"); 
-        fieldToRename.select().pressShortcut(SWT.ALT | SWT.SHIFT, 'R');
-        
-        //bot.sleep(1000);
-        //SWTBotTreeItem fieldToRename = TestUtilsUtil.selectInTree(tree, 1, 1);
-        //TestUtilsUtil.waitUntilSelected(bot, tree, fieldToRename);
-     
-        // Wait for refactoring dialog        
-        Matcher<Shell> matcher = WidgetMatcherFactory.withText("Rename Field");
-        ICondition waitForRefactoringDialog = Conditions.waitForShell(matcher);     
-        bot.waitUntilWidgetAppears(waitForRefactoringDialog);
-                
-        // change variable name
-        SWTBotShell renameDialog = bot.activeShell();      
-        SWTBot dialogBot = renameDialog.bot();
-        dialogBot.textWithLabel("New name:").setText("aVeryLongNewName");    
-        System.out.println("Starting refactoring");
-        dialogBot.button(IDialogConstants.OK_ID).click();
-        
-        // Compare content of editor after refactoring to its solution
-        bot.waitUntil(Conditions.shellCloses(renameDialog));
-        editor = bot.activeEditor().toTextEditor();
-        String content = editor.getText();
-        System.out.println(content);      
-        //bot.sleep(100000);          
-        
-        //String solution = "";
-        //assertEquals(solution,content);
+        JMLPreferencesHelper.setProjectJMLProfile(javaProject.getProject(), JMLPreferencesHelper.getDefaultJMLProfile());
     }
     
-    @AfterClass
-    public static void closeEditor() {
-       editor.close();
+    
+    // Get the String representation of the file named oracleName from oracleFolder
+    private String getOracle(IFolder oracleFolder, String fileName) throws CoreException {
+        
+        IFile fileToRead = oracleFolder.getFile(fileName + JDTUtil.JAVA_FILE_EXTENSION_WITH_DOT); 
+
+        String oracleString = ResourceUtil.readFrom(fileToRead);  // can throw CoreException
+        
+        return oracleString;
+    }
+    
+    // Creates a folder in workspace named folderName in parentFolder and extracts files from copyFrom into newly created folder 
+    private IFolder createFolderAndCopyFiles(IContainer parentFolder, String folderName, String copyFrom) throws CoreException{
+        
+        IFolder createdFolder = TestUtilsUtil.createFolder(parentFolder, folderName);
+        BundleUtil.extractFromBundleToWorkspace(Activator.PLUGIN_ID, copyFrom, createdFolder);
+      
+        TestUtilsUtil.waitForBuild(); 
+
+        return createdFolder;
+    }
+    
+    // selects className and outlineString in the outline tree, starts renaming and changes field's name to newName
+    private void executeRenaming(String className, String fieldToChange, String newName){
+        
+        SWTBotTree tree = TestUtilsUtil.getOutlineView(bot).bot().tree(); 
+        SWTBotTreeItem fieldToRename = TestUtilsUtil.selectInTree(tree, className, fieldToChange);
+        fieldToRename.select().pressShortcut(SWT.ALT | SWT.SHIFT, 'R');
+                
+        // Change variable name in rename dialog
+        SWTBotShell renameDialog = bot.shell("Rename Field");      
+        SWTBot renameDialogBot = renameDialog.bot();
+        renameDialogBot.textWithLabel("New name:").setText(newName);
+     
+        // start renaming and wait till finished
+        renameDialogBot.button(IDialogConstants.OK_LABEL).click();
+        bot.waitUntil(Conditions.shellCloses(renameDialog));
+    }
+ 
+    // Gets the content from active editor and replaces \n with \r\n 
+    // because oracle files were created with windows 
+    private String getContentAfterRefactoring(){
+        SWTBotEclipseEditor editor = bot.activeEditor().toTextEditor();
+        
+        String content = editor.getText();
+        
+        editor.close();
+        
+        return content.replaceAll("(\n)", "\r\n");
+    }
+    
+    @Test
+    public void test1() throws InterruptedException, CoreException {
+        
+        IFolder testFolder = createFolderAndCopyFiles(srcFolder,PACKAGE_NAME,PATH_TO_TESTS);
+        IFolder oracleFolder = createFolderAndCopyFiles(project,"oracle",PATH_TO_ORACLE);
+        
+        String oracleString = getOracle(oracleFolder, CLASS_NAME);
+        
+        TestUtilsUtil.openEditor(testFolder.getFile(CLASS_NAME + JDTUtil.JAVA_FILE_EXTENSION_WITH_DOT));
+        
+        executeRenaming(CLASS_NAME, "balance : int", "aVeryLongNewName");
+        
+        String afterRenaming = getContentAfterRefactoring();
+        
+        // Compare content of editor after renaming to its oracle
+        assertEquals(oracleString,afterRenaming);
     }
 }
