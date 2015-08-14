f253b83d5fbea7c3c14693394787a96f8529e883
Maksim <whoooooz@list.ru>
2015-08-14 10:57:38 +0200

    MoveParticipantSMethod.java now initializes and has initial logic

--- JMLEditing/src/plugins/org.key_project.jmlediting.profile.jmlref/src/org/key_project/jmlediting/profile/jmlref/refactoring/MoveParticipantSMethod.java
+++ JMLEditing/src/plugins/org.key_project.jmlediting.profile.jmlref/src/org/key_project/jmlediting/profile/jmlref/refactoring/MoveParticipantSMethod.java
@@ -1,42 +1,54 @@
 package org.key_project.jmlediting.profile.jmlref.refactoring;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.internal.core.PackageFragment;
 import org.eclipse.jdt.internal.core.SourceMethod;
 import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.TextChange;
+import org.eclipse.ltk.core.refactoring.TextFileChange;
 import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
 import org.eclipse.ltk.core.refactoring.participants.MoveParticipant;
+import org.eclipse.text.edits.MultiTextEdit;
+import org.eclipse.text.edits.ReplaceEdit;
+import org.key_project.util.jdt.JDTUtil;
 
 public class MoveParticipantSMethod extends MoveParticipant {
 
     private SourceMethod methodToMove;        // file
     private String methName;
     
-    private String oldClassName;                // file name
-    private String newClassName;
-    
-    private String fOldPackName;            // old package name
-    private String fNewPackName;            // new package name
+    private String oldClassFullQualName;                // file name
+    private String newClassFullQualName;
     
     @SuppressWarnings("restriction")
     @Override
     protected boolean initialize(Object element) {
         if(element instanceof SourceMethod){
             methodToMove=(SourceMethod) element;
+            
+            //if(methodToMove.)
+            
             methName=methodToMove.getElementName();
-            System.out.println(methName);
-            //System.out.println(((SourceMethod) element).getParent().getElementName());
-            // get the old and new package name , because we only want to replace package names, otherwise nested classes problem        
-            // fOldPackName = fOldFullQualName.substring(0, fOldFullQualName.indexOf(fDocName)-1);
-            // fNewPackName = ((PackageFragment) getArguments().getDestination()).getElementName();  
-
+            oldClassFullQualName=((IType) methodToMove.getParent()).getFullyQualifiedName();
+            newClassFullQualName=((IType) getArguments().getDestination()).getFullyQualifiedName();
+            
             return true;
         }else{
             return false;
@@ -45,22 +57,81 @@ public class MoveParticipantSMethod extends MoveParticipant {
 
     @Override
     public String getName() {
-        // TODO Auto-generated method stub
-        return null;
+        return "JML Field Move Participant";
     }
 
     @Override
     public RefactoringStatus checkConditions(IProgressMonitor pm,
             CheckConditionsContext context) throws OperationCanceledException {
-        // TODO Auto-generated method stub
-        return null;
+        return new RefactoringStatus();
     }
 
     @Override
     public Change createChange(IProgressMonitor pm) throws CoreException,
             OperationCanceledException {
-        // TODO Auto-generated method stub
-        return null;
+
+        // Only non empty change objects will be added
+        ArrayList<TextFileChange> changesToFilesWithoutJavaChanges = new ArrayList<TextFileChange>();
+
+        // Find out the projects which need to be checked: active project plus all dependencies
+        ArrayList<IJavaProject> projectsToCheck = new ArrayList<IJavaProject>(Arrays.asList(JDTUtil.getAllJavaProjects()));
+
+        try {
+            // Look through all source files in each package and project
+            for (final IJavaProject project : projectsToCheck) {
+                for (final IPackageFragment pac : project.getPackageFragments()) {
+                    for (final ICompilationUnit unit : pac
+                            .getCompilationUnits()) {
+
+                        final ArrayList<ReplaceEdit> changesToJML = new ArrayList<ReplaceEdit>();
+                                //computeNeededChangesToJML(unit, project);
+
+                        // Get scheduled changes to the java code from the rename processor
+                        final TextChange changesToJavaCode = getTextChange(unit);
+
+                        // add our edits to the java changes
+                        // JDT will compute the shifts and the preview
+                        if (changesToJavaCode != null) {
+                            for (final ReplaceEdit edit : changesToJML) {
+                                changesToJavaCode.addEdit(edit);
+                            }
+                        }
+                        else {
+                            // In case changes to the JML code needs to be done (but not to the java code)
+                            if (!changesToJML.isEmpty()){
+
+                                // Gather all the edits to the text (JML annotations) in a MultiTextEdit
+                                // and add those to a change object for the given file
+                                TextFileChange tfChange = new TextFileChange("", (IFile) unit.getCorrespondingResource());                         
+                                MultiTextEdit allEdits = new MultiTextEdit();
+
+                                for (final ReplaceEdit edit: changesToJML) {
+                                    allEdits.addChild(edit);
+                                }
+
+                                tfChange.setEdit(allEdits);
+
+                                changesToFilesWithoutJavaChanges.add(tfChange);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (final JavaModelException e) {
+            return null;
+        }
+
+        // Return null if only shared changes, otherwise gather changes to JML for classes with no java changes.
+        if (changesToFilesWithoutJavaChanges.isEmpty())
+            return null;
+        else {
+            CompositeChange allChangesToFilesWithoutJavaChanges = new CompositeChange("Changes to JML");
+            for (TextFileChange change : changesToFilesWithoutJavaChanges){
+                allChangesToFilesWithoutJavaChanges.add(change);
+            }
+            return allChangesToFilesWithoutJavaChanges;
+        }
     }
 
 }
