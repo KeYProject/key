fa053cc5f6bd4b64af9259603e609068c1204afc
Maksim <whoooooz@list.ru>
2015-08-17 18:41:54 +0200

    RefactoringMoveMethods tests sind fertig

--- JMLEditing/src/tests/org.key_project.jmlediting.profile.jmlref.test/data/template/refactoringMoveTest/moveMethodTest/test3/oracle/Main.java
+++ JMLEditing/src/tests/org.key_project.jmlediting.profile.jmlref.test/data/template/refactoringMoveTest/moveMethodTest/test3/oracle/Main.java
@@ -2,15 +2,15 @@ package mainpack;
 public class Main {
     
     /*@
-      @ public invariant test1p2.Params.go()<100;
+      @ public invariant x==2 && test3p2.Params.go()<100;
       @*/
     int x=0;
     
     /*@ 
       @ normal_behavior
-      @ ensures test1p2.Params.go() == 42;
+      @ ensures true && test3p2.Params.go() == 42 && x==2;
       @*/
     public static void bla() {
-        test1p1.Settings.x = 42;
+        test3p1.Settings.x = 42;
     }
 }
---
+++ JMLEditing/src/tests/org.key_project.jmlediting.profile.jmlref.test/data/template/refactoringMoveTest/moveMethodTest/test4/src/test4p1/Settings.java
@@ -0,0 +1,11 @@
+package test4p1;
+
+public class Settings {
+    public static int x;
+    
+    public class RandomSettings{
+        public int y;
+    }
+    
+    
+}
