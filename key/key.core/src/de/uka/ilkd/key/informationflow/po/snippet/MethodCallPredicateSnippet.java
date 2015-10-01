8c4b16104b663e65c731ccbd80377c1ec2c7496f
Mattias Ulbrich <ulbrich@kit.edu>
2015-10-01 18:19:33 +0200

    fixed information flow predicate generation

--- key/key.core/src/de/uka/ilkd/key/informationflow/po/snippet/MethodCallPredicateSnippet.java
+++ key/key.core/src/de/uka/ilkd/key/informationflow/po/snippet/MethodCallPredicateSnippet.java
@@ -4,8 +4,13 @@
  */
 package de.uka.ilkd.key.informationflow.po.snippet;
 
+import org.key_project.util.collection.ImmutableArray;
+import org.key_project.util.collection.ImmutableList;
+
 import de.uka.ilkd.key.java.StatementBlock;
+import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.op.IProgramMethod;
+import de.uka.ilkd.key.logic.sort.Sort;
 import de.uka.ilkd.key.speclang.LoopInvariant;
 import de.uka.ilkd.key.util.MiscTools;
 
@@ -24,5 +29,28 @@ class MethodCallPredicateSnippet extends TwoStateMethodPredicateSnippet {
         final String nameString =
                 MiscTools.toValidTacletName("RELATED_BY_" + pm.getUniqueName()).toString();
         return nameString;
+    }
+
+    @Override
+    protected Sort[] generateContApplArgumentSorts(
+            ImmutableList<Term> termList, IProgramMethod pm) {
+
+        Sort[] argSorts = new Sort[termList.size()];
+        ImmutableArray<Sort> pmSorts = pm.argSorts();
+
+        int i = 0;
+        for (final Term arg : termList) {
+            // bugfix: Take the first argument sorts from the definition of
+            // the method rather than from the actually provided arguments.
+            // aug 2015 SG + MU
+            if(i < pmSorts.size() - 1) {
+                argSorts[i] = pmSorts.get(i+1);
+            } else {
+                argSorts[i] = arg.sort();
+            }
+            i++;
+        }
+
+        return argSorts;
     }
 }
