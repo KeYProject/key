df4790bedf7897087b17eae3dbca5b59c3df0a9d
Stefan Pilot <hans.stefan.pilot@googlemail.com>
2015-12-01 03:42:38 +0100

    [Fixes] Linux: Configure View does not work very good, Context Menus instantly collapse on trying to choose an option. [Improves] Made changing a View's place (middle, right, left, bottom) very much more efficient.

--- key/key.nui/src/de/uka/ilkd/key/nui/NUIController.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/NUIController.java
@@ -6,14 +6,15 @@ import java.util.Map;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.RadioMenuItem;
 
 public class NUIController {
 
@@ -56,59 +57,51 @@ public class NUIController {
     protected void handleAboutWindow(ActionEvent e) {
         System.out.println("Clicked 'About'.");
     }
-
+    
     @FXML
     protected void handleLoadComponent(ActionEvent e) {
-
-        MenuItem clickedEntry = (MenuItem) e.getSource();
-        Map<Object, Object> m = clickedEntry.getProperties();
+        RadioMenuItem clickedItem = (RadioMenuItem) e.getSource();
+        Map<Object, Object> m = clickedItem.getParentMenu().getProperties();
         String componentName = (String) m.get("componentName");
         String componentResource = (String) m.get("componentResource");
-
-        // Remove component from pane
+  
+        // Does the component already exist? Then just change its place
         if (posComponent.containsKey(componentName)) {
-            Pane pane = posComponent.get(componentName);
-            pane.getChildren().removeIf(p -> p.getId().equals(componentName));
-        }
-
-        // Determine proper menu button
-        MenuButton mb = null;
-        switch (componentName) {
-        case "proofView":
-            mb = ButtonProofView;
-            break;
-        case "goalView":
-            mb = ButtonGoalView;
-            break;
-        case "openProofsView":
-            mb = ButtonOpenProofsView;
-            break;
-        case "treeView":
-            mb = ButtonTreeView;
-            break;
-        }
-        // Update text of menu button
-        mb.setText(clickedEntry.getText());
-
-        // Add component to selected pane and update text of menu button
-        switch (clickedEntry.getId()) {
-        case "toLeft":
+            Node existingcomponent = null;
+            for(Node n : posComponent.get(componentName).getChildren()){
+                if(n.getId().equals(componentName)) existingcomponent = n; break;
+            }
+            switch(clickedItem.getText()) {
+            case "left":     left.getChildren().add(existingcomponent); posComponent.replace(componentName, left); break;
+            case "middle": middle.getChildren().add(existingcomponent); posComponent.replace(componentName, middle); break;
+            case "right":   right.getChildren().add(existingcomponent); posComponent.replace(componentName, right); break;
+            case "bottom":   down.getChildren().add(existingcomponent); posComponent.replace(componentName, down); break;
+            default: /* hide was chosen */ 
+                posComponent.get(componentName).getChildren().remove(existingcomponent);
+                posComponent.remove(componentName);
+                statustext.setText("View " + componentName + " hidden.");
+            }
+            
+        } else // Component did not already exist, thus it must be created
+        
+        switch(clickedItem.getText()) {
+        case "left":
             componentFactory.createComponent(componentName, left,
                     componentResource, posComponent);
             break;
-        case "toMiddle":
+        case "middle":
             componentFactory.createComponent(componentName, middle,
                     componentResource, posComponent);
             break;
-        case "toRight":
+        case "right":
             componentFactory.createComponent(componentName, right,
                     componentResource, posComponent);
             break;
-        case "toDown":
+        case "down":
             componentFactory.createComponent(componentName, down,
                     componentResource, posComponent);
         default:
-            break;
+            break; 
         }
     }
 }
