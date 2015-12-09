ed3953cd7b5a68eb857ac3c05324c534de688666
Patrick Jattke <patrick.jattke@stud.tu-darmstadt.de>
2015-12-09 19:04:59 +0100

    User Story: #14470 Icons im Beweisbaum - Intermediate Commit - Added code functionality to assign more icons (linked node, interactive   node, etc.) - Added icon graphics

--- key/key.nui/src/de/uka/ilkd/key/nui/IconFactory.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/IconFactory.java
@@ -13,103 +13,138 @@ import javafx.scene.image.ImageView;
  */
 public class IconFactory {
 
-    private IconFactory() {
-    }
+	private IconFactory() {
+	}
 
-    private static String folderRoot = "components/images/";
-    private static Image keyHole = getImage("ekey-mono.png");
-    private static Image keyHoleClosed = getImage("keyproved.png");
-    private static Image keyFolderBlue = getImage("folder-blue.png");
-    private static Image keyFolderGray = getImage("folder-gray.png");
+	private static String folderRoot = "components/images/TreeViewController/";
 
-    /**
-     * Returns an Image based on the given imageFilename in the directory
-     * folderRoot
-     * 
-     * @param imageFilename
-     *            The name of the image
-     * @return Image object of JavaFX
-     */
-    private static Image getImage(String imageFilename) {
-        InputStream is = IconFactory.class
-                .getResourceAsStream(folderRoot + imageFilename);
-        return new Image(is);
-    }
+	// Inner Nodes
+	private static Image keyOpenInnerNode = getImage("openNode.png");
+	private static Image keyLinkedInnerNode = getImage("linkedNode.png");
+	private static Image keyClosedInnerNode = getImage("closedNode.png");
+	private static Image keyInteractiveInnerNode = getImage("interactiveNode.png");
 
-    /**
-     * Scales an given image to a desired size indicated by x (width) and y
-     * (height) and returns a ImageView with the scaled image in it. Uses the
-     * smoothing option.
-     * 
-     * @param image
-     *            The image which should be scaled
-     * @param x
-     *            The desired width
-     * @param y
-     *            The desired height
-     * @return an ImageView containing the scaled Image
-     */
-    public static ImageView scaleIcon(Image image, int x, int y) {
-        ImageView v = new ImageView(image);
-        v.setFitWidth(x);
-        v.setFitHeight(y);
-        v.setSmooth(true);
-        return v;
-    }
+	// Leafs
+	private static Image keyInteractiveGoal = getImage("interactiveGoal.png");
+	private static Image keyClosedGoal = getImage("closedGoal.png");
+	private static Image keyOpenGoal = getImage("openGoal.png");
 
-    /**
-     * Returns the image keyHole scaled to the given size, where x indicates the
-     * width and y the height.
-     * 
-     * @param x
-     *            The desired width of the image
-     * @param y
-     *            The desired height of the image
-     * @return an ImageView object containing the scaled image
-     */
-    public static ImageView keyHole(int x, int y) {
-        return scaleIcon(keyHole, x, y);
-    }
+	/**
+	 * Returns an Image based on the given imageFilename in the directory
+	 * folderRoot
+	 * 
+	 * @param imageFilename
+	 *            The name of the image
+	 * @return Image object of JavaFX
+	 */
+	private static Image getImage(String imageFilename) {
+		InputStream is = IconFactory.class.getResourceAsStream(folderRoot + imageFilename);
+		return new Image(is);
+	}
 
-    /**
-     * Returns the image keyHoleClosed scaled to the given size, where x
-     * indicates the width and y the height.
-     * 
-     * @param x
-     *            The desired width of the image
-     * @param y
-     *            The desired height of the image
-     * @return an ImageView object containing the scaled image
-     */
-    public static ImageView keyHoleClosed(int x, int y) {
-        return scaleIcon(keyHoleClosed, x, y);
-    }
+	/**
+	 * Scales an given image to a desired size indicated by x (width) and y
+	 * (height) and returns a ImageView with the scaled image in it. Uses the
+	 * smoothing option.
+	 * 
+	 * @param image
+	 *            The image which should be scaled
+	 * @param x
+	 *            The desired width
+	 * @param y
+	 *            The desired height
+	 * @return an ImageView containing the scaled Image
+	 */
+	public static ImageView scaleIcon(Image image, int x, int y) {
+		ImageView v = new ImageView(image);
+		v.setFitWidth(x);
+		v.setFitHeight(y);
+		v.setSmooth(true);
+		return v;
+	}
 
-    /**
-     * Returns the image keyFolderBlue scaled to the given size, where x
-     * indicates the width and y the height.
-     * 
-     * @param x
-     *            The desired width of the image
-     * @param y
-     *            The desired height of the image
-     * @return an ImageView object containing the scaled image
-     */
-    public static ImageView keyFolderBlue(int x, int y) {
-        return scaleIcon(keyFolderBlue, x, y);
-    }
+	/**
+	 * * Returns the icon scaleIcond to size (width, height) = (x, y).
+	 * 
+	 * @ param x the desired width of the icon @ param y the desired height of
+	 * the icon
+	 * 
+	 * @return the scaleIcond keyOpenInnerNode
+	 */
+	public static ImageView getKeyOpenInnerNode(int x, int y) {
+		return scaleIcon(keyOpenInnerNode, x, y);
+	}
 
-    /**
-     * Returns the image keyFolderGray scaled to the given size, where x
-     * indicates the width and y the height.
-     * 
-     * @param x
-     *            The desired width of the image
-     * @param y
-     *            The desired height of the image
-     * @return an ImageView object containing the scaled image
-     */
-    public static ImageView keyFolderGray(int x, int y) {
-        return scaleIcon(keyFolderGray, x, y);
-    }
+	/**
+	 * Returns the icon scaleIcond to size (width, height) = (x, y).
+	 * 
+	 * @ param x the desired width of the icon @ param y the desired height of
+	 * the icon
+	 * 
+	 * @return the scaleIcond keyLinkedInnerNode
+	 */
+	public static ImageView getKeyLinkedInnerNode(int x, int y) {
+		return scaleIcon(keyLinkedInnerNode, x, y);
+	}
+
+	/**
+	 * Returns the icon scaleIcond to size (width, height) = (x, y).
+	 * 
+	 * @ param x the desired width of the icon @ param y the desired height of
+	 * the icon
+	 * 
+	 * @return the scaleIcond keyClosedInnerNode
+	 */
+	public static ImageView getKeyClosedInnerNode(int x, int y) {
+		return scaleIcon(keyClosedInnerNode, x, y);
+	}
+
+	/**
+	 * Returns the icon scaleIcond to size (width, height) = (x, y).
+	 * 
+	 * @ param x the desired width of the icon @ param y the desired height of
+	 * the icon
+	 * 
+	 * @return the scaleIcond keyInteractiveInnerNode
+	 */
+	public static ImageView getKeyInteractiveInnerNode(int x, int y) {
+		return scaleIcon(keyInteractiveInnerNode, x, y);
+	}
+
+	/**
+	 * Returns the icon scaleIcond to size (width, height) = (x, y).
+	 * 
+	 * @ param x the desired width of the icon @ param y the desired height of
+	 * the icon
+	 * 
+	 * @return the scaleIcond keyInteractiveGoal
+	 */
+	public static ImageView getKeyInteractiveGoal(int x, int y) {
+		return scaleIcon(keyInteractiveGoal, x, y);
+	}
+
+	/**
+	 * Returns the icon scaleIcond to size (width, height) = (x, y).
+	 * 
+	 * @ param x the desired width of the icon @ param y the desired height of
+	 * the icon
+	 * 
+	 * @return the scaleIcond keyClosedGoal
+	 */
+	public static ImageView getKeyClosedGoal(int x, int y) {
+		return scaleIcon(keyClosedGoal, x, y);
+	}
+
+	/**
+	 * Returns the icon scaleIcond to size (width, height) = (x, y).
+	 * 
+	 * @ param x the desired width of the icon @ param y the desired height of
+	 * the icon
+	 * 
+	 * @return the scaleIcond keyOpenGoal
+	 */
+	public static ImageView getKeyOpenGoal(int x, int y) {
+		return scaleIcon(keyOpenGoal, x, y);
+	}
+
 }
--- key/key.nui/src/de/uka/ilkd/key/nui/components/TreeViewController.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/components/TreeViewController.java
@@ -5,8 +5,10 @@ import java.util.Iterator;
 
 import de.uka.ilkd.key.control.KeYEnvironment;
 import de.uka.ilkd.key.nui.IconFactory;
+import de.uka.ilkd.key.proof.Goal;
 import de.uka.ilkd.key.proof.Node;
 import de.uka.ilkd.key.proof.Proof;
+import de.uka.ilkd.key.proof.ProofVisitor;
 import de.uka.ilkd.key.proof.init.JavaProfile;
 import de.uka.ilkd.key.proof.io.ProblemLoaderException;
 import javafx.fxml.FXML;
@@ -19,139 +21,172 @@ import javafx.scene.control.Label;
  */
 public class TreeViewController {
 
-    @FXML
-    private TreeView<Label> proofTreeView;
+	@FXML
+	private TreeView<Label> proofTreeView;
 
-    // defines the width and height of an icon
-    private static int iconSize = 15;
+	// defines the width and height of an icon
+	private static int iconSize = 15;
 
-    /**
-     * Initialization method for scene, displays the default proof
-     */
-    public void initialize() {
-    	// add css file to view
-    	String cssPath = this.getClass().getResource("treeView.css").toExternalForm(); 
-    	proofTreeView.getStylesheets().add(cssPath);
+	private Proof p;
 
-    	// load and display proof
-    	Proof p = loadProof("example02.proof");
-    	displayProof(p);
-    }
+	/**
+	 * Initialization method for scene, displays the default proof
+	 */
+	public void initialize() {
+		// add css file to view
+		String cssPath = this.getClass().getResource("treeView.css").toExternalForm();
+		proofTreeView.getStylesheets().add(cssPath);
 
-    /**
-     * shows a proof in the treeView
-     * 
-     * @param p
-     *            the proof to display
-     */
-    public void displayProof(Proof p) {
-    	// get the root node
-    	Node pRoot = p.root();
+		// load and display proof
+		Proof p = loadProof("example02.proof");
+		displayProof(p);
+	}
 
-    	// convert proof to fxtree
-    	TreeItem<Label> fxRoot = new TreeItem<Label>(new Label("proof tree"));
-    	fxRoot.setExpanded(true);
-    	addPNodeToFXNode(pRoot, fxRoot);
+	/**
+	 * shows a proof in the treeView
+	 * 
+	 * @param p
+	 *            the proof to display
+	 */
+	public void displayProof(Proof p) {
+		// get the root node
+		Node pRoot = p.root();
 
-    	// display tree
-    	proofTreeView.setRoot(fxRoot);
-    }
+		// convert proof to fxtree
+		TreeItem<Label> fxRoot = new TreeItem<Label>(new Label("proof tree"));
+		fxRoot.setExpanded(true);
+		addPNodeToFXNode(pRoot, fxRoot);
 
-    /**
-     * converts a proof node to an fxnode recursively and adds it to an fxparent
-     * 
-     * @param proofNode
-     *            the proof node which should be converted into an fxtree
-     * @param fxParent
-     *            the node where the converted proofNode should be added to
-     */
-    private void addPNodeToFXNode(Node proofNode, TreeItem<Label> fxParent) {
-    	// create an fxNode and add it to the fxparent
-    	String nodeName = proofNode.serialNr() + ": " + proofNode.name();
-    	TreeItem<Label> fxNode = new TreeItem<Label>(new Label(nodeName));
-    	fxParent.getChildren().add(fxNode);
+		// display tree
+		proofTreeView.setRoot(fxRoot);
+	}
 
-    	// determine number of children and add all children recursively
-    	int numChildren = proofNode.childrenCount();
-    	if (numChildren == 0) {
-    		setNodeIcon(fxNode);
-    	}
-    	else if (numChildren == 1) {
-    		setNodeIcon(fxParent);
-    		// add child's subtree to parent
-    		addPNodeToFXNode(proofNode.child(0), fxParent);
-    	}
-    	else if (numChildren > 1) {
-    		// for each child create a branch node and add it to the fxparent
-    		for (Iterator<Node> childrenIterator = proofNode
-    				.childrenIterator(); childrenIterator.hasNext();) {
-    			// get next child
-    			Node child = childrenIterator.next();
+	/**
+	 * converts a proof node to an fxnode recursively and adds it to an fxparent
+	 * 
+	 * @param proofNode
+	 *            the proof node which should be converted into an fxtree
+	 * @param fxParent
+	 *            the node where the converted proofNode should be added to
+	 */
+	private void addPNodeToFXNode(Node proofNode, TreeItem<Label> fxParent) {
+		// create an fxNode and add it to the fxparent
+		String nodeName = proofNode.serialNr() + ": " + proofNode.name();
+		TreeItem<Label> fxNode = new TreeItem<Label>(new Label(nodeName));
+		fxParent.getChildren().add(fxNode);
 
-    			// define branch label for new node, create node and set icon
-    			String branchLabel = child.getNodeInfo().getBranchLabel();
-    			if (branchLabel == null) {
-    				branchLabel = "Case "
-    						+ (child.parent().getChildNr(child) + 1);
-    			}
-    			TreeItem<Label> branch = new TreeItem<Label>(new Label(branchLabel));
-    			setNodeIcon(branch);
-    			branch.getValue().getStyleClass().add("branch");
+		// determine number of children and add all children recursively
+		int numChildren = proofNode.childrenCount();
+		if (numChildren == 0) {
+			setNodeIcon(proofNode, fxNode);
+		} else if (numChildren == 1) {
+			setNodeIcon(proofNode, fxParent);
+			// add child's subtree to parent
+			addPNodeToFXNode(proofNode.child(0), fxParent);
+		} else if (numChildren > 1) {
+			// for each child create a branch node and add it to the fxparent
+			for (Iterator<Node> childrenIterator = proofNode.childrenIterator(); childrenIterator.hasNext();) {
+				// get next child
+				Node child = childrenIterator.next();
 
-    			// add node to parent
-    			fxParent.getChildren().add(branch);
+				// define branch label for new node, create node and set icon
+				String branchLabel = child.getNodeInfo().getBranchLabel();
+				if (branchLabel == null) {
+					branchLabel = "Case " + (child.parent().getChildNr(child) + 1);
+				}
+				TreeItem<Label> branch = new TreeItem<Label>(new Label(branchLabel));
+				setNodeIcon(child, branch);
+				branch.getValue().getStyleClass().add("branch");
 
-    			// call function recursively with current child
-    			addPNodeToFXNode(child, branch);
-    		}
-    	}
-    }
+				// add node to parent
+				fxParent.getChildren().add(branch);
 
-    /**
-     * Sets an Icon on a given treeItem based on the treeItem's text
-     * 
-     * @param treeItem
-     *            The tree item where the graphics should be placed on
-     */
-    private void setNodeIcon(TreeItem<Label> treeItem) {
-    	String lbl = treeItem.toString().toLowerCase();
-    	if (lbl.contains("closed goal")) {
-    		treeItem.setGraphic(IconFactory.keyHoleClosed(iconSize, iconSize));
-    	}
-    	else if (lbl.contains("open goal")) {
-    		treeItem.setGraphic(IconFactory.keyHole(iconSize, iconSize));
-    	}
-    	else if (lbl.contains("case")) {
-    		treeItem.setGraphic(IconFactory.keyFolderGray(iconSize, iconSize));
-    	}
-    	else if (lbl.contains("cut")) {
-    		treeItem.setGraphic(IconFactory.keyFolderBlue(iconSize, iconSize));
-    	}
-    }
-  
+				// call function recursively with current child
+				addPNodeToFXNode(child, branch);
+			}
+		}
+	}
 
-    /**
-     * Loads the given proof file. Checks if the proof file exists and the proof
-     * is not null, and fails if the proof could not be loaded.
-     *
-     * @param proofFileName
-     *            The file name of the proof file to load.
-     * @return The loaded proof.
-     */
-    private Proof loadProof(String proofFileName) {
-        File proofFile = new File(
-                "resources//de/uka//ilkd//key//examples//" + proofFileName);
-        try {
-            KeYEnvironment<?> environment = KeYEnvironment.load(
-                    JavaProfile.getDefaultInstance(), proofFile, null, null,
-                    null, true);
-            Proof proof = environment.getLoadedProof();
+	/**
+	 * Sets an Icon on a given treeItem based on the information gathered from
+	 * the proofNode
+	 * 
+	 * @param proofNode
+	 *            The node item which contains the information to determine the
+	 *            right icon
+	 * @param treeItem
+	 *            The tree item where the graphics should be placed on
+	 */
+	private void setNodeIcon(Node proofNode, TreeItem<Label> treeItem) {
+		// node is not a leaf
+		if (!(proofNode.leaf())) {
+			if (proofNode.isClosed()) {
+				// all goals below this node are closed
+				treeItem.setGraphic(IconFactory.getKeyClosedInnerNode(iconSize, iconSize));
+				return;
+			} else {
+				class FindGoalVisitor implements ProofVisitor {
+					private boolean isLinked = false;
 
-            return proof;
-        }
-        catch (ProblemLoaderException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+					public boolean isLinked() {
+						return this.isLinked;
+					}
+
+					public void visit(Proof proof, Node visitedNode) {
+						Goal g;
+						if ((g = proof.getGoal(visitedNode)) != null && g.isLinked()) {
+							this.isLinked = true;
+						}
+					}
+				}
+				FindGoalVisitor v = new FindGoalVisitor();
+
+				p.breadthFirstSearch(proofNode, v);
+				if (v.isLinked()) {
+					treeItem.setGraphic(IconFactory.getKeyLinkedInnerNode(iconSize, iconSize));
+					return;
+				}
+			}
+
+		treeItem.setGraphic(IconFactory.getKeyOpenInnerNode(iconSize, iconSize));
+		}
+		// node is a leaf
+		else {
+			Goal goal = p.getGoal(proofNode);
+			if (goal == null || proofNode.isClosed()) {
+				treeItem.setGraphic(IconFactory.getKeyClosedGoal(20, 20));
+			} else {
+				if (goal.isLinked()) {
+					treeItem.setGraphic(IconFactory.getKeyLinkedInnerNode(20, 20));
+				} else if (!goal.isAutomatic()) {
+					treeItem.setGraphic(IconFactory.getKeyInteractiveGoal(20, 20));
+				} else {
+					treeItem.setGraphic(IconFactory.getKeyOpenGoal(20, 20));
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Loads the given proof file. Checks if the proof file exists and the proof
+	 * is not null, and fails if the proof could not be loaded.
+	 *
+	 * @param proofFileName
+	 *            The file name of the proof file to load.
+	 * @return The loaded proof.
+	 */
+	private Proof loadProof(String proofFileName) {
+		File proofFile = new File("resources//de/uka//ilkd//key//examples//" + proofFileName);
+		try {
+			KeYEnvironment<?> environment = KeYEnvironment.load(JavaProfile.getDefaultInstance(), proofFile, null, null,
+					null, true);
+			Proof proof = environment.getLoadedProof();
+			this.p = proof;
+			return proof;
+		} catch (ProblemLoaderException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
