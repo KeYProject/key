b1df436e8edcb184388f6a4580e20e3232bf68a3
Seena Vellaramkalayil <seena94@gmx.de>
2015-12-11 21:26:06 +0100

    implemented Goals View on KeYIDE

---
+++ KeYIDE/src/plugins/org.key_project.keyide.ui/src/org/key_project/keyide/ui/views/GoalsPage.java
@@ -0,0 +1,361 @@
+package org.key_project.keyide.ui.views;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.part.Page;
+import org.key_project.key4eclipse.common.ui.provider.ImmutableCollectionContentProvider;
+import org.key_project.keyide.ui.providers.BranchFolder;
+import org.key_project.keyide.ui.providers.GoalsLabelProvider;
+import org.key_project.util.eclipse.swt.SWTUtil;
+
+import de.uka.ilkd.key.control.AutoModeListener;
+import de.uka.ilkd.key.control.KeYEnvironment;
+import de.uka.ilkd.key.core.KeYSelectionEvent;
+import de.uka.ilkd.key.core.KeYSelectionListener;
+import de.uka.ilkd.key.core.KeYSelectionModel;
+import de.uka.ilkd.key.proof.Goal;
+import de.uka.ilkd.key.proof.Node;
+import de.uka.ilkd.key.proof.Proof;
+import de.uka.ilkd.key.proof.ProofEvent;
+import de.uka.ilkd.key.proof.ProofTreeEvent;
+import de.uka.ilkd.key.proof.ProofTreeListener;
+
+/**
+ * The {@link IGoalsPage} which is returned by the active {@link KeYEditor}. The
+ * aim of this {@link IGoalsPage} is to show all open {@link Goal}s by using a
+ * {link TableViewer}.
+ * 
+ * @author Seena Vellaramkalayil
+ *
+ */
+public class GoalsPage extends Page implements IGoalsPage {
+
+ 
+   private Proof proof;
+
+   private TableViewer viewer;
+
+   private KeYEnvironment<?> environment;
+
+   private KeYSelectionModel selectionModel;
+
+   private ImmutableCollectionContentProvider contentProvider;
+
+   private LabelProvider labelProvider;
+
+   /**
+    * The {link KeySelectionListener} listens to changes on the current
+    * {@link KeYSelectionModel}.
+    */
+   private KeYSelectionListener keySelectionListener = new KeYSelectionListener() {
+
+      @Override
+      public void selectedNodeChanged(KeYSelectionEvent e) {
+         handleSelectedNodeChanged(e);
+      }
+
+      @Override
+      public void selectedProofChanged(KeYSelectionEvent e) {
+         handleSelectedProofChanged(e);
+      }
+
+   };
+
+   /**
+    * The {@link ProofTreeListener} listens to any changes that are made on the
+    * current {@link Proof}.
+    */
+   private ProofTreeListener proofTreeListener = new ProofTreeListener() {
+
+      @Override
+      public void proofExpanded(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void proofIsBeingPruned(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void proofPruned(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void proofStructureChanged(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void proofClosed(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void proofGoalRemoved(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void proofGoalsAdded(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void proofGoalsChanged(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+      @Override
+      public void smtDataUpdate(ProofTreeEvent e) {
+         handleProofChanged();
+      }
+
+   };
+
+   /**
+    * The {@link AutoModeListener} listens to starting and stopping the auto
+    * mode.
+    */
+   private final AutoModeListener autoModeListener = new AutoModeListener() {
+
+      @Override
+      public void autoModeStarted(ProofEvent e) {
+         handleAutoModeStarted();
+      }
+
+      @Override
+      public void autoModeStopped(ProofEvent e) {
+         handleAutoModeStopped(e);
+      }
+
+   };
+
+   /**
+    * The {@link ISelectionListener} listens to selection changes that are made
+    * on any other views. If a change is made on the {@link GoalsView}, the
+    * selectionModel has to be updated.
+    */
+   private final ISelectionListener selectionListener = new ISelectionListener() {
+
+      @Override
+      public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+         if (selection instanceof IStructuredSelection
+               && part instanceof GoalsView) {
+            Object selectedObj = SWTUtil.getFirstElement(selection);
+            if (selectedObj instanceof Goal) {
+               selectionModel.setSelectedGoal((Goal) selectedObj);
+            }
+         }
+      }
+
+   };
+
+   /**
+    * Constructor.
+    * 
+    * @param proof
+    *           The current {@link Proof} for receiving the goals.
+    * @param environment
+    *           The {@link KeYEnvironment} to add the autoModeListener to.
+    * @param selectionModel
+    *           The {@link KeYSelectionModel} to add the keySelectionListener
+    *           to.
+    */
+   public GoalsPage(Proof proof, KeYEnvironment<?> environment,
+         KeYSelectionModel selectionModel) {
+
+      this.proof = proof;
+      this.environment = environment;
+      this.selectionModel = selectionModel;
+      this.proof.addProofTreeListener(proofTreeListener);
+      this.environment.getProofControl().addAutoModeListener(autoModeListener);
+      this.selectionModel.addKeYSelectionListener(keySelectionListener);
+   }
+
+   
+   /**
+    * Method handles selection changes on the proof.
+    * @param e 
+    */
+   protected void handleSelectedProofChanged(KeYSelectionEvent e) {
+      if (e.getSource().getSelectedNode().proof() == proof) {
+         updateSelectionThreadSafe();
+      }
+   }
+
+   
+   /**
+    * Method handles selection changes on a node.
+    * @param e
+    */
+   protected void handleSelectedNodeChanged(KeYSelectionEvent e) {
+      updateSelectionThreadSafe();
+   }
+
+   
+   /**
+    * This method sets a new input to the viewer and is called when the proof 
+    * has changed.
+    */
+   protected void handleProofChanged() {
+      viewer.setInput(proof.openGoals());
+   }
+
+   
+   /**
+    * Method handles the page when the auto mode stops.
+    * @param e
+    */
+   protected void handleAutoModeStopped(ProofEvent e) {
+      if (e.getSource() == proof) {
+         proof.addProofTreeListener(proofTreeListener);
+         updateProofThreadSafe();
+      }
+   }
+
+   
+   /**
+    * Method handles the page when the auto mode starts.
+    */
+   protected void handleAutoModeStarted() {
+      proof.removeProofTreeListener(proofTreeListener);
+   }
+
+   
+   
+   /**
+    * Method executes {@link #handleProofChanged()} asynchronously and thread safe.
+    */
+   protected void updateProofThreadSafe() {
+      if (!getControl().getDisplay().isDisposed()) {
+         getControl().getDisplay().asyncExec(new Runnable() {
+            @Override
+            public void run() {
+               if (!getControl().isDisposed()) {
+                  handleProofChanged();
+               }
+            }
+         });
+      }
+
+   }
+
+   
+   /**
+    * Method updates the selection of the viewer when there are changes.
+    */
+   protected void updateSelectedNode() {
+      Node mediatorNode = selectionModel.getSelectedNode();
+      Object selectedNode = getSelectedNode(viewer.getSelection());
+      if (mediatorNode != selectedNode) {
+         viewer.setSelection(
+               SWTUtil.createSelection(proof.getGoal(mediatorNode)), true);
+      }
+
+   }
+
+   
+   /**
+    * Method returns the selected {@link Node}.
+    * @param selection
+    * @return The selected {@link Node} or {@code null} if no {@link Node} is selected.
+    */
+   protected Node getSelectedNode(ISelection selection) {
+      Object selectedObj = SWTUtil.getFirstElement(selection);
+      if (selectedObj instanceof Node) {
+         return (Node) selectedObj;
+      } else if (selectedObj instanceof BranchFolder) {
+         return ((BranchFolder) selectedObj).getChild();
+      } else if (selectedObj instanceof Goal) {
+         return ((Goal) selectedObj).node();
+      } else {
+         return null;
+      }
+   }
+
+   
+   /**
+    * Executes {@link #updateSelectedNode()} asynchronously and thread safe.
+    */
+   protected void updateSelectionThreadSafe() {
+      if (!getControl().getDisplay().isDisposed()) {
+         getControl().getDisplay().asyncExec(new Runnable() {
+            @Override
+            public void run() {
+               if (!getControl().isDisposed()) {
+                  updateSelectedNode();
+               }
+            }
+         });
+      }
+   }
+   
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public void createControl(Composite parent) {
+      this.viewer = new TableViewer(parent);
+      this.contentProvider = new ImmutableCollectionContentProvider();
+      this.labelProvider = new GoalsLabelProvider();
+      viewer.setContentProvider(contentProvider);
+      viewer.setLabelProvider(labelProvider);
+      viewer.setInput(proof.openGoals());
+      
+      //set header "Open Goals" on the view
+      Table table = viewer.getTable();
+      TableColumn heading = new TableColumn(table, SWT.LEFT);
+      heading.setText("Open Goals");
+      heading.setWidth(500);
+      table.setHeaderVisible(true);
+
+      //allow listening to selection changes of this viewer
+      getSite().setSelectionProvider(viewer);
+      getSite().getPage().addSelectionListener(selectionListener);
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public Control getControl() {
+      return viewer.getControl();
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public void setFocus() {
+      if (viewer.getControl() != null && !viewer.getControl().isDisposed()) {
+         viewer.getControl().setFocus();
+      }
+   }
+
+   /**
+    * {@inheritDoc}
+    */
+   @Override
+   public void dispose() {
+      if (viewer.getControl() != null) {
+         viewer.getControl().dispose();
+      }
+      environment.getProofControl().removeAutoModeListener(autoModeListener);
+      proof.removeProofTreeListener(proofTreeListener);
+      selectionModel.removeKeYSelectionListener(keySelectionListener);
+      super.dispose();
+   }
+
+}
---
+++ KeYIDE/src/plugins/org.key_project.keyide.ui/src/org/key_project/keyide/ui/views/IGoalsPage.java
@@ -0,0 +1,12 @@
+package org.key_project.keyide.ui.views;
+
+import org.eclipse.ui.part.IPage;
+
+/**
+ * An {@link IPage} shown in a {@link GoalView} 
+ * @author Seena Vellaramkalayil
+ *
+ */
+public interface IGoalsPage extends IPage {
+
+}
