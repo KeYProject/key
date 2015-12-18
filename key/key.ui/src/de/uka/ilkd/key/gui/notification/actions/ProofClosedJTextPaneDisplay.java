c0ec468301ca3073d9debfd6ee22f991cae74568
Dominic Scheurer <scheurer@cs.tu-darmstadt.de>
2015-12-18 14:07:59 +0100

    [ProofStatistics] Beautified proof statistics window, added statistics about interactive rule apps (types of rules)

--- key/key.ui/src/de/uka/ilkd/key/gui/notification/actions/ProofClosedJTextPaneDisplay.java
+++ key/key.ui/src/de/uka/ilkd/key/gui/notification/actions/ProofClosedJTextPaneDisplay.java
@@ -16,14 +16,23 @@
  */
 package de.uka.ilkd.key.gui.notification.actions;
 
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Frame;
 
+import javax.swing.BorderFactory;
+import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.UIManager;
 
+import de.uka.ilkd.key.gui.MainWindow;
+import de.uka.ilkd.key.gui.actions.ShowProofStatistics;
+import de.uka.ilkd.key.gui.configuration.Config;
 import de.uka.ilkd.key.gui.notification.events.NotificationEvent;
 import de.uka.ilkd.key.gui.notification.events.ProofClosedNotificationEvent;
 import de.uka.ilkd.key.proof.Proof;
-import de.uka.ilkd.key.util.Pair;
+import de.uka.ilkd.key.util.Debug;
 
 /**
  * Displays a JOptionPane informing about a closed proof
@@ -45,19 +54,35 @@ public class ProofClosedJTextPaneDisplay extends ShowDisplayPane {
         if (pcne instanceof ProofClosedNotificationEvent) {
             Proof proof = ((ProofClosedNotificationEvent)pcne).getProof();
             if (proof != null) {
-                String statistics = "";
-                for (Pair<String, String> x: proof.getStatistics().getSummary()) {
-                    if ("".equals(x.second)) statistics += "\n";
-                    statistics += x.first+": "+ x.second+"\n";
-                }
-                setMessage("Proved.\n\nStatistics:\n"+statistics);
+                setMessage(ShowProofStatistics.getHTMLStatisticsMessage(proof));
             }
         } else {
             setMessage("Proof Closed. No statistics available.");
         }
+        
+        JEditorPane contentPane = new JEditorPane("text/html", getMessage());
+        contentPane.setEditable(false);
+        contentPane.setBorder(BorderFactory.createEmptyBorder());
+        contentPane.setCaretPosition(0);
+        contentPane.setBackground(MainWindow.getInstance().getBackground());
+        contentPane.setSize(new Dimension(10, 360));
+        contentPane.setPreferredSize(new Dimension(contentPane.getPreferredSize().width + 15, 360));
+        
+        JScrollPane scrollPane = new JScrollPane(contentPane);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        
+        Font myFont = UIManager.getFont(Config.KEY_FONT_PROOF_TREE);
+        if (myFont != null) {
+            contentPane.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
+            contentPane.setFont(myFont);
+        } else {
+            Debug.out("KEY_FONT_PROOF_TREE not available. Use standard font.");
+        }
+        
         JOptionPane.showMessageDialog
-            (parentComponent, getMessage(),
+            (parentComponent, scrollPane,
                     "Proof closed", JOptionPane.INFORMATION_MESSAGE);
+        
         return true;
     }
 }
