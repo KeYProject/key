f0adc1c78173a11f62b5a2cf337f8b362727c677
Stefan Pilot <hans.stefan.pilot@googlemail.com>
2016-01-07 01:09:34 +0100

    Adds what could soon become a search functionality. Untested!

---
+++ key/key.nui/src/de/uka/ilkd/key/nui/controller/SearchViewController.java
@@ -0,0 +1,40 @@
+package de.uka.ilkd.key.nui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SearchViewController implements Initializable {
+    
+    @FXML
+    TextField SearchTextField;
+    @FXML
+    Button PreviousButton;
+    @FXML
+    Button NextButton;
+    @FXML
+    Button SearchButton;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+       SearchTextField.requestFocus();
+       SearchButton.setDisable(false);
+    }
+    
+    public void handleSearchButton(ActionEvent e){
+        System.out.println(SearchTextField.getText());
+    }
+    
+    public void handleNextButton(ActionEvent e){
+        System.out.println("'>' Button was pressed");
+    }
+    
+    public void handlePreviousButton(ActionEvent e){
+        System.out.println("'<' Button was pressed");
+    }
+}
--- key/key.nui/src/de/uka/ilkd/key/nui/controller/TreeViewController.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/controller/TreeViewController.java
@@ -2,6 +2,9 @@ package de.uka.ilkd.key.nui.controller;
 
 import java.io.File;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import de.uka.ilkd.key.control.KeYEnvironment;
@@ -13,66 +16,68 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeCell;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.util.Callback;
 
 /**
  * controller for the treeView GUI element to visualize proofs
- * @author  Patrick Jattke
- * @author  Matthias Schultheis
+ * 
+ * @author Patrick Jattke
+ * @author Matthias Schultheis
  * @version 1.1
  */
-public class TreeViewController implements Initializable{
+public class TreeViewController implements Initializable {
     /**
      * The proofTree view of the GUI.
      */
     @FXML
     private TreeView<NUINode> proofTreeView;
-    
+
     /**
      * the visualizer for displaying a proof tree
      */
-	ProofTreeVisualizer visualizer;
-	
-	/**
-	 * TODO
-	 */
-	private static TreeViewController instance;
+    ProofTreeVisualizer visualizer;
+
+    /**
+     * TODO
+     */
+    private static TreeViewController instance;
 
     /**
      * Initialization method for scene; loads the default proof
      */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
-    	instance = this;
 
-    	// set cell factory for rendering cells
-    	proofTreeView.setCellFactory(new Callback <  TreeView<NUINode>, TreeCell<NUINode>  >(){
+        instance = this;
+
+        // set cell factory for rendering cells
+        proofTreeView.setCellFactory(new Callback<TreeView<NUINode>, TreeCell<NUINode>>() {
             @Override
             public TreeCell<NUINode> call(TreeView<NUINode> p) {
                 return new ProofTreeCell();
             }
         });
-    	
-    	// Create a new tree visualizer instance for processing the conversion
-    	// de.uka.ilkd.key.proof.Node --> de.uka.ilkd.key.nui.NUI.prooftree.NUINode 
-    	// --> TreeItem<NUINode> (JavaFX)
-    	visualizer = new ProofTreeVisualizer(proofTreeView);
-    	
+
+        // Create a new tree visualizer instance for processing the conversion
+        // de.uka.ilkd.key.proof.Node -->
+        // de.uka.ilkd.key.nui.NUI.prooftree.NUINode
+        // --> TreeItem<NUINode> (JavaFX)
+        visualizer = new ProofTreeVisualizer(proofTreeView);
+
         // add CSS file to view
-        String cssPath = this.getClass()
-                .getResource("../components/treeView.css").toExternalForm();
+        String cssPath = this.getClass().getResource("../components/treeView.css").toExternalForm();
         visualizer.addStylesheet(cssPath);
-        
+
         // load and display proof in visualizer
         Proof p = loadProof("gcd.twoJoins.proof");
         displayProof(p);
     }
-    
-    public void handleOpenSearchView(){
+
+    public void handleOpenSearchView() {
         NUIController.getInstance().createComponent(".searchView", NUIController.Place.LEFT, ".searchView.fxml");
-     // TODO this is ugly
+        // TODO this is ugly
     }
 
     /**
@@ -82,7 +87,7 @@ public class TreeViewController implements Initializable{
      *            The proof file which should be displayed
      */
     private void displayProof(Proof proof) {
-    	visualizer.loadProofTree(proof);
+        visualizer.loadProofTree(proof);
         visualizer.visualizeProofTree();
     }
 
@@ -98,8 +103,7 @@ public class TreeViewController implements Initializable{
         String examplesRoot = "resources//de/uka//ilkd//key//examples//";
         File proofFile = new File(examplesRoot + proofFileName);
         try {
-            KeYEnvironment<?> environment = KeYEnvironment.load(
-                    JavaProfile.getDefaultInstance(), proofFile, null, null,
+            KeYEnvironment<?> environment = KeYEnvironment.load(JavaProfile.getDefaultInstance(), proofFile, null, null,
                     null, true);
             Proof proof = environment.getLoadedProof();
             return proof;
@@ -109,17 +113,85 @@ public class TreeViewController implements Initializable{
             return null;
         }
     }
+
     /**
      * TODO
+     * 
      * @return
      */
-    public static TreeViewController getInstance(){
+    public static TreeViewController getInstance() {
         return instance;
     }
-    
-    public void search(String term){
-      //  for(){
-            
-      //  }
+
+    /**
+     * TODO
+     */
+    private TreeItem<NUINode> currentFoundObject;
+
+    /**
+     * TODO
+     */
+    private HashMap<String, TreeItem<NUINode>> searchMap;
+
+    /**
+     * TODO
+     * 
+     * @param term
+     */
+    public void search(String term) {
+        if (searchMap == null)
+            initializeSearchMap();
+        if (searchMap.containsKey(term)) { // das muss angepasst werden, damit
+                                           // auch teil-hits gefunden werden
+            TreeItem<NUINode> foundObject = searchMap.get(term);
+            // found Object highlighten
+            if (currentFoundObject != null) {
+                // currentFoundObject dis-highlighten
+            }
+            currentFoundObject = foundObject;
+        }
+    }
+
+    /**
+     * TODO
+     */
+    private void initializeSearchMap() {
+        searchMap = new HashMap<>();
+        List<TreeItem<NUINode>> l = treeToList(proofTreeView.getRoot());
+        for (TreeItem<NUINode> t : l) {
+            searchMap.put(t.getValue().getLabel(), t);
+        }
+    }
+
+    /**
+     * TODO
+     * 
+     * @param t
+     * @return
+     */
+    private List<TreeItem<NUINode>> treeToList(TreeItem<NUINode> t) {
+        if (t == null)
+            throw new IllegalArgumentException();
+        return treeToList(t, new LinkedList<TreeItem<NUINode>>());
+    }
+
+    /**
+     * TODO
+     * 
+     * @param t
+     * @param l
+     * @return
+     */
+    private List<TreeItem<NUINode>> treeToList(TreeItem<NUINode> t, List<TreeItem<NUINode>> l) {
+        if (t == null || l == null)
+            throw new IllegalArgumentException();
+
+        l.add(t);
+        if (!t.getChildren().isEmpty())
+            for (TreeItem<NUINode> ti : t.getChildren()) {
+                l.addAll(treeToList(ti, new LinkedList<TreeItem<NUINode>>()));
+            }
+
+        return l;
     }
 }
