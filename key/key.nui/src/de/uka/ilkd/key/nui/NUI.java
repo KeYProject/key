f307c6e426d444960c71e9b4604f064e571f3edb
Patrick Jattke <patrick.jattke@stud.tu-darmstadt.de>
2016-01-24 19:57:45 +0100

    [CodeReview] NUI.java finished.

--- key/key.nui/src/de/uka/ilkd/key/nui/NUI.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/NUI.java
@@ -1,7 +1,6 @@
 package de.uka.ilkd.key.nui;
 
 import java.io.File;
-import java.util.Arrays;
 
 import javafx.application.Application;
 import javafx.scene.Parent;
@@ -9,33 +8,52 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * Main Class for initializing the GUI
+ * Main Class for initializing the GUI.
  * 
  * @author Florian Breitfelder
  *
  */
 public class NUI extends Application {
-    
-    public static File initialProofFile;
-    
-    public static void main(String[] args) {
-        if(args.length != 0)
-            initialProofFile = new File(System.getProperty("user.dir") + args[0]);
-        launch(args);
-    }
-    
-    /**
-     * When program is starting method "start" is called
-     */
-    @Override
-    public void start(Stage stage) throws Exception {
-        ComponentFactory.setInstance("components/");
-        Parent root = ComponentFactory.getInstance().createNUISceneGraph();
 
-        // Load scene and set preferences
-        Scene scene = new Scene(root);
-        stage.setTitle("KeY");
-        stage.setScene(scene);
-        stage.show();
-    }
+	/**
+	 * The proof file initially (at program startup) loaded.
+	 */
+	private static File initialProofFile;
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args
+	 *            The arguments passed to the program.
+	 */
+	public static void main(final String... args) {
+		if (args.length != 0) {
+			initialProofFile = new File(System.getProperty("user.dir") + args[0]);
+		}
+		launch(args);
+	}
+
+	/**
+	 * When program is starting method "start" is called.
+	 */
+	@Override
+	public final void start(final Stage stage) {
+		ComponentFactory.setInstance("components/");
+		final Parent root = ComponentFactory.getInstance().createNUISceneGraph();
+
+		// Load scene and set preferences
+		final Scene scene = new Scene(root);
+		stage.setTitle("KeY");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	/**
+	 * Returns the proof file initially loaded.
+	 * 
+	 * @return initialProofFile the proof file initially loaded
+	 */
+	public static File getInitialProofFile() {
+		return initialProofFile;
+	}
 }
