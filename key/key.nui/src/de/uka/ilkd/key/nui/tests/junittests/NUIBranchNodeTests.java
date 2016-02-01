3c923fbf728e2602910edbe16f398ca6ef5cac83
Stefan Pilot <hans.stefan.pilot@googlemail.com>
2016-02-01 11:59:00 +0100

    cleans up imports

--- key/key.nui/src/de/uka/ilkd/key/nui/tests/junittests/NUIBranchNodeTests.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/tests/junittests/NUIBranchNodeTests.java
@@ -3,13 +3,9 @@ package de.uka.ilkd.key.nui.tests.junittests;
 import static org.junit.Assert.*;
 
 import java.io.File;
-
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import de.uka.ilkd.key.control.KeYEnvironment;
-import de.uka.ilkd.key.nui.controller.TreeViewController;
 import de.uka.ilkd.key.nui.prooftree.ProofTreeVisualizer;
 import de.uka.ilkd.key.proof.Proof;
 import de.uka.ilkd.key.proof.init.JavaProfile;
@@ -19,15 +15,15 @@ public class NUIBranchNodeTests {
 
     private static String TESTFILE_01 = "resources//de/uka//ilkd//key//examples//example01.proof";
     private static ProofTreeVisualizer ptVisualizer;
-    
+
     @BeforeClass
-    public static void setUpBeforeClass() {        
+    public static void setUpBeforeClass() {
         ptVisualizer = new ProofTreeVisualizer(null);
         File proofFile = new File(TESTFILE_01);
         KeYEnvironment<?> environment = null;
         try {
-            environment = KeYEnvironment.load(JavaProfile.getDefaultInstance(),
-                    proofFile, null, null, null, true);
+            environment = KeYEnvironment.load(JavaProfile.getDefaultInstance(), proofFile, null,
+                    null, null, true);
         }
         catch (ProblemLoaderException e) {
             // TODO Auto-generated catch block
@@ -53,27 +49,26 @@ public class NUIBranchNodeTests {
         String SEARCH_TERM_04 = "inEqSimp_contradInEq0";
         String SEARCH_TERM_05 = "qeq_literals";
         String SEARCH_TERM_06 = "CUT: a <= -1 | a >= 1 FALSE";
-        
+
         // Search01
         assertTrue(searchAndCompareSize(SEARCH_TERM_01, 6));
-        
+
         // Search02
         assertTrue(searchAndCompareSize(SEARCH_TERM_02, 9));
-        
+
         // Search03
         assertTrue(searchAndCompareSize(SEARCH_TERM_03, 25));
-        
+
         // Search04
         assertTrue(searchAndCompareSize(SEARCH_TERM_04, 4));
-        
+
         // Search05
         assertTrue(searchAndCompareSize(SEARCH_TERM_05, 49));
-        
+
         // Search06
         assertTrue(searchAndCompareSize(SEARCH_TERM_06, 2));
-    }  
-    
-    
+    }
+
     /**
      * Tests for search terms which return no matches.
      */
@@ -86,7 +81,7 @@ public class NUIBranchNodeTests {
         // TODO this could be extended
         
     }
-    
+
     /**
      * Tests for search terms containing special characters, like spaces, &, $,
      * etc.
