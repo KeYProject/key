8f80e3bd8a6c1fbecfc01ae816f93a85bc4451fa
Patrick Jattke <patrick.jattke@stud.tu-darmstadt.de>
2016-02-09 10:50:31 -0800

    shell script changed

--- key/scripts/jenkins/archiveFiles.sh
+++ key/scripts/jenkins/archiveFiles.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+cd #!/bin/sh -x
 export KEY_VERSION="2.5.$BUILD_NUMBER"
 export ANT_HOME=/opt/ant/
 export ANT_OPTS="-Xmx2048m -Xms512m"
@@ -6,13 +6,14 @@ export PATH=$PATH:/home/hudson/key/bin/
 export STATISTICS_DIR="$JENKINS_HOME/userContent/reports-$JOB_NAME"
 unset DISPLAY
 
-FILES=*.htm*
+FILES=../key.nui/reports/*.htm*
 PREFIX="${BUILD_NUMBER}_"
 
-cd ../key.nui/reports/
-mkdir -p "$STATISTICS_DIR"
+mkdir -p "$STATISTICS_DIR/$BUILD_NUMBER"
 for file in $FILES
 do
-	cp $file "$STATISTICS_DIR/$BUILD_NUMBER/$PREFIX${f%%.*}.${f##*.}"
+	echo "../key.nui/reports/$file"
+	echo "$STATISTICS_DIR/$BUILD_NUMBER/$file"
+	cp "../key.nui/reports/$file" "$STATISTICS_DIR/$BUILD_NUMBER/$file"
 done
 
