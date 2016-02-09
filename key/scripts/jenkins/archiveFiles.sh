eea3e0ec31218c33ab503d3289d5d388e0f6ecc2
Patrick Jattke <patrick.jattke@stud.tu-darmstadt.de>
2016-02-09 11:05:09 -0800

    fixed a path issue in the shell script

--- key/scripts/jenkins/archiveFiles.sh
+++ key/scripts/jenkins/archiveFiles.sh
@@ -7,13 +7,11 @@ export STATISTICS_DIR="$JENKINS_HOME/userContent/reports-$JOB_NAME"
 unset DISPLAY
 
 FILES=../key.nui/reports/*.htm*
-PREFIX="${BUILD_NUMBER}_"
 
 mkdir -p "$STATISTICS_DIR/$BUILD_NUMBER"
 for file in $FILES
 do
-	echo "../key.nui/reports/$file"
-	echo "$STATISTICS_DIR/$BUILD_NUMBER/$file"
-	cp "../key.nui/reports/$file" "$STATISTICS_DIR/$BUILD_NUMBER/$file"
+	FILENAME=$(basename $file)
+	cp "$file" "$STATISTICS_DIR/$BUILD_NUMBER/$FILENAME"
 done
 
