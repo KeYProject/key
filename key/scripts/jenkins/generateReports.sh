c1a442701baaf8fdc810093fdb0d6edb19bbdd0e
Patrick Jattke <patrick.jattke@stud.tu-darmstadt.de>
2016-02-25 13:20:07 +0100

    - Removed script generateReports.sh - Integrated ANT task invocation of generateReports and runNUITests into   Jenkins Job

--- key/scripts/jenkins/generateReports.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd key/key.nui
-export KEY_VERSION="2.5.$BUILD_NUMBER"
-export ANT_HOME=/opt/ant/
-export ANT_OPTS="-Xmx2048m -Xms512m"
-export PATH=$PATH:/home/hudson/key/bin/
-ant -logger org.apache.tools.ant.NoBannerLogger checkstyle
-ant -logger org.apache.tools.ant.NoBannerLogger pmd
-ant -logger org.apache.tools.ant.NoBannerLogger findbugs
