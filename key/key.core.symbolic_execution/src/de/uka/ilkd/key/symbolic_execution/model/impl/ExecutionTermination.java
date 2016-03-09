c3eef6fb58739d272d8eaccf734aa375616856a0
Leonard Goetz <leonard.goetz@stud.tu-darmstadt.de>
2016-03-09 19:47:38 +0100

    Removed duplicate code for lazyComputeIsBranchVerified and lazyComputeExceptionSort which is now in the util class

--- key/key.core.symbolic_execution/src/de/uka/ilkd/key/symbolic_execution/model/impl/ExecutionTermination.java
+++ key/key.core.symbolic_execution/src/de/uka/ilkd/key/symbolic_execution/model/impl/ExecutionTermination.java
@@ -126,61 +126,11 @@ public class ExecutionTermination extends AbstractExecutionNode<SourceElement> i
    @Override
    public Sort getExceptionSort() {
       if (exceptionSort == null) {
-         exceptionSort = lazyComputeExceptionSort();
+         exceptionSort = SymbolicExecutionUtil.lazyComputeExceptionSort(getProofNode(), exceptionVariable);
       }
       return exceptionSort;
    }
    
-   /**
-    * Computes the exception {@link Sort} lazily when {@link #getExceptionSort()}
-    * is called the first time. 
-    * @return The exception {@link Sort}.
-    */
-   protected Sort lazyComputeExceptionSort() {
-      Sort result = null;
-      if (exceptionVariable != null) {
-         // Search final value of the exceptional variable which is used to check if the verified program terminates normally
-         ImmutableArray<Term> value = null;
-         for (SequentFormula f : getProofNode().sequent().succedent()) {
-            Pair<ImmutableList<Term>,Term> updates = TermBuilder.goBelowUpdates2(f.formula());
-            Iterator<Term> iter = updates.first.iterator();
-            while (value == null && iter.hasNext()) {
-               value = extractValueFromUpdate(iter.next(), exceptionVariable);
-            }
-         }
-         // An exceptional termination is found if the exceptional variable is not null
-         if (value != null && value.size() == 1) {
-            result = value.get(0).sort();
-         }
-      }
-      return result;
-   }
-   
-   /**
-    * Utility method to extract the value of the {@link IProgramVariable}
-    * from the given update term.
-    * @param term The given update term.
-    * @param variable The {@link IProgramVariable} for that the value is needed.
-    * @return The found value or {@code null} if it is not defined in the given update term.
-    */
-   protected ImmutableArray<Term> extractValueFromUpdate(Term term,
-                                                         IProgramVariable variable) {
-      ImmutableArray<Term> result = null;
-      if (term.op() instanceof ElementaryUpdate) {
-         ElementaryUpdate update = (ElementaryUpdate)term.op();
-          if (ObjectUtil.equals(variable, update.lhs())) {
-            result = term.subs();
-         }
-      }
-      else if (term.op() instanceof UpdateJunctor) {
-         Iterator<Term> iter = term.subs().iterator();
-         while (result == null && iter.hasNext()) {
-            result = extractValueFromUpdate(iter.next(), variable);
-         }
-      }
-      return result;
-   }
-
    /**
     * {@inheritDoc}
     */
@@ -207,46 +157,8 @@ public class ExecutionTermination extends AbstractExecutionNode<SourceElement> i
    @Override
    public boolean isBranchVerified() {
       if (branchVerified == null) {
-         branchVerified = Boolean.valueOf(lazyComputeBranchVerified());
+         branchVerified = Boolean.valueOf(SymbolicExecutionUtil.lazyComputeIsBranchVerified(getProofNode()));
       }
       return branchVerified.booleanValue();
-   }
-   
-   /**
-    * Computes the value of {@link #isBranchVerified()} lazily.
-    * @return The branch verified state.
-    */
-   protected boolean lazyComputeBranchVerified() {
-      if (!isDisposed()) {
-         // Find uninterpreted predicate
-         Term predicate = AbstractOperationPO.getUninterpretedPredicate(getProof());
-         // Check if node can be treated as verified/closed
-         if (predicate != null) {
-            boolean verified = true;
-            Iterator<Node> leafsIter = getProofNode().leavesIterator();
-            while (verified && leafsIter.hasNext()) {
-               Node leaf = leafsIter.next();
-               if (!leaf.isClosed()) {
-                  final Term toSearch = predicate;
-                  SequentFormula topLevelPredicate = CollectionUtil.search(leaf.sequent().succedent(), new IFilter<SequentFormula>() {
-                     @Override
-                     public boolean select(SequentFormula element) {
-                        return toSearch.op() == element.formula().op();
-                     }
-                  });
-                  if (topLevelPredicate == null) {
-                     verified = false;
-                  }
-               }
-            }
-            return verified;
-         }
-         else {
-            return getProofNode().isClosed();
-         }
-      }
-      else {
-         return false;
-      }
    }
 }
