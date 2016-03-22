163f2b0f4f4d6a18b8d2c9653b1d3e0d91dd92bf
Stefan Pilot <hans.stefan.pilot@googlemail.com>
2016-03-22 16:29:36 +0100

    Wraps the dirty exception throwing operations in FilteringHandler into a local class

--- key/key.nui/src/de/uka/ilkd/key/nui/prooftree/FilteringHandler.java
+++ key/key.nui/src/de/uka/ilkd/key/nui/prooftree/FilteringHandler.java
@@ -6,6 +6,7 @@ import java.lang.annotation.Annotation;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -37,12 +38,7 @@ public class FilteringHandler {
     /**
      * Prefix for binary class files.
      */
-    private static final String BINARY_NAME_PREFIX = "de.uka.ilkd.key.nui.prooftree.filter.";
-
-    /**
-     * Path where filter classes are stored.
-     */
-    private static final String FILTER_PATH = "filter/";
+    private static final String BIN_NAME_PREFX = "de.uka.ilkd.key.nui.prooftree.filter.";
 
     /**
      * The data model.
@@ -77,7 +73,9 @@ public class FilteringHandler {
 
     /**
      * Applies the given {@link ProofTreeFilter} to the ProofTree.
-     * @param proofTreeFilter the filter to apply.
+     * 
+     * @param proofTreeFilter
+     *            the filter to apply.
      */
     public void filterBy(final ProofTreeFilter proofTreeFilter) {
         filtersMap.put(proofTreeFilter, true);
@@ -86,6 +84,7 @@ public class FilteringHandler {
 
     /**
      * TODO
+     * 
      * @return
      */
     public DataModel getDataModel() {
@@ -125,7 +124,9 @@ public class FilteringHandler {
 
     /**
      * Stops applying the referenced {@link ProofTreeFilter} to the ProofTree.
-     * @param proofTreeFilter the filter to stop applying.
+     * 
+     * @param proofTreeFilter
+     *            the filter to stop applying.
      */
     public void stopFilteringBy(final ProofTreeFilter proofTreeFilter) {
         filtersMap.remove(proofTreeFilter);
@@ -202,78 +203,133 @@ public class FilteringHandler {
     }
 
     /**
+     * Wrapper class used to read filter rules from the file system.
+     * 
+     * @author Florian Breitfelder
+     * @author Stefan Pilot
+     *
+     */
+    @SuppressWarnings("PMD.AtLeastOneConstructor")
+    private class reflectionWrapper {
+
+        /**
+         * Path where filter classes are stored.
+         */
+        private static final String FILTER_PATH = "filter/";
+        /**
+         * Stores the URLs of all filters.
+         */
+        private transient final List<URL> listOfURLs = new ArrayList<>();
+        /**
+         * Stores the File Names of all filters.
+         */
+        private transient final List<String> listOfFileNames = new ArrayList<>();
+
+        /**
+         * Retrieves the filters URLs and names if KeY is run from a .jar file.
+         * 
+         * @param jarFile
+         *            the .jar file
+         * @return A SimpleImmutableEntry containing a List&lt;URL&gt; and a
+         *         List&lt;String&gt;
+         * @throws IOException
+         *             Rethrow this as a runtime exception.
+         */
+        @SuppressWarnings("PMD.AvoidInstantiatingObjectsInsideLoops")
+        private SimpleImmutableEntry<List<URL>, List<String>> getFilterFilesInJar(final File jarFile)
+                throws IOException {
+            final JarFile jar = new JarFile(jarFile);
+            final Enumeration<JarEntry> entries = jar.entries();
+            jar.close();
+            while (entries.hasMoreElements()) {
+                final String fileName = entries.nextElement().getName();
+
+                if (fileName.matches("(de/uka/ilkd/key/nui/prooftree/filter/).*(.class)")) {
+                    listOfURLs.add(new File(fileName).toURI().toURL());
+                    listOfFileNames.add(
+                            fileName.substring(fileName.lastIndexOf('/') + 1, fileName.length()));
+                }
+            }
+            return new SimpleImmutableEntry<>(listOfURLs, listOfFileNames);
+        }
+
+        /**
+         * Retrieves the filters URLs and names if KeY is built from scratch
+         * (and not to a .jar file)
+         * 
+         * @return A SimpleImmutableEntry containing a List&lt;URL&gt; and a
+         *         List&lt;String&gt;
+         * @throws MalformedURLException
+         *             Rethrow this as a runtime exception.
+         */
+        private SimpleImmutableEntry<List<URL>, List<String>> getFilterFilesInIDE()
+                throws MalformedURLException {
+            // Run with IDE
+            // Look for all class files in PATH and store their urls
+            final File[] files = new File(getClass().getResource(FILTER_PATH).getPath())
+                    .listFiles();
+            for (final File file : files) {
+                if (file.isFile() && file.getName().matches(".*(.class)")) {
+                    final URL urlClassFile = file.toURI().toURL();
+                    listOfURLs.add(urlClassFile);
+                    listOfFileNames.add(file.getName());
+                }
+            }
+            return new SimpleImmutableEntry<>(listOfURLs, listOfFileNames);
+        }
+
+        /**
+         * Retrieves the filters URLs and names.
+         * 
+         * @return A SimpleImmutableEntry containing a List&lt;URL&gt; and a
+         *         List&lt;String&gt;
+         */
+        public SimpleImmutableEntry<List<URL>, List<String>> getFilterFiles() {
+            // path of the jar file
+            final File jarFile = new File(
+                    getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+            try {
+                if (jarFile.isFile()) { // Run with JAR file
+                    return getFilterFilesInJar(jarFile);
+                }
+                return getFilterFilesInIDE();
+            }
+            catch (IOException e) {
+                throw new RuntimeException(
+                        "An IO Exception occured when trying to load filter rules.", e);
+            }
+        }
+    }
+
+    /**
      * Searches for applicable filters in the package
      * de.uka.ilkd.key.nui.prooftree.filter.
      * 
      * @return A list of filters
      */
     private List<ProofTreeFilter> searchFilterClasses() {
+
+        final SimpleImmutableEntry<List<URL>, List<String>> files = new reflectionWrapper()
+                .getFilterFiles();
+
         final List<ProofTreeFilter> filters = new LinkedList<>();
 
-        // path of the jar file
-        final File jarFile = new File(
-                getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+        // Convert listOfURLs to an array of URLs. This array is needed for
+        // the classLoader
+        final URL[] urls = files.getKey().toArray(new URL[] {});
 
-        final ArrayList<URL> listOfURLs = new ArrayList<>();
-        final ArrayList<String> listOfFileNames = new ArrayList<>();
+        // initialize classLoader
+        final URLClassLoader classLoader = new URLClassLoader(urls);
 
-        if (jarFile.isFile()) { // Run with JAR file
-            try (JarFile jar = new JarFile(jarFile)) {
-                final Enumeration<JarEntry> entries = jar.entries();
-                while (entries.hasMoreElements()) {
-                    final String fileName = entries.nextElement().getName();
+        for (final String fileName : files.getValue()) {
+            // build binaryClassName to load class with classLoader
+            final String binaryClassName = BIN_NAME_PREFX
+                    + fileName.substring(0, fileName.lastIndexOf('.'));
 
-                    if (fileName.matches("(de/uka/ilkd/key/nui/prooftree/filter/).*(.class)")) {
-                        listOfURLs.add(new File(fileName).toURI().toURL());
-                        listOfFileNames.add(fileName.substring(fileName.lastIndexOf('/') + 1,
-                                fileName.length()));
-                    }
-                }
-            }
-            catch (IOException e) {
-
-                // TODO Auto-generated catch block
-                // TODO maybe we should throw a RuntimeException
-                e.printStackTrace();
-            }
-        }
-        else {// Run with IDE
-              // Look for all class files in PATH and store their urls
-            final File[] files = new File(getClass().getResource(FILTER_PATH).getPath())
-                    .listFiles();
-
-            for (final File file : files) {
-                if (file.isFile() && file.getName().matches(".*(.class)")) {
-                    try {
-                        final URL urlClassFile = file.toURI().toURL();
-                        listOfURLs.add(urlClassFile);
-                        listOfFileNames.add(file.getName());
-                    }
-                    catch (MalformedURLException e) {
-                        // TODO Auto-generated catch block
-                        // TODO maybe we should throw a RuntimeException
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        try {
-            // Convert listOfURLs to an array of URLs. This array is needed for
-            // the classLoader
-            final URL[] urls = listOfURLs.toArray(new URL[] {});
-
-            // initialize classLoader
-            final URLClassLoader classLoader = new URLClassLoader(urls);
-
-            for (final String fileName : listOfFileNames) {
-                // build binaryClassName to load class with classLoader
-                final String binaryClassName = BINARY_NAME_PREFIX
-                        + fileName.substring(0, fileName.lastIndexOf('.'));
-
-                // Load possible filter class
-
+            // Load possible filter class
+            try {
                 final Class<?> myClass = classLoader.loadClass(binaryClassName);
+
                 // Load annotations of the class
                 final Annotation[] annotations = myClass
                         .getAnnotationsByType(FilterAnnotation.class);
@@ -287,14 +343,17 @@ public class FilteringHandler {
                      */
                     if (((FilterAnnotation) annotation).isFilter()) {
                         filters.add((ProofTreeFilter) myClass.newInstance());
-
                     }
                 }
             }
+            catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
             classLoader.close();
         }
-        catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-                | IOException e) {
+        catch (IOException e) {
             e.printStackTrace();
         }
 
