f2760b0752bc84809a40f16e3b9eef4023f9ab39
Leonard Goetz <leonard.goetz@stud.tu-darmstadt.de>
2016-03-29 21:00:22 +0200

    Reformatted some code

--- SymbolicExecutionDebugger/src/tests/org.key_project.sed.key.ui.test/src/org/key_project/sed/key/ui/test/testcase/swtbot/SWTBotSymbolicExecutionTreePruneTest.java
+++ SymbolicExecutionDebugger/src/tests/org.key_project.sed.key.ui.test/src/org/key_project/sed/key/ui/test/testcase/swtbot/SWTBotSymbolicExecutionTreePruneTest.java
@@ -33,128 +33,139 @@ import org.key_project.util.test.util.TestUtilsUtil;
 
 /**
  * Tests the update of the Symbolic Execution Tree caused by a proof prune.
+ * 
  * @author Leonard Goetz
  *
  */
 public class SWTBotSymbolicExecutionTreePruneTest extends AbstractKeYDebugTargetTestCase {
-	
-	/**
-	 * Tests the update of the Symbolic Execution Tree caused by a prune.
-	 * @throws Exception
-	 */
-	@Test
-	public void testPruning() throws Exception {
 
-		IKeYDebugTargetTestExecutor executor = new IKeYDebugTargetTestExecutor() {
+   /**
+    * Tests the update of the Symbolic Execution Tree caused by a prune.
+    * 
+    * @throws Exception
+    */
+   @Test
+   public void testPruning() throws Exception {
 
-			@Override
-			public void test(final SWTWorkbenchBot bot, IJavaProject project, IMethod method, String targetName, SWTBotView debugView, SWTBotTree debugTree, final ISEDebugTarget target, ILaunch launch) throws Exception {
-				// resume on thread
-				TestUtilsUtil.closeView(ExecutionTreeView.VIEW_ID);
-				SWTBotTreeItem item = TestSedCoreUtil.selectInDebugTree(debugView, 0, 0, 0);
-				resume(bot, item, target);
+      IKeYDebugTargetTestExecutor executor = new IKeYDebugTargetTestExecutor() {
 
-				// Wait until diagram is completely constructed
-				TestUtilsUtil.openView(ExecutionTreeView.VIEW_ID);
-				TestUtilsUtil.waitForJobs();
+         @Override
+         public void test(final SWTWorkbenchBot bot, IJavaProject project, IMethod method, String targetName, SWTBotView debugView, SWTBotTree debugTree, final ISEDebugTarget target, ILaunch launch) throws Exception {
+            // resume on thread
+            TestUtilsUtil.closeView(ExecutionTreeView.VIEW_ID);
+            SWTBotTreeItem item = TestSedCoreUtil.selectInDebugTree(debugView, 0, 0, 0);
+            resume(bot, item, target);
 
-				// path to the test files
-				String pathToOracleFiles = "data/number/oracle";
-				
-				// test diagram before prune
-				assertDiagram(bot, project.getProject(), "Number.set", "data/number/oracle", null);
-				
-				SWTBotView manualView = bot.viewById(ManualView.VIEW_ID);
-				SWTBotTree tree = manualView.bot().tree();
-				
-				// prune branch node
-				TestUtilsUtil.selectInTree(tree, "Null Reference (n = null)");
-				TestUtilsUtil.clickContextMenu(tree, "Prune Proof");
-				TestSedCoreUtil.waitForDebugTreeInterface();
-				TestUtilsUtil.sleep(10000); // TODO need a better solution
-				// test diagram after prune
-				assertDiagram(bot, project.getProject(), "NumberBranchNode.set", pathToOracleFiles, null);
-				
-				// prune node
-				TestUtilsUtil.selectInTree(tree, "10:if (this.content==n.content) {                         return  true; }                 else  {                         return  false; }");
-				TestUtilsUtil.clickContextMenu(tree, "Prune Proof");
-				TestSedCoreUtil.waitForDebugTreeInterface();
-				TestUtilsUtil.sleep(10000); // TODO need a better solution
-				// test diagram after prune
-				assertDiagram(bot, project.getProject(), "NumberNode.set", pathToOracleFiles, null);
-				
-				// resume on thread
-				final SWTBotTreeItem item2 = TestSedCoreUtil.selectInDebugTree(debugView, 0, 0, 0);
-				DebugTargetResumeSuspendListener.run(bot, target, true, new Runnable() {
-					@Override
-					public void run() {
-						resume(bot, item2, target);
-					}
-				});
-				TestUtilsUtil.sleep(10000); // TODO need a better solution
-				// test diagram after resume
-				assertDiagram(bot, project.getProject(), "NumberResume.set", pathToOracleFiles, null);
-				
-			}
+            // Wait until diagram is completely constructed
+            TestUtilsUtil.openView(ExecutionTreeView.VIEW_ID);
+            TestUtilsUtil.waitForJobs();
 
-			@Override
-			public void configureDebugPerspective(SWTWorkbenchBot bot, IPerspectiveDescriptor debugPerspective) throws Exception {
-				TestUtilsUtil.openView(ManualView.VIEW_ID);
-			}
+            // path to the test files
+            String pathToOracleFiles = "data/number/oracle";
 
-			@Override
-			public void cleanupDebugPerspective(SWTWorkbenchBot bot, IPerspectiveDescriptor debugPerspective) throws Exception {
-				if (TestUtilsUtil.findView(ManualView.VIEW_ID) != null) {
-					TestUtilsUtil.closeView(ManualView.VIEW_ID);
-				}
-			}
-		};
+            // test diagram before prune
+            assertDiagram(bot, project.getProject(), "Number.set", "data/number/oracle", null);
 
-		doKeYDebugTargetTest("SWTBotSymbolicExecutionTreePruneTest_testPruning", Activator.PLUGIN_ID, "data/number/test", false, false, createMethodSelector("Number", "equals", "QNumber;"), null, null, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, 8, executor);
-	}
+            SWTBotView manualView = bot.viewById(ManualView.VIEW_ID);
+            SWTBotTree tree = manualView.bot().tree();
 
-	/**
-	    * Ensures that the current diagram matches the oracle files.
-	    * @param bot The {@link SWTWorkbenchBot} to use.
-	    * @param project The {@link IProject} to which the diagram will be saved to.
-	    * @param pathToSetFile The path to the SET file in the created project.
-	    * @param pathToOracleFiles The path to the oracle files.
-	    * @param fileNameSuffix A file name suffix.
-	    * @throws Exception Occurred Exception
-	    */
-	public static void assertDiagram(SWTWorkbenchBot bot, IProject project, String pathToSetFile, String pathToOracleFiles, String fileNameSuffix) throws Exception {
-		// Open Save diagram wizard
-		SWTBotView executionTreeView = bot.viewById(ExecutionTreeView.VIEW_ID);
-		executionTreeView.setFocus();
-		TestUtilsUtil.getToolbarButtonWithId(executionTreeView, "saveAs").click();
-		// Finish wizard
-		SWTBotShell wizardShell = bot.shell("Save Symbolic Execution Tree Diagram");
-		wizardShell.bot().tree().select(project.getName());
-		final String fileName = "Current" + IOUtil.getFileNameWithoutExtension(pathToSetFile) + (fileNameSuffix != null ? fileNameSuffix : "");
-		wizardShell.bot().text(1).setText(fileName);
-		TestUtilsUtil.clickDirectly(wizardShell.bot().button("Next >"));
-		TestUtilsUtil.clickDirectly(wizardShell.bot().button("Finish"));
-		bot.waitUntil(Conditions.shellCloses(wizardShell));
-		// set true to generate new test files in the oracle dir which is defined by pathToOracleFiles
-		if (false) {
-			// Save oracle files
-			File targetOracleDirectory = new File(pathToOracleFiles);
-			ResourceUtil.copyIntoFileSystem(project.getFile(fileName + ExecutionTreeUtil.DOMAIN_FILE_EXTENSION_WITH_DOT), new File(targetOracleDirectory, fileName + ExecutionTreeUtil.DOMAIN_FILE_EXTENSION_WITH_DOT));
-			ResourceUtil.copyIntoFileSystem(project.getFile(fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT), new File(targetOracleDirectory, fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT));
-		} else {
-			// Read diagram files
-			String expectedDiagramFile = IOUtil.readFrom(BundleUtil.openInputStream(Activator.PLUGIN_ID, pathToOracleFiles + "/" + fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT));
-			String currentDiagramFile = ResourceUtil.readFrom(project.getFile(fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT));
+            // prune branch node
+            TestUtilsUtil.selectInTree(tree, "Null Reference (n = null)");
+            TestUtilsUtil.clickContextMenu(tree, "Prune Proof");
+            TestSedCoreUtil.waitForDebugTreeInterface();
+            TestUtilsUtil.sleep(10000); // TODO need a better solution
+            // test diagram after prune
+            assertDiagram(bot, project.getProject(), "NumberBranchNode.set", pathToOracleFiles, null);
 
-			// delete IDs in both files because they differ
-			expectedDiagramFile = expectedDiagramFile.replaceAll("value=\"_[a-zA-Z0-9\\-]*", "#######");
-			currentDiagramFile = currentDiagramFile.replaceAll("value=\"_[a-zA-Z0-9\\-]*", "#######");
-			
-			// Compare diagram files
-			if (!StringUtil.equalIgnoreWhiteSpace(expectedDiagramFile, currentDiagramFile)) {
-				// Let test fail to have an improved comparison dialog
-				assertEquals(expectedDiagramFile, currentDiagramFile); 
-			}
-		}
-	}
+            // prune node
+            TestUtilsUtil.selectInTree(tree, "10:if (this.content==n.content) {                         return  true; }                 else  {                         return  false; }");
+            TestUtilsUtil.clickContextMenu(tree, "Prune Proof");
+            TestSedCoreUtil.waitForDebugTreeInterface();
+            TestUtilsUtil.sleep(10000); // TODO need a better solution
+            // test diagram after prune
+            assertDiagram(bot, project.getProject(), "NumberNode.set", pathToOracleFiles, null);
+
+            // resume on thread
+            final SWTBotTreeItem item2 = TestSedCoreUtil.selectInDebugTree(debugView, 0, 0, 0);
+            DebugTargetResumeSuspendListener.run(bot, target, true, new Runnable() {
+               @Override
+               public void run() {
+                  resume(bot, item2, target);
+               }
+            });
+            TestUtilsUtil.sleep(10000); // TODO need a better solution
+            // test diagram after resume
+            assertDiagram(bot, project.getProject(), "NumberResume.set", pathToOracleFiles, null);
+
+         }
+
+         @Override
+         public void configureDebugPerspective(SWTWorkbenchBot bot, IPerspectiveDescriptor debugPerspective) throws Exception {
+            TestUtilsUtil.openView(ManualView.VIEW_ID);
+         }
+
+         @Override
+         public void cleanupDebugPerspective(SWTWorkbenchBot bot, IPerspectiveDescriptor debugPerspective) throws Exception {
+            if (TestUtilsUtil.findView(ManualView.VIEW_ID) != null) {
+               TestUtilsUtil.closeView(ManualView.VIEW_ID);
+            }
+         }
+      };
+
+      doKeYDebugTargetTest("SWTBotSymbolicExecutionTreePruneTest_testPruning", Activator.PLUGIN_ID, "data/number/test", false, false, createMethodSelector("Number", "equals", "QNumber;"), null, null, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE,
+            Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.FALSE, Boolean.TRUE, 8, executor);
+   }
+
+   /**
+    * Ensures that the current diagram matches the oracle files.
+    * 
+    * @param bot
+    *           The {@link SWTWorkbenchBot} to use.
+    * @param project
+    *           The {@link IProject} to which the diagram will be saved to.
+    * @param pathToSetFile
+    *           The path to the SET file in the created project.
+    * @param pathToOracleFiles
+    *           The path to the oracle files.
+    * @param fileNameSuffix
+    *           A file name suffix.
+    * @throws Exception
+    *            Occurred Exception
+    */
+   public static void assertDiagram(SWTWorkbenchBot bot, IProject project, String pathToSetFile, String pathToOracleFiles, String fileNameSuffix) throws Exception {
+      // Open Save diagram wizard
+      SWTBotView executionTreeView = bot.viewById(ExecutionTreeView.VIEW_ID);
+      executionTreeView.setFocus();
+      TestUtilsUtil.getToolbarButtonWithId(executionTreeView, "saveAs").click();
+      // Finish wizard
+      SWTBotShell wizardShell = bot.shell("Save Symbolic Execution Tree Diagram");
+      wizardShell.bot().tree().select(project.getName());
+      final String fileName = "Current" + IOUtil.getFileNameWithoutExtension(pathToSetFile) + (fileNameSuffix != null ? fileNameSuffix : "");
+      wizardShell.bot().text(1).setText(fileName);
+      TestUtilsUtil.clickDirectly(wizardShell.bot().button("Next >"));
+      TestUtilsUtil.clickDirectly(wizardShell.bot().button("Finish"));
+      bot.waitUntil(Conditions.shellCloses(wizardShell));
+      // set true to generate new test files in the oracle dir which is
+      // defined by pathToOracleFiles
+      if (false) {
+         // Save oracle files
+         File targetOracleDirectory = new File(pathToOracleFiles);
+         ResourceUtil.copyIntoFileSystem(project.getFile(fileName + ExecutionTreeUtil.DOMAIN_FILE_EXTENSION_WITH_DOT), new File(targetOracleDirectory, fileName + ExecutionTreeUtil.DOMAIN_FILE_EXTENSION_WITH_DOT));
+         ResourceUtil.copyIntoFileSystem(project.getFile(fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT), new File(targetOracleDirectory, fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT));
+      } else {
+         // Read diagram files
+         String expectedDiagramFile = IOUtil.readFrom(BundleUtil.openInputStream(Activator.PLUGIN_ID, pathToOracleFiles + "/" + fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT));
+         String currentDiagramFile = ResourceUtil.readFrom(project.getFile(fileName + ExecutionTreeUtil.DIAGRAM_FILE_EXTENSION_WITH_DOT));
+
+         // delete IDs in both files because they differ
+         expectedDiagramFile = expectedDiagramFile.replaceAll("value=\"_[a-zA-Z0-9\\-]*", "#######");
+         currentDiagramFile = currentDiagramFile.replaceAll("value=\"_[a-zA-Z0-9\\-]*", "#######");
+
+         // Compare diagram files
+         if (!StringUtil.equalIgnoreWhiteSpace(expectedDiagramFile, currentDiagramFile)) {
+            // Let test fail to have an improved comparison dialog
+            assertEquals(expectedDiagramFile, currentDiagramFile);
+         }
+      }
+   }
 }
