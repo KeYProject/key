7e20945d252e9f314ace5266c12bac94951b064b
Kai Wallisch <kai.wallisch@ira.uka.de>
2016-06-20 21:10:45 +0200

    [AbstractFeatureStrategy] Declared methods as static, where possible.

--- key/key.core/src/de/uka/ilkd/key/strategy/AbstractFeatureStrategy.java
+++ key/key.core/src/de/uka/ilkd/key/strategy/AbstractFeatureStrategy.java
@@ -66,10 +66,6 @@ import de.uka.ilkd.key.strategy.termfeature.SubTermFeature;
 import de.uka.ilkd.key.strategy.termfeature.TermFeature;
 import de.uka.ilkd.key.strategy.termgenerator.TermGenerator;
 
-
-/**
- *
- */
 public abstract class AbstractFeatureStrategy implements Strategy {
 
     private final Proof proof;
@@ -125,19 +121,19 @@ public abstract class AbstractFeatureStrategy implements Strategy {
         return ConstTermFeature.createConst ( c ( a ) );
     }
 
-    protected TermFeature inftyTermConst() {
+    protected static TermFeature inftyTermConst() {
         return ConstTermFeature.createConst ( infty () );
     }
 
-    protected Feature add (Feature a, Feature b) {
+    protected static Feature add (Feature a, Feature b) {
         return SumFeature.createSum ( a, b );
     }
 
-    protected Feature add (Feature a, Feature b, Feature c) {
+    protected static Feature add (Feature a, Feature b, Feature c) {
         return SumFeature.createSum ( a, b, c );
     }
 
-    protected Feature add (Feature... features) {
+    protected static Feature add (Feature... features) {
         return SumFeature.createSum ( features );
     }
 
@@ -145,7 +141,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
         return BinarySumTermFeature.createSum ( a, b );
     }
 
-    protected TermFeature add (TermFeature a, TermFeature b, TermFeature c) {
+    protected static TermFeature add (TermFeature a, TermFeature b, TermFeature c) {
         // could be done more efficiently
         return add ( a, add ( b, c ) );
     }
@@ -162,11 +158,11 @@ public abstract class AbstractFeatureStrategy implements Strategy {
         return ifZero ( a, longConst ( 0 ), b );
     }
     
-    protected Feature or(Feature a, Feature b, Feature c) {
+    protected static Feature or(Feature a, Feature b, Feature c) {
         return or ( a, or ( b, c ) );
     }
     
-    protected Feature or(Feature... features) {
+    protected static Feature or(Feature... features) {
         Feature orFeature = inftyConst();
         for (Feature f : features) {
             orFeature = or( orFeature, f);
@@ -174,7 +170,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
         return orFeature;
     }
 
-    protected Feature ifZero (Feature cond, Feature thenFeature) {
+    protected static Feature ifZero (Feature cond, Feature thenFeature) {
         return ShannonFeature.createConditionalBinary ( cond, thenFeature );
     }
 
@@ -196,7 +192,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
                                                             elseFeature );
     }
 
-    protected Feature not(Feature f) {
+    protected static Feature not(Feature f) {
         return ifZero ( f, inftyConst (), longConst ( 0 ) );
     }
     
@@ -206,22 +202,18 @@ public abstract class AbstractFeatureStrategy implements Strategy {
                         longTermConst(0) );
     }
     
-    protected Feature eq(Feature a, Feature b) {
+    protected static Feature eq(Feature a, Feature b) {
         return CompareCostsFeature.eq ( a, b );
     }
     
-    protected Feature less(Feature a, Feature b) {
+    protected static Feature less(Feature a, Feature b) {
         return CompareCostsFeature.less ( a, b );
     }
 
-    protected Feature leq(Feature a, Feature b) {
+    protected static Feature leq(Feature a, Feature b) {
         return CompareCostsFeature.leq ( a, b );
     }
 
-    /**
-     * @param names
-     * @param priority
-     */
     protected Feature ifHeuristics (String[] names, int priority) {
         return ConditionalFeature.createConditional ( getFilterFor ( names ),
                 c ( priority ), c ( 0 ) );
@@ -290,7 +282,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
      * trigger variable of a taclet. If the trigger variable
      * is not instantiated for a particular taclet app, an error will be raised
      */
-    protected ProjectionToTerm instOfTriggerVariable() {
+    protected static ProjectionToTerm instOfTriggerVariable() {
         return new TriggerVariableInstantiationProjection();
     }
 
@@ -300,7 +292,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
      * schema variables <code>schemaVar</code>. If <code>schemaVar</code>
      * is not instantiated for a particular taclet app, an error will be raised
      */
-    protected ProjectionToTerm instOf(String schemaVar) {
+    protected static ProjectionToTerm instOf(String schemaVar) {
         return SVInstantiationProjection.create ( new Name ( schemaVar ), true );
     }
 
@@ -310,37 +302,37 @@ public abstract class AbstractFeatureStrategy implements Strategy {
      * partial and undefined for those taclet applications that do not
      * instantiate <code>schemaVar</code>
      */
-    protected ProjectionToTerm instOfNonStrict(String schemaVar) {
+    protected static ProjectionToTerm instOfNonStrict(String schemaVar) {
         return SVInstantiationProjection.create ( new Name ( schemaVar ), false );
     }
     
-    protected ProjectionToTerm subAt(ProjectionToTerm t, PosInTerm pit) {
+    protected static ProjectionToTerm subAt(ProjectionToTerm t, PosInTerm pit) {
         return SubtermProjection.create ( t, pit );
     }
     
-    protected ProjectionToTerm sub(ProjectionToTerm t, int index) {
+    protected static ProjectionToTerm sub(ProjectionToTerm t, int index) {
         return SubtermProjection.create ( t, PosInTerm.getTopLevel().down ( index ) );
     }
     
-    protected ProjectionToTerm opTerm(Operator op, ProjectionToTerm[] subTerms) {
+    protected static ProjectionToTerm opTerm(Operator op, ProjectionToTerm[] subTerms) {
         return TermConstructionProjection.create ( op, subTerms );
     }
 
-    protected ProjectionToTerm opTerm(Operator op, ProjectionToTerm subTerm) {
+    protected static ProjectionToTerm opTerm(Operator op, ProjectionToTerm subTerm) {
         return opTerm ( op, new ProjectionToTerm[] { subTerm } );
     }
 
-    protected ProjectionToTerm opTerm(Operator op,
+    protected static ProjectionToTerm opTerm(Operator op,
                                       ProjectionToTerm subTerm0,
                                       ProjectionToTerm subTerm1) {
         return opTerm ( op, new ProjectionToTerm[] { subTerm0, subTerm1 } );
     }
 
-    protected Feature isInstantiated(String schemaVar) {
+    protected static Feature isInstantiated(String schemaVar) {
         return InstantiatedSVFeature.create ( new Name ( schemaVar ) );
     }
 
-    protected Feature isTriggerVariableInstantiated() {
+    protected static Feature isTriggerVariableInstantiated() {
         return TriggerVarInstantiatedFeature.INSTANCE;
     }
     
@@ -378,7 +370,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
         return let ( buf, t1, applyTF ( t2, eq ( buf ) ) );
     }
 
-    protected Feature contains(ProjectionToTerm bigTerm,
+    protected static Feature contains(ProjectionToTerm bigTerm,
                                ProjectionToTerm searchedTerm) {
         final TermBuffer buf = new TermBuffer ();
         return let ( buf, searchedTerm,
@@ -386,7 +378,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
                                not ( rec ( any (), not ( eq ( buf ) ) ) ) ) );
     }
     
-    protected Feature println(ProjectionToTerm t) {
+    protected static Feature println(ProjectionToTerm t) {
         return applyTF ( t, PrintTermFeature.INSTANCE );
     }
     
@@ -400,7 +392,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
      * error of <code>schemaVar</code> is not instantiated for a particular
      * taclet app
      */
-    protected Feature applyTF(String schemaVar, TermFeature tf) {
+    protected static Feature applyTF(String schemaVar, TermFeature tf) {
         return applyTF ( instOf ( schemaVar ), tf );
     }
 
@@ -410,7 +402,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
      * simply returns zero if <code>schemaVar</code> is not instantiated for a
      * particular taclet app
      */
-    protected Feature applyTFNonStrict(String schemaVar, TermFeature tf) {
+    protected static Feature applyTFNonStrict(String schemaVar, TermFeature tf) {
         return applyTFNonStrict ( instOfNonStrict ( schemaVar ), tf );
     }
 
@@ -428,7 +420,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
      * projection <code>term</code>. If <code>term</code> is undefined for
      * a particular rule app, zero is returned
      */
-    protected Feature applyTFNonStrict(ProjectionToTerm term, TermFeature tf) {
+    protected static Feature applyTFNonStrict(ProjectionToTerm term, TermFeature tf) {
         return ApplyTFFeature.createNonStrict ( term, tf, NumberRuleAppCost.getZeroCost() );
     }
 
@@ -469,7 +461,7 @@ public abstract class AbstractFeatureStrategy implements Strategy {
         return oneOf ( new Feature[] { feature0, feature1 } );
     }
     
-    protected Feature sum(TermBuffer x, TermGenerator gen, Feature body) {
+    protected static Feature sum(TermBuffer x, TermGenerator gen, Feature body) {
         return ComprehendedSumFeature.create ( x, gen, body );
     }
     
@@ -508,11 +500,11 @@ public abstract class AbstractFeatureStrategy implements Strategy {
     }
 
     
-    protected Feature isSubSortFeature(ProjectionToTerm s1, ProjectionToTerm s2) {        
+    protected static Feature isSubSortFeature(ProjectionToTerm s1, ProjectionToTerm s2) {        
         return SortComparisonFeature.create(s1, s2, SortComparisonFeature.SUBSORT);
     }
     
-    protected Feature implicitCastNecessary(ProjectionToTerm s1) {
+    protected static Feature implicitCastNecessary(ProjectionToTerm s1) {
         return ImplicitCastNecessary.create(s1);
     }
     
