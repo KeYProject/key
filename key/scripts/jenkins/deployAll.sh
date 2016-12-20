9e840f287e923a9435637b55a5c2a9e3cb20c5df
Richard Bubel <bubel@pc-bubel.se.informatik.tu-darmstadt.de>
2016-12-20 07:49:13 +0100

    Fixed version numbers in Jenkins build

--- key/scripts/jenkins/deployAll.sh
+++ key/scripts/jenkins/deployAll.sh
@@ -3,6 +3,6 @@ cd key/scripts
 export ANT_HOME=/opt/ant/
 export ANT_OPTS="-Xmx2048m -Xms512m"
 export PATH=$PATH:/home/hudson/key/bin/
-export KEY_VERSION="2.5.$BUILD_NUMBER"
+export KEY_VERSION="2.6.$BUILD_NUMBER"
 ant -logger org.apache.tools.ant.NoBannerLogger deployAll
 
