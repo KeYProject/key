75e04431ea5e97fa54d6e74f14cf2e89b587fc9f
Richard Bubel <bubel@pc-bubel.se.informatik.tu-darmstadt.de>
2016-12-20 16:21:30 +0100

    fix deploy to 2.6

--- key/scripts/jenkins/generateDoc.sh
+++ key/scripts/jenkins/generateDoc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -x
 # API Doc generation to come
-export KEY_VERSION="2.6.$BUILD_NUMBER"
+export KEY_VERSION="2.6.0"
 export ANT_HOME=/opt/ant/
 export ANT_OPTS="-Xmx2048m -Xms512m"
 export PATH=$PATH:/home/hudson/key/bin/
