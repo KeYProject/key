6bf689ca0397078f834973d264bfc2d34d70f3d3
Jonas Schiffl <uvdes@student.kit.edu>
2017-03-20 11:17:30 +0100

    selecting highlight now prints the whole sequent, as intended

--- key/key.ui/src/de/uka/ilkd/key/gui/nodeviews/SequentView.java
+++ key/key.ui/src/de/uka/ilkd/key/gui/nodeviews/SequentView.java
@@ -478,6 +478,7 @@ public abstract class SequentView extends JEditorPane {
 	public void setFilter(SequentPrintFilter sequentPrintFilter) {
 		this.filter = sequentPrintFilter;
 		this.filter.setSequent(getMainWindow().getMediator().getSelectedNode().sequent());
+		printSequent();
 	}
 
 }
