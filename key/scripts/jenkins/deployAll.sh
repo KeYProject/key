94e8a1b0ecc8e9e74005d09e6f9a3f63c46a1d83
Richard Bubel <bubel@cs.tu-darmstadt.de>
2017-10-10 11:16:20 +0200

    Updated version number

--- key/scripts/jenkins/deployAll.sh
+++ key/scripts/jenkins/deployAll.sh
@@ -3,6 +3,6 @@ cd key/scripts
 export ANT_HOME=/opt/ant/
 export ANT_OPTS="-Xmx2048m -Xms512m"
 export PATH=$PATH:/home/hudson/key/bin/
-export KEY_VERSION="2.6.2"
+export KEY_VERSION="2.6.3"
 ant -logger org.apache.tools.ant.NoBannerLogger deployAll
 
