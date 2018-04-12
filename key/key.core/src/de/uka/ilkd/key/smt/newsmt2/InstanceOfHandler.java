8f1ddec16d42a19b63e808286dc1cb7d68e87597
Jonas Schiffl <uvdes@student.kit.edu>
2018-04-12 10:55:53 +0200

    instanceof handler works now

--- key/key.core/src/de/uka/ilkd/key/smt/newsmt2/InstanceOfHandler.java
+++ key/key.core/src/de/uka/ilkd/key/smt/newsmt2/InstanceOfHandler.java
@@ -27,15 +27,13 @@ public class InstanceOfHandler implements SMTHandler {
         SortDependingFunction op = (SortDependingFunction) term.op();
         SExpr inner = trans.translate(term.sub(0));
         if (op.toString().contains("exactInstance")) { // TODO das muss besser
-                                                       // gehen
-            SExpr res = new SExpr("=", Type.BOOL,
-                    new SExpr("typeof", Type.BOOL, inner),
-                    SExpr.sortExpr(op.getSortDependingOn()));
+            // gehen
+            SExpr res = new SExpr("exactinstanceof", Type.BOOL, inner,
+                SExpr.sortExpr(op.getSortDependingOn()));
             return res;
         } else if (op.toString().contains("instance")) { // TODO das auch
-            SExpr res = new SExpr("subtype", Type.BOOL,
-                    new SExpr("typeof", Type.BOOL, inner),
-                    SExpr.sortExpr(op.getSortDependingOn()));
+            SExpr res = new SExpr("instanceof", Type.BOOL, inner,
+                SExpr.sortExpr(op.getSortDependingOn()));
             return res;
         } else {
             return null;
