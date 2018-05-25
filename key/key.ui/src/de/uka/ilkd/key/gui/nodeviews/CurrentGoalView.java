c7e8cf231904831d78dc8fe71dbe4a27d856def7
Mattias Ulbrich <ulbrich@kit.edu>
2018-05-25 21:08:01 +0200

    simplify comparator implementation in CurrentGoalView

--- key/key.ui/src/de/uka/ilkd/key/gui/nodeviews/CurrentGoalView.java
+++ key/key.ui/src/de/uka/ilkd/key/gui/nodeviews/CurrentGoalView.java
@@ -232,13 +232,7 @@ public class CurrentGoalView extends SequentView implements Autoscroll {
                             o1.getFilteredFormula(), 1000);
                     int o2age = computeSeqFormulaAge(getMainWindow().getMediator().getSelectedNode(),
                             o2.getFilteredFormula(), 1000);
-                    if (o1age > o2age) {
-                        return 1;
-                    } else if (o1age < o2age) {
-                        return -1;
-                    } else {
-                        return 0;
-                    }
+                    return o1age - o2age;
                 }
             });
             for (SequentPrintFilterEntry entry : entryList) {
