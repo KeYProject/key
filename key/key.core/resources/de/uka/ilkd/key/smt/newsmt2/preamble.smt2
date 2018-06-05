bfbd1586c1977c83d574cf282a4790d71ddd79ec
Jonas Schiffl <uvdes@student.kit.edu>
2018-06-05 18:04:57 +0200

    fixed store/select errors -- no exceptions now

--- key/key.core/resources/de/uka/ilkd/key/smt/newsmt2/preamble.smt2
+++ key/key.core/resources/de/uka/ilkd/key/smt/newsmt2/preamble.smt2
@@ -38,11 +38,15 @@
 
 (declare-fun wellFormed (sort_heap) Bool)
 
-(declare-fun keyselect (sort_heap U U) U)
-(assert (forall ((h sort_heap) (u2 U) (u3 U)) (subtype (typeof (keyselect h u2 u3)) sort_any)))
-
 (declare-const field_created U)
+(declare-fun keystore (sort_heap U U U) sort_heap)
+(assert (forall ((h sort_heap) (o U) (f U) (v U)) (ite (= f field_created) 
+		(= h (keystore h o f v)) 
+		(= (store h o f v) (keystore h o f v)))))
 
-(assert (forall ((h sort_heap) (o1 U) (f1 U) (v U) (o2 U) (f2 U)) (ite (= f1 field_created)
-		(= (keyselect (store h o1 f1 v) o2 f2) f1)
-		(ite (and (= o1 o2) (= f1 f2)) (= (keyselect (store h o1 f1 v) o2 f2) v) (= (keyselect (store h o1 f1 v) o2 f2) (select h o2 f2))))))
+; (declare-fun keyselect (sort_heap U U) U)
+; (assert (forall ((h sort_heap) (u2 U) (u3 U)) (subtype (typeof (keyselect h u2 u3)) sort_any)))
+
+; (assert (forall ((h sort_heap) (o1 U) (f1 U) (v U) (o2 U) (f2 U)) (ite (= f1 field_created)
+; 		(= (keyselect (store h o1 f1 v) o2 f2) f1)
+; 		(ite (and (= o1 o2) (= f1 f2)) (= (keyselect (store h o1 f1 v) o2 f2) v) (= (keyselect (store h o1 f1 v) o2 f2) (select h o2 f2))))))
--- key/key.core/src/de/uka/ilkd/key/smt/newsmt2/HeapHandler.java
+++ key/key.core/src/de/uka/ilkd/key/smt/newsmt2/HeapHandler.java
@@ -9,7 +9,7 @@ import de.uka.ilkd.key.smt.newsmt2.SExpr.Type;
 
 public class HeapHandler implements SMTHandler {
 
-    public static final String HEAP = "Heap";
+    public static final String HEAP = "heap";
     public static final String WELL_FORMED = "wellFormed";
     public static final String KEYSELECT = "keyselect";
     private Services services;
@@ -21,15 +21,16 @@ public class HeapHandler implements SMTHandler {
 
     @Override
     public boolean canHandle(Term term) {
-        return term.sort().toString().contains(HEAP)
+        return term.op().toString().contains(HEAP)
             || term.toString().contains(WELL_FORMED)
-            || services.getTypeConverter().getHeapLDT().isSelectOp(term.op());
+            || services.getTypeConverter().getHeapLDT().isSelectOp(term.op())
+            || term.op().toString().equals("store");
     }
 
     @Override
     public SExpr handle(MasterHandler trans, Term term) throws SMTTranslationException {
 
-        if (term.sort().toString().contains(HEAP)) {
+        if (term.op().toString().contains(HEAP)) {
             String symbol = term.toString();
             if (!trans.isKnownSymbol(symbol)) {
                 trans.addDeclaration(new SExpr(SExpr.DECLARE_CONST, term.toString(), "sort_heap"));
@@ -42,17 +43,29 @@ public class HeapHandler implements SMTHandler {
             return new SExpr(WELL_FORMED, Type.BOOL, trans.translate(term.sub(0)));
         }
 
+        if (term.op().toString().equals("store")) {
+            SExpr st1 = trans.translate(term.sub(0));
+            SExpr st2 = trans.coerce(trans.translate(term.sub(1)), Type.UNIVERSE);
+            SExpr st3 = trans.coerce(trans.translate(term.sub(2)), Type.UNIVERSE);
+            SExpr st4 = trans.coerce(trans.translate(term.sub(3)), Type.UNIVERSE);
+            return new SExpr("keystore", Type.UNIVERSE, st1, st2, st3, st4);
+        }
+
         SortDependingFunction op = (SortDependingFunction) term.op();
         Sort dep = op.getSortDependingOn();
 
-        SExpr se1 = trans.translate(term.sub(0));
-        SExpr se2 = trans.translate(term.sub(1));
-        SExpr se3 = trans.translate(term.sub(2));
+        if (services.getTypeConverter().getHeapLDT().isSelectOp(op)) {
+            SExpr se1 = trans.translate(term.sub(0));
+            SExpr se2 = trans.translate(term.sub(1));
+            SExpr se3 = trans.translate(term.sub(2));
 
-        if (dep == Sort.ANY) {
-            return new SExpr(KEYSELECT, SExpr.Type.UNIVERSE, se1, se2, se3);
+            if (dep == Sort.ANY) {
+                return new SExpr("select", SExpr.Type.UNIVERSE, se1, se2, se3);
+            }
+
+            return SExpr.castExpr(SExpr.sortExpr(dep), new SExpr("select", Type.UNIVERSE, se1, se2, se3));
         }
 
-        return SExpr.castExpr(SExpr.sortExpr(dep), new SExpr(KEYSELECT, SExpr.Type.UNIVERSE, se1, se2, se3));
+        return new SExpr("unreachable");
     }
 }
