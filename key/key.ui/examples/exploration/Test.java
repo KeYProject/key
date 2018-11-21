ee568af5b177ae10d076787a22fef24e994e80b4
Florian Lanzinger <uiekn@student.kit.edu>
2018-11-21 10:54:01 +0100

    Add "very_simple" method to examples

--- key/key.ui/examples/exploration/Test.java
+++ key/key.ui/examples/exploration/Test.java
@@ -16,4 +16,18 @@ public class Test {
       @ ensures \old(b) == 42;
       @*/
     public void test_arg(int a, int b) { }
+    
+    // This contract contains an intentional mistake.
+    /*@ public normal_behavior
+      @ requires x <= 0;
+      @ ensures \result > 0;
+      @*/
+    public int very_simple(int x){
+        if(x > 0) {
+            x--;
+        } else {
+            x++;
+        }
+        return ++x;
+    }
 }
