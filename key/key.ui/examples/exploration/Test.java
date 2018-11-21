7fa48a670506790a3f8238ef486f83e3a643b3d5
Florian Lanzinger <uiekn@student.kit.edu>
2018-11-21 19:55:52 +0100

    Add mroe examples

--- key/key.ui/examples/exploration/Test.java
+++ key/key.ui/examples/exploration/Test.java
@@ -17,6 +17,32 @@ public class Test {
       @*/
     public void test_arg(int a, int b) { }
     
+    /*@ public normal_behavior
+      @ requires b || !b;
+      @*/
+    public void test2(boolean b) { }
+    
+    /*@ public normal_behavior
+      @ ensures \result == (\sum int i; 0 <= i && i < arr.length; arr[i]);
+      @ assignable \nothing;
+      @*/
+    public int sum(int[] arr) {
+        int i = 0;
+        int result = 0;
+        
+        // The range check (loop_invariant 0 <= i && i <= arr.length) is intentionally missing.
+        /*@ loop_invariant result == (\sum int j; 0 <= j && j < i; arr[i]);
+          @ decreases arr.length - i;
+          @ assignable \nothing;
+          @*/
+        while (i < arr.length) {
+            result += arr[i];
+            ++i;
+        }
+        
+        return result;
+    }
+    
     // This contract contains an intentional mistake.
     /*@ public normal_behavior
       @ requires x <= 0;
