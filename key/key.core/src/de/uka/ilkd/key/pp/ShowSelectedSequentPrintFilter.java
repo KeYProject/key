2813b0b1a177c434b53486d9b9c6bcb3f9991bca
Florian Lanzinger <uiekn@student.kit.edu>
2018-11-28 17:15:10 +0100

    More documentation

--- key/key.core/src/de/uka/ilkd/key/pp/ShowSelectedSequentPrintFilter.java
+++ key/key.core/src/de/uka/ilkd/key/pp/ShowSelectedSequentPrintFilter.java
@@ -3,11 +3,13 @@ package de.uka.ilkd.key.pp;
 import org.key_project.util.collection.ImmutableList;
 import org.key_project.util.collection.ImmutableSLList;
 
+import com.sun.accessibility.internal.resources.accessibility;
+
 import de.uka.ilkd.key.logic.PosInOccurrence;
 import de.uka.ilkd.key.logic.SequentFormula;
 
 /**
- * This filter takes a {@link PosInOccurrence} and only shows the subterm at that position.
+ * This filter takes a {@link PosInOccurrence} and only shows the sub-term at that position.
  * 
  * @author lanzinger
  */
@@ -15,6 +17,11 @@ public class ShowSelectedSequentPrintFilter extends SequentPrintFilter {
     
     private PosInOccurrence pos;
 
+    /**
+     * Create a new {@link ShowSelectedSequentPrintFilter}.
+     * 
+     * @param pos the position of the only sub-term to show.
+     */
     public ShowSelectedSequentPrintFilter(PosInOccurrence pos) {
         this.pos = pos;
     }
@@ -40,6 +47,15 @@ public class ShowSelectedSequentPrintFilter extends SequentPrintFilter {
         }
     }
     
+    /**
+     * An Entry in {@link accessibility} {@link ShowSelectedSequentPrintFilter}.
+     * 
+     * The only entry created for such a filter contains the sub-term at the specified position as
+     * filtered term ({@link #getFilteredFormula()}) and that sub-term's top-level term as
+     * the original ({@link #getOriginalFormula()}).
+     * 
+     * @author lanzinger
+     */
     public static class Entry implements SequentPrintFilterEntry {
         
         private SequentFormula filtered;
