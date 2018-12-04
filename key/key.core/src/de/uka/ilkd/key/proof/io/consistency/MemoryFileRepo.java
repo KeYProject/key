d3dec9abc98662b509b27b5b6fd5997bbca83b48
Wolfram Pfeifer <uvdcn@student.kit.edu>
2018-12-04 07:57:05 +0100

    added stubs of Memory-/TrivialFileRepo to get them into version control

---
+++ key/key.core/src/de/uka/ilkd/key/proof/io/consistency/MemoryFileRepo.java
@@ -0,0 +1,33 @@
+package de.uka.ilkd.key.proof.io.consistency;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+/**
+ * This class uses the memory as a store for the proof-relevant files.
+ *
+ * @author Wolfram Pfeifer
+ */
+public class MemoryFileRepo extends AbstractFileRepo {
+
+    @Override
+    public InputStream getFile(Path path) throws FileNotFoundException, IOException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public OutputStream createOutputStream(Path path) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public InputStream getInputStream(Path p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
---
+++ key/key.core/src/de/uka/ilkd/key/proof/io/consistency/TrivialFileRepo.java
@@ -0,0 +1,36 @@
+package de.uka.ilkd.key.proof.io.consistency;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+/**
+ * This FileRepo does not cache any files but writes to / reads from the original files on disk.
+ * It can be used for recreating the old behavior of KeY without a FileRepo.
+ * @author Wolfram Pfeifer
+ */
+public class TrivialFileRepo extends AbstractFileRepo {
+
+    @Override
+    public InputStream getFile(Path path) throws FileNotFoundException, IOException {
+        files.add(path);
+        return new FileInputStream(path.toFile());
+    }
+
+    @Override
+    public OutputStream createOutputStream(Path path) throws FileNotFoundException {
+        // TODO: create correct zip structure here, or other solution for saving zip
+        files.add(path);
+
+        return new FileOutputStream(path.toFile());
+    }
+
+    @Override
+    public InputStream getInputStream(Path p) throws FileNotFoundException {
+        return new FileInputStream(p.toFile());
+    }
+}
