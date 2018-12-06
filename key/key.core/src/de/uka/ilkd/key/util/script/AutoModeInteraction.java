16da9c49b7c8b8f502072532da551d461f60fae8
Florian Lanzinger <uiekn@student.kit.edu>
2018-12-06 12:56:58 +0100

    Fix Interaction hierarchy

--- key/key.core/src/de/uka/ilkd/key/util/script/AutoModeInteraction.java
+++ key/key.core/src/de/uka/ilkd/key/util/script/AutoModeInteraction.java
@@ -3,7 +3,7 @@ package de.uka.ilkd.key.util.script;
 import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.proof.Node;
 
-public class AutoModeInteraction extends Interaction {
+public class AutoModeInteraction extends NodeInteraction {
     public AutoModeInteraction(Node node) {
         super(node);
     }
