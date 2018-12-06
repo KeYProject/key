56b8b2c3546913bc184a47813a6de9e3b8ee3f6e
Florian Lanzinger <uiekn@student.kit.edu>
2018-12-06 16:16:45 +0100

    Improve interaction output

--- key/key.core/src/de/uka/ilkd/key/util/script/RuleInteraction.java
+++ key/key.core/src/de/uka/ilkd/key/util/script/RuleInteraction.java
@@ -1,14 +1,17 @@
 package de.uka.ilkd.key.util.script;
 
+import java.util.Iterator;
+
+import org.key_project.util.collection.ImmutableMapEntry;
+
 import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.op.SchemaVariable;
+import de.uka.ilkd.key.pp.LogicPrinter;
 import de.uka.ilkd.key.proof.Node;
 import de.uka.ilkd.key.rule.RuleApp;
 import de.uka.ilkd.key.rule.TacletApp;
 import de.uka.ilkd.key.rule.inst.InstantiationEntry;
-import org.key_project.util.collection.ImmutableMapEntry;
-
-import java.util.Iterator;
 
 /**
  * @author weigl
@@ -44,8 +47,8 @@ public final class RuleInteraction extends NodeInteraction {
             Iterator<ImmutableMapEntry<SchemaVariable, InstantiationEntry<?>>> iter = tapp.instantiations().pairIterator();
             while (iter.hasNext()) {
                 ImmutableMapEntry<SchemaVariable, InstantiationEntry<?>> entry = iter.next();
-                String p = "inst_" + entry.key().proofToString();
-                String v = entry.value().getInstantiation().toString();
+                String p = "inst_" + entry.key().toString();
+                String v = LogicPrinter.quickPrintTerm((Term) entry.value().getInstantiation(), services);
                 sb.append("\n    ").append(p).append("=`").append(v).append("`");
             }
 
