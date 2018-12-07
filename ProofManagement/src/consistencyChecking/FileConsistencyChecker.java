88d3f6655a5e3636ea42879e589f1bfb8c262517
Jonas Schiffl <jonas.schiffl@student.kit.edu>
2018-12-07 14:11:31 +0100

    merge function

--- ProofManagement/src/consistencyChecking/FileConsistencyChecker.java
+++ ProofManagement/src/consistencyChecking/FileConsistencyChecker.java
@@ -3,26 +3,65 @@ package consistencyChecking;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.HashSet;
 
 import org.key_project.util.collection.ImmutableList;
+import org.key_project.util.java.IOUtil;
+
 import io.PackageHandler;
 
 public class FileConsistencyChecker {
+    
+    public Path merge(ImmutableList<Path> paths, Path newZProofDir) {
+        if (new SettingsChecker().check(paths) && listOfPathsConsistent(paths)) {
+
+            // only add source files from first package
+            PackageHandler referencePh = new PackageHandler(paths.head());
+
+            try {
+                for (Path sourceFile : referencePh.getClasspathFiles()) {
+                    Path relativePath = paths.head().relativize(sourceFile);
+                    Path newAbsolutePath = newZProofDir.resolve(relativePath);
+                    Files.copy(sourceFile, newAbsolutePath);
+                }
+
+                // add proof and key files from all packages
+                for (Path p : paths) {
+                    PackageHandler ph = new PackageHandler(p);
+
+                    for (Path proofFilePath : ph.getProofFiles()) {
+                        Path relativePath = p.relativize(proofFilePath);
+                        Path newAbsolutePath = newZProofDir.resolve(relativePath);
+                        Files.copy(proofFilePath, newAbsolutePath);
+                    }
+                    for (Path keyFilePath : ph.getKeYFiles()) {
+                        Path relativePath = p.relativize(keyFilePath);
+                        Path newAbsolutePath = newZProofDir.resolve(relativePath);
+                        Files.copy(keyFilePath, newAbsolutePath);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return newZProofDir;
+    }
     
     private boolean listOfPathsConsistent(ImmutableList<Path> paths) {
         boolean res = true;
         Path reference = paths.head();
         for (Path p : paths.tail()) {
-            res &= packagesConsistent(p,  reference);
+            res &= classpathsConsistent(p,  reference);
         }
         return res;
     }
     
-    private boolean packagesConsistent(Path pathA, Path pathB) {
+    private boolean classpathsConsistent(Path pathA, Path pathB) {
         PackageHandler pa = new PackageHandler(pathA);
         PackageHandler pb = new PackageHandler(pathB);
         ImmutableList<Path> classpathFilesA = null;
@@ -50,8 +89,12 @@ public class FileConsistencyChecker {
             e.printStackTrace();
         }
         
-//        for (Path p : )
-        return false;
+        for (Path p : mapA.keySet()) {
+            if (!mapB.containsKey(p) || !(mapA.get(p) == mapB.get(p))) {
+                return false;
+            }
+        }
+        return true;
     }
     
     public static byte[] createChecksum(String filename) throws Exception {
