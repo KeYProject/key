6ac1dbb34c78db88bcf5ae7d5f5f050b27d5de85
Jonas Schiffl <jonas.schiffl@student.kit.edu>
2018-12-07 11:56:18 +0100

    package consistency check infrastructure

--- ProofManagement/src/consistencyChecking/FileConsistencyChecker.java
+++ ProofManagement/src/consistencyChecking/FileConsistencyChecker.java
@@ -1,17 +1,56 @@
 package consistencyChecking;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
 import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import org.key_project.util.collection.ImmutableList;
+import io.PackageHandler;
 
 public class FileConsistencyChecker {
     
-    private boolean filesConsistent(ImmutableList<Path> paths) {
-        for (Path p : paths) {
-            PackageHandler ph;
+    private boolean listOfPathsConsistent(ImmutableList<Path> paths) {
+        boolean res = true;
+        Path reference = paths.head();
+        for (Path p : paths.tail()) {
+            res &= packagesConsistent(p,  reference);
         }
+        return res;
+    }
+    
+    private boolean packagesConsistent(Path pathA, Path pathB) {
+        PackageHandler pa = new PackageHandler(pathA);
+        PackageHandler pb = new PackageHandler(pathB);
+        ImmutableList<Path> classpathFilesA = null;
+        ImmutableList<Path> classpathFilesB = null;
+        try {
+            classpathFilesA = pa.getClasspathFiles();
+            classpathFilesB = pb.getClasspathFiles();
+            if (classpathFilesA.size() != classpathFilesB.size()) {
+                return false;
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        
+        HashMap<Path, byte[]> mapA= new HashMap<Path, byte[]>();
+        HashMap<Path, byte[]> mapB= new HashMap<Path, byte[]>();
+        try {
+            for (Path p : classpathFilesA) {
+                mapA.put(p, createChecksum(p.getFileName().toString()));
+            }
+            for (Path p : classpathFilesB) {
+                mapB.put(p, createChecksum(p.getFileName().toString()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+//        for (Path p : )
         return false;
     }
     
