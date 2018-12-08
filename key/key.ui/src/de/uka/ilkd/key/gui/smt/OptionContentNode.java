e7a22472154f1c4af955141ef497e354ef1a2da9
Mattias Ulbrich <ulbrich@kit.edu>
2018-12-08 21:08:29 +0100

    authorship, indentation

--- key/key.ui/src/de/uka/ilkd/key/gui/smt/OptionContentNode.java
+++ key/key.ui/src/de/uka/ilkd/key/gui/smt/OptionContentNode.java
@@ -3,19 +3,22 @@ package de.uka.ilkd.key.gui.smt;
 import javax.swing.JComponent;
 import javax.swing.tree.DefaultMutableTreeNode;
 
-public class OptionContentNode extends DefaultMutableTreeNode{
-        private static final long serialVersionUID = 1L;
-        private final JComponent component;
-        
-       
-        public OptionContentNode(String title,JComponent component) {
-                super();
-                this.component = component;
-                this.setUserObject(title);
-        }
+/**
+ * @author Mihai Herda, 2018
+ */
 
-        public JComponent getComponent(){
-                return component;
-        }
-                
+public class OptionContentNode extends DefaultMutableTreeNode{
+    private static final long serialVersionUID = 1L;
+    private final JComponent component;
+
+    public OptionContentNode(String title,JComponent component) {
+        super();
+        this.component = component;
+        this.setUserObject(title);
+    }
+
+    public JComponent getComponent(){
+        return component;
+    }
+
 }
