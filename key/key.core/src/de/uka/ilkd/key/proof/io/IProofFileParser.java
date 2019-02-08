93dfc117d70dafd926464f0491465a625afe066b
Dominic Steinhöfel <steinhoefel@cs.tu-darmstadt.de>
2019-02-08 16:55:51 +0100

    Fixed merge problem

--- key/key.core/src/de/uka/ilkd/key/proof/io/IProofFileParser.java
+++ key/key.core/src/de/uka/ilkd/key/proof/io/IProofFileParser.java
@@ -20,7 +20,7 @@ import de.uka.ilkd.key.parser.KeYParser;
 /**
  * Defines the required which a {@link KeYParser} needs to parse a *.proof file
  * and to apply the rules again.
- * 
+ *
  * @author Martin Hentschel
  */
 public interface IProofFileParser {
@@ -28,14 +28,13 @@ public interface IProofFileParser {
     /**
      * Enumeration of the different syntactic elements occurring in a saved
      * proof tree representation.
-     * 
+     *
      * TODO: ProofSaver should not hardcode ids Enum names should be used
      * instead of rawnames (old proofs should be converted)
-     * 
+     *
      * @author Richard Bubel
      */
     static enum ProofElementID {
-<<<<<<< HEAD
         BRANCH("branch"), RULE("rule"), TERM("term"), FORMULA("formula"), INSTANTIATION(
                 "inst"), ASSUMES_FORMULA_IN_SEQUENT("ifseqformula"), ASSUMES_FORMULA_DIRECT(
                 "ifdirectformula"), RULESET("heur"), BUILT_IN_RULE("builtin"), CONTRACT(
@@ -48,24 +47,6 @@ public interface IProofFileParser {
                 "autoModeTime"), KeY_LOG("keyLog"), KeY_USER("keyUser"), KeY_VERSION(
                 "keyVersion"), KeY_SETTINGS("keySettings"), OPEN_GOAL(
                 "opengoal");
-=======
-        BRANCH("branch"), RULE("rule"), TERM("term"), FORMULA("formula"),
-        INSTANTIATION( "inst"), ASSUMES_FORMULA_IN_SEQUENT("ifseqformula"),
-        ASSUMES_FORMULA_DIRECT( "ifdirectformula"), RULESET("heur"),
-        BUILT_IN_RULE("builtin"), CONTRACT( "contract"),
-        ASSUMES_INST_BUILT_IN("ifInst"), MERGE_ABSTRACTION_PREDICATES(
-        "abstractionPredicates"), MERGE_PREDICATE_ABSTRACTION_LATTICE_TYPE(
-        "latticeType"), MERGE_PROCEDURE("mergeProc"), NUMBER_MERGE_PARTNERS(
-        "nrMergePartners"), MERGE_NODE("mergeNode"), MERGE_ID("mergeId"),
-        MERGE_DIST_FORMULA( "distFormula"), MERGE_USER_CHOICES("userChoices"),
-        USER_INTERACTION( "userinteraction"), PROOF_SCRIPT("proofscript"),
-        NEW_NAMES("newnames"), AUTOMODE_TIME( "autoModeTime"),
-        KeY_LOG("keyLog"), KeY_USER("keyUser"), KeY_VERSION( "keyVersion"),
-        KeY_SETTINGS("keySettings"), OPEN_GOAL( "opengoal"),
-        LAZYSE_SYST_PH("loopHoleSymbStPH"), LAZYSE_PATHC_PH("loopHolePathCPH"),
-        LAZYSE_SYST_INST("loopHoleSymbStInst"),
-        LAZYSE_PATHC_INST("loopHolePathCInst");
->>>>>>> 380cd4455b... Checkstyle fixes
 
         private String rawName;
 
