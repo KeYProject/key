4d1ce65126405312c9192db510f57a1a777b0e51
Jonas Schiffl <jonas.schiffl@kit.edu>
2019-02-28 14:45:28 +0100

    SMT Preparation macro added, with hotkey ctrl+shift+Y

--- key/key.core/src/de/uka/ilkd/key/macros/scripts/SMTPreparationMacro.java
+++
@@ -1,9 +0,0 @@
-package de.uka.ilkd.key.macros.scripts;
-
-/**
- * A macro that performs all simplifications that are necessary in order to perform a translation of the sequent to SMT.
- * These include symbolic execution and heap simplification.
- * @author js
- */
-public class SMTPreparationMacro {
-}
