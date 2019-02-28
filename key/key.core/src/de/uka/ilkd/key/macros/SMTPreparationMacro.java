986ba2d68db92c2c00600815b5b10aacf343df31
Jonas Schiffl <jonas.schiffl@kit.edu>
2019-02-28 14:55:10 +0100

    checkstyle

--- key/key.core/src/de/uka/ilkd/key/macros/SMTPreparationMacro.java
+++ key/key.core/src/de/uka/ilkd/key/macros/SMTPreparationMacro.java
@@ -1,8 +1,8 @@
 package de.uka.ilkd.key.macros;
 
 /**
- * A macro that performs all simplifications that are necessary in order to perform a translation of the sequent to SMT.
- * These include symbolic execution and heap simplification.
+ * A macro that performs all simplifications that are necessary in order to perform a translation
+ * of the sequent to SMT. These include symbolic execution and heap simplification.
  * @author js
  */
 public class SMTPreparationMacro extends SequentialProofMacro {
