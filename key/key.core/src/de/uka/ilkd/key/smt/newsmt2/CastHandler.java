56c766b7a4d99ab9faadc51cbf8473cee5778426
Jonas Schiffl <jonas.schiffl@kit.edu>
2019-03-15 14:37:36 +0100

    Fixed Nullptr in canHandle() method

--- key/key.core/src/de/uka/ilkd/key/smt/newsmt2/CastHandler.java
+++ key/key.core/src/de/uka/ilkd/key/smt/newsmt2/CastHandler.java
@@ -3,6 +3,7 @@ package de.uka.ilkd.key.smt.newsmt2;
 import de.uka.ilkd.key.java.Services;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.TermBuilder;
+import de.uka.ilkd.key.logic.op.Operator;
 import de.uka.ilkd.key.logic.op.SortDependingFunction;
 import de.uka.ilkd.key.smt.SMTTranslationException;
 import de.uka.ilkd.key.smt.newsmt2.SExpr.Type;
@@ -18,6 +19,10 @@ public class CastHandler implements SMTHandler {
 
     @Override
     public boolean canHandle(Term term) {
+        Operator op = term.op();
+        if (!(op instanceof SortDependingFunction)) {
+            return false;
+        }
         SortDependingFunction dep = term.sort().getCastSymbol(services);
         return term.op() instanceof SortDependingFunction && ((SortDependingFunction) (term.op())).isSimilar(dep);
     }
