ecbff806b6fff46774a4fad09fa6f1690ecdf059
Alexander Weigl <weigl@kit.edu>
2019-05-11 01:50:37 +0200

    fixing merge error, wrong folder

--- key/key.core/src/test/java/testcase/proofBundle/complexBundleGeneration/a/lang/System.java
+++
@@ -1,9 +0,0 @@
-package java.lang;
-
-public class System {
-
-    public static final java.io.InputStream in;
-    public static final java.io.PrintStream out;
-    public static final java.io.PrintStream err;
-
-}
