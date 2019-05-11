30700aa5a73c9c4f6b7b622c6fb9f52bcdc94877
Alexander Weigl <weigl@kit.edu>
2019-05-11 02:08:34 +0200

    fixing jenkins scripts

--- key/scripts/jenkins/deployAll.sh
+++ key/scripts/jenkins/deployAll.sh
@@ -9,6 +9,6 @@ fi
 
 mkdir -p key/deployment/
 #debugging for the start
-ls -lR key.ui/build/
+ls -lR key.ui/build/distributions/*.zip key.ui/build/libs/key*exe.jar
 mv key.ui/build/distributions/*.zip key/deployment/
 mv key.ui/build/libs/key*exe.jar key/deployment/
