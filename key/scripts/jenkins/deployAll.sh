e5bdc8f94012d024d04b0feddbc264a835ccaab5
Alexander Weigl <weigl@kit.edu>
2019-05-11 02:05:19 +0200

    add missing files

--- key/scripts/jenkins/deployAll.sh
+++ key/scripts/jenkins/deployAll.sh
@@ -3,10 +3,12 @@ cd key/
 export GRADLE_OPTS="-Dorg.gradle.daemon=false"
 ./gradlew --parallel compileTest :key.ui:shadowJar :key.ui:distZip
 
-mkdir -p key/deployment/
+if [ $? -gt 0 ]; then
+  exit $?
+fi
 
+mkdir -p key/deployment/
 #debugging for the start
 ls -lR key.ui/build/
-
 mv key.ui/build/distributions/*.zip key/deployment/
 mv key.ui/build/libs/key*exe.jar key/deployment/
