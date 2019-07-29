c61ed810498ca786f7d38c75e1ea7ba9d0071831
Michael Kirsten <kirsten@kit.edu>
2019-07-29 16:28:19 +0200

    Pressing the OK button did throw an exception

--- key/key.ui/src/main/java/de/uka/ilkd/key/gui/actions/LicenseAction.java
+++ key/key.ui/src/main/java/de/uka/ilkd/key/gui/actions/LicenseAction.java
@@ -36,6 +36,8 @@ public class LicenseAction extends MainWindowAction {
     public static final String KEY_FALLBACK = (KeYConstants.COPYRIGHT + "\nKeY is protected by the "
             + "GNU General Public License v2");
 
+    private static final long serialVersionUID = 5606343347731759150L;
+
     public LicenseAction(MainWindow mainWindow) {
         super(mainWindow);
         setName("License");
@@ -86,7 +88,7 @@ public class LicenseAction extends MainWindowAction {
         pane.addTab("Third party libraries", createLicenseViewer(readStream(thirdPartyLic, "")));
 
         JButton ok = new JButton("OK");
-        ok.addActionListener(e -> ((JFrame) ((JButton) e.getSource())
+        ok.addActionListener(e -> ((JDialog) ((JButton) e.getSource())
                 .getTopLevelAncestor()).dispose());
         fr.getContentPane().add(ok, BorderLayout.SOUTH);
         fr.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
