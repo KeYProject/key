d337cf847968d9d47751a848b91feb368bc8687f
Wolfram Pfeifer <uvdcn@student.kit.edu>
2020-01-13 19:22:56 +0100

    debug output for String Template

--- ProofManagement/src/main/java/org/key_project/proofmanagement/io/report/Report.java
+++ ProofManagement/src/main/java/org/key_project/proofmanagement/io/report/Report.java
@@ -3,14 +3,20 @@ package org.key_project.proofmanagement.io.report;
 import org.key_project.proofmanagement.check.CheckerData;
 import org.key_project.proofmanagement.check.PathNode;
 import org.stringtemplate.v4.*;
+import org.stringtemplate.v4.misc.ObjectModelAdaptor;
+import org.stringtemplate.v4.misc.STMessage;
+import org.stringtemplate.v4.misc.STNoSuchPropertyException;
+import sun.util.resources.cldr.zh.CalendarData_zh_Hans_HK;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
 
 public class Report {
     private final CheckerData result;
@@ -31,10 +37,48 @@ public class Report {
         Path resPath = Paths.get(url.toURI());
 
         STGroup group = new STRawGroupDir(resPath.toString(), '$', '$');
+        group.registerModelAdaptor(Object.class, new ObjectModelAdaptor() {
+            @Override
+            public synchronized Object getProperty(Interpreter interp, ST self, Object o, Object property, String propertyName) throws STNoSuchPropertyException {
+                Method m = tryGetMethod(o.getClass(), propertyName);
+                if (m != null) {
+                    try {
+                        Object resilt = m.invoke(o);
+                        System.out.println(resilt);
+                        return resilt;
+                    } catch (Exception e) {
+                        throwNoSuchProperty(o.getClass(), propertyName, e);
+                    }
+                }
+
+                return super.getProperty(interp, self, o, property, propertyName);
+            }
+        });
 
         // STGroupFile groupFile = new STGroupFile(resPath.toString());
         //ST st = groupFile.getInstanceOf("document");
         ST st = group.getInstanceOf("report");
+        group.setListener(new STErrorListener() {
+            @Override
+            public void compileTimeError(STMessage msg) {
+                throw new RuntimeException(msg.cause);
+            }
+
+            @Override
+            public void runTimeError(STMessage msg) {
+                throw new RuntimeException(msg.toString(), msg.cause);
+            }
+
+            @Override
+            public void IOError(STMessage msg) {
+                throw new RuntimeException(msg.cause);
+            }
+
+            @Override
+            public void internalError(STMessage msg) {
+                throw new RuntimeException(msg.cause);
+            }
+        });
 
         st.add("title", "test report 2.0");
 
