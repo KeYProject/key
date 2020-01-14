eabce6cd7316580d4218a29e31ebf6a39ffd7ce0
Wolfram Pfeifer <uvdcn@student.kit.edu>
2020-01-14 11:17:21 +0100

    fixed MalformedURLException in Location

--- key/key.core/src/main/java/de/uka/ilkd/key/parser/Location.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/parser/Location.java
@@ -13,6 +13,7 @@
 
 package de.uka.ilkd.key.parser;
 
+import de.uka.ilkd.key.util.MiscTools;
 import org.antlr.runtime.RecognitionException;
 
 import java.net.MalformedURLException;
@@ -57,7 +58,7 @@ public final class Location {
         if (filename != null) {
             // catch-rethrow to avoid to change the interface too much
             try {
-                url = new URL(filename);
+                url = new URL("file:/" + filename);
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
