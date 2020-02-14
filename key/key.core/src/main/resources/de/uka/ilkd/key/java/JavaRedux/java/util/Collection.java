f510affb733ee4a3e6336479fe587c370d009c47
Florian Lanzinger <uiekn@student.kit.edu>
2020-02-14 14:34:34 +0100

    Add some contracts to JavaRedux.java.util

--- key/key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/util/Collection.java
+++ key/key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/util/Collection.java
@@ -5,20 +5,61 @@ package java.util;
 
 public interface Collection extends java.lang.Iterable
 {
+    
+   //@ public instance ghost \seq seq;
 
+   /*@ public normal_behavior
+     @ ensures \result == seq.length;
+     @ assignable \nothing;
+     @ determines \result \by seq.length;
+     @ */
    public int size();
+   
+   /*@ public normal_behavior
+     @ ensures \result == (size() == 0);
+     @ assignable \nothing;
+     @ determines \result \by seq.length;
+     @*/
    public boolean isEmpty();
-   public boolean contains(java.lang.Object arg0);
-   public java.util.Iterator iterator();
-   public java.lang.Object[] toArray();
-   public java.lang.Object[] toArray(java.lang.Object[] arg0);
+   
+   /*@ public normal_behavior
+     @ ensures seq == \seq_concat(\old(seq), \seq_singleton(arg0));
+     @ assignable seq;
+     @ determines seq \by seq, arg0;
+     @*/
    public boolean add(java.lang.Object arg0);
+   
    public boolean remove(java.lang.Object arg0);
-   public boolean containsAll(java.util.Collection arg0);
+   
+   /*@ public normal_behavior
+     @ ensures seq == \seq_concat(\old(seq), arg0.seq);
+     @ assignable seq;
+     @ determines seq \by seq, arg0.seq;
+     @*/
    public boolean addAll(java.util.Collection arg0);
    public boolean removeAll(java.util.Collection arg0);
    public boolean retainAll(java.util.Collection arg0);
    public void clear();
-   public boolean equals(java.lang.Object arg0);
-   public int hashCode();
+   
+   /*@ public normal_behavior
+     @ ensures \result == (\exists \bigint i; 0 <= i && i < seq.length; ((String)seq[i]) == arg0);
+     @ assignable \nothing;
+     @ determines \result \by seq, arg0;
+     @*/
+   public boolean contains(String arg0);
+   public boolean containsAll(java.util.Collection arg0);
+   
+   /*@ public normal_behavior
+     @ ensures \result.index == 0;
+     @ ensures \result.seq == seq;
+     @ ensures \fresh(\result) && \fresh(\result.*);
+     @ ensures \typeof(\result) == \type(java.util.CollectionIterator);
+     @ assignable \nothing;
+     @ determines \result.seq \by seq;
+     @ determines \result.index \by \nothing;
+     @*/
+   public java.util.Iterator iterator();
+   
+   public java.lang.Object[] toArray();
+   public java.lang.Object[] toArray(java.lang.Object[] arg0);
 }
---
+++ key/key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/util/CollectionIterator.java
@@ -0,0 +1,3 @@
+package java.util;
+
+public final class CollectionIterator implements Iterator { }
