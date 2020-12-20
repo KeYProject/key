9849f7ba35acf4aa12e01450d8c7352c2adc0b83
Alexander Weigl <weigl@kit.edu>
2020-12-20 23:35:26 +0100

    change settings for gitlab repo

--- key/scripts/mvndeploy/deploy.sh
+++ key/scripts/mvndeploy/deploy.sh
@@ -1,12 +1,14 @@
-DEPLOYURL=https://maven.pkg.github.com/KeYProject/key
+# PROJECT 53  = key-public/key
+DEPLOYURL=https://git.key-project.org/api/v4/projects/35/packages/maven
 
 MVNDEPLOY="mvn -s settings.xml deploy:deploy-file -DrepositoryId=gpr -Durl=$DEPLOYURL"
 
 
 # recoderkey
-# $MVNDEPLOY -DpomFile=recoderKey.pom.xml -Dfile=../../key.core/lib/recoderkey.jar
+$MVNDEPLOY -DpomFile=recoderKey.pom.xml -Dfile=../../key.core/lib/recoderkey.jar
 
 # docking-frames-core
 #$MVNDEPLOY -DpomFile=docking-frames-core.pom.xml -Dfile=../../key.ui/lib/docking-frames-core.jar
 
-$MVNDEPLOY -DpomFile=docking-frames-common.pom.xml -Dfile=../../key.ui/lib/docking-frames-common.jar
+# docking-frames-common
+#$MVNDEPLOY -DpomFile=docking-frames-common.pom.xml -Dfile=../../key.ui/lib/docking-frames-common.jar
