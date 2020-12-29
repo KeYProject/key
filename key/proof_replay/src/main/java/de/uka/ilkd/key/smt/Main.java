6c3dd1ba090835004072d074a5a9ab9d516446f8
Wolfram Pfeifer <uvdcn@student.kit.edu>
2020-12-29 16:22:29 +0100

    code to create some statistics

--- key/proof_replay/src/main/java/de/uka/ilkd/key/smt/Main.java
+++ key/proof_replay/src/main/java/de/uka/ilkd/key/smt/Main.java
@@ -6,27 +6,107 @@ import de.uka.ilkd.key.api.ProofManagementApi;
 import de.uka.ilkd.key.proof.Node;
 import de.uka.ilkd.key.proof.Proof;
 import de.uka.ilkd.key.proof.io.ProblemLoaderException;
+import de.uka.ilkd.key.proof.io.ProofSaver;
 import de.uka.ilkd.key.settings.ProofIndependentSettings;
 import de.uka.ilkd.key.settings.SMTSettings;
-import org.key_project.util.helper.FindResources;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+
+import static java.nio.file.StandardOpenOption.APPEND;
 
 public class Main {
+    private static final Path VALID_LIST_PATH = Paths.get("C:\\Users\\Banach\\Desktop\\MA\\experiments\\valid_list.txt");
+
+    private static final Set<Path> VALID_SET = new HashSet<>();
+
+    private static final List<StatEntry> STATS = new ArrayList<>();
+
+    private static final PrintStream STDOUT = System.out;
+    private static final PrintStream STDERR = System.err;
+
+    private static class StatEntry {
+        final long replayTime;
+        final Path p;
+        final ProofState state;
+
+        StatEntry(long replayTime, Path p, ProofState state) {
+            this.replayTime = replayTime;
+            this.p = p;
+            this.state = state;
+        }
+    }
+
+    private enum ProofState {
+        ERROR,
+        OPEN,
+        CLOSED
+    }
+
     public static void main(String[] args) {
 
+        if (Files.exists(VALID_LIST_PATH)) {
+            run();
+        } else {
+            runFirstTime();
+        }
+    }
+
+    private static void run() {
+        List<String> pathStrings = null;
+        try {
+            pathStrings = Files.readAllLines(VALID_LIST_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        for (String s : pathStrings) {
+            Path p = Paths.get(s);
+            VALID_SET.add(p);
+            processFile(p);
+        }
+        printStatistics();
+    }
+
+    private static void printStatistics() {
+        // print to console
+        System.setOut(STDOUT);
+        System.setErr(STDERR);
+
+        System.out.print("filename");
+        System.out.print("            ");
+        System.out.print("state");
+        System.out.print("            ");
+        System.out.print("replay time");
+        System.out.println();
+
+        for (StatEntry statEntry : STATS) {
+            System.out.print(statEntry.p.getFileName());
+            System.out.print("            ");
+            System.out.print(statEntry.state);
+            System.out.print("            ");
+            System.out.print(statEntry.replayTime);
+            System.out.println();
+        }
+    }
+
+    private static void runFirstTime() {
         //Path exampleDir = FindResources.getExampleDirectory().toPath().toAbsolutePath().normalize();
         List<Path> dirs = new LinkedList<>();
         dirs.add(Paths.get("C:\\Users\\Banach\\Desktop\\KeY\\key\\key\\key.ui\\examples\\newBook\\Using_KeY"));
         dirs.add(Paths.get("C:\\Users\\Banach\\Desktop\\KeY\\key\\key\\key.ui\\examples\\smt"));
         dirs.add(Paths.get("C:\\Users\\Banach\\Desktop\\KeY\\key\\key\\key.ui\\examples\\standard_key"));
         dirs.add(Paths.get("C:\\Users\\Banach\\Desktop\\KeY\\key\\key\\key.ui\\examples\\firstTouch"));
+
+        try {
+            Files.createDirectories(VALID_LIST_PATH.getParent());
+            Files.createFile(VALID_LIST_PATH);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
         try {
             for (Path dir : dirs) {
@@ -41,18 +121,7 @@ public class Main {
                     @Override
                     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
                         System.out.println("Visiting " + file.toString());
-                        if (file.toString().endsWith(".key")) {
-                            try {
-                                System.out.println("Processing " + file.toString());
-                                Path outPath = Paths
-                                    .get("C:\\Users\\Banach\\Desktop\\MA\\experiments\\benchmark\\",
-                                        file.getName(file.getNameCount() - 2) +
-                                            file.getFileName().toString() + "_output.smt2");
-                                runZ3ToFile(file.toFile(), outPath.toFile());
-                            } catch (ProblemLoaderException e) {
-                                e.printStackTrace();
-                            }
-                        }
+                        processFile(file);
                         return FileVisitResult.CONTINUE;
                     }
 
@@ -69,9 +138,28 @@ public class Main {
                     }
                 });
             }
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            // can not continue in a useful manner
+            System.exit(-1);
         } catch (Throwable e) {
             // continue even if an exception is thrown
             e.printStackTrace();
+        }
+    }
+
+    private static void processFile(Path file) {
+        if (file.toString().endsWith(".key")) {
+            try {
+                System.out.println("Processing " + file.toString());
+                Path outPath = Paths
+                    .get("C:\\Users\\Banach\\Desktop\\MA\\experiments\\benchmark\\",
+                        file.getName(file.getNameCount() - 2) +
+                            file.getFileName().toString() + "_output.smt2");
+                runZ3ToFile(file.toFile(), outPath.toFile());
+            } catch (ProblemLoaderException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -93,7 +181,8 @@ public class Main {
         Node n = papi.getFirstOpenGoal().getProofNode();
         Proof proof = n.proof();
 
-        SMTProblem problem = new SMTProblem(n.sequent(), proof.getServices());
+        //SMTProblem problem = new SMTProblem(n.sequent(), proof.getServices());
+        SMTProblem problem = new SMTProblem(proof.openGoals().head());
 
         SMTSettings settings = new SMTSettings(proof.getSettings().getSMTSettings(),
             ProofIndependentSettings.DEFAULT_INSTANCE.getSMTSettings(), proof);
@@ -103,17 +192,24 @@ public class Main {
             @Override
             public void launcherStopped(SolverLauncher launcher,
                                         Collection<SMTSolver> finishedSolvers) {
-                System.out.println("Z3 finished.");
+                System.out.println("Z3 finished (" + finishedSolvers.size() + " solvers).");
                 // we exactly have that single solver
+                if (finishedSolvers.size() != 1) {
+                    return;
+                }
                 SMTSolver z3 = finishedSolvers.iterator().next();
                 String output = z3.getSolverOutput();
 
                 if (z3.getFinalResult().isValid() == SMTSolverResult.ThreeValuedTruth.VALID) {
                     try {
+                        appendValid(input.toPath());
                         Files.write(outPath.toPath(), output.getBytes());
+                        tryReplay(problem, input.toPath(), outPath.toPath());
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    System.setOut(STDOUT);
+                    System.setErr(STDERR);
                 }
             }
 
@@ -125,5 +221,55 @@ public class Main {
             }
         });
         launcher.launch(problem, proof.getServices(), SolverType.Z3_NEW_TL_SOLVER);
+    }
+
+    private static <T> void appendValid(Path keyPath) {
+        try {
+            if (!VALID_SET.contains(keyPath)) {
+                VALID_SET.add(keyPath);
+                Files.write(VALID_LIST_PATH, Collections.singleton(keyPath.toString()), APPEND);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void tryReplay(SMTProblem problem, Path inPath, Path outPath) {
+        try {
+            SMTReplayer replayer = new SMTReplayer(problem);
+
+            // prepare logging stdout to file
+            Path dir = outPath.getParent();
+            Path fileName = outPath.getFileName();
+            String name = fileName.toString().substring(0, fileName.toString().lastIndexOf('.'));
+            Path log = dir.resolve(name + ".log");
+            Path proofPath = dir.resolve(name + ".proof");
+
+            PrintStream printStream = new PrintStream(log.toFile());
+            System.setOut(printStream);
+            System.setErr(printStream);
+
+            long time = System.currentTimeMillis();
+            replayer.replay();
+            Proof proof = replayer.getProof();
+            long timeDiff = System.currentTimeMillis() - time;
+
+            if (proof.closed()) {
+                System.out.println("Proof is closed!");
+
+                ProofSaver saver = new ProofSaver(proof, proofPath.toFile());
+                saver.save();
+
+                STATS.add(new StatEntry(timeDiff, inPath, ProofState.CLOSED));
+            } else {
+                System.out.println("Proof is still open (" + proof.openGoals().size() + " goals)!");
+                STATS.add(new StatEntry(timeDiff, inPath, ProofState.OPEN));
+            }
+
+        } catch (Throwable e) {
+            // error in replay -> log to file
+            e.printStackTrace();
+            STATS.add(new StatEntry(-1, inPath, ProofState.ERROR));
+        }
     }
 }
