e1225ec93ed819f96cd04beb1b00141af3bc4136
Julian Wiesler <wiesleju@gmail.com>
2022-01-10 13:55:51 +0100

    Use a specialized version of RuleContext::getText in TextualJMLAssertStatement::getClauseText inserting spaces between leaves

--- key/key.core/src/main/java/de/uka/ilkd/key/speclang/jml/pretranslation/TextualJMLAssertStatement.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/speclang/jml/pretranslation/TextualJMLAssertStatement.java
@@ -1,8 +1,8 @@
 package de.uka.ilkd.key.speclang.jml.pretranslation;
 
-import org.key_project.util.collection.ImmutableSLList;
-
 import de.uka.ilkd.key.speclang.njml.LabeledParserRuleContext;
+import org.antlr.v4.runtime.RuleContext;
+import org.key_project.util.collection.ImmutableSLList;
 
 /**
  * A JML assert/assume statement.
@@ -22,8 +22,38 @@ public class TextualJMLAssertStatement extends TextualJMLConstruct {
         return context;
     }
 
+    /**
+     * Transforms a rule context to a text representation.
+     * Does the same as `context.getText` but inserts a space between all children of RuleContexts.
+     *
+     * This assumes the following tree layout:
+     * `RuleContext (-> RuleContext)* -> Some leaf`
+     */
+    private static String ruleContextToText(RuleContext context) {
+        if (context.getChildCount() == 0) {
+            return "";
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < context.getChildCount(); i++) {
+            if (i > 0) {
+                builder.append(' ');
+            }
+
+            var child = context.getChild(i);
+            if (child instanceof RuleContext) {
+                builder.append(ruleContextToText((RuleContext) child));
+            } else {
+                builder.append(child.getText());
+            }
+        }
+
+        return builder.toString();
+    }
+
     public String getClauseText() {
-        return context.first.getText().substring(kind.toString().length());
+        var text = ruleContextToText(context.first);
+        return text.substring(kind.toString().length());
     }
 
     public Kind getKind() {
