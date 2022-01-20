f0c61f6700d526721b16ffd9da27e8531973971b
Julian Wiesler <wiesleju@gmail.com>
2022-01-20 18:24:08 +0100

    Add copy parseable to clipyboard to InnerNodeView as well

--- key/key.ui/src/main/java/de/uka/ilkd/key/gui/nodeviews/InnerNodeViewMenu.java
+++ key/key.ui/src/main/java/de/uka/ilkd/key/gui/nodeviews/InnerNodeViewMenu.java
@@ -37,5 +37,6 @@ public class InnerNodeViewMenu extends SequentViewMenu<InnerNodeView> {
         addExtensionMenu();
         addSeparator();
         addClipboardItem(control);
+        addParseableClipboardItem(control);
     }
 }
