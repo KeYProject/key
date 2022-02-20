08fb8549dc75fd251f5009e29c2358c605237ec7
Alexander Weigl <weigl@kit.edu>
2022-02-20 14:15:28 +0100

    add assertions and oracle

---
+++ key/key.java/pipelineTests/simple/expected/06_ClassInitializeMethodBuilder/Test.java
@@ -0,0 +1,110 @@
+public class Test {
+
+    public static int abc;
+
+    static {
+        // should be resolved to 2
+        abc = 1 + 1;
+    }
+
+    public int memberVar;
+
+    {
+        memberVar = 42;
+    }
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classInitializationInProgress;
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classErroneous;
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classInitialized;
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classPrepared;
+
+    static private void $clprepare() {
+    }
+
+    static public void $clinit() {
+        if (!@($classInitialized)) {
+            if (!@($classInitializationInProgress)) {
+                if (!@(this.$classPrepared)) {
+                    @($clprepare());
+                }
+                if (@($classErroneous)) {
+                    throw new java.lang.NoClassDefFoundError();
+                }
+                @($classInitializationInProgress) = true;
+                try {
+                    @(java.lang.Object.$clinit());
+                } catch (java.lang.Error err) {
+                    @($classInitializationInProgress) = false;
+                    @($classErroneous) = true;
+                    throw err;
+                } catch (java.lang.Throwable twa) {
+                    @($classInitializationInProgress) = false;
+                    @($classErroneous) = true;
+                    throw new java.lang.ExceptionInInitializerError(twa);
+                }
+                @($classInitializationInProgress) = false;
+                @($classErroneous) = false;
+                @($classInitialized) = true;
+            }
+        }
+    }
+}
+
+public class SubClass extends Test {
+
+    public int memberVar;
+
+    {
+        memberVar = 41;
+    }
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classInitializationInProgress;
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classErroneous;
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classInitialized;
+
+    @javax.annotation.processing.Generated()
+    static private boolean $classPrepared;
+
+    static private void $clprepare() {
+    }
+
+    static public void $clinit() {
+        if (!@($classInitialized)) {
+            if (!@($classInitializationInProgress)) {
+                if (!@(this.$classPrepared)) {
+                    @($clprepare());
+                }
+                if (@($classErroneous)) {
+                    throw new java.lang.NoClassDefFoundError();
+                }
+                @($classInitializationInProgress) = true;
+                try {
+                    @(Test.$clinit());
+                } catch (java.lang.Error err) {
+                    @($classInitializationInProgress) = false;
+                    @($classErroneous) = true;
+                    throw err;
+                } catch (java.lang.Throwable twa) {
+                    @($classInitializationInProgress) = false;
+                    @($classErroneous) = true;
+                    throw new java.lang.ExceptionInInitializerError(twa);
+                }
+                @($classInitializationInProgress) = false;
+                @($classErroneous) = false;
+                @($classInitialized) = true;
+            }
+        }
+    }
+}
--- key/key.java/src/test/java/KeyJavaPipelineTest.java
+++ key/key.java/src/test/java/KeyJavaPipelineTest.java
@@ -1,17 +1,20 @@
+import com.github.javaparser.Problem;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.key.KeyPassiveExpression;
 import com.github.javaparser.printer.DefaultPrettyPrinter;
 import com.github.javaparser.printer.DefaultPrettyPrinterVisitor;
 import com.github.javaparser.printer.Printer;
-import com.github.javaparser.printer.SourcePrinter;
 import com.github.javaparser.printer.configuration.DefaultPrinterConfiguration;
 import com.github.javaparser.printer.configuration.PrinterConfiguration;
+import com.google.common.truth.Truth;
 import de.uka.ilkd.key.java.JavaService;
 import de.uka.ilkd.key.java.transformations.KeyJavaPipeline;
 import de.uka.ilkd.key.java.transformations.pipeline.JavaTransformer;
 import de.uka.ilkd.key.java.transformations.pipeline.TransformationPipelineServices;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,24 +22,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Stream;
 
 /**
  * @author Alexander Weigl
  * @version 1 (19.02.22)
  */
-public class KeyJavaPipelineTest {
+class KeyJavaPipelineTest {
     public KeyJavaPipeline createPipelineTest(File testFolder) throws FileNotFoundException {
         var js = new JavaService(Collections.singleton(testFolder));
         var inputFolder = new File(testFolder, "input");
         final var jp = js.createJavaParser();
         var files = inputFolder.listFiles();
         var cu = new ArrayList<CompilationUnit>();
-        for (File it : files) {
+        for (File it : Objects.requireNonNull(files)) {
             var r = jp.parse(it);
+            if (!r.isSuccessful()) {
+                for (Problem problem : r.getProblems()) {
+                    System.out.println(problem);
+                }
+                throw new IllegalStateException();
+            }
             var c = r.getResult().get();
             cu.add(c);
         }
@@ -53,17 +61,39 @@ public class KeyJavaPipelineTest {
         return kjp2;
     }
 
-    @Test
-    void simple() throws FileNotFoundException {
-        var pt = createPipelineTest(new File("pipelineTests/simple"));
+    @TestFactory
+    Stream<DynamicTest> simple() throws IOException {
+        return generatePipelineTests(new File("pipelineTests/simple").getAbsoluteFile());
+    }
+
+    private Stream<DynamicTest> generatePipelineTests(File testFolder) throws IOException {
+        var pt = createPipelineTest(testFolder);
         pt.apply();
+        var expected = new File(testFolder, "expected").toPath();
+        var actual = new File(testFolder, "actual").toPath();
+
+        return Files.walk(expected)
+                .filter(Files::isRegularFile)
+                .map(it ->
+                        DynamicTest.dynamicTest(it.toString(), () -> checkEqualFile(it, expected, actual)));
+    }
+
+    private void checkEqualFile(Path expectedFile, Path expectedFolder, Path actualFolder) throws IOException {
+        var actualFile = actualFolder.resolve(expectedFolder.relativize(expectedFile));
+        if (!Files.exists(actualFile)) {
+            Assertions.fail("Actual file " + actualFile + " does not exists.");
+        }
+        var expected = Files.readString(expectedFile);
+        var actual = Files.readString(actualFile);
+
+        Truth.assertThat(actual).isEqualTo(expected);
     }
 
     private static class DebugOutputTransformer extends JavaTransformer {
         final File outputFolder;
         final Set<File> alreadyWritten = new HashSet<>();
         private static final Logger LOGGER = LoggerFactory.getLogger(DebugOutputTransformer.class);
-        private Printer myPrinter = new DefaultPrettyPrinter(
+        private final Printer myPrinter = new DefaultPrettyPrinter(
                 MyPrintVisitor::new,
                 new DefaultPrinterConfiguration());
 
@@ -95,10 +125,6 @@ public class KeyJavaPipelineTest {
     private static class MyPrintVisitor extends DefaultPrettyPrinterVisitor {
         public MyPrintVisitor(PrinterConfiguration configuration) {
             super(configuration);
-        }
-
-        public MyPrintVisitor(PrinterConfiguration configuration, SourcePrinter printer) {
-            super(configuration, printer);
         }
 
         @Override
