04512a8f6a27177fe802ca1ec08c0fc567ccd7ed
Julian Wiesler <wiesleju@gmail.com>
2022-11-24 14:28:15 +0100

    Better spec math mode switching test

--- key/key.core/src/test/resources/testcase/specMath/bigint/Test.java
+++ key/key.core/src/test/resources/testcase/specMath/bigint/Test.java
@@ -8,6 +8,8 @@ public /*@ spec_bigint_math @*/ class Test {
 	  @ }
 	  @*/
 
+	//@ ghost boolean field = Test.acceptInt(1 + 2);
+
 	/*@ model_behaviour
 	  @ requires true;
 	  @ static model boolean acceptBigInt(\bigint v) {
@@ -15,22 +17,30 @@ public /*@ spec_bigint_math @*/ class Test {
 	  @ }
 	  @*/
 
+	public int spacer3;
+
 	/*@ model_behaviour
-	  @ requires true;
-	  @ static model boolean test() {
-	  @ 	return true;
+	  @ requires Test.acceptInt(1 + 2);
+	  @ ensures Test.acceptInt(1 + 2);
+	  @ accessible t[Test.acceptInt(1 + 2) ? 1 : 0..t.length];
+	  @ static spec_java_math model boolean testModel(int[] t) {
+	  @ 	return Test.acceptInt(1 + 2);
 	  @ }
 	  @*/
 
-	// ghost boolean field = Test.acceptBigInt(1 + 2);
+	public int spacer2;
 
 	/*@ model_behaviour
 	  @ requires Test.acceptBigInt(1 + 2);
 	  @ ensures Test.acceptBigInt(1 + 2);
-	  @ static model boolean testModel() {
+	  @ accessible t[Test.acceptBigInt(1 + 2) ? 1 : 0..t.length];
+	  @ static model boolean testBigModel(int[] t) {
 	  @ 	return Test.acceptBigInt(1 + 2);
 	  @ }
 	  @*/
+
+	// Model methods math mode infects methods
+	public int spacer;
 
 	/*@ exceptional_behavior
 	  @ signals (Exception) Test.acceptBigInt(1 + 2);
@@ -43,7 +53,7 @@ public /*@ spec_bigint_math @*/ class Test {
 	  @ determines field \by Test.acceptBigInt(1 + 2);
 	  @*/
 	public void test() {
-		// ghost boolean t = Test.acceptBigInt(1 + 2);
+		// ghost boolean t = Test.acceptBigInt(1 + 2) && \java_math(Test.acceptInt(1 + 2));
 		// set t = Test.acceptBigInt(1 + 2);
 		//@ assert Test.acceptBigInt(1 + 2);
 		/*@ normal_behaviour
@@ -64,6 +74,42 @@ public /*@ spec_bigint_math @*/ class Test {
 		/*@ loop_invariant Test.acceptBigInt(1 + 2);
 	      @ decreases Test.acceptBigInt(1 + 2);
 	      @ determines field \by Test.acceptBigInt(1 + 2);
+	      @*/
+		for (int i = 0; i < 10; i++) {}
+	}
+
+	/*@ exceptional_behavior
+	  @ signals (Exception) Test.acceptInt(1 + 2);
+	  @*/
+	/*@ normal_behaviour
+	  @ requires Test.acceptInt(1 + 2);
+	  @ ensures Test.acceptInt(1 + 2);
+	  @ diverges Test.acceptInt(1 + 2);
+	  @ measured_by Test.acceptInt(1 + 2);
+	  @ determines field \by Test.acceptInt(1 + 2);
+	  @*/
+	public /*@ spec_java_math @*/ void testJavaMath() {
+		// ghost boolean t = Test.acceptInt(1 + 2) && \bigint_math(Test.acceptBigInt(1 + 2));
+		// set t = Test.acceptInt(1 + 2);
+		//@ assert Test.acceptInt(1 + 2);
+		/*@ normal_behaviour
+	      @ requires Test.acceptInt(1 + 2);
+	      @ ensures Test.acceptInt(1 + 2);
+	      @ diverges Test.acceptInt(1 + 2);
+	      @ breaks () Test.acceptInt(1 + 2);
+	      @ continues () Test.acceptInt(1 + 2);
+	      @ determines field \by Test.acceptInt(1 + 2);
+	      @*/
+		{;;}
+
+		/*@ exceptional_behavior
+	      @ returns Test.acceptInt(1 + 2);
+	      @*/
+		{;;}
+
+		/*@ loop_invariant Test.acceptInt(1 + 2);
+	      @ decreases Test.acceptInt(1 + 2);
+	      @ determines field \by Test.acceptInt(1 + 2);
 	      @*/
 		for (int i = 0; i < 10; i++) {}
 	}
