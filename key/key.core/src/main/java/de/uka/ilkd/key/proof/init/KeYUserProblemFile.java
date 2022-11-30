4f8ac27fa90c6e7db9c836daea2c7f618ab101aa
Richard Bubel <bubel@cs.tu-darmstadt.de>
2022-11-30 12:47:01 +0100

    Fix #1702: Allow declaration of function symbols in custom key file and to use them in JML via \dl
    The JML specifications were read before the sort, functions etc declaration. This order is now switched.

--- key/key.core/src/main/java/de/uka/ilkd/key/proof/init/KeYUserProblemFile.java
+++ key/key.core/src/main/java/de/uka/ilkd/key/proof/init/KeYUserProblemFile.java
@@ -96,16 +96,17 @@ public final class KeYUserProblemFile extends KeYFile implements ProofOblInput {
         settings.getChoiceSettings().updateWith(ci.getActivatedChoices());
         initConfig.setActivatedChoices(settings.getChoiceSettings().getDefaultChoicesAsSet());
 
-        // read in-code specifications
         ImmutableSet<PositionedString> warnings = DefaultImmutableSet.nil();
+        // read key file itself
+        ImmutableSet<PositionedString> parent = super.read();
+        warnings = warnings.union(parent);
+
+        // read in-code specifications
         SLEnvInput slEnvInput = new SLEnvInput(readJavaPath(), readClassPath(), readBootClassPath(),
             getProfile(), null);
         slEnvInput.setInitConfig(initConfig);
         warnings = warnings.union(slEnvInput.read());
 
-        // read key file itself
-        ImmutableSet<PositionedString> parent = super.read();
-        warnings = warnings.union(parent);
         return warnings;
     }
 
