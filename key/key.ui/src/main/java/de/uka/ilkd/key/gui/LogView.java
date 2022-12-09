f4ab59ce5faacd34b4828ac7a748b64f3b51c50d
Julian Wiesler <wiesleju@gmail.com>
2022-12-09 15:55:42 +0100

    Force log file format to utf-8 and don't crash on empty lines

--- key/key.ui/src/main/java/de/uka/ilkd/key/gui/LogView.java
+++ key/key.ui/src/main/java/de/uka/ilkd/key/gui/LogView.java
@@ -227,7 +227,7 @@ public class LogView implements KeYGuiExtension, KeYGuiExtension.StatusLine {
             try (var reader = Files.newBufferedReader(logFile)) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    if (line.charAt(0) == '#')
+                    if (line.isEmpty() || line.charAt(0) == '#')
                         continue;
                     String[] fields = line.split("[|]");
                     boolean skipByMsgFilter = msgFilterApply && !fields[5].contains(msgFilter);
