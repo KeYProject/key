0b5198c0e1e23c3da862e2c2e5c899a74fd040f3
Richard Bubel <bubel@cs.tu-darmstadt.de>
2022-12-21 13:49:10 +0100

    Fix synchronization problem in ProgressListenerManager
    Using thread-safe CopyOnWriteList under the assumption that adding and removing listeners is much less frequent than notifying listeners

--- key/recoder/src/main/java/recoder/util/ProgressListenerManager.java
+++ key/recoder/src/main/java/recoder/util/ProgressListenerManager.java
@@ -1,5 +1,7 @@
 package recoder.util;
 
+import java.util.concurrent.CopyOnWriteArrayList;
+
 /**
  * Helper class that handles progress listener registration and broadcasts. Caches and reuses the
  * event object, so listeners should not store the event object.
@@ -11,7 +13,7 @@ public final class ProgressListenerManager {
 
     private final ReuseableProgressEvent progressEvent;
     private final Object source;
-    private ProgressListener[] progressListeners = new ProgressListener[0];
+    private final CopyOnWriteArrayList<ProgressListener> progressListeners = new CopyOnWriteArrayList<>();
 
     public ProgressListenerManager(Object source) {
         this.source = source;
@@ -23,74 +25,49 @@ public final class ProgressListenerManager {
     }
 
     public void fireProgressEvent(int workCount) {
-        int length = progressListeners.length;
-        if (length > 0) {
+        if (!progressListeners.isEmpty()) {
             progressEvent.setWorkDoneCount(workCount);
-            for (int i = 0; i < length; i++) {
-                progressListeners[i].workProgressed(progressEvent);
+            for (var pl : progressListeners) {
+                pl.workProgressed(progressEvent);
             }
         }
     }
 
     public void fireProgressEvent(int workCount, Object state) {
-        int length = progressListeners.length;
-        if (length > 0) {
+        if (!progressListeners.isEmpty()) {
             progressEvent.setWorkDoneCount(workCount);
             progressEvent.setState(state);
-            for (int i = 0; i < length; i++) {
-                progressListeners[i].workProgressed(progressEvent);
+            for (var pl : progressListeners) {
+                pl.workProgressed(progressEvent);
             }
         }
     }
 
     public void fireProgressEvent(int workCount, int newMaximum) {
-        int length = progressListeners.length;
-        if (length > 0) {
+        if (!progressListeners.isEmpty()) {
             progressEvent.setWorkDoneCount(workCount);
             progressEvent.setWorkToDoCount(newMaximum);
-            for (int i = 0; i < length; i++) {
-                progressListeners[i].workProgressed(progressEvent);
+            for (var pl : progressListeners) {
+                pl.workProgressed(progressEvent);
             }
         }
     }
 
     public void fireProgressEvent(int workCount, int newMaximum, Object state) {
-        int length = progressListeners.length;
-        if (length > 0) {
+        if (!progressListeners.isEmpty()) {
             progressEvent.setWork(workCount, newMaximum, state);
-            for (int i = 0; i < length; i++) {
-                progressListeners[i].workProgressed(progressEvent);
+            for (var pl : progressListeners) {
+                pl.workProgressed(progressEvent);
             }
         }
     }
 
     public void addProgressListener(ProgressListener l) {
-        synchronized (progressListeners) {
-            int length = progressListeners.length;
-            ProgressListener[] newListeners = new ProgressListener[length + 1];
-            System.arraycopy(progressListeners, 0, newListeners, 0, length);
-            newListeners[length] = l;
-            progressListeners = newListeners;
-        }
+        progressListeners.addIfAbsent(l);
     }
 
     public void removeProgressListener(ProgressListener chl) {
-        synchronized (progressListeners) {
-            int length = progressListeners.length;
-            for (int i = length - 1; i >= 0; i -= 1) {
-                if (progressListeners[i] == chl) {
-                    ProgressListener[] newListeners = new ProgressListener[length - 1];
-                    if (i > 0) {
-                        System.arraycopy(progressListeners, 0, newListeners, 0, i);
-                    }
-                    if (i < length - 1) {
-                        System.arraycopy(progressListeners, i + 1, newListeners, i, length - 1 - i);
-                    }
-                    progressListeners = newListeners;
-                    break;
-                }
-            }
-        }
+        progressListeners.remove(chl);
     }
 
     private class ReuseableProgressEvent extends ProgressEvent {
