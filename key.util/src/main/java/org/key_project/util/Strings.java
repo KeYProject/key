d1bc0a46aece6489ab928d391de21d1ffa3967cc
Mattias Ulbrich <ulbrich@kit.edu>
2023-06-26 13:11:23 +0200

    [small] merging 2 string util classes

--- key.util/src/main/java/org/key_project/util/Strings.java
+++ key.util/src/main/java/org/key_project/util/Strings.java
@@ -1,7 +1,6 @@
 package org.key_project.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.key_project.util.java.StringUtil;
 
 /**
  * Helper functions for {@link String}s
@@ -11,43 +10,19 @@ import java.util.regex.Pattern;
  */
 public class Strings {
     /**
-     * Checks whether a string contains another one as a whole word (i.e., separated by whitespaces
-     * or a semicolon at the end).
-     *
-     * @param s string to search in
-     * @param word string to be searched for
+     * @deprecated This class has been merged with {@link org.key_project.util.java.StringUtil}.
+     * Call {@link org.key_project.util.java.StringUtil#containsWholeWord(String, String)}
      */
+    @Deprecated
     public static boolean containsWholeWord(String s, String word) {
-        Pattern p = Pattern.compile("\\b" + word + "\\b");
-        Matcher m = p.matcher(s);
-        return m.find();
-        /*
-         * if (s == null || word == null) { return false; } int i = -1; final int wl =
-         * word.length(); while (true) { i = s.indexOf(word, i + 1); if (i < 0 || i >= s.length())
-         * break; if (i == 0 || Character.isWhitespace(s.charAt(i - 1))) { if (i + wl == s.length()
-         * || Character.isWhitespace(s.charAt(i + wl)) || s.charAt(i + wl) == ';') { return true; }
-         * } } return false;
-         */
+        return StringUtil.containsWholeWord(s, word);
     }
 
-
     /**
-     * There are different kinds of JML markers. See Section 4.4 "Annotation markers" of the JML
-     * reference manual.
-     *
-     * @param comment
-     * @return
+     * @deprecated This class has been merged with {@link org.key_project.util.java.StringUtil}.
+     * Call {@link org.key_project.util.java.StringUtil#isJMLComment(String)}
      */
     public static boolean isJMLComment(String comment) {
-        try {
-            return (comment.startsWith("/*@") || comment.startsWith("//@")
-                    || comment.startsWith("/*+KeY@") || comment.startsWith("//+KeY@")
-                    || (comment.startsWith("/*-") && !comment.startsWith("KeY", 3)
-                            && comment.contains("@"))
-                    || (comment.startsWith("//-") && !comment.startsWith("KeY", 3)
-                            && comment.contains("@")));
-        } catch (IndexOutOfBoundsException e) {
-            return false;
-        }
+        return StringUtil.isJMLComment(comment);
     }
 }
--- key.util/src/main/java/org/key_project/util/java/StringUtil.java
+++ key.util/src/main/java/org/key_project/util/java/StringUtil.java
@@ -3,6 +3,7 @@ package org.key_project.util.java;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 import javax.swing.*;
@@ -432,5 +433,45 @@ public final class StringUtil {
      */
     public static String replaceNewlines(String text, String with) {
         return NEWLINE_PATTERN.matcher(text).replaceAll(with);
+    }
+
+    /**
+     * Checks whether a string contains another one as a whole word (i.e., separated by whitespaces
+     * or a semicolon at the end).
+     *
+     * @param s string to search in
+     * @param word string to be searched for
+     */
+    public static boolean containsWholeWord(String s, String word) {
+        Pattern p = Pattern.compile("\\b" + word + "\\b");
+        Matcher m = p.matcher(s);
+        return m.find();
+        /*
+         * if (s == null || word == null) { return false; } int i = -1; final int wl =
+         * word.length(); while (true) { i = s.indexOf(word, i + 1); if (i < 0 || i >= s.length())
+         * break; if (i == 0 || Character.isWhitespace(s.charAt(i - 1))) { if (i + wl == s.length()
+         * || Character.isWhitespace(s.charAt(i + wl)) || s.charAt(i + wl) == ';') { return true; }
+         * } } return false;
+         */
+    }
+
+    /**
+     * There are different kinds of JML markers. See Section 4.4 "Annotation markers" of the JML
+     * reference manual.
+     *
+     * @param comment
+     * @return
+     */
+    public static boolean isJMLComment(String comment) {
+        try {
+            return (comment.startsWith("/*@") || comment.startsWith("//@")
+                    || comment.startsWith("/*+KeY@") || comment.startsWith("//+KeY@")
+                    || (comment.startsWith("/*-") && !comment.startsWith("KeY", 3)
+                    && comment.contains("@"))
+                    || (comment.startsWith("//-") && !comment.startsWith("KeY", 3)
+                    && comment.contains("@")));
+        } catch (IndexOutOfBoundsException e) {
+            return false;
+        }
     }
 }
