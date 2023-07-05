cf97c33b3448eeca8e7e1d34c2bb353ec3006868
Arne Keller <arne.keller@posteo.de>
2023-07-05 15:28:05 +0200

    Stop interface when saving a proof
    This avoids a long UI freeze when saving a proof with cached branches

--- key.ui/src/main/java/de/uka/ilkd/key/gui/WindowUserInterfaceControl.java
+++ key.ui/src/main/java/de/uka/ilkd/key/gui/WindowUserInterfaceControl.java
@@ -357,9 +357,12 @@ public class WindowUserInterfaceControl extends AbstractMediatorUserInterfaceCon
             }
             String errorMsg;
             try {
+                getMediator().stopInterface(true);
                 errorMsg = saver.save();
             } catch (IOException e) {
                 errorMsg = e.toString();
+            } finally {
+                getMediator().startInterface(true);
             }
             if (errorMsg != null) {
                 mainWindow.notify(
