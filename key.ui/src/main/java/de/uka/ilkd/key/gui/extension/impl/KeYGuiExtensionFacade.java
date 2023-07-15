85357af588dc0ca9bfa7326daeab355146f6ffd1
Arne Keller <arne.keller@posteo.de>
2023-07-15 18:24:38 +0200

    Remove dead code

--- key.ui/src/main/java/de/uka/ilkd/key/gui/extension/impl/KeYGuiExtensionFacade.java
+++ key.ui/src/main/java/de/uka/ilkd/key/gui/extension/impl/KeYGuiExtensionFacade.java
@@ -81,18 +81,7 @@ public final class KeYGuiExtensionFacade {
 
     }
 
-    /*
-     * public static Optional<Action> getMainMenuExtensions(String name) {
-     * Spliterator<KeYGuiExtension.MainMenu> iter =
-     * ServiceLoader.load(KeYGuiExtension.MainMenu.class).spliterator(); return
-     * StreamSupport.stream(iter, false) .flatMap(it -> it.getMainMenuActions(mainWindow).stream())
-     * .filter(Objects::nonNull) .filter(it -> it.getValue(Action.NAME).equals(name)) .findAny(); }
-     */
-
     // region Menu Helper
-    private static void sortActionsIntoMenu(List<Action> actions, JMenuBar menuBar) {
-        actions.forEach(act -> sortActionIntoMenu(act, menuBar, new JMenu()));
-    }
 
     private static Iterator<String> getMenuPath(Action act) {
         Object path = act.getValue(KeyAction.PATH);
