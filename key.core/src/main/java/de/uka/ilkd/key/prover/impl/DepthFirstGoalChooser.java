654b48825e8e3390371642126a5aee10ac5f4de3
Wolfram Pfeifer <wolfram.pfeifer@kit.edu>
2023-07-21 00:44:05 +0200

    disabled goal filtering since it interferes with caching and the use is unclear

--- key.core/src/main/java/de/uka/ilkd/key/prover/impl/DepthFirstGoalChooser.java
+++ key.core/src/main/java/de/uka/ilkd/key/prover/impl/DepthFirstGoalChooser.java
@@ -70,7 +70,8 @@ public class DepthFirstGoalChooser extends DefaultGoalChooser {
 
         nextGoals = ImmutableSLList.nil();
 
-        newGoals = newGoals.filter(Goal::isAutomatic);
+        // This line interferes with proof caching. What is this needed for?
+        //newGoals = newGoals.filter(Goal::isAutomatic);
 
         // Remove "node" and goals contained within "newGoals"
         while (!selectedList.isEmpty()) {
