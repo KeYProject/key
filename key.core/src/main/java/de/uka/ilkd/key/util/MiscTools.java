94190c5fda984d3164f6e8f2bb4e1018b0054c7b
Mattias Ulbrich <ulbrich@kit.edu>
2023-08-20 15:13:27 +0200

    spotless: adding a space

--- key.core/src/main/java/de/uka/ilkd/key/util/MiscTools.java
+++ key.core/src/main/java/de/uka/ilkd/key/util/MiscTools.java
@@ -817,7 +817,7 @@ public final class MiscTools {
 
         try {
             URI uri = new URI(source);
-            if(uri.getScheme() != null) {
+            if (uri.getScheme() != null) {
                 // use this URI only if there is an explicit scheme;
                 // otherwise parse it as a filename
                 return uri;
