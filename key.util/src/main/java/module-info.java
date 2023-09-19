75957e2f708a8a10ed5caaed3911bac6304cc571
Drodt <daniel.drodt@tu-darmstadt.de>
2023-09-19 11:46:59 +0200

    Turn key.util into module

---
+++ key.util/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module org.key_project.util {
+    exports org.key_project.util.bean;
+    exports org.key_project.util.collection;
+    exports org.key_project.util.bitops;
+    exports org.key_project.util.helper;
+    exports org.key_project.util.java;
+    exports org.key_project.util.java.thread;
+    exports org.key_project.util.lookup;
+    exports org.key_project.util.reflection;
+    exports org.key_project.util.testcategories;
+    exports org.key_project.util;
+}
