e847ac25fafa2adfefca1bc7a60ab8f74b7b4a5a
Drodt <daniel.drodt@tu-darmstadt.de>
2023-09-19 10:13:32 +0200

    Add LanguageElement

--- key.ncore/src/main/java/module-info.java
+++ key.ncore/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 module org.key_project.core {
 
   /* requires, exports, uses */
-
+   exports org.key_project.logic;
 }
---
+++ key.ncore/src/main/java/org/key_project/logic/LanguageElement.java
@@ -0,0 +1,7 @@
+package org.key_project.logic;
+
+/**
+ * This interface is used for AST elements of target languages, e.g., Java or Rust.
+ */
+public interface LanguageElement {
+}
