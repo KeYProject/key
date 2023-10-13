3ed82bada3d1e1de81979f48fd25344be31756da
Alexander Weigl <weigl@kit.edu>
2023-10-13 13:26:27 +0200

    wip

---
+++ keyext.api/src/main/java/org/keyproject/key/api/remoteclient/MessageType.java
@@ -0,0 +1,31 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package org.keyproject.key.api.remoteclient;
+
+public enum MessageType {
+    Unused,
+    /**
+     * An error message.
+     */
+    Error,
+    /**
+     * A warning message.
+     */
+    Warning,
+    /**
+     * An information message.
+     */
+    Info,
+    /**
+     * A log message.
+     */
+    Log,
+    /**
+     * A debug message.
+     *
+     * @proposed
+     * @since 3.18.0
+     */
+    Debug
+}
---
+++ keyext.api/src/main/java/org/keyproject/key/api/remoteclient/ShowMessageRequestParams.java
@@ -0,0 +1,22 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package org.keyproject.key.api.remoteclient;
+
+public record ShowMessageRequestParams(
+        /**
+         * The message type. See {@link MessageType}
+         */
+        MessageType type,
+
+        /**
+         * The actual message
+         */
+        String message,
+
+        /**
+         * The message action items to present.
+         *
+         */
+        MessageActionItem[] actions) {
+}
