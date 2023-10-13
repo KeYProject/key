ffe4539006c10a450883bd5bb3dc1e50027f3562
Alexander Weigl <weigl@kit.edu>
2023-10-13 13:26:27 +0200

    wip

---
+++ keyext.api/src/main/java/org/keyproject/key/api/remoteclient/LogMessageParams.java
@@ -0,0 +1,17 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package org.keyproject.key.api.remoteclient;
+
+record LogMessageParams(
+        /**
+         * The message type. See {@link MessageType}
+         */
+        MessageType type,
+
+        /**
+         * The actual message
+         */
+        String message) {
+
+}
---
+++ keyext.api/src/main/java/org/keyproject/key/api/remoteclient/ShowDocumentResult.java
@@ -0,0 +1,12 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package org.keyproject.key.api.remoteclient;
+
+public record ShowDocumentResult(
+        /**
+         * A boolean indicating if the show was successful.
+         */
+        boolean success) {
+
+}
