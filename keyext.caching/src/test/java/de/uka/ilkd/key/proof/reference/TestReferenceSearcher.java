3cfd95093787e42be3a0be080d9d40baa901fb72
Arne Keller <arne.keller@posteo.de>
2023-11-02 10:56:07 +0100

    Fix testcase location

--- keyext.caching/src/test/java/de/uka/ilkd/key/proof/reference/TestReferenceSearcher.java
+++ keyext.caching/src/test/java/de/uka/ilkd/key/proof/reference/TestReferenceSearcher.java
@@ -34,11 +34,11 @@ class TestReferenceSearcher {
 
         KeYEnvironment<DefaultUserInterfaceControl> env =
             KeYEnvironment.load(new File(testCaseDirectory,
-                "../key.ui/examples/heap/verifyThis15_1_RelaxedPrefix/relax.proof"));
+                "../../../../../key.ui/examples/heap/verifyThis15_1_RelaxedPrefix/relax.proof"));
         Proof p = env.getLoadedProof();
         KeYEnvironment<DefaultUserInterfaceControl> env2 =
             KeYEnvironment.load(new File(testCaseDirectory,
-                "../key.ui/examples/heap/verifyThis15_1_RelaxedPrefix/relax.proof"));
+                "../../../../../key.ui/examples/heap/verifyThis15_1_RelaxedPrefix/relax.proof"));
         Proof p2 = env2.getLoadedProof();
 
         DefaultListModel<Proof> previousProofs = new DefaultListModel<>();
