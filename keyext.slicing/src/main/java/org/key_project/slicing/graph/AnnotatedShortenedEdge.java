bfcdbca8552a8659cd6a21ec3add60d11e29edec
Arne Keller <arne.keller@posteo.de>
2024-01-11 10:54:21 +0100

    Javadocs

--- keyext.slicing/src/main/java/org/key_project/slicing/graph/AnnotatedShortenedEdge.java
+++ keyext.slicing/src/main/java/org/key_project/slicing/graph/AnnotatedShortenedEdge.java
@@ -1,3 +1,6 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
 package org.key_project.slicing.graph;
 
 import de.uka.ilkd.key.proof.Node;
@@ -9,15 +12,34 @@ import de.uka.ilkd.key.rule.RuleApp;
  * @author Arne Keller
  */
 public class AnnotatedShortenedEdge extends AnnotatedEdge {
+    /**
+     * The initial node in this shortened chain.
+     */
     private Node initial;
+    /**
+     * The last node in this shortened chain.
+     */
     private Node last;
 
+    /**
+     * Create a new shortened edge.
+     *
+     * @param initial the initial node for this shortened chain
+     * @param last the last node of this shortened chain
+     * @param consumesInput whether the input graph node is consumed
+     */
     public AnnotatedShortenedEdge(Node initial, Node last, boolean consumesInput) {
         super(last, consumesInput);
         this.initial = initial;
         this.last = last;
     }
 
+    /**
+     * Get the proper label for this edge.
+     * Will list both the initial and the last node's ruleapp.
+     *
+     * @return label for this edge (to use in DOT export)
+     */
     public String getEdgeLabel() {
         var sb = new StringBuilder();
         RuleApp ruleApp1 = initial.getAppliedRuleApp();
