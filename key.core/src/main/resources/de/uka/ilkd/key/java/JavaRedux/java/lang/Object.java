c6d7323f93ef6b07eb12abbe42f172415e13dc0e
Florian Lanzinger <lanzinger@kit.edu>
2024-02-22 22:12:37 +0100

    Add packed field and havocPacked method to Object class

--- key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/Object.java
+++ key.core/src/main/resources/de/uka/ilkd/key/java/JavaRedux/java/lang/Object.java
@@ -1,7 +1,15 @@
 package java.lang;
 
-public class Object {  
+public class Object {
 
+    //@ public ghost \TYPE packed;
+
+    //TODO Workaround for KeY not supporting set statement for \TYPE variable because Recoder is terrible
+
+    /*@ public normal_behavior
+      @   assignable packed;
+      @*/
+    public void havocPacked();
     
     /*@ public normal_behavior
       @   assignable \nothing;
