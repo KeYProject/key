e7946092c66d81dae1b8d438b9b47deb7bbcad51
Nils Buchholz <nils.buchholz@student.kit.edu>
2024-03-02 22:57:45 +0100

    Added handlers for integers

---
+++ keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IntegerOpHandler.java
@@ -0,0 +1,69 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package de.uka.ilkd.key.gui.isabelletranslation;
+
+import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.ldt.IntegerLDT;
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.op.Operator;
+import de.uka.ilkd.key.smt.SMTTranslationException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * This SMT translation handler takes care of integer expressions.
+ * <p>
+ * This includes the unary and binary integer operations and relational operations.
+ *
+ * @author Jonas Schiffl
+ */
+public class IntegerOpHandler implements IsabelleHandler {
+    private final Map<Operator, String> supportedOperators = new HashMap<>();
+    private IntegerLDT integerLDT;
+
+    @Override
+    public void init(IsabelleMasterHandler masterHandler, Services services, Properties handlerSnippets,
+                     String[] handlerOptions) {
+        supportedOperators.clear();
+        this.integerLDT = services.getTypeConverter().getIntegerLDT();
+
+        supportedOperators.put(integerLDT.getAdd(), "+");
+        supportedOperators.put(integerLDT.getMul(), "*");
+        supportedOperators.put(integerLDT.getSub(), "-");
+        supportedOperators.put(integerLDT.getDiv(), "euclDiv");
+        supportedOperators.put(integerLDT.getMod(), "euclMod");
+        supportedOperators.put(integerLDT.getNeg(), "-");
+
+        supportedOperators.put(integerLDT.getLessOrEquals(), "<=");
+        supportedOperators.put(integerLDT.getLessThan(), "<");
+        supportedOperators.put(integerLDT.getGreaterOrEquals(), ">=");
+        supportedOperators.put(integerLDT.getGreaterThan(), ">");
+
+        //TODO add jArithmetics locale declaration
+    }
+
+    @Override
+    public boolean canHandle(Operator op) {
+        return supportedOperators.containsKey(op);
+    }
+
+    @Override
+    public StringBuilder handle(IsabelleMasterHandler trans, Term term) throws SMTTranslationException {
+        List<StringBuilder> children = trans.translate(term.subs());
+        Operator op = term.op();
+
+        StringBuilder result = new StringBuilder();
+        result.append("((");
+        result.append(supportedOperators.get(op));
+        result.append(")");
+        for (StringBuilder child : children) {
+            result.append(" ").append(child);
+        }
+        result.append(")");
+        return result;
+    }
+}
