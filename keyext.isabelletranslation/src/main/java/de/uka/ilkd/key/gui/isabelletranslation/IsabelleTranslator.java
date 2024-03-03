2876c25d9f09528b18c298683e2461fd469d4937
Nils Buchholz <nils.buchholz@student.kit.edu>
2024-03-03 18:37:17 +0100

    Removed some unused fields and methods in IsabelleTranslator

--- keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslator.java
+++ keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslator.java
@@ -7,7 +7,6 @@ import de.uka.ilkd.key.logic.Sequent;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.op.*;
 import de.uka.ilkd.key.logic.sort.Sort;
-import de.uka.ilkd.key.logic.sort.SortImpl;
 import de.uka.ilkd.key.util.Debug;
 import org.key_project.util.collection.ImmutableArray;
 
@@ -25,16 +24,6 @@ public class IsabelleTranslator {
     private final HashMap<Function, StringBuilder> usedFunctions = new HashMap<>();
 
     private final HashMap<Function, StringBuilder> usedPredicates = new HashMap<>();
-
-    private final HashMap<Sort, StringBuilder> intrinsicSorts = new HashMap<>();
-
-    private final HashMap<Function, StringBuilder> intrinsicFunctions = new HashMap<>();
-
-    private static final StringBuilder INTSTRING = new StringBuilder("Int");
-
-    private static final StringBuilder BOOL = new StringBuilder("bool");
-
-    private static final String GAP = "          ";
 
     private static final StringBuilder FALSESTRING = new StringBuilder("False");
 
@@ -54,47 +43,12 @@ public class IsabelleTranslator {
 
     private static final StringBuilder IMPLYSTRING = new StringBuilder("-->");
 
-    private static final StringBuilder PLUSSTRING = new StringBuilder("+");
-
-    private static final StringBuilder MINUSSTRING = new StringBuilder("-");
-
-    private static final StringBuilder MULTSTRING = new StringBuilder("*");
-
-    private static final StringBuilder DIVSTRING = new StringBuilder("div");
-
-    private static final StringBuilder LTSTRING = new StringBuilder("<");
-
-    private static final StringBuilder GTSTRING = new StringBuilder(">");
-
-    private static final StringBuilder LEQSTRING = new StringBuilder("<=");
-
-    private static final StringBuilder GEQSTRING = new StringBuilder(">=");
-
-    private static final StringBuilder NULLSTRING = new StringBuilder("null");
-
-    private static final StringBuilder NULLSORTSTRING = new StringBuilder("NULLSORT");
-
-    private static final StringBuilder LOGICALIFTHENELSE = new StringBuilder("ite");
-
-    private static final StringBuilder TERMIFTHENELSE = new StringBuilder("ite");
-
-    private static final StringBuilder DISTINCT = new StringBuilder("distinct");
-
-    private static final Sort BOOL_SORT = new SortImpl(new Name("boolean"));
 
     private static final String LINE_ENDING = "\n";
 
     public IsabelleTranslator(Services services) {
         //TODO add intrinsic sorts and functions that shouldnt be overridden
         IntegerLDT integerLDT = services.getTypeConverter().getIntegerLDT();
-
-        intrinsicSorts.put(integerLDT.targetSort(), new StringBuilder("int"));
-        intrinsicSorts.put(BOOL_SORT, new StringBuilder("bool"));
-
-        intrinsicFunctions.put(integerLDT.getAdd(), PLUSSTRING);
-        intrinsicFunctions.put(integerLDT.getSub(), MINUSSTRING);
-        intrinsicFunctions.put(integerLDT.getMul(), MULTSTRING);
-        intrinsicFunctions.put(integerLDT.getDiv(), DIVSTRING);
     }
 
     public final StringBuilder translateProblem(Sequent sequent, Services services) throws IllegalFormulaException {
@@ -110,8 +64,7 @@ public class IsabelleTranslator {
 
         StringBuilder result = new StringBuilder();
         result.append("theory Translation imports Main begin").append(LINE_ENDING);
-
-        result.append(getSortDeclarations());
+        
         for (StringBuilder preamble : masterHandler.getPreambles()) {
             result.append(LINE_ENDING).append(preamble).append(LINE_ENDING);
         }
@@ -207,14 +160,6 @@ public class IsabelleTranslator {
         return result;
     }
 
-    private StringBuilder getFunctionDeclarations() {
-        StringBuilder declarations = new StringBuilder();
-        for (Function fun : usedFunctions.keySet()) {
-            if (!intrinsicFunctions.containsKey(fun))
-                declarations.append(getFunctionDeclaration(fun)).append(LINE_ENDING);
-        }
-        return declarations;
-    }
 
     private StringBuilder getFunctionDeclaration(Function fun) {
         //TODO duplicate handling? Isabelle function handling?
@@ -227,47 +172,6 @@ public class IsabelleTranslator {
         }
         result.append(translateSort(fun.sort())).append("\"");
         return result;
-    }
-
-    private StringBuilder getPredicateDeclarations() {
-        StringBuilder declarations = new StringBuilder();
-        for (Function fun : usedPredicates.keySet()) {
-            declarations.append(getPredicateDeclaration(fun)).append(LINE_ENDING);
-        }
-        return declarations;
-    }
-
-    private StringBuilder getPredicateDeclaration(Function fun) {
-        //TODO duplicate handling? Isabelle function handling?
-        StringBuilder result = new StringBuilder();
-        result.append("fixes ");
-        result.append(usedPredicates.get(fun));
-        result.append(":: \"");
-        for (Sort sort : fun.argSorts()) {
-            result.append(translateSort(sort)).append("=>");
-        }
-        result.append(BOOL).append("\"");
-        return result;
-    }
-
-    private StringBuilder getFreeVariableDeclarations() {
-        //TODO implement
-        return new StringBuilder();
-    }
-
-
-    private StringBuilder getSortDeclarations() {
-        StringBuilder declaration = new StringBuilder();
-        for (Sort sort : usedSorts.keySet()) {
-            if (!intrinsicSorts.containsKey(sort))
-                declaration.append(getSortDeclaration(sort));
-        }
-        return declaration;
-    }
-
-    private StringBuilder getSortDeclaration(Sort sort) {
-        StringBuilder result = new StringBuilder();
-        return result.append("typedecl ").append(usedSorts.get(sort)).append(LINE_ENDING);
     }
 
     private StringBuilder translateTerm(Term term, List<QuantifiableVariable> quantifiedVariables, Services services) throws IllegalFormulaException {
@@ -445,11 +349,5 @@ public class IsabelleTranslator {
     private StringBuilder translateImplication(StringBuilder arg1, StringBuilder arg2) {
         StringBuilder toReturn = new StringBuilder();
         return surroundBrackets(toReturn.append(arg1).append(IMPLYSTRING).append(arg2));
-    }
-
-    protected StringBuilder translateComment(int newLines, String comment) {
-        StringBuilder buffer = new StringBuilder();
-        buffer.append("\n".repeat(Math.max(0, newLines)));
-        return buffer.append(GAP + "; ").append(comment);
     }
 }
