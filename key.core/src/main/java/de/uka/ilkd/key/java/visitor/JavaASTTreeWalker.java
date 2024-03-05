01809fb443b12040218bb3463d824d610f4474a1
Tobias Reinhold <reinhold-tobias@gmx.de>
2024-03-05 22:27:04 +0100

    implement more methods of JavaASTTreeWalker

--- key.core/src/main/java/de/uka/ilkd/key/java/visitor/JavaASTTreeWalker.java
+++ key.core/src/main/java/de/uka/ilkd/key/java/visitor/JavaASTTreeWalker.java
@@ -7,9 +7,25 @@ import de.uka.ilkd.key.java.NonTerminalProgramElement;
 import de.uka.ilkd.key.java.SourceElement;
 
 public class JavaASTTreeWalker implements TreeWalker {
+    /**
+     * The root of the tree that is being walked.
+     */
     private SourceElement root;
 
+    /**
+     * The node of the tree the walker is currently at.
+     */
     private SourceElement currentNode;
+
+    /**
+     * The stack used to store the path from the root to the current node.
+     */
+    private Stack stack;
+
+    /**
+     * The index of the next child of {@code currentNode} to be visited.
+     */
+    private int nextChildToVisitIndex;
 
     @Override
     public SourceElement getRoot() {
@@ -24,7 +40,11 @@ public class JavaASTTreeWalker implements TreeWalker {
     @Override
     public SourceElement firstChild() {
         if (currentNode instanceof NonTerminalProgramElement ntpe && ntpe.getChildCount() > 0) {
+            // The index of the next child of the stored node to be visited is 1, as the first child
+            // at index 0 is visited.
+            stack.push(new NonTerminalProgramElementChildIndexPair(ntpe, 1));
             currentNode = ntpe.getChildAt(0);
+            nextChildToVisitIndex = 0;
             return currentNode;
         }
         return null;
@@ -33,7 +53,13 @@ public class JavaASTTreeWalker implements TreeWalker {
     @Override
     public SourceElement lastChild() {
         if (currentNode instanceof NonTerminalProgramElement ntpe && ntpe.getChildCount() > 0) {
+            // The index of the next child of the stored node to be visited is childCount, as the
+            // last child at index childCount - 1 is visited.
+            // This signals that no more children are left to be visited.
+            stack.push(
+                new NonTerminalProgramElementChildIndexPair(ntpe, ntpe.getChildCount()));
             currentNode = ntpe.getChildAt(ntpe.getChildCount() - 1);
+            nextChildToVisitIndex = 0;
             return currentNode;
         }
         return null;
@@ -51,30 +77,129 @@ public class JavaASTTreeWalker implements TreeWalker {
 
     @Override
     public SourceElement nextSibling() {
+        if (currentNode != root && !stack.empty()) {
+            final NonTerminalProgramElementChildIndexPair parent = stack.peek();
+            final NonTerminalProgramElement parentNode = parent.getNonTerminalProgramElement();
+            final int parentChildIndex = parent.getNextChildToVisitIndex();
+            if (parentChildIndex < parentNode.getChildCount()) {
+                // The index of the next child of the parent on the stack that should be visited is
+                // increased by one
+                parent.setNextChildToVisitIndex(parentChildIndex + 1);
+                currentNode = parentNode.getChildAt(parentChildIndex);
+                nextChildToVisitIndex = 0;
+                return currentNode;
+            }
+        }
         return null;
     }
 
     @Override
     public SourceElement previousSibling() {
+        if (currentNode != root && !stack.empty()) {
+            final NonTerminalProgramElementChildIndexPair parent = stack.peek();
+            final NonTerminalProgramElement parentNode = parent.getNonTerminalProgramElement();
+            final int parentChildIndex = parent.getNextChildToVisitIndex();
+            // parentChildIndex > 1 means that the current node is not the first child of its parent
+            // => it has a previous sibling
+            if (parentChildIndex > 1) {
+                // The index of the next child of the parent on the stack that should be visited is
+                // decreased by one
+                parent.setNextChildToVisitIndex(parentChildIndex - 1);
+                currentNode = parentNode.getChildAt(parentChildIndex - 2);
+                nextChildToVisitIndex = 0;
+                return currentNode;
+            }
+        }
         return null;
     }
 
     @Override
     public SourceElement parentNode() {
-        return null;
+        if (stack.empty()) {
+            return null;
+        }
+        final NonTerminalProgramElementChildIndexPair parent = stack.pop();
+        currentNode = parent.getNonTerminalProgramElement();
+        nextChildToVisitIndex = parent.getNextChildToVisitIndex();
+        return currentNode;
     }
 
-    private record SourceElementChildIndexPair(SourceElement sourceElement, int childIndex) {}
+
+    // ----------------- internal classes for easier handling of the tree -----------------
 
     /**
-     * Class used to store pairs of {@link SourceElement}s and how many children of each have been
-     * already visited.
+     * A pair consisting of a {@link NonTerminalProgramElement} and how many children of it have
+     * already been visited.
      */
-    private class Stack {
+    private static class NonTerminalProgramElementChildIndexPair {
+        /**
+         * The {@link NonTerminalProgramElement} of the pair.
+         */
+        final NonTerminalProgramElement nonTerminalProgramElement;
+
+        /**
+         * The index of the next child of {@code nonTerminalProgramElement} that should to be
+         * visited.
+         */
+        int nextChildToVisitIndex;
+
+        /**
+         * Constructs a new pair with the given {@code nonTerminalProgramElement} and index of the
+         * next child to be visited.
+         *
+         * @param nonTerminalProgramElement the {@link NonTerminalProgramElement} of the pair
+         * @param nextChildToVisitIndex the index of the next child of
+         *        {@code nonTerminalProgramElement} that
+         *        should to be visited
+         */
+        NonTerminalProgramElementChildIndexPair(NonTerminalProgramElement nonTerminalProgramElement,
+                int nextChildToVisitIndex) {
+            this.nonTerminalProgramElement = nonTerminalProgramElement;
+            this.nextChildToVisitIndex = nextChildToVisitIndex;
+        }
+
+        /**
+         * Sets the index of the next child of {@code nonTerminalProgramElement} that should to be
+         * visited.
+         *
+         * @param nextChildToVisitIndex the index of the next child of
+         *        {@code nonTerminalProgramElement} that
+         *        should to be visited
+         */
+        void setNextChildToVisitIndex(int nextChildToVisitIndex) {
+            this.nextChildToVisitIndex = nextChildToVisitIndex;
+        }
+
+        /**
+         * Returns the index of the next child of {@code nonTerminalProgramElement} that should to
+         * be visited.
+         *
+         * @return the index of the next child of {@code nonTerminalProgramElement} that should to
+         *         be visited
+         */
+        int getNextChildToVisitIndex() {
+            return nextChildToVisitIndex;
+        }
+
+        /**
+         * Returns the {@link NonTerminalProgramElement} of the pair.
+         *
+         * @return the {@link NonTerminalProgramElement} of the pair
+         */
+        NonTerminalProgramElement getNonTerminalProgramElement() {
+            return nonTerminalProgramElement;
+        }
+    }
+
+    /**
+     * Class used to store pairs of {@link NonTerminalProgramElement}s and how many children of each
+     * have been already visited.
+     */
+    private static class Stack {
         /**
          * The array that is backing the stack.
          */
-        SourceElementChildIndexPair[] stack;
+        NonTerminalProgramElementChildIndexPair[] stack;
 
         /**
          * The number of elements contained in the stack.
@@ -88,7 +213,7 @@ public class JavaASTTreeWalker implements TreeWalker {
          */
         public Stack() {
             final int standardInitialCapacity = 16;
-            stack = new SourceElementChildIndexPair[standardInitialCapacity];
+            stack = new NonTerminalProgramElementChildIndexPair[standardInitialCapacity];
         }
 
         /**
@@ -96,39 +221,54 @@ public class JavaASTTreeWalker implements TreeWalker {
          *
          * @param element the element to be pushed onto the stack
          */
-        void push(SourceElementChildIndexPair element) {
+        void push(NonTerminalProgramElementChildIndexPair element) {
             if (count >= stack.length) {
                 resizeStack();
             }
             stack[count++] = element;
-            return;
         }
 
         /**
-         * Removes and returns the topmost element of the stack.
+         * Removes and returns the topmost element of the stack or {@code null} if the stack is
+         * empty.
          *
-         * @return the topmost element of the stack
+         * @return the topmost element of the stack or {@code null} if the stack is empty
          */
-        SourceElementChildIndexPair pop() {
+        NonTerminalProgramElementChildIndexPair pop() {
+            if (count == 0) {
+                return null;
+            }
             return stack[--count];
         }
 
         /**
-         * Returns the number of elements currently contained in the stack.
+         * Returns the topmost element of the stack without removing it or {@code null} if the stack
+         * is empty.
          *
-         * @return the number of elements currently contained in the stack
+         * @return the topmost element of the stack or {@code null} if the stack is empty
          */
-        int size() {
-            return count;
+        NonTerminalProgramElementChildIndexPair peek() {
+            if (count == 0) {
+                return null;
+            }
+            return stack[count - 1];
+        }
+
+        /**
+         * Returns whether the stack is empty.
+         *
+         * @return {@code true} if the stack is empty, {@code false} otherwise
+         */
+        boolean empty() {
+            return count == 0;
         }
 
         /**
          * Empties the stack.
-         * <p>
-         * {@code count} is simply set to 0.
          */
-        void reset() {
+        void clear() {
             count = 0;
+            stack = new NonTerminalProgramElementChildIndexPair[stack.length];
         }
 
         /**
@@ -137,8 +277,8 @@ public class JavaASTTreeWalker implements TreeWalker {
          * Currently, the stack size is simply doubled.
          */
         void resizeStack() {
-            SourceElementChildIndexPair[] newStack =
-                new SourceElementChildIndexPair[stack.length * 2];
+            NonTerminalProgramElementChildIndexPair[] newStack =
+                new NonTerminalProgramElementChildIndexPair[stack.length * 2];
             System.arraycopy(stack, 0, newStack, 0, count);
             stack = newStack;
         }
