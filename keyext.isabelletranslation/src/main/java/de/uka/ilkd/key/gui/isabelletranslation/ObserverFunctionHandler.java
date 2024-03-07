f7067b3b0b2f6fd181ecf5cbef24b936a148a510
Nils Buchholz <nils.buchholz@student.kit.edu>
2024-03-07 18:00:30 +0100

    added ObserverFunctionHandler

---
+++ keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/ObserverFunctionHandler.java
@@ -0,0 +1,39 @@
+package de.uka.ilkd.key.gui.isabelletranslation;
+
+import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.op.ObserverFunction;
+import de.uka.ilkd.key.logic.op.Operator;
+import de.uka.ilkd.key.logic.op.SortedOperator;
+import de.uka.ilkd.key.smt.SMTTranslationException;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ObserverFunctionHandler implements IsabelleHandler {
+    @Override
+    public void init(IsabelleMasterHandler masterHandler, Services services, Properties handlerSnippets, String[] handlerOptions) throws IOException {
+
+    }
+
+    @Override
+    public boolean canHandle(Operator op) {
+        return (op instanceof ObserverFunction);
+    }
+
+    @Override
+    public StringBuilder handle(IsabelleMasterHandler trans, Term term) throws SMTTranslationException {
+        if (!trans.isKnownSymbol(term)) {
+            Operator op = term.op();
+            Matcher m = Pattern.compile("<(.*?)>").matcher(op.name().toString());
+            if (!m.find()) {
+                throw new SMTTranslationException("Couldn't translate ObserverFunction: " + op.name().toString());
+            }
+            String functionName = m.group(1);
+            trans.addKnownSymbol(term, new StringBuilder(functionName));
+        }
+        return UninterpretedSymbolsHandler.getFunctionTranslation(trans, term, (SortedOperator) term.op(), trans.getKnownSymbol(term).toString());
+    }
+}
