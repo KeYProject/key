db4921a8eb278e053508b1c88b4e80e3e749de28
Drodt <daniel.drodt@tu-darmstadt.de>
2024-03-14 09:07:21 +0100

    Spotless

--- key.ncore.calculus/src/main/java/org/key_project/prover/rules/SwitchBranchSVWrapper.java
+++ key.ncore.calculus/src/main/java/org/key_project/prover/rules/SwitchBranchSVWrapper.java
@@ -1,6 +1,10 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
 package de.uka.ilkd.key.java.recoderext;
 
 import de.uka.ilkd.key.logic.op.SchemaVariable;
+
 import recoder.java.ProgramElement;
 import recoder.java.SourceVisitor;
 import recoder.java.Statement;
