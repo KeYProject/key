5abe6d1c5251c7bdca8711be73e66cb9d5229b0e
Alexander Weigl <weigl@kit.edu>
2024-03-31 23:03:43 +0200

    add module-info.java

---
+++ key.core.rifl/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Alexander Weigl 
+ * @version 1 (31.03.24)
+ */
+module org.key_project.core.rifl {
+    exports de.uka.ilkd.key.util.rifl;
+    requires org.key_project.core;
+    requires key.recoder;
+    requires java.xml;
+    requires org.slf4j;
+    requires org.key_project.util;
+}
