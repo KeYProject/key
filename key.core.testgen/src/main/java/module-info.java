dc253d7b121357f9914a9348c323de873f03f149
Alexander Weigl <weigl@kit.edu>
2024-04-04 03:15:32 +0200

    add provided services

--- key.core.testgen/src/main/java/module-info.java
+++ key.core.testgen/src/main/java/module-info.java
@@ -1,16 +1,11 @@
+import de.uka.ilkd.key.macros.ProofMacro;
+import de.uka.ilkd.key.testgen.macros.TestGenMacro;
+
 /**
  *
  * @author Alexander Weigl
  * @version 1 (04.04.24)
  */
-module org.key_project.core.testgen {
-    requires org.key_project.core;
-    requires org.slf4j;
-    requires org.key_project.util;
-    requires org.key_project.ncore;
-    requires org.jspecify;
-    exports de.uka.ilkd.key.testgen.smt.testgen;
-    exports de.uka.ilkd.key.testgen.macros;
-    exports de.uka.ilkd.key.testgen.smt.counterexample;
-    exports de.uka.ilkd.key.testgen.settings;
-}
+module org.key_project.core.testgen{requires org.key_project.core;requires org.slf4j;requires org.key_project.util;requires org.key_project.ncore;requires org.jspecify;exports de.uka.ilkd.key.testgen.smt.testgen;exports de.uka.ilkd.key.testgen.macros;exports de.uka.ilkd.key.testgen.smt.counterexample;exports de.uka.ilkd.key.testgen.settings;
+
+provides ProofMacro with TestGenMacro;}
--- keyext.ui.testgen/src/main/java/module-info.java
+++ keyext.ui.testgen/src/main/java/module-info.java
@@ -1,13 +1,10 @@
+import de.uka.ilkd.key.gui.extension.api.KeYGuiExtension;
+
 /**
  *
  * @author Alexander Weigl
  * @version 1 (31.03.24)
  */
-module keyext.ui.testgen {
-    requires java.desktop;
-    requires org.key_project.ui;
-    requires org.slf4j;
-    requires org.key_project.core;
-    requires org.key_project.core.testgen;
-    requires org.jspecify;
-}
+module keyext.ui.testgen{requires java.desktop;requires org.key_project.ui;requires org.slf4j;requires org.key_project.core;requires org.key_project.core.testgen;requires org.jspecify;
+
+provides KeYGuiExtension with de.uka.ilkd.key.gui.testgen.TestgenExtension;}
