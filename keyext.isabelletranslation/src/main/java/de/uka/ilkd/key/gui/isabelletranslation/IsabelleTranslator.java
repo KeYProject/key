a727966e83e9e660614aae34087c0ce8caa62205
BookWood <nils.buchholz@gmail.com>
2024-04-15 18:09:25 +0200

    splitting into preamble and sequent

--- keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslator.java
+++ keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslator.java
@@ -6,6 +6,7 @@ import de.uka.ilkd.key.logic.SequentFormula;
 import de.uka.ilkd.key.logic.Term;
 import de.uka.ilkd.key.logic.sort.ArraySort;
 import de.uka.ilkd.key.logic.sort.Sort;
+import de.uka.ilkd.key.proof.Goal;
 
 import java.io.IOException;
 import java.util.*;
@@ -20,7 +21,8 @@ public class IsabelleTranslator {
         this.services = services;
     }
 
-    public final StringBuilder translateProblem(Sequent sequent) throws IllegalFormulaException {
+    public final IsabelleProblem translateProblem(Goal goal) throws IllegalFormulaException {
+        Sequent sequent = goal.sequent();
         List<Term> antecedents = sequent.antecedent().asList().stream().map(SequentFormula::formula).toList();
         List<Term> succedents = sequent.succedent().asList().stream().map(SequentFormula::formula).toList();
         IsabelleMasterHandler masterHandler;
@@ -41,11 +43,11 @@ public class IsabelleTranslator {
             throw new RuntimeException(message.toString());
         }
 
-        StringBuilder result = new StringBuilder();
-        result.append("theory Translation imports Main begin").append(LINE_ENDING);
+        StringBuilder translationPreamble = new StringBuilder();
+        translationPreamble.append("theory TranslationPreamble imports Main begin").append(LINE_ENDING);
 
         for (StringBuilder preamble : masterHandler.getPreambles()) {
-            result.append(LINE_ENDING).append(preamble).append(LINE_ENDING);
+            translationPreamble.append(LINE_ENDING).append(preamble).append(LINE_ENDING);
         }
 
         Sort nullSort = services.getNamespaces().sorts().lookup("Null");
@@ -55,42 +57,42 @@ public class IsabelleTranslator {
             String sortName = masterHandler.translateSortName(sort);
             String UNIV = sortName + "_UNIV";
 
-            result.append("(* generated declaration for sort: ").append(sort.name().toString()).append(" *)").append(LINE_ENDING);
-            result.append("lemma ex_").append(UNIV).append(":");
-            result.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), "{bottom}")).append(LINE_ENDING);
-            result.append("  by simp").append(LINE_ENDING).append(LINE_ENDING);
+            translationPreamble.append("(* generated declaration for sort: ").append(sort.name().toString()).append(" *)").append(LINE_ENDING);
+            translationPreamble.append("lemma ex_").append(UNIV).append(":");
+            translationPreamble.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), "{bottom}")).append(LINE_ENDING);
+            translationPreamble.append("  by simp").append(LINE_ENDING).append(LINE_ENDING);
 
 
-            result.append("consts").append(LINE_ENDING).append(UNIV).append("::\"any set\"").append(LINE_ENDING);
-            result.append(LINE_ENDING);
+            translationPreamble.append("consts").append(LINE_ENDING).append(UNIV).append("::\"any set\"").append(LINE_ENDING);
+            translationPreamble.append(LINE_ENDING);
 
-            result.append("specification (").append(UNIV).append(") ");
-            result.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
-            result.append("  using ex_").append(UNIV).append(" by blast").append(LINE_ENDING);
-            result.append(LINE_ENDING);
+            translationPreamble.append("specification (").append(UNIV).append(") ");
+            translationPreamble.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
+            translationPreamble.append("  using ex_").append(UNIV).append(" by blast").append(LINE_ENDING);
+            translationPreamble.append(LINE_ENDING);
 
 
             String UNIV_spec_lemma_name = UNIV + "_specification";
-            result.append("lemma ").append(UNIV_spec_lemma_name).append(":").append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
-            result.append("  by (metis (mono_tags, lifting) ").append(UNIV).append("_def someI_ex ex_").append(UNIV).append(")").append(LINE_ENDING);
-            result.append(LINE_ENDING);
+            translationPreamble.append("lemma ").append(UNIV_spec_lemma_name).append(":").append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
+            translationPreamble.append("  by (metis (mono_tags, lifting) ").append(UNIV).append("_def someI_ex ex_").append(UNIV).append(")").append(LINE_ENDING);
+            translationPreamble.append(LINE_ENDING);
 
-            result.append("typedef ").append(sortName).append(" = \"").append(UNIV).append("\"").append(LINE_ENDING);
+            translationPreamble.append("typedef ").append(sortName).append(" = \"").append(UNIV).append("\"").append(LINE_ENDING);
             String repName = sortName + "2any";
             String absName = "any2" + sortName;
 
-            result.append("  morphisms ").append(repName).append(" ").append(absName).append(LINE_ENDING);
-            result.append("  using ").append(UNIV_spec_lemma_name).append(" by auto").append(LINE_ENDING).append(LINE_ENDING);
+            translationPreamble.append("  morphisms ").append(repName).append(" ").append(absName).append(LINE_ENDING);
+            translationPreamble.append("  using ").append(UNIV_spec_lemma_name).append(" by auto").append(LINE_ENDING).append(LINE_ENDING);
 
-            result.append("declare [[coercion ").append(repName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
+            translationPreamble.append("declare [[coercion ").append(repName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
 
 
             String IsabelleTypeUniverseOfSort = "(UNIV::" + sortName + " set)";
-            result.append("lemma ").append(sortName).append("_type_specification[simp]:")
+            translationPreamble.append("lemma ").append(sortName).append("_type_specification[simp]:")
                     .append(getUnivSpec(masterHandler, sortParentsMap.get(sort), IsabelleTypeUniverseOfSort))
                     .append(LINE_ENDING);
-            result.append("  using ").append(UNIV_spec_lemma_name).append(" using type_definition.Rep_range type_definition_").append(sortName).append(" by blast").append(LINE_ENDING);
-            result.append(LINE_ENDING).append(LINE_ENDING);
+            translationPreamble.append("  using ").append(UNIV_spec_lemma_name).append(" using type_definition.Rep_range type_definition_").append(sortName).append(" by blast").append(LINE_ENDING);
+            translationPreamble.append(LINE_ENDING).append(LINE_ENDING);
 
             for (Sort parentSort : sortParentsMap.get(sort)) {
                 if (parentSort == Sort.ANY) {
@@ -98,106 +100,106 @@ public class IsabelleTranslator {
                 }
                 String parentSortName = masterHandler.translateSortName(parentSort);
                 String parentSortInj = sortName + "2" + parentSortName;
-                result.append(LINE_ENDING).append("fun ").append(parentSortInj).append(" where \"").append(parentSortInj)
+                translationPreamble.append(LINE_ENDING).append("fun ").append(parentSortInj).append(" where \"").append(parentSortInj)
                         .append(" x = ").append("any2").append(parentSortName).append(" (").append(repName).append(" x)\"").append(LINE_ENDING);
-                result.append("declare [[coercion ").append(parentSortInj).append("]]").append(LINE_ENDING).append(LINE_ENDING);
+                translationPreamble.append("declare [[coercion ").append(parentSortInj).append("]]").append(LINE_ENDING).append(LINE_ENDING);
             }
 
-            result.append("instantiation ").append(sortName).append("::any").append(LINE_ENDING);
-            result.append("begin").append(LINE_ENDING);
+            translationPreamble.append("instantiation ").append(sortName).append("::any").append(LINE_ENDING);
+            translationPreamble.append("begin").append(LINE_ENDING);
             String to_any_fun_Name = "to_any_" + sortName;
-            result.append("fun ").append(to_any_fun_Name)
+            translationPreamble.append("fun ").append(to_any_fun_Name)
                     .append(" where \"").append(to_any_fun_Name).append(" x = ").append(repName).append(" x\"")
                     .append(LINE_ENDING);
             String cast_fun_Name = "cast_" + sortName;
-            result.append("fun ").append(cast_fun_Name)
+            translationPreamble.append("fun ").append(cast_fun_Name)
                     .append("  where \"").append(cast_fun_Name).append(" x = ").append(absName).append(" x\"")
                     .append(LINE_ENDING);
-            result.append("instance by standard").append(LINE_ENDING);
-            result.append("end").append(LINE_ENDING).append(LINE_ENDING);
+            translationPreamble.append("instance by standard").append(LINE_ENDING);
+            translationPreamble.append("end").append(LINE_ENDING).append(LINE_ENDING);
 
             if (nullSort.extendsTrans(sort)) {
                 String null_to_sort_name = "Null2" + sortName;
-                result.append("fun ").append(null_to_sort_name).append(" where \"").append(null_to_sort_name)
+                translationPreamble.append("fun ").append(null_to_sort_name).append(" where \"").append(null_to_sort_name)
                         .append(" x = ").append(absName).append("(Null2any x)\"").append(LINE_ENDING);
-                result.append("declare [[coercion Null2").append(sortName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
+                translationPreamble.append("declare [[coercion Null2").append(sortName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
             }
 
             if (sort instanceof ArraySort) {
-                result.append("instantiation ").append(sortName).append("::array").append(LINE_ENDING);
-                result.append("begin").append(LINE_ENDING);
+                translationPreamble.append("instantiation ").append(sortName).append("::array").append(LINE_ENDING);
+                translationPreamble.append("begin").append(LINE_ENDING);
 
                 String element_type_name = "element_type_" + sortName;
                 String elementSortName = masterHandler.translateSortName(((ArraySort) sort).elementSort());
                 String elementSortType = "Abs_javaDL_type ((UNIV::" + elementSortName + " set)::any set)";
-                result.append("fun ").append(element_type_name)
+                translationPreamble.append("fun ").append(element_type_name)
                         .append(" where \"").append(element_type_name)
                         .append(" (x::").append(sortName).append(")").append(" = ")
                         .append(elementSortType).append("\"")
                         .append(LINE_ENDING);
 
-                result.append("instance by standard").append(LINE_ENDING);
-                result.append("end").append(LINE_ENDING).append(LINE_ENDING);
+                translationPreamble.append("instance by standard").append(LINE_ENDING);
+                translationPreamble.append("end").append(LINE_ENDING).append(LINE_ENDING);
             }
 
             String typeConstName = sortName + "_type";
-            result.append("definition \"").append(typeConstName).append(" = Abs_javaDL_type ").append(IsabelleTypeUniverseOfSort).append("\"");
+            translationPreamble.append("definition \"").append(typeConstName).append(" = Abs_javaDL_type ").append(IsabelleTypeUniverseOfSort).append("\"");
 
-            result.append(LINE_ENDING).append(LINE_ENDING);
+            translationPreamble.append(LINE_ENDING).append(LINE_ENDING);
         }
+        translationPreamble.append("end");
 
-        result.append("locale varsAndFunctions");
+        StringBuilder sequentTranslation = new StringBuilder("theory Translation imports Main TranslationPreamble begin").append(LINE_ENDING);
+        sequentTranslation.append("locale varsAndFunctions");
         List<StringBuilder> locales = masterHandler.getLocales();
 
         boolean locale_empty = true;
 
         if (!locales.isEmpty()) {
-            result.append(" = ");
-            result.append(locales.remove(0));
+            sequentTranslation.append(" = ");
+            sequentTranslation.append(locales.remove(0));
             locale_empty = false;
         }
         for (StringBuilder locale : locales) {
-            result.append(" + ").append(locale);
+            sequentTranslation.append(" + ").append(locale);
         }
 
         List<StringBuilder> constDecls = masterHandler.getConstDeclarations();
         if (!constDecls.isEmpty() && locale_empty) {
-            result.append(" = ");
-            result.append(locales.remove(0));
+            sequentTranslation.append(" = ");
+            sequentTranslation.append(locales.remove(0));
             locale_empty = false;
         } else if (!locale_empty) {
-            result.append(" + ").append(LINE_ENDING);
+            sequentTranslation.append(" + ").append(LINE_ENDING);
         }
         for (StringBuilder constDecl : constDecls) {
-            result.append(LINE_ENDING).append(constDecl);
+            sequentTranslation.append(LINE_ENDING).append(constDecl);
         }
-        result.append(LINE_ENDING);
+        sequentTranslation.append(LINE_ENDING);
 
 
-        result.append("begin").append(LINE_ENDING);
+        sequentTranslation.append("begin").append(LINE_ENDING);
 
-        result.append("theorem solve: ");
+        sequentTranslation.append("theorem solve: ");
         for (int i = 0; i < antecedentTranslations.size(); i++) {
             StringBuilder antecedentFormula = antecedentTranslations.get(i);
-            result.append(LINE_ENDING).append("assumes antecedent_").append(i).append(":\"").append(antecedentFormula).append("\"");
+            sequentTranslation.append(LINE_ENDING).append("assumes antecedent_").append(i).append(":\"").append(antecedentFormula).append("\"");
         }
-        result.append(LINE_ENDING);
-        result.append("shows \"");
+        sequentTranslation.append(LINE_ENDING);
+        sequentTranslation.append("shows \"");
         if (succedentTranslations.isEmpty()) {
-            result.append("False");
+            sequentTranslation.append("False");
         } else {
-            result.append(succedentTranslations.get(0));
+            sequentTranslation.append(succedentTranslations.get(0));
         }
         for (int i = 1; i < succedentTranslations.size(); i++) {
 
             StringBuilder succedentFormula = succedentTranslations.get(i);
-            result.append(LINE_ENDING).append("\\<or>").append(succedentFormula);
+            sequentTranslation.append(LINE_ENDING).append("\\<or>").append(succedentFormula);
         }
-        result.append("\"");
-        result.append(LINE_ENDING).append(LINE_ENDING);
-        result.append("(* Solve here *)").append(LINE_ENDING);
+        sequentTranslation.append("\"");
 
-        return result;
+        return new IsabelleProblem(goal, translationPreamble.toString(), sequentTranslation.toString());
     }
 
     private static String getUnivSpec(IsabelleMasterHandler masterHandler, Set<Sort> parents, String insert) {
--- keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/TranslationAction.java
+++ keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/TranslationAction.java
@@ -26,12 +26,10 @@ import java.util.List;
 public class TranslationAction extends MainWindowAction {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TranslationAction.class);
-    private final Path isabelle_path;
 
     public TranslationAction(MainWindow mainWindow) {
         super(mainWindow);
         setName("Translate to Isabelle");
-        this.isabelle_path = IsabelleTranslationSettings.getInstance().getIsabellePath();
     }
 
     @Override
@@ -41,140 +39,36 @@ public class TranslationAction extends MainWindowAction {
         generateTranslation();
     }
 
-    private Theory beginTheory(String thyText, Path source, Isabelle isabelle) {
-        MLFunction3<Path, TheoryHeader, scala.collection.immutable.List<Theory>, Theory> begin_theory =
-                MLValue.compileFunction("fn (path, header, parents) => Resources.begin_theory path header parents", isabelle,
-                        Implicits.pathConverter(), Implicits.theoryHeaderConverter(), new ListConverter<>(Implicits.theoryConverter()), Implicits.theoryConverter());
-        MLFunction2<String, Position, TheoryHeader> header_read = MLValue.compileFunction("fn (text,pos) => Thy_Header.read pos text", isabelle,
-                de.unruh.isabelle.mlvalue.Implicits.stringConverter(), Implicits.positionConverter(), Implicits.theoryHeaderConverter());
-        TheoryHeader header = header_read.apply(thyText, Position.none(isabelle), isabelle, de.unruh.isabelle.mlvalue.Implicits.stringConverter(), Implicits.positionConverter())
-                .retrieveNow(Implicits.theoryHeaderConverter(), isabelle);
-        Path topDir = source.getParent();
-        return begin_theory.apply(topDir, header, header.imports(isabelle).map((String name) -> Theory.apply(name, isabelle)), isabelle,
-                        Implicits.pathConverter(), Implicits.theoryHeaderConverter(), new ListConverter<>(Implicits.theoryConverter()))
-                .retrieveNow(Implicits.theoryConverter(), isabelle);
-    }
 
     private void generateTranslation() {
         KeYMediator mediator = getMediator();
         IsabelleTranslator translator = new IsabelleTranslator(mediator.getServices());
         try {
             File translationFile = new File(IsabelleTranslationSettings.getInstance().getTranslationPath() +  "\\Translation.thy");
-            StringBuilder translation = translator.translateProblem(mediator.getSelectedGoal().sequent());
+            File translationPreambleFile = new File(IsabelleTranslationSettings.getInstance().getTranslationPath() +  "\\TranslationPreamble.thy");
+            IsabelleProblem translation = translator.translateProblem(mediator.getSelectedGoal());
 
-            LOGGER.info("Starting isabelle...");
-            Isabelle isabelle;
             try {
-                Isabelle.Setup setup = JIsabelle.setup(isabelle_path);
-                isabelle = new Isabelle(setup);
-            } catch (Exception e) {
-                LOGGER.error("Can't find Isabelle at {}", isabelle_path);
+                Files.createDirectories(translationFile.toPath().getParent());
+                Files.write(translationFile.toPath(), translation.getSequentTranslation().getBytes());
+                Files.write(translationPreambleFile.toPath(), translation.getPreamble().getBytes());
+                LOGGER.info("Saved to: " + translationFile.toPath() + " and " + translationPreambleFile.toPath());
+            } catch (IOException e) {
+                LOGGER.error("Failed to save translation", e);
                 return;
             }
 
-            LOGGER.info("Parsing theory...");
-
-            Theory thy0 = beginTheory(translation.toString(), translationFile.toPath(), isabelle);
-            ToplevelState toplevel = ToplevelState.apply(isabelle);
-
-            MLFunction2<Theory, String, scala.collection.immutable.List<Tuple2<Transition, String>>> parse_text = MLValue.compileFunction("""
-                            fn (thy, text) => let
-                            val transitions = Outer_Syntax.parse_text thy (K thy) Position.start text
-                            fun addtext symbols [tr] =
-                                  [(tr, implode symbols)]
-                              | addtext _ [] = []
-                              | addtext symbols (tr::nextTr::trs) = let
-                                  val (this,rest) = Library.chop (Position.distance_of (Toplevel.pos_of tr, Toplevel.pos_of nextTr) |> Option.valOf) symbols
-                                  in (tr, implode this) :: addtext rest (nextTr::trs) end
-                            in addtext (Symbol.explode text) transitions end""", isabelle,
-                    Implicits.theoryConverter(), de.unruh.isabelle.mlvalue.Implicits.stringConverter(), new ListConverter<>(new Tuple2Converter<>(Implicits.transitionConverter(), de.unruh.isabelle.mlvalue.Implicits.stringConverter())));
-
-            MLFunction3<Object, Transition, ToplevelState, ToplevelState> command_exception = MLValue.compileFunction("fn (int, tr, st) => Toplevel.command_exception int tr st", isabelle,
-                    de.unruh.isabelle.mlvalue.Implicits.booleanConverter(), Implicits.transitionConverter(), Implicits.toplevelStateConverter(), Implicits.toplevelStateConverter());
-
-
-            List<Tuple2<Transition, String>> transitionsAndTexts = new ArrayList<>();
-            parse_text.apply(thy0, translation.toString(), isabelle,
-                            Implicits.theoryConverter(), de.unruh.isabelle.mlvalue.Implicits.stringConverter())
-                    .retrieveNow(new ListConverter<>(new Tuple2Converter<>(Implicits.transitionConverter(), de.unruh.isabelle.mlvalue.Implicits.stringConverter())), isabelle)
-                    .foreach(transitionsAndTexts::add);
-
-            for (Tuple2<Transition, String> transitionAndText : transitionsAndTexts) {
-                //println(s"""Transition: "${text.strip}"""")
-                toplevel = command_exception.apply(Boolean.TRUE, transitionAndText._1(), toplevel, isabelle,
-                                de.unruh.isabelle.mlvalue.Implicits.booleanConverter(), Implicits.transitionConverter(), Implicits.toplevelStateConverter())
-                        .retrieveNow(Implicits.toplevelStateConverter(), isabelle);
-            }
-            LOGGER.info("Finished Parsing");
-
-            String sledgehammer = thy0.importMLStructureNow("Sledgehammer", isabelle);
-            String Sledgehammer_Commands = thy0.importMLStructureNow("Sledgehammer_Commands", isabelle);
-            String Sledgehammer_Prover = thy0.importMLStructureNow("Sledgehammer_Prover", isabelle);
-
-            MLFunction4<ToplevelState, Theory, scala.collection.immutable.List<String>, scala.collection.immutable.List<String>, Tuple2<Object, Tuple2<String, scala.collection.immutable.List<String>>>> normal_with_Sledgehammer =
-                    MLValue.compileFunction(
-                            """
-                                    fn (state, thy, adds, dels) =>
-                                      let
-                                         val override = {add=[],del=[],only=false};
-                                         fun go_run (state, thy) =
-                                            let
-                                               val p_state = Toplevel.proof_of state;
-                                               val ctxt = Proof.context_of p_state;
-                                               val params =\s""" + Sledgehammer_Commands + """
-                                    .default_params thy
-                                                    [("timeout","30"),("verbose","true")];
-                                                 val results =\s"""
-                                    + sledgehammer + """
-                                    .run_sledgehammer params\s""" + Sledgehammer_Prover + """
-                                    .Normal NONE 1 override p_state;
-                                                 val (result, (outcome, step)) = results;
-                                               in
-                                                 (result, (""" + sledgehammer + """
-                                    .short_string_of_sledgehammer_outcome outcome, [YXML.content_of step]))
-                                               end;
-                                        in
-                                          Timeout.apply (Time.fromSeconds 35) go_run (state, thy) end
-                                    """, isabelle, Implicits.toplevelStateConverter(), Implicits.theoryConverter(),
-                            new ListConverter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter()),
-                            new ListConverter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter()),
-                            (new Tuple2Converter<>(de.unruh.isabelle.mlvalue.Implicits.booleanConverter(), new Tuple2Converter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter(), new ListConverter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter())))));
-
-            Builder<String, scala.collection.immutable.List<String>> listBuilder = scala.collection.immutable.List.newBuilder();
-            scala.collection.immutable.List<String> emptyList = listBuilder.result();
-
-            Tuple2<Object, Tuple2<String, scala.collection.immutable.List<String>>> result;
-            LOGGER.info("Sledgehammering...");
-            try {
-                result = normal_with_Sledgehammer.apply(toplevel, thy0, emptyList, emptyList, isabelle, Implicits.toplevelStateConverter(), Implicits.theoryConverter(),
-                                new ListConverter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter()),
-                                new ListConverter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter()))
-                        .retrieveNow(new Tuple2Converter<>(de.unruh.isabelle.mlvalue.Implicits.booleanConverter(), new Tuple2Converter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter(), new ListConverter<>(de.unruh.isabelle.mlvalue.Implicits.stringConverter()))), isabelle);
-            } catch (Exception exception) {
-                result = new Tuple2<>(Boolean.FALSE, new Tuple2<>("", emptyList));
-            }
-            isabelle.destroy();
-
-            LOGGER.info("Sledgehammer result: " + result);
+            SledgehammerResult result = translation.sledgehammer();
 
             //TODO needs its own action to enable undo, etc. and naming reworks
-            if ((Boolean) result._1()) {
-                IBuiltInRuleApp app = SMTRuleApp.RULE.createApp("Isabelle " + result._2()._2().head());
+            if (result.isSuccessful()) {
+                IBuiltInRuleApp app = SMTRuleApp.RULE.createApp("Isabelle " + result.getTactic());
                 app.tryToInstantiate(mediator.getSelectedGoal());
                 mediator.getSelectedGoal().apply(app);
             }
 
 
 
-
-            try {
-                Files.createDirectories(translationFile.toPath().getParent());
-                Files.write(translationFile.toPath(), translation.toString().getBytes());
-                LOGGER.info("Saved to: " + translationFile.toPath());
-            } catch (IOException e) {
-                //TODO handle exception
-                throw new RuntimeException(e);
-            }
             /*
             List<Path> filePaths = new ArrayList<>();
             filePaths.add(translationFile.toPath());
