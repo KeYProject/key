3800765c46075bcf98954f862d31b0c769d322a2
Nils Buchholz <nils.buchholz@student.kit.edu>
2024-04-16 15:50:35 +0200

    now built around having a ROOT session and unchanging preamble

--- keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslator.java
+++ keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslator.java
@@ -49,6 +49,9 @@ public class IsabelleTranslator {
         for (StringBuilder preamble : masterHandler.getPreambles()) {
             translationPreamble.append(LINE_ENDING).append(preamble).append(LINE_ENDING);
         }
+        translationPreamble.append("end");
+
+        StringBuilder sequentTranslation = new StringBuilder("theory Translation imports Main KeYTranslations.TranslationPreamble begin").append(LINE_ENDING);
 
         Sort nullSort = services.getNamespaces().sorts().lookup("Null");
 
@@ -57,42 +60,42 @@ public class IsabelleTranslator {
             String sortName = masterHandler.translateSortName(sort);
             String UNIV = sortName + "_UNIV";
 
-            translationPreamble.append("(* generated declaration for sort: ").append(sort.name().toString()).append(" *)").append(LINE_ENDING);
-            translationPreamble.append("lemma ex_").append(UNIV).append(":");
-            translationPreamble.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), "{bottom}")).append(LINE_ENDING);
-            translationPreamble.append("  by simp").append(LINE_ENDING).append(LINE_ENDING);
+            sequentTranslation.append("(* generated declaration for sort: ").append(sort.name().toString()).append(" *)").append(LINE_ENDING);
+            sequentTranslation.append("lemma ex_").append(UNIV).append(":");
+            sequentTranslation.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), "{bottom}")).append(LINE_ENDING);
+            sequentTranslation.append("  by simp").append(LINE_ENDING).append(LINE_ENDING);
 
 
-            translationPreamble.append("consts").append(LINE_ENDING).append(UNIV).append("::\"any set\"").append(LINE_ENDING);
-            translationPreamble.append(LINE_ENDING);
+            sequentTranslation.append("consts").append(LINE_ENDING).append(UNIV).append("::\"any set\"").append(LINE_ENDING);
+            sequentTranslation.append(LINE_ENDING);
 
-            translationPreamble.append("specification (").append(UNIV).append(") ");
-            translationPreamble.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
-            translationPreamble.append("  using ex_").append(UNIV).append(" by blast").append(LINE_ENDING);
-            translationPreamble.append(LINE_ENDING);
+            sequentTranslation.append("specification (").append(UNIV).append(") ");
+            sequentTranslation.append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
+            sequentTranslation.append("  using ex_").append(UNIV).append(" by blast").append(LINE_ENDING);
+            sequentTranslation.append(LINE_ENDING);
 
 
             String UNIV_spec_lemma_name = UNIV + "_specification";
-            translationPreamble.append("lemma ").append(UNIV_spec_lemma_name).append(":").append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
-            translationPreamble.append("  by (metis (mono_tags, lifting) ").append(UNIV).append("_def someI_ex ex_").append(UNIV).append(")").append(LINE_ENDING);
-            translationPreamble.append(LINE_ENDING);
+            sequentTranslation.append("lemma ").append(UNIV_spec_lemma_name).append(":").append(getUnivSpec(masterHandler, sortParentsMap.get(sort), UNIV)).append(LINE_ENDING);
+            sequentTranslation.append("  by (metis (mono_tags, lifting) ").append(UNIV).append("_def someI_ex ex_").append(UNIV).append(")").append(LINE_ENDING);
+            sequentTranslation.append(LINE_ENDING);
 
-            translationPreamble.append("typedef ").append(sortName).append(" = \"").append(UNIV).append("\"").append(LINE_ENDING);
+            sequentTranslation.append("typedef ").append(sortName).append(" = \"").append(UNIV).append("\"").append(LINE_ENDING);
             String repName = sortName + "2any";
             String absName = "any2" + sortName;
 
-            translationPreamble.append("  morphisms ").append(repName).append(" ").append(absName).append(LINE_ENDING);
-            translationPreamble.append("  using ").append(UNIV_spec_lemma_name).append(" by auto").append(LINE_ENDING).append(LINE_ENDING);
+            sequentTranslation.append("  morphisms ").append(repName).append(" ").append(absName).append(LINE_ENDING);
+            sequentTranslation.append("  using ").append(UNIV_spec_lemma_name).append(" by auto").append(LINE_ENDING).append(LINE_ENDING);
 
-            translationPreamble.append("declare [[coercion ").append(repName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
+            sequentTranslation.append("declare [[coercion ").append(repName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
 
 
             String IsabelleTypeUniverseOfSort = "(UNIV::" + sortName + " set)";
-            translationPreamble.append("lemma ").append(sortName).append("_type_specification[simp]:")
+            sequentTranslation.append("lemma ").append(sortName).append("_type_specification[simp]:")
                     .append(getUnivSpec(masterHandler, sortParentsMap.get(sort), IsabelleTypeUniverseOfSort))
                     .append(LINE_ENDING);
-            translationPreamble.append("  using ").append(UNIV_spec_lemma_name).append(" using type_definition.Rep_range type_definition_").append(sortName).append(" by blast").append(LINE_ENDING);
-            translationPreamble.append(LINE_ENDING).append(LINE_ENDING);
+            sequentTranslation.append("  using ").append(UNIV_spec_lemma_name).append(" using type_definition.Rep_range type_definition_").append(sortName).append(" by blast").append(LINE_ENDING);
+            sequentTranslation.append(LINE_ENDING).append(LINE_ENDING);
 
             for (Sort parentSort : sortParentsMap.get(sort)) {
                 if (parentSort == Sort.ANY) {
@@ -100,56 +103,55 @@ public class IsabelleTranslator {
                 }
                 String parentSortName = masterHandler.translateSortName(parentSort);
                 String parentSortInj = sortName + "2" + parentSortName;
-                translationPreamble.append(LINE_ENDING).append("fun ").append(parentSortInj).append(" where \"").append(parentSortInj)
+                sequentTranslation.append(LINE_ENDING).append("fun ").append(parentSortInj).append(" where \"").append(parentSortInj)
                         .append(" x = ").append("any2").append(parentSortName).append(" (").append(repName).append(" x)\"").append(LINE_ENDING);
-                translationPreamble.append("declare [[coercion ").append(parentSortInj).append("]]").append(LINE_ENDING).append(LINE_ENDING);
+                sequentTranslation.append("declare [[coercion ").append(parentSortInj).append("]]").append(LINE_ENDING).append(LINE_ENDING);
             }
 
-            translationPreamble.append("instantiation ").append(sortName).append("::any").append(LINE_ENDING);
-            translationPreamble.append("begin").append(LINE_ENDING);
+            sequentTranslation.append("instantiation ").append(sortName).append("::any").append(LINE_ENDING);
+            sequentTranslation.append("begin").append(LINE_ENDING);
             String to_any_fun_Name = "to_any_" + sortName;
-            translationPreamble.append("fun ").append(to_any_fun_Name)
+            sequentTranslation.append("fun ").append(to_any_fun_Name)
                     .append(" where \"").append(to_any_fun_Name).append(" x = ").append(repName).append(" x\"")
                     .append(LINE_ENDING);
             String cast_fun_Name = "cast_" + sortName;
-            translationPreamble.append("fun ").append(cast_fun_Name)
+            sequentTranslation.append("fun ").append(cast_fun_Name)
                     .append("  where \"").append(cast_fun_Name).append(" x = ").append(absName).append(" x\"")
                     .append(LINE_ENDING);
-            translationPreamble.append("instance by standard").append(LINE_ENDING);
-            translationPreamble.append("end").append(LINE_ENDING).append(LINE_ENDING);
+            sequentTranslation.append("instance by standard").append(LINE_ENDING);
+            sequentTranslation.append("end").append(LINE_ENDING).append(LINE_ENDING);
 
             if (nullSort.extendsTrans(sort)) {
                 String null_to_sort_name = "Null2" + sortName;
-                translationPreamble.append("fun ").append(null_to_sort_name).append(" where \"").append(null_to_sort_name)
+                sequentTranslation.append("fun ").append(null_to_sort_name).append(" where \"").append(null_to_sort_name)
                         .append(" x = ").append(absName).append("(Null2any x)\"").append(LINE_ENDING);
-                translationPreamble.append("declare [[coercion Null2").append(sortName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
+                sequentTranslation.append("declare [[coercion Null2").append(sortName).append("]]").append(LINE_ENDING).append(LINE_ENDING);
             }
 
             if (sort instanceof ArraySort) {
-                translationPreamble.append("instantiation ").append(sortName).append("::array").append(LINE_ENDING);
-                translationPreamble.append("begin").append(LINE_ENDING);
+                sequentTranslation.append("instantiation ").append(sortName).append("::array").append(LINE_ENDING);
+                sequentTranslation.append("begin").append(LINE_ENDING);
 
                 String element_type_name = "element_type_" + sortName;
                 String elementSortName = masterHandler.translateSortName(((ArraySort) sort).elementSort());
                 String elementSortType = "Abs_javaDL_type ((UNIV::" + elementSortName + " set)::any set)";
-                translationPreamble.append("fun ").append(element_type_name)
+                sequentTranslation.append("fun ").append(element_type_name)
                         .append(" where \"").append(element_type_name)
                         .append(" (x::").append(sortName).append(")").append(" = ")
                         .append(elementSortType).append("\"")
                         .append(LINE_ENDING);
 
-                translationPreamble.append("instance by standard").append(LINE_ENDING);
-                translationPreamble.append("end").append(LINE_ENDING).append(LINE_ENDING);
+                sequentTranslation.append("instance by standard").append(LINE_ENDING);
+                sequentTranslation.append("end").append(LINE_ENDING).append(LINE_ENDING);
             }
 
             String typeConstName = sortName + "_type";
-            translationPreamble.append("definition \"").append(typeConstName).append(" = Abs_javaDL_type ").append(IsabelleTypeUniverseOfSort).append("\"");
+            sequentTranslation.append("definition \"").append(typeConstName).append(" = Abs_javaDL_type ").append(IsabelleTypeUniverseOfSort).append("\"");
 
-            translationPreamble.append(LINE_ENDING).append(LINE_ENDING);
+            sequentTranslation.append(LINE_ENDING).append(LINE_ENDING);
         }
-        translationPreamble.append("end");
+        sequentTranslation.append("end");
 
-        StringBuilder sequentTranslation = new StringBuilder("theory Translation imports Main Draft.TranslationPreamble begin").append(LINE_ENDING);
         sequentTranslation.append("locale varsAndFunctions");
         List<StringBuilder> locales = masterHandler.getLocales();
 
