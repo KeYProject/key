b36672c220dc32060cabbf04629220d44b46bf02
Nils Buchholz <nils.buchholz@student.kit.edu>
2024-04-16 20:00:28 +0200

    load settings at startup

--- keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslationExtension.java
+++ keyext.isabelletranslation/src/main/java/de/uka/ilkd/key/gui/isabelletranslation/IsabelleTranslationExtension.java
@@ -5,23 +5,17 @@ import de.uka.ilkd.key.gui.MainWindow;
 import de.uka.ilkd.key.gui.extension.api.ContextMenuAdapter;
 import de.uka.ilkd.key.gui.extension.api.ContextMenuKind;
 import de.uka.ilkd.key.gui.extension.api.KeYGuiExtension;
-import de.uka.ilkd.key.gui.settings.SettingsDialog;
 import de.uka.ilkd.key.gui.settings.SettingsProvider;
 import de.uka.ilkd.key.pp.PosInSequent;
-import de.uka.ilkd.key.settings.Configuration;
-import de.uka.ilkd.key.util.properties.Properties;
 import org.jspecify.annotations.NonNull;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @KeYGuiExtension.Info(name = "Translation", optional = true,
         experimental = true)
-public class IsabelleTranslationExtension implements KeYGuiExtension, KeYGuiExtension.Settings, KeYGuiExtension.ContextMenu {
+public class IsabelleTranslationExtension implements KeYGuiExtension, KeYGuiExtension.Settings, KeYGuiExtension.ContextMenu, KeYGuiExtension.Startup {
 
     @Override
     public SettingsProvider getSettings() {
@@ -48,5 +42,10 @@ public class IsabelleTranslationExtension implements KeYGuiExtension, KeYGuiExte
     @Override
     public @NonNull List<Action> getContextActions(@NonNull KeYMediator mediator, @NonNull ContextMenuKind kind, @NonNull Object underlyingObject) {
         return adapter.getContextActions(mediator, kind, underlyingObject);
+    }
+
+    @Override
+    public void init(MainWindow window, KeYMediator mediator) {
+        IsabelleTranslationSettings.getInstance();
     }
 }
