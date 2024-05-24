2f93fbe6a6328c36d1a19c1c796ce4f658be1929
Alexander Weigl <weigl@kit.edu>
2024-05-24 22:34:43 +0200

    enabling multi-threading in TGMain

--- key.core/src/main/java/de/uka/ilkd/key/macros/SequentialProofMacro.java
+++ key.core/src/main/java/de/uka/ilkd/key/macros/SequentialProofMacro.java
@@ -18,6 +18,8 @@ import org.key_project.prover.engine.TaskStartedInfo.TaskKind;
 import org.key_project.prover.sequent.PosInOccurrence;
 import org.key_project.util.collection.ImmutableList;
 
+import org.jspecify.annotations.Nullable;
+
 /**
  * The abstract class SequentialProofMacro can be used to create compound macros which sequentially
  * apply macros one after the other. This works only for macros which use
