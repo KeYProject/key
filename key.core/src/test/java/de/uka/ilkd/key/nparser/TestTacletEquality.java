ec65160c32a0773b73913a9a87b79ce0d96d09fb
Wolfram Pfeifer <wolfram.pfeifer@kit.edu>
2024-06-24 11:58:28 +0200

    spotless

--- key.core/src/test/java/de/uka/ilkd/key/nparser/TestTacletEquality.java
+++ key.core/src/test/java/de/uka/ilkd/key/nparser/TestTacletEquality.java
@@ -25,7 +25,6 @@ import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
