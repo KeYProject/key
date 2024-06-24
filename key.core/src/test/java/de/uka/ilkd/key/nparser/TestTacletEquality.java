f2ddbd6ee1742f32a1d7687ac10422b5cd8839ae
Wolfram Pfeifer <wolfram.pfeifer@kit.edu>
2024-06-24 11:35:57 +0200

    update oracle for taclet equality test

--- key.core/src/test/java/de/uka/ilkd/key/nparser/TestTacletEquality.java
+++ key.core/src/test/java/de/uka/ilkd/key/nparser/TestTacletEquality.java
@@ -25,6 +25,7 @@ import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -123,6 +124,11 @@ public class TestTacletEquality {
         } catch (IOException e) {
             Assertions.fail("Exception for opening " + path, e);
         }
+    }
+
+    // @Test
+    public void createOracle() {
+        createNewOracle();
     }
 
     @ParameterizedTest
