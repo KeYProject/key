47b65dd5ec6094bbfc9241244f405455ec7d5860
Wolfram Pfeifer <wolfram.pfeifer@kit.edu>
2024-07-03 12:37:18 +0200

    removed more license headers

--- key.core/src/main/java/de/uka/ilkd/key/rule/TacletAttributes.java
+++ key.core/src/main/java/de/uka/ilkd/key/rule/TacletAttributes.java
@@ -1,17 +1,4 @@
-// This file is part of KeY - Integrated Deductive Software Design
-//
-// Copyright (C) 2001-2011 Universitaet Karlsruhe (TH), Germany
-//                         Universitaet Koblenz-Landau, Germany
-//                         Chalmers University of Technology, Sweden
-// Copyright (C) 2011-2014 Karlsruhe Institute of Technology, Germany
-//                         Technical University Darmstadt, Germany
-//                         Chalmers University of Technology, Sweden
-//
-// The KeY system is protected by the GNU General
-// Public License. See LICENSE.TXT for details.
-//
-
-/** class contains optional attributes of a Taclet. 
+/** class contains optional attributes of a Taclet.
  */
 
 package de.uka.ilkd.key.rule;
