13414d8bf747c0fa2e9b794877ba3b5fb689d068
Drodt <daniel.drodt@tu-darmstadt.de>
2024-07-03 11:29:46 +0200

    Add taclet files

--- keyext.rusty_while/src/main/java/org/key_project/rusty/ldt/BoolLDT.java
+++ keyext.rusty_while/src/main/java/org/key_project/rusty/ldt/BoolLDT.java
@@ -1,3 +1,6 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
 package org.key_project.rusty.ldt;
 
 import org.key_project.logic.Name;
@@ -6,7 +9,7 @@ import org.key_project.logic.op.Function;
 import org.key_project.rusty.Services;
 
 public class BoolLDT extends LDT {
-    public static final Name NAME = new Name("boolean");
+    public static final Name NAME = new Name("bool");
 
     /** the boolean literals as function symbols and terms */
     private final Function bool_true;
