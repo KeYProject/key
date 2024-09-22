1e9cdd7f2b2e7fa7371f7edb085e85814afaadd8
BookWood <nils.buchholz@gmail.com>
2024-09-22 13:40:07 +0200

    add new IsabelleResult to replace SledgehammerResult prototype

---
+++ keyext.isabelletranslation/src/main/java/org/key_project/isabelletranslation/IsabelleResult.java
@@ -0,0 +1,62 @@
+package org.key_project.isabelletranslation;
+
+public class IsabelleResult {
+    enum Type {
+        SUCCESS,
+        ERROR,
+        TIMEOUT
+    }
+
+
+
+    private final Type type;
+
+    private final long computationTime;
+
+    private final String successfulTactic;
+
+    private final Exception exception;
+
+    private IsabelleResult(Type type, long computationTime, String successfulTactic, Exception exception) {
+        this.type = type;
+        this.computationTime = computationTime;
+        this.successfulTactic = successfulTactic;
+        this.exception = exception;
+    }
+
+    public boolean isSuccessful() {
+        return type == Type.SUCCESS;
+    }
+
+    public static IsabelleResult getTimeoutResult(long computationTime) {
+        return new IsabelleResult(Type.TIMEOUT, computationTime, null, null);
+    }
+
+    public static IsabelleResult getErrorResult(Exception exception) {
+        return new IsabelleResult(Type.ERROR, -1, null, exception);
+    }
+
+    public static IsabelleResult getSuccessResult(String successfulTactic, long computationTime) {
+        return new IsabelleResult(Type.SUCCESS, computationTime, successfulTactic, null);
+    }
+
+    public boolean isError() {
+        return type == Type.ERROR;
+    }
+
+    public boolean isTimeout() {
+        return type == Type.TIMEOUT;
+    }
+
+    public long getComputationTime() {
+        return this.computationTime;
+    }
+
+    public Exception getException() {
+        return this.exception;
+    }
+
+    public String getSuccessfulTactic() {
+        return this.successfulTactic;
+    }
+}
