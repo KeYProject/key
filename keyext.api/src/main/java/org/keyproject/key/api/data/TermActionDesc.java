cc86b3c2bd3116212f6a5b265f8703fe772af29d
Alexander Weigl <weigl@kit.edu>
2024-10-20 16:12:52 +0200

    wip

--- keyext.api/src/main/java/org/keyproject/key/api/data/TermActionDesc.java
+++ keyext.api/src/main/java/org/keyproject/key/api/data/TermActionDesc.java
@@ -6,10 +6,30 @@ package org.keyproject.key.api.data;
 import org.keyproject.key.api.data.KeyIdentifications.TermActionId;
 
 /**
+ * This class represents an action that can be executed on a term.
+ *
  * @author Alexander Weigl
  * @version 1 (13.10.23)
  */
-public record TermActionDesc(TermActionId commandId, String displayName, String description,
+public record TermActionDesc(
+        /**
+         * Unique identification
+         */
+        TermActionId commandId,
+        /**
+         * A string to display to the user.
+         */
+        String displayName,
+        /**
+         * Long description of the action for the user.
+         */
+        String description,
+        /**
+         * A string identifying a category
+         */
         String category,
+        /**
+         * Kind of the action
+         */
         TermActionKind kind) {
 }
