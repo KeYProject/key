4711487768f3703f1ecef3313e24b63fdcd7f95f
Richard Bubel <bubel@cs.tu-darmstadt.de>
2025-01-08 08:55:32 +0100

    Added comment to Layoutable interface

--- key.core/src/main/java/de/uka/ilkd/key/logic/op/Layoutable.java
+++ key.core/src/main/java/de/uka/ilkd/key/logic/op/Layoutable.java
@@ -2,6 +2,13 @@ package de.uka.ilkd.key.logic.op;
 
 import de.uka.ilkd.key.util.pp.Layouter;
 
+/**
+ * Current workaround to factor out layout method from SchemaVariable
+ * This inteface should be removed and the knowledge how to print a parseable
+ * representation of a variable declaration moved out of the logic package. The reason
+ * is that logic elements should not be responsible to know how their concrete syntax
+ * looks like.
+ */
 public interface Layoutable {
     /**
      * Creates a parseable string representation
