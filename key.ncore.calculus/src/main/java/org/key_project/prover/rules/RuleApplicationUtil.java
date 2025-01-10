f135815dfc7bb453ba5c935e8109c3ff0344db1d
Richard Bubel <bubel@cs.tu-darmstadt.de>
2025-01-10 08:00:19 +0100

    Minor cleanup

---
+++ key.ncore.calculus/src/main/java/org/key_project/prover/rules/RuleApplicationUtil.java
@@ -0,0 +1,4 @@
+package org.key_project.prover.rules;
+
+public class RuleApplicationUtil {
+}
--- key.ncore.calculus/src/main/java/org/key_project/prover/rules/VariableCondition.java
+++ key.ncore.calculus/src/main/java/org/key_project/prover/rules/VariableCondition.java
@@ -29,6 +29,5 @@ public interface VariableCondition {
      *         otherwise
      */
     MatchConditions check(SchemaVariable var, SyntaxElement instCandidate,
-            MatchConditions matchCond,
-            LogicServices services);
+            MatchConditions matchCond, LogicServices services);
 }
