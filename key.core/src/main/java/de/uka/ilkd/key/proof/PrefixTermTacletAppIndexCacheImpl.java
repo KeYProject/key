2ccb7260f18ab65425de03041ab619aaca024c4f
Richard Bubel <bubel@cs.tu-darmstadt.de>
2025-01-26 10:17:19 +0100

    Progress on generalization of taclet application indices - use as far as possible ncore term and operators - removal of unnecessary casts

--- key.core/src/main/java/de/uka/ilkd/key/proof/PrefixTermTacletAppIndexCacheImpl.java
+++ key.core/src/main/java/de/uka/ilkd/key/proof/PrefixTermTacletAppIndexCacheImpl.java
@@ -5,9 +5,8 @@ package de.uka.ilkd.key.proof;
 
 import java.util.Map;
 
-import de.uka.ilkd.key.logic.Term;
-import de.uka.ilkd.key.logic.op.QuantifiableVariable;
-
+import org.key_project.logic.Term;
+import org.key_project.logic.op.QuantifiableVariable;
 import org.key_project.util.collection.ImmutableList;
 
 import org.slf4j.Logger;
