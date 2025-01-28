07344a29db0cf52a6543c92ac284e9df8871ec42
Drodt <daniel.drodt@tu-darmstadt.de>
2025-01-28 14:59:38 +0100

    Fix RewriteTacletGoalTemplate

--- keyext.rusty/src/main/java/org/key_project/rusty/rule/tacletbuilder/RewriteTacletGoalTemplate.java
+++ keyext.rusty/src/main/java/org/key_project/rusty/rule/tacletbuilder/RewriteTacletGoalTemplate.java
@@ -52,4 +52,9 @@ public class RewriteTacletGoalTemplate extends TacletGoalTemplate {
     public Term replaceWith() {
         return replacewith;
     }
+
+    @Override
+    public Object replaceWithExpressionAsObject() {
+        return replacewith;
+    }
 }
