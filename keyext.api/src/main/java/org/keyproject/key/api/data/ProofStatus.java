317751d9287fbfcbf2d05f801999e1cb5ffb9915
Samuel Teuber <teuber@kit.edu>
2025-02-20 13:20:28 +0100

    First (incomplete) working proof management endpoint

---
+++ keyext.api/src/main/java/org/keyproject/key/api/data/ProofStatus.java
@@ -0,0 +1,12 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package org.keyproject.key.api.data;
+
+import de.uka.ilkd.key.proof.Proof;
+
+public record ProofStatus(KeyIdentifications.ProofId id, int openGoals, int closedGoals) {
+    public static ProofStatus from(KeyIdentifications.ProofId id, Proof proof) {
+        return new ProofStatus(id, proof.openGoals().size(), proof.closedGoals().size());
+    }
+}
