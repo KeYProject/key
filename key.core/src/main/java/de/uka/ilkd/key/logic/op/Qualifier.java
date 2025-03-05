fae2e8ceb1da97a684f4a21b079c48d71ffa53c3
Drodt <daniel.drodt@tu-darmstadt.de>
2025-03-05 12:51:26 +0100

    Improve JavaDocs

--- key.core/src/main/java/de/uka/ilkd/key/logic/op/Qualifier.java
+++ key.core/src/main/java/de/uka/ilkd/key/logic/op/Qualifier.java
@@ -7,6 +7,20 @@ import java.util.WeakHashMap;
 
 import org.key_project.logic.TerminalSyntaxElement;
 
+/**
+ * This class is primarily used in logic operations where qualifiers (e.g., types or sorts)
+ * are referenced multiple times, such as in {@link ObserverFunction} and
+ * {@link SortDependingFunction}.
+ * It helps to ensure that qualifiers maintain identity-based equality when reused.
+ * <p>
+ * As a qualifier is part of the structure of a symbol, e.g., function symbols, it has to be its own
+ * AST node. Hence, it is part of the structure reached by navigating through
+ * {@link org.key_project.logic.SyntaxElement} and
+ * {@link org.key_project.logic.SyntaxElementCursor}.
+ * </p>
+ *
+ * @param <T> The type of the qualifier object being wrapped.
+ */
 public class Qualifier<T> implements TerminalSyntaxElement {
     private final T qualifier;
 
@@ -16,6 +30,13 @@ public class Qualifier<T> implements TerminalSyntaxElement {
         this.qualifier = qualifier;
     }
 
+    /**
+     * Get the instance for this qualifier. If none exist yet, create one.
+     *
+     * @param qualifier the qualifier for which we create an instance
+     * @return new instance
+     * @param <T> The type of the qualifier object being wrapped.
+     */
     synchronized static <T> Qualifier<T> create(T qualifier) {
         if (INSTANCES.containsKey(qualifier)) {
             return (Qualifier<T>) INSTANCES.get(qualifier);
