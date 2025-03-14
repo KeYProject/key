e8a5bcc452f030238d8d2bffa2aa878ea9d441cd
Alexander Weigl <weigl@kit.edu>
2025-03-14 23:59:56 +0100

    move the package away from macro

---
+++ key.core/src/main/java/de/uka/ilkd/key/scripts/AxiomCommand.java
@@ -0,0 +1,56 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
+package de.uka.ilkd.key.scripts;
+
+import java.util.Map;
+
+import de.uka.ilkd.key.logic.Term;
+import de.uka.ilkd.key.logic.op.SchemaVariable;
+import de.uka.ilkd.key.rule.NoPosTacletApp;
+import de.uka.ilkd.key.rule.Taclet;
+import de.uka.ilkd.key.rule.TacletApp;
+import de.uka.ilkd.key.scripts.meta.Option;
+
+import org.key_project.logic.Name;
+
+/**
+ * The axiom command takes one argument: a formula to which the command is applied.
+ *
+ * @see AssumeCommand The assume command is a synonym for the axiom command.
+ */
+public class AxiomCommand extends AbstractCommand<AxiomCommand.FormulaParameter> {
+    private static final Name TACLET_NAME = new Name("introduceAxiom");
+
+    public AxiomCommand() {
+        super(FormulaParameter.class);
+    }
+
+    @Override
+    public FormulaParameter evaluateArguments(EngineState state, Map<String, Object> arguments)
+            throws Exception {
+        return state.getValueInjector().inject(this, new FormulaParameter(), arguments);
+    }
+
+    @Override
+    public String getName() {
+        return "axiom";
+    }
+
+    @Override
+    public void execute(FormulaParameter parameter) throws ScriptException, InterruptedException {
+        Taclet cut =
+            state.getProof().getEnv().getInitConfigForEnvironment().lookupActiveTaclet(TACLET_NAME);
+        TacletApp app = NoPosTacletApp.createNoPosTacletApp(cut);
+        SchemaVariable sv = app.uninstantiatedVars().iterator().next();
+
+        app = app.addCheckedInstantiation(sv, parameter.formula, state.getProof().getServices(),
+            true);
+        state.getFirstOpenAutomaticGoal().apply(app);
+    }
+
+    public static class FormulaParameter {
+        @Option("#2")
+        public Term formula;
+    }
+}
