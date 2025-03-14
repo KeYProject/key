d2da6ce7a3ebbe35646222aa3b06e95dfea83881
Alexander Weigl <weigl@kit.edu>
2025-03-14 23:33:17 +0100

    revival of this branch:
    * higher-order commands
    * integrate documentation

--- key.core/src/main/java/de/uka/ilkd/key/macros/scripts/AbstractCommand.java
+++ key.core/src/main/java/de/uka/ilkd/key/macros/scripts/AbstractCommand.java
@@ -13,24 +13,32 @@ import de.uka.ilkd.key.macros.scripts.meta.ArgumentsLifter;
 import de.uka.ilkd.key.macros.scripts.meta.ProofScriptArgument;
 import de.uka.ilkd.key.proof.Proof;
 
-/**
- * <p>
- * <b>Inheritance:</b>
- * </p>
- *
- * @param <T>
- * @author Alexander Weigl
- */
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+/// A base class for {@link ProofScriptCommand}s.
+/// This class brings automatically analyzes and meta-data of the given parameter class using
+/// reflection and
+/// decorators.
+///
+/// To implement your own command, you need to define two classes a POJO carrying the parameters,
+/// and child class of
+/// {@link AbstractCommand}. You need to override [AbstractCommand#execute(Object)] to implement the
+/// command logic.
+///
+/// @param <T> the expected parameter class
+/// @author Alexander Weigl
+@NullMarked
 public abstract class AbstractCommand<T> implements ProofScriptCommand<T> {
-    protected Proof proof;
-    protected Services service;
-    protected EngineState state;
-    protected AbstractUserInterfaceControl uiControl;
+    protected @Nullable Proof proof;
+    protected @Nullable Services service;
+    protected @Nullable EngineState state;
+    protected @Nullable AbstractUserInterfaceControl uiControl;
 
     /**
      * Documentation of this command.
      */
-    protected String documentation = null;
+    protected @Nullable String documentation = null;
 
     /**
      * ...
@@ -76,12 +84,13 @@ public abstract class AbstractCommand<T> implements ProofScriptCommand<T> {
         }
     }
 
-    /**
-     * @param args
-     * @throws ScriptException
-     * @throws InterruptedException
-     */
-    protected void execute(T args) throws ScriptException, InterruptedException {}
+    /// Executes the command logic with the given parameters `args`.
+    ///
+    /// @param args an instance of the parameters
+    /// @throws ScriptException if something happened during execution
+    /// @throws InterruptedException if thread was interrupted during execution
+    protected void execute(T args) throws ScriptException, InterruptedException {
+    }
 
     @Override
     public String getDocumentation() {
