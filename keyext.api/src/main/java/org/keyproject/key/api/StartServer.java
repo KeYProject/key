0414f48adebd64c04d1863fef853f6543f1c2650
Samuel Teuber <teuber@kit.edu>
2025-03-24 18:56:43 +0100

    Null serialization (necessary due to python class initialization), node labels

--- keyext.api/src/main/java/org/keyproject/key/api/StartServer.java
+++ keyext.api/src/main/java/org/keyproject/key/api/StartServer.java
@@ -200,8 +200,8 @@ public class StartServer implements Runnable {
     public static void configureJson(GsonBuilder gsonBuilder) {
         gsonBuilder.registerTypeAdapter(File.class, new KeyAdapter.FileTypeAdapter());
         gsonBuilder.registerTypeAdapter(Throwable.class, new KeyAdapter.ThrowableAdapter());
-        gsonBuilder.registerTypeAdapterFactory(
-            RuntimeClassNameTypeAdapterFactory.of(Object.class, "$class"));
+        gsonBuilder.registerTypeAdapterFactory(RuntimeClassNameTypeAdapterFactory.of(Object.class, "$class"));
+        gsonBuilder.serializeNulls();
     }
 
     public static Launcher<ClientApi> launch(OutputStream out, InputStream in, KeyApiImpl keyApi) {
