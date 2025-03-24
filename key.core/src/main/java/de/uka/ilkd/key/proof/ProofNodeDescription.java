c1f5cfa6bc8abe26263c822fd1b916c32a6735bd
Samuel Teuber <teuber@kit.edu>
2025-03-24 18:55:27 +0100

    Extracted node description function

---
+++ key.core/src/main/java/de/uka/ilkd/key/proof/ProofNodeDescription.java
@@ -0,0 +1,42 @@
+package de.uka.ilkd.key.proof;
+
+import java.io.Serializable;
+
+public class ProofNodeDescription implements Serializable {
+    /**
+     * Collects the information from the tree to which branch the current node belongs:
+     * <ul>
+     * <li>Invariant Initially Valid</li>
+     * <li>Body Preserves Invariant</li>
+     * <li>Use Case</li>
+     * <li>...</li>
+     * </ul>
+     *
+     * @param node the current node
+     * @return a String containing the path information to display
+     */
+    public static String collectPathInformation(Node node) {
+        while (node != null) {
+            if (node.getNodeInfo() != null && node.getNodeInfo().getBranchLabel() != null) {
+                String label = node.getNodeInfo().getBranchLabel();
+                /*
+                 * Are there other interesting labels? Please feel free to add them here.
+                 */
+                if (label.equals("Invariant Initially Valid")
+                        || label.equals("Invariant Preserved and Used") // for loop scope invariant
+                        || label.equals("Body Preserves Invariant") || label.equals("Use Case")
+                        || label.equals("Show Axiom Satisfiability") || label.startsWith("Pre (")
+                        || label.startsWith("Exceptional Post (") // exceptional postcondition
+                        || label.startsWith("Post (") // postcondition of a method
+                        || label.contains("Normal Execution") || label.contains("Null Reference")
+                        || label.contains("Index Out of Bounds") || label.contains("Validity")
+                        || label.contains("Precondition") || label.contains("Usage")) {
+                    return label;
+                }
+            }
+            node = node.parent();
+        }
+        // if no label was found we have to prove the postcondition
+        return "Show Postcondition/Modifiable";
+    }
+}
