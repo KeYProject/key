32d83a3a45d614309670d87fd63454853b0f8dcd
Drodt <daniel.drodt@tu-darmstadt.de>
2025-04-30 11:52:01 +0200

    Remove TODO

--- key.ncore.calculus/src/main/java/org/key_project/prover/rules/conditions/NewDependingOn.java
+++ key.ncore.calculus/src/main/java/org/key_project/prover/rules/conditions/NewDependingOn.java
@@ -6,7 +6,6 @@ package org.key_project.prover.rules.conditions;
 import org.key_project.logic.op.sv.SchemaVariable;
 
 /**
- * TODO: remove?
  * <br>
  * class containing a pair of SchemaVariables, the first one being a TermSV, the second one a
  * FormulaSV, representing a "c new depending on phi" statement within a varcond of a taclet
