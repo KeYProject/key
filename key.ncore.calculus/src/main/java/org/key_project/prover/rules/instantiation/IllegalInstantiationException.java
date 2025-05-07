20b4000cef346e8511f4f7c491430d0c5f5762d8
Drodt <daniel.drodt@tu-darmstadt.de>
2025-05-07 13:36:44 +0200

    Remove Java Serialization

--- key.ncore.calculus/src/main/java/org/key_project/prover/rules/instantiation/IllegalInstantiationException.java
+++ key.ncore.calculus/src/main/java/org/key_project/prover/rules/instantiation/IllegalInstantiationException.java
@@ -3,22 +3,12 @@
  * SPDX-License-Identifier: GPL-2.0-only */
 package org.key_project.prover.rules.instantiation;
 
-import java.io.Serial;
 
 /**
  * this exception is thrown if an invalid instantiation for a schema variable was given
  */
 public class IllegalInstantiationException extends RuntimeException {
-
-    /**
-     *
-     */
-    @Serial
-    private static final long serialVersionUID = -9139512430789901488L;
-
     public IllegalInstantiationException(String description) {
         super(description);
     }
-
-
 }
