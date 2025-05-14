d7ff0f3ef2dc009a17f655fb2846ce40b4042743
Drodt <daniel.drodt@tu-darmstadt.de>
2025-05-14 11:11:44 +0200

    Spotless

--- key.ncore.calculus/src/main/java/org/key_project/prover/rules/TacletApplPart.java
+++ key.ncore.calculus/src/main/java/org/key_project/prover/rules/TacletApplPart.java
@@ -10,7 +10,8 @@ import org.key_project.prover.sequent.Sequent;
 import org.key_project.util.collection.ImmutableList;
 
 /**
- * Container for the application part of a Taclet. It contains an assumes-sequent, taclet application restrictions, a list of new
+ * Container for the application part of a Taclet. It contains an assumes-sequent, taclet
+ * application restrictions, a list of new
  * variables and a list of variable pairs indicating the NotFreeIn relation, a list of
  * NewDependingOn relations, and a list of general variable conditions.
  */
