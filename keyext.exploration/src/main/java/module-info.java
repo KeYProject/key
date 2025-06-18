31a957f5082ed56bc8b90160dfd41dd88676fcd8
Alexander Weigl <weigl@kit.edu>
2025-06-18 18:37:53 +0200

    added module-info.java
    error in docking frames version, cannot find resource bundle

--- keyext.exploration/src/main/java/module-info.java
+++ keyext.exploration/src/main/java/module-info.java
@@ -4,4 +4,12 @@
  * @version 1 (31.03.24)
  */
 module keyext.exploration {
+    requires java.desktop;
+    requires org.key_project.ui;
+    requires org.key_project.core;
+    requires org.jspecify;
+    requires org.key_project.prover;
+    requires org.key_project.ncore;
+    requires org.key_project.util;
+    requires dockingframes.common;
 }
