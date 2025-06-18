bca82b7525f8a7e302bad9566913277941b56a61
Alexander Weigl <weigl@kit.edu>
2025-06-18 18:25:58 +0200

    works no, freemarker is strange with null values

--- keyext.proofmanagement/src/main/java/org/key_project/proofmanagement/io/ProofBundleHandler.java
+++ keyext.proofmanagement/src/main/java/org/key_project/proofmanagement/io/ProofBundleHandler.java
@@ -14,6 +14,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
+import org.jspecify.annotations.Nullable;
 import org.key_project.proofmanagement.check.PathNode;
 import org.key_project.proofmanagement.check.ProofManagementException;
 
@@ -115,7 +116,7 @@ public abstract class ProofBundleHandler implements Closeable {
      * @return the bootclasspath or null, if none is specified
      * @throws IOException if the bundle can not be opened/accessed
      */
-    public abstract Path getBootclasspath() throws IOException;
+    public abstract @Nullable Path getBootclasspath() throws IOException;
 
     /**
      * Returns a tree of the complete file hierarchy inside the bundle.
