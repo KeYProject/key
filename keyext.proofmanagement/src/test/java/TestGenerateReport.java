2ec28a3f0521820b61dc8cefd57b121d6d834903
Alexander Weigl <weigl@kit.edu>
2025-06-18 15:57:38 +0200

    a working simplified version with freemarker

--- keyext.proofmanagement/src/test/java/TestGenerateReport.java
+++ keyext.proofmanagement/src/test/java/TestGenerateReport.java
@@ -1,9 +1,14 @@
-import de.uka.ilkd.key.util.HelperClassForTests;
-import org.junit.jupiter.api.Test;
-import org.key_project.proofmanagement.Main;
-
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
 import java.io.IOException;
 import java.nio.file.Files;
+
+import de.uka.ilkd.key.util.HelperClassForTests;
+
+import org.key_project.proofmanagement.Main;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * @author Alexander Weigl
