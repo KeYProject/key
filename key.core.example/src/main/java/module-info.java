619b04b0a6beb9e48341c33409de1aae203aad08
Alexander Weigl <weigl@kit.edu>
2025-06-19 13:17:51 +0200

    fix dockingframes, check naming
    # Conflicts:
    #	build.gradle
    #	key.core.example/src/main/java/org/key_project/Main.java

--- key.core.example/src/main/java/module-info.java
+++ key.core.example/src/main/java/module-info.java
@@ -1,7 +1,14 @@
+import org.jspecify.annotations.NullMarked;
+
 /**
  *
  * @author Alexander Weigl 
  * @version 1 (31.03.24)
  */
-module org.key_project.core.example {
+@NullMarked
+module key.core.example {
+    requires org.jspecify;
+    requires org.slf4j;
+    requires key.core;
+    requires org.key_project.util;
 }
