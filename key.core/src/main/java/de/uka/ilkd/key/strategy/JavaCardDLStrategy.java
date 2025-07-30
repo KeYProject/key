7988fb5efebaa59744ba983273d0c70bab92f1aa
Drodt <daniel.drodt@tu-darmstadt.de>
2025-07-30 09:42:22 +0200

    Modularize int assign rules

--- key.core/src/main/java/de/uka/ilkd/key/strategy/JavaCardDLStrategy.java
+++ key.core/src/main/java/de/uka/ilkd/key/strategy/JavaCardDLStrategy.java
@@ -184,9 +184,6 @@ public class JavaCardDLStrategy extends AbstractFeatureStrategy {
         bindRuleSet(d, "simplify", -4500);
         bindRuleSet(d, "simplify_enlarging", -2000);
         bindRuleSet(d, "simplify_ENLARGING", -1900);
-        bindRuleSet(d, "simplify_expression", -100);
-        bindRuleSet(d, "executeIntegerAssignment", -100);
-        bindRuleSet(d, "executeDoubleAssignment", -100);
         bindRuleSet(d, "simplify_int", inftyConst());
 
         bindRuleSet(d, "javaIntegerSemantics",
