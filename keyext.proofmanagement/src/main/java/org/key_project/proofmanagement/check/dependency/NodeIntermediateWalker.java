2f35dcfbc74762b5d3ff28eb92b61a9276655a71
PiisRational <gecho.gaecho@gmail.com>
2025-09-27 15:58:12 +0200

    add the proofs

--- keyext.proofmanagement/src/main/java/org/key_project/proofmanagement/check/dependency/NodeIntermediateWalker.java
+++ keyext.proofmanagement/src/main/java/org/key_project/proofmanagement/check/dependency/NodeIntermediateWalker.java
@@ -5,6 +5,9 @@ package org.key_project.proofmanagement.check.dependency;
 
 import de.uka.ilkd.key.proof.io.intermediate.NodeIntermediate;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * Walks an intermediate proof representation tree as created when loading a *.proof file.
  *
@@ -13,7 +16,7 @@ import de.uka.ilkd.key.proof.io.intermediate.NodeIntermediate;
  */
 public abstract class NodeIntermediateWalker {
     /** the root where the walker starts */
-    private NodeIntermediate root;
+    private final NodeIntermediate root;
 
     /**
      * create a walker starting from the given root
@@ -26,19 +29,38 @@ public abstract class NodeIntermediateWalker {
 
     /** starts the walker */
     public void start() {
-        walk(root);
+        walkIteratively();
     }
 
     /**
-     * walks the tree while performing specified action
+     * Walks the tree while performing specified action.
+     *
+     * @deprecated Might run into stack overflow for medium to long proofs, use
+     * {@link #walkIteratively()} instead.    
      *
      * @param node the current position of the walker in tree
      */
-    protected void walk(NodeIntermediate node) {
+    @Deprecated()
+    protected void walkRecursively(NodeIntermediate node) {
         doAction(node);
 
         for (NodeIntermediate child : node.getChildren()) {
-            walk(child);
+            walkRecursively(child);
+        }
+    }
+
+    /**
+     * Walks the tree while performing specified action. This iterative variant avoids stack
+     * overflows and is thus preferred. It performs a breadth-first search traversal.
+     */
+    protected void walkIteratively () {
+        Deque<NodeIntermediate> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            NodeIntermediate node = queue.pollFirst();
+            doAction(node);
+            queue.addAll(node.getChildren());
         }
     }
 
