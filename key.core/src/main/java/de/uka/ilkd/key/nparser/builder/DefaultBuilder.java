4a48b8f324203eac110718819a5523effed3a072
Mattias Ulbrich <ulbrich@kit.edu>
2025-10-10 15:52:13 +0200

    introduce match identifiers with leading '?'

--- key.core/src/main/java/de/uka/ilkd/key/nparser/builder/DefaultBuilder.java
+++ key.core/src/main/java/de/uka/ilkd/key/nparser/builder/DefaultBuilder.java
@@ -292,7 +292,7 @@ public class DefaultBuilder extends AbstractBuilder<Object> {
 
     @Override
     public String visitSimple_ident(KeYParser.Simple_identContext ctx) {
-        return ctx.IDENT().getText();
+        return ctx.id.getText();
     }
 
     @Override
