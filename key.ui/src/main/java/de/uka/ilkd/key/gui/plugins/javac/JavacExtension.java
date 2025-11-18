74af1d7a68d15eb278cad28dc7cb1d2c51225750
PiisRational <gecho.gaecho@gmail.com>
2025-11-18 20:59:07 +0100

    save proof independent settings, that are not used by the configuration

--- key.ui/src/main/java/de/uka/ilkd/key/gui/plugins/javac/JavacExtension.java
+++ key.ui/src/main/java/de/uka/ilkd/key/gui/plugins/javac/JavacExtension.java
@@ -178,6 +178,9 @@ public class JavacExtension
                     updateLabel(data);
                 })).get();
             } catch (InterruptedException | ExecutionException ex) {
+                lblStatus.setForeground(COLOR_ERROR.get());
+                lblStatus.setIcon(ICON_ERROR.get(16));
+                lblStatus.setText("Javac error");
                 throw new RuntimeException(ex);
             }
         }
