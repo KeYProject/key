bd374029209e303557a62e2707c460836db9059d
Alexander Weigl <weigl@kit.edu>
2025-11-22 14:08:30 +0100

    fixing separation issues
    * if IF is missing and a IF contract loaded a warning is
      on first discovery.

--- key.core/src/main/java/de/uka/ilkd/key/speclang/jml/translation/JMLSpecFactory.java
+++ key.core/src/main/java/de/uka/ilkd/key/speclang/jml/translation/JMLSpecFactory.java
@@ -180,7 +180,9 @@ public class JMLSpecFactory {
                     clauses.requires.get(heap), clauses.requiresFree.get(heap), clauses.measuredBy,
                     clauses.assignables.get(heap), !clauses.hasAssignable.get(heap), progVars,
                     clauses.accessibles.get(heap), clauses.infFlowSpecs, false);
-                symbDatas = symbDatas.add(symbData);
+                if (symbData != null) {
+                    symbDatas = symbDatas.add(symbData);
+                }
             } else if (clauses.diverges.equals(tb.tt())) {
                 InformationFlowContract symbData = cf.createInformationFlowContract(
                     pm.getContainerType(), pm, pm.getContainerType(),
@@ -188,7 +190,9 @@ public class JMLSpecFactory {
                     clauses.requires.get(heap), clauses.requiresFree.get(heap), clauses.measuredBy,
                     clauses.assignables.get(heap), !clauses.hasAssignable.get(heap), progVars,
                     clauses.accessibles.get(heap), clauses.infFlowSpecs, false);
-                symbDatas = symbDatas.add(symbData);
+                if (symbData != null) {
+                    symbDatas = symbDatas.add(symbData);
+                }
             } else {
                 InformationFlowContract symbData1 = cf.createInformationFlowContract(
                     pm.getContainerType(), pm, pm.getContainerType(),
@@ -197,13 +201,19 @@ public class JMLSpecFactory {
                     clauses.requiresFree.get(heap), clauses.measuredBy,
                     clauses.assignables.get(heap), !clauses.hasAssignable.get(heap), progVars,
                     clauses.accessibles.get(heap), clauses.infFlowSpecs, false);
+                if (symbData1 != null) {
+                    symbDatas = symbDatas.add(symbData1);
+                }
+
                 InformationFlowContract symbData2 = cf.createInformationFlowContract(
                     pm.getContainerType(), pm, pm.getContainerType(),
                     JModality.JavaModalityKind.BOX,
                     clauses.requires.get(heap), clauses.requiresFree.get(heap), clauses.measuredBy,
                     clauses.assignables.get(heap), !clauses.hasAssignable.get(heap), progVars,
                     clauses.accessibles.get(heap), clauses.infFlowSpecs, false);
-                symbDatas = symbDatas.add(symbData1).add(symbData2);
+                if (symbData2 != null) {
+                    symbDatas = symbDatas.add(symbData2);
+                }
             }
         }
         return symbDatas;
