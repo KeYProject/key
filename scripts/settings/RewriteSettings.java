82693565907ac18c9bc5efdf80ead107163870bc
Alexander Weigl <weigl@kit.edu>
2025-11-22 16:29:59 +0100

    added gzip support

--- scripts/settings/RewriteSettings.java
+++ scripts/settings/RewriteSettings.java
@@ -2,20 +2,22 @@
  * KeY is licensed under the GNU General Public License Version 2
  * SPDX-License-Identifier: GPL-2.0-only */
 
+import de.uka.ilkd.key.nparser.KeYLexer;
+import de.uka.ilkd.key.nparser.ParsingFacade;
+import de.uka.ilkd.key.settings.ProofSettings;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
+
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Iterator;
-
-import de.uka.ilkd.key.nparser.KeYLexer;
-import de.uka.ilkd.key.nparser.ParsingFacade;
-import de.uka.ilkd.key.settings.ProofSettings;
-
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.Token;
-import org.antlr.v4.runtime.misc.ParseCancellationException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 /// This is a java program for rewriting the settings of KeY files from the old format to the new format.
 /// The old format was a string contain a properties file (`key=value`). The new format is JSON-like tree structure
@@ -25,12 +27,12 @@ import org.antlr.v4.runtime.misc.ParseCancellationException;
 /// 1. Compile key to receive a fatjar
 /// ```sh
 /// key> gradle assemble
-/// ```
+///```
 ///
 /// 2. Run this program using the shadow jar. Requires a rather new JDK/JRE:
 /// ```sh
 /// java -cp ../../key.ui/build/libs/key-2.12.4-dev-exe.jar RewriteSettings.java <files>
-/// ```
+///```
 /// (no compilation needed)
 ///
 /// @author Alexander Weigl
@@ -38,18 +40,18 @@ import org.antlr.v4.runtime.misc.ParseCancellationException;
 public class RewriteSettings {
     public static void main(String[] args) throws IOException {
         if (args.length == 0) {
-            args = new String[] {
-                "../../key.core/src/test/resources/testcase/parser/MultipleRecursion/MultipleRecursion[MultipleRecursion__a()]_JML_normal_behavior_operation_contract_0.proof" };
+            args = new String[]{
+                    "../../key.core/src/test/resources/testcase/parser/MultipleRecursion/MultipleRecursion[MultipleRecursion__a()]_JML_normal_behavior_operation_contract_0.proof"};
         }
 
         for (String arg : args) {
             var path = Paths.get(arg);
             var files = Files.isDirectory(
-                path) ? Files.walk(path).filter(it -> Files.isRegularFile(it)
-                        && (it.getFileName().toString().endsWith(".key") ||
-                                it.getFileName().toString().endsWith(".proof")))
-                        .toList()
-                        : Collections.singletonList(path);
+                    path) ? Files.walk(path).filter(it -> Files.isRegularFile(it)
+                            && (it.getFileName().toString().endsWith(".key") ||
+                            it.getFileName().toString().endsWith(".proof")))
+                    .toList()
+                    : Collections.singletonList(path);
             for (var file : files) {
                 rewrite(file);
             }
@@ -58,13 +60,20 @@ public class RewriteSettings {
     }
 
     private static void rewrite(Path file) throws IOException {
-        var lex = ParsingFacade.createLexer(file);
+        boolean isGzip = file.getFileName().toString().endsWith(".gz");
+        KeYLexer lex;
+        if (isGzip) {
+            var input = CharStreams.fromStream(new GZIPInputStream(Files.newInputStream(file)));
+            lex = ParsingFacade.createLexer(input);
+        } else {
+            lex = ParsingFacade.createLexer(file);
+        }
         lex.setProofIsEOF(false);
         var ctx = lex.getAllTokens();
         var output = new StringBuilder();
 
         boolean hit = false;
-        for (Iterator<? extends Token> iterator = ctx.iterator(); iterator.hasNext();) {
+        for (Iterator<? extends Token> iterator = ctx.iterator(); iterator.hasNext(); ) {
             var token = iterator.next();
             if (token.getType() == KeYLexer.KEYSETTINGS) {
                 output.append(token.getText());
@@ -99,7 +108,13 @@ public class RewriteSettings {
 
         try {
             ParsingFacade.parseFile(CharStreams.fromString(output.toString()));
-            Files.writeString(file, output.toString());
+            if (!isGzip) {
+                Files.writeString(file, output.toString());
+            } else {
+                try (var out = new GZIPOutputStream(Files.newOutputStream(file))) {
+                    out.write(output.toString().getBytes(Charset.defaultCharset()));
+                }
+            }
         } catch (ParseCancellationException e) {
             System.err.printf("Error parsing after rewrite file %s: %s", file, e.getMessage());
             System.err.println(output);
@@ -107,3 +122,4 @@ public class RewriteSettings {
         }
     }
 }
+
