7b42bfa2c1be182ef7bad2e9ad6783031b0d3d70
Alexander Weigl <weigl@kit.edu>
2025-11-28 03:00:00 +0100

    fixing the missing javadoc in the api generator

--- keyext.api/src/main/java/org/keyproject/key/api/remoteclient/ShowDocumentParams.java
+++ keyext.api/src/main/java/org/keyproject/key/api/remoteclient/ShowDocumentParams.java
@@ -3,35 +3,27 @@
  * SPDX-License-Identifier: GPL-2.0-only */
 package org.keyproject.key.api.remoteclient;
 
-import de.uka.ilkd.key.pp.Range;
+import org.jspecify.annotations.Nullable;
+import org.keyproject.key.api.data.TextRange;
+
+/**
+ * @param uri The uri to show.
+ * @param external Indicates to show the resource in an external program.
+ *        To show, for example, `https://code.visualstudio.com/`
+ *        in the default WEB browser set `external` to `true`.
+ * @param takeFocus An optional property to indicate whether the editor
+ *        showing the document should take focus or not.
+ *        Clients might ignore this property if an external
+ *        program is started.
+ * @param selection An optional selection range if the document is a text
+ *        document. Clients might ignore the property if an
+ *        external program is started or the file is not a text
+ *        file.
+ */
 
 public record ShowDocumentParams(
-        /**
-         * The uri to show.
-         */
         String uri,
-
-        /**
-         * Indicates to show the resource in an external program.
-         * To show, for example, `https://code.visualstudio.com/`
-         * in the default WEB browser set `external` to `true`.
-         */
-        Boolean external,
-
-        /**
-         * An optional property to indicate whether the editor
-         * showing the document should take focus or not.
-         * Clients might ignore this property if an external
-         * program is started.
-         */
-        Boolean takeFocus,
-
-        /**
-         * An optional selection range if the document is a text
-         * document. Clients might ignore the property if an
-         * external program is started or the file is not a text
-         * file.
-         */
-        Range selection) {
-
+        boolean external,
+        boolean takeFocus,
+        @Nullable TextRange selection) {
 }
