5b54ef6d1e23bd1bb20bdb505bdb6f30d73e8369
Drodt <daniel.drodt@tu-darmstadt.de>
2026-03-19 10:22:53 +0100

    Add comment

--- key.core/src/main/java/de/uka/ilkd/key/logic/sort/SortAlias.java
+++ key.core/src/main/java/de/uka/ilkd/key/logic/sort/SortAlias.java
@@ -7,5 +7,10 @@ import org.key_project.logic.Name;
 import org.key_project.logic.Named;
 import org.key_project.logic.sort.Sort;
 
+/// An alias for a sort, allowing for more ergonomic usage of parametric sorts. E.g., `LocSet` may
+/// be defined in the future as `\alias LocSet = Set<[Tuple2<[Object, Field]>]>`.
+/// These aliases are managed by the Services, where sort lookup takes them into account and
+/// directly resolves to the aliased sort.
+/// Hence, aliases are syntactic sugar in KeY files and are not directly part of the logic.
 public record SortAlias(Name name, Sort aliasedSort) implements Named {
 }
