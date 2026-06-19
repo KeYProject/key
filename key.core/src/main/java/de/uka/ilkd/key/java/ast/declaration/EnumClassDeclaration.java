9fc257a94216b37ac3cdaf57cd83b572ede176d6
Alexander Weigl <wadoon@users.noreply.github.com>
2026-06-19 19:02:10 +0200

    Enum Class Declaration fix
    # Conflicts:
    #	key.core/src/main/resources/de/uka/ilkd/key/proof/rules/java5.key

--- key.core/src/main/java/de/uka/ilkd/key/java/ast/declaration/EnumClassDeclaration.java
+++ key.core/src/main/java/de/uka/ilkd/key/java/ast/declaration/EnumClassDeclaration.java
@@ -3,17 +3,19 @@
  * SPDX-License-Identifier: GPL-2.0-only */
 package de.uka.ilkd.key.java.ast.declaration;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import de.uka.ilkd.key.java.ast.abstraction.KeYJavaType;
 import de.uka.ilkd.key.java.ast.abstraction.Type;
 import de.uka.ilkd.key.logic.JavaDLFieldNames;
 import de.uka.ilkd.key.logic.ProgramElementName;
 import de.uka.ilkd.key.logic.op.IProgramVariable;
-import de.uka.ilkd.key.logic.op.ProgramVariable;
 
 import org.key_project.util.ExtList;
+
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
 
 /**
  * This class is used for wrapping an enum into a standard class type.
@@ -22,27 +24,30 @@ import org.key_project.util.ExtList;
  * In addition the programvariables that represent enum constants are memorized. Thus this class is
  * able to have queries on the enum constants.
  *
+ * mulbrich: Update 2025 (a mere 19 years later):
+ * Updated from the old heap model to the new one.
+ *
  * @author mulbrich
- * @since 2006-12-10
+ * @since 2006-12-10, updated 2025-10-24 by MU
  */
 
 public class EnumClassDeclaration extends ClassDeclaration {
 
     /**
      * store the program variables which represent the enum constants
+     * in a lookup map from name to (ordinal index, program variable)
      */
-    private final List<IProgramVariable> constants = new ArrayList<>();
+    private final Map<String, Pair<@NonNull Integer, @NonNull IProgramVariable>> constants =
+        new HashMap<>();
 
     /**
      * create a new EnumClassDeclaration that describes an enum defintion. It merely wraps a
      * ClassDeclaration but has memory about which fields have been declared as enum constants.
      *
-     * @param children
-     *        children in the ast (members)
-     * @param fullName
-     *        of the class/enum
-     * @param isLibrary
-     *        see class constructor
+     * @param children children in the ast (members)
+     * @param fullName of the class/enum
+     * @param isLibrary see class constructor
+     * @param enumConstantDeclarations the declarations for the enum constants
      */
     // TODO javaparser
     public EnumClassDeclaration(
@@ -50,10 +55,12 @@ public class EnumClassDeclaration extends ClassDeclaration {
     /* , List<EnumConstantDeclaration> enumConstantDeclarations */) {
         super(children, fullName, isLibrary);
 
-        // for (EnumConstantDeclaration ecd : enumConstantDeclarations) {
-        // String constName = ecd.getEnumConstantSpecification().getName();
-        // constants.add(findAttr(constName));
-        // }
+        int ordinal = 0;
+        for (EnumConstantDeclaration ecd : enumConstantDeclarations) {
+            String constName = ecd.getEnumConstantSpecification().getName();
+            constants.put(constName, new Pair<>(ordinal, findAttr(constName)));
+            ordinal++;
+        }
     }
 
     /*
@@ -142,4 +149,16 @@ public class EnumClassDeclaration extends ClassDeclaration {
         }
     }
 
+
+    /**
+     * get the constant with the given name, including its ordinal index.
+     *
+     * @param fieldName the name of the enum constant
+     * @return a pair of (index, program variable) of the enum constant with the given name or null
+     *         if there is no such constant
+     */
+    public @Nullable Pair<@NonNull Integer, @NonNull IProgramVariable> getConstant(
+            String fieldName) {
+        return constants.get(fieldName);
+    }
 }
