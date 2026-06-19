171497975d2a23ae5d6f7b0b10cb053b3235f1b1
Alexander Weigl <weigl@kit.edu>
2026-06-19 09:06:34 +0200

    rename key.symbex

--- key.core.symbolic_execution/src/main/java/de/uka/ilkd/key/symbolic_execution/label/LoopInvariantNormalBehaviorTermLabelUpdate.java
+++ key.core.symbolic_execution/src/main/java/de/uka/ilkd/key/symbolic_execution/label/LoopInvariantNormalBehaviorTermLabelUpdate.java
@@ -1,7 +1,7 @@
 /* This file is part of KeY - https://key-project.org
  * KeY is licensed under the GNU General Public License Version 2
  * SPDX-License-Identifier: GPL-2.0-only */
-package de.uka.ilkd.key.rule.label;
+package de.uka.ilkd.key.symbolic_execution.label;
 
 import java.util.Set;
 
@@ -20,12 +20,12 @@ import org.key_project.util.collection.ImmutableList;
 import org.key_project.util.collection.ImmutableSLList;
 
 /**
- * Makes sure that {@link SymbolicExecutionUtil#LOOP_BODY_LABEL} is introduced when a
- * {@link WhileInvariantRule} is applied.
+ * Makes sure that {@link SymbolicExecutionUtil#LOOP_INVARIANT_NORMAL_BEHAVIOR_LABEL} is introduced
+ * when a {@link WhileInvariantRule} is applied.
  *
  * @author Martin Hentschel
  */
-public class LoopBodyTermLabelUpdate implements TermLabelUpdate {
+public class LoopInvariantNormalBehaviorTermLabelUpdate implements TermLabelUpdate {
     /**
      * {@inheritDoc}
      */
@@ -42,9 +42,9 @@ public class LoopBodyTermLabelUpdate implements TermLabelUpdate {
             PosInOccurrence applicationPosInOccurrence, JTerm applicationTerm, JTerm modalityTerm,
             Rule rule, RuleApp ruleApp, Object hint, JTerm tacletTerm, JTerm newTerm,
             Set<TermLabel> labels) {
-        if (rule instanceof WhileInvariantRule && "LoopBodyModality".equals(hint)
+        if (rule instanceof WhileInvariantRule && "LoopBodyImplication".equals(hint)
                 && SymbolicExecutionUtil.hasSymbolicExecutionLabel(modalityTerm)) {
-            labels.add(SymbolicExecutionUtil.LOOP_BODY_LABEL);
+            labels.add(SymbolicExecutionUtil.LOOP_INVARIANT_NORMAL_BEHAVIOR_LABEL);
         }
     }
 }
