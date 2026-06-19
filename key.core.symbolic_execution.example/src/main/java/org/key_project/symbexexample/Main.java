a138ce114baff3f3866a845964312a30b519b957
Alexander Weigl <weigl@kit.edu>
2026-06-19 09:03:48 +0200

    rename key.core.symbolic_execution.example

--- key.core.symbolic_execution.example/src/main/java/org/key_project/symbexexample/Main.java
+++ key.core.symbolic_execution.example/src/main/java/org/key_project/symbexexample/Main.java
@@ -1,7 +1,7 @@
 /* This file is part of KeY - https://key-project.org
  * KeY is licensed under the GNU General Public License Version 2
  * SPDX-License-Identifier: GPL-2.0-only */
-package org.key_project.example;
+package org.key_project.symbexexample;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
