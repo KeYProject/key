12372fc6da8ba87a95019451e9491b9cd858ef15
Alexander Weigl <weigl@kit.edu>
2026-07-12 22:16:25 +0200

    fix test cases, adapt Java-Redux

--- key.core/src/main/java/de/uka/ilkd/key/java/transformations/pipeline/TryWithResourceReducer.java
+++ key.core/src/main/java/de/uka/ilkd/key/java/transformations/pipeline/TryWithResourceReducer.java
@@ -1,4 +1,10 @@
+/* This file is part of KeY - https://key-project.org
+ * KeY is licensed under the GNU General Public License Version 2
+ * SPDX-License-Identifier: GPL-2.0-only */
 package de.uka.ilkd.key.java.transformations.pipeline;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.Parameter;
@@ -10,41 +16,39 @@ import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
 
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-
 /// Rewrites every try-with-resources statement into the equivalent nested
 /// try/finally form that javac itself generates, i.e. for
 /// ```java
 /// try (Resource r = open()) {
-///     body;
+/// body;
 /// }
 /// ```
 /// this produces:
 /// ```java
 /// try {
-///     Resource r = open();
-///     Throwable primaryExc$1 = null;
-///     try {
-///         body;
-///     } catch (Throwable t$1) {
-///         primaryExc$1 = t$1;
-///         throw t$1;
-///     } finally {
-///         if (r != null) {
-///             if (primaryExc$1 != null) {
-///                 try { r.close(); } catch (Throwable suppressedExc$1) {
-///                     primaryExc$1.addSuppressed(suppressedExc$1);
-///                 }
-///             } else { r.close(); }
-///         }
-///     }
+/// Resource r = open();
+/// Throwable primaryExc$1 = null;
+/// try {
+/// body;
+/// } catch (Throwable t$1) {
+/// primaryExc$1 = t$1;
+/// throw t$1;
+/// } finally {
+/// if (r != null) {
+/// if (primaryExc$1 != null) {
+/// try { r.close(); } catch (Throwable suppressedExc$1) {
+/// primaryExc$1.addSuppressed(suppressedExc$1);
+/// }
+/// } else { r.close(); }
+/// }
+/// }
 /// }
 /// ```
 /// Multiple resources are handled by nesting (last-declared resource closes first),
 /// and any catch/finally clauses that were attached to the original try are kept
 /// on an outer try that wraps the whole resource-management block, exactly as
 /// javac does it.
+///
 /// @author weigl, Claude Sonnet
 public class TryWithResourceReducer implements JavaTransformer {
     private final AtomicInteger counter = new AtomicInteger();
@@ -91,18 +95,17 @@ public class TryWithResourceReducer implements JavaTransformer {
             BlockStmt result = new BlockStmt();
 
             Expression resourceExpr = resources.get(index);
-            String resourceName;
+            Expression resourceName;
 
-            if (resourceExpr instanceof VariableDeclarationExpr) {
+            if (resourceExpr instanceof VariableDeclarationExpr vde) {
                 // try (Resource r = new Resource()) { ... }
-                VariableDeclarationExpr vde = (VariableDeclarationExpr) resourceExpr;
                 VariableDeclarator vd = vde.getVariable(0);
-                resourceName = vd.getNameAsString();
+                resourceName = vd.getNameAsExpression();
                 result.addStatement(new ExpressionStmt(vde.clone()));
             } else {
                 // Java 9+ form: try (existingEffectivelyFinalVar) { ... }
                 // No declaration needed -- the variable already exists in scope.
-                resourceName = resourceExpr.toString();
+                resourceName = resourceExpr;
             }
 
             int id = counter.incrementAndGet();
@@ -118,14 +121,17 @@ public class TryWithResourceReducer implements JavaTransformer {
 
             // catch (Throwable t$n) { primaryExc$n = t$n; throw t$n; }
             CatchClause catchClause = new CatchClause();
-            catchClause.setParameter(new Parameter(new ClassOrInterfaceType(null, "Throwable"), throwableName));
+            catchClause.setParameter(
+                new Parameter(new ClassOrInterfaceType(null, "Throwable"), throwableName));
             BlockStmt catchBody = new BlockStmt();
             catchBody.addStatement(new ExpressionStmt(
-                    new AssignExpr(new NameExpr(primaryExcName), new NameExpr(throwableName), AssignExpr.Operator.ASSIGN)));
+                new AssignExpr(new NameExpr(primaryExcName), new NameExpr(throwableName),
+                    AssignExpr.Operator.ASSIGN)));
             catchBody.addStatement(new ThrowStmt(new NameExpr(throwableName)));
             catchClause.setBody(catchBody);
 
-            // finally { if (r != null) { if (primaryExc$n != null) { try { r.close(); } catch (Throwable s) { primaryExc$n.addSuppressed(s); } } else { r.close(); } } }
+            // finally { if (r != null) { if (primaryExc$n != null) { try { r.close(); } catch
+            // (Throwable s) { primaryExc$n.addSuppressed(s); } } else { r.close(); } } }
             BlockStmt finallyBlock = new BlockStmt();
 
             IfStmt excCheck = new IfStmt();
@@ -137,10 +143,12 @@ public class TryWithResourceReducer implements JavaTransformer {
             closeWithSuppression.setTryBlock(closeTryBlock);
 
             CatchClause suppressCatch = new CatchClause();
-            suppressCatch.setParameter(new Parameter(new ClassOrInterfaceType(null, "Throwable"), suppressedExcName));
+            suppressCatch.setParameter(
+                new Parameter(new ClassOrInterfaceType(null, "Throwable"), suppressedExcName));
             BlockStmt suppressBody = new BlockStmt();
             suppressBody.addStatement(new ExpressionStmt(new MethodCallExpr(
-                    new NameExpr(primaryExcName), "addSuppressed", NodeList.nodeList(new NameExpr(suppressedExcName)))));
+                new NameExpr(primaryExcName), "addSuppressed",
+                NodeList.nodeList(new NameExpr(suppressedExcName)))));
             suppressCatch.setBody(suppressBody);
             closeWithSuppression.setCatchClauses(NodeList.nodeList(suppressCatch));
 
@@ -151,7 +159,7 @@ public class TryWithResourceReducer implements JavaTransformer {
             excCheck.setElseStmt(plainClose);
 
             IfStmt nullCheck = new IfStmt();
-            nullCheck.setCondition(notNull(new NameExpr(resourceName)));
+            nullCheck.setCondition(notNull(resourceName.clone()));
             BlockStmt nullCheckThen = new BlockStmt();
             nullCheckThen.addStatement(excCheck);
             nullCheck.setThenStmt(nullCheckThen);
@@ -171,12 +179,14 @@ public class TryWithResourceReducer implements JavaTransformer {
             return new BinaryExpr(e, new NullLiteralExpr(), BinaryExpr.Operator.NOT_EQUALS);
         }
 
-        private static MethodCallExpr closeCall(String resourceName) {
-            return new MethodCallExpr(new NameExpr(resourceName), "close");
+        private static MethodCallExpr closeCall(Expression resourceName) {
+            return new MethodCallExpr(resourceName.clone(), "close");
         }
 
-        private static Statement declareVar(String typeName, String varName, Expression initializer) {
-            VariableDeclarator vd = new VariableDeclarator(new ClassOrInterfaceType(null, typeName), varName, initializer);
+        private static Statement declareVar(String typeName, String varName,
+                Expression initializer) {
+            VariableDeclarator vd = new VariableDeclarator(new ClassOrInterfaceType(null, typeName),
+                varName, initializer);
             return new ExpressionStmt(new VariableDeclarationExpr(vd));
         }
     }
