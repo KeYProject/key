24def5aae90d354d0eef08acd3f7658b9047c7e5
Alexander Weigl <weigl@kit.edu>
2026-07-23 23:27:52 +0200

    disable transformations

--- key.core/src/main/java/de/uka/ilkd/key/java/transformations/KeYJavaPipeline.java
+++ key.core/src/main/java/de/uka/ilkd/key/java/transformations/KeYJavaPipeline.java
@@ -38,11 +38,11 @@ public class KeYJavaPipeline {
 
     public static KeYJavaPipeline createDefault(TransformationPipelineServices pipelineServices) {
         KeYJavaPipeline p = new KeYJavaPipeline(pipelineServices);
-        p.add(new TextblockTransformer());
-        p.add(new MultiCatchReducer());
-        p.add(new TryWithResourceReducer());
+        //        p.add(new TextblockTransformer());
+        //p.add(new MultiCatchReducer());
+        //p.add(new TryWithResourceReducer());
         p.add(new EnumClassBuilder(pipelineServices));
-        p.add(new RecordClassBuilder(pipelineServices));
+        //p.add(new RecordClassBuilder(pipelineServices));
         p.add(new JMLTransformer(pipelineServices));
         p.add(new JmlDocRemoval(pipelineServices));
         p.add(new ImplicitFieldAdder(pipelineServices));
