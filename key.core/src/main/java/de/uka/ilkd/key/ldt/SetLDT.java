222809178bee2feb1a90418c9f7683de04e976e0
Drodt <daniel.drodt@tu-darmstadt.de>
2026-08-12 08:36:46 +0200

    Start locset as set

---
+++ key.core/src/main/java/de/uka/ilkd/key/ldt/SetLDT.java
@@ -0,0 +1,74 @@
+package de.uka.ilkd.key.ldt;
+
+import de.uka.ilkd.key.java.Services;
+import de.uka.ilkd.key.java.ast.expression.Expression;
+import de.uka.ilkd.key.java.ast.expression.Operator;
+import de.uka.ilkd.key.java.ast.expression.literal.Literal;
+import de.uka.ilkd.key.java.ast.reference.ExecutionContext;
+import de.uka.ilkd.key.logic.JTerm;
+import de.uka.ilkd.key.logic.TermServices;
+import de.uka.ilkd.key.logic.op.ParametricFunctionDecl;
+import org.jspecify.annotations.Nullable;
+import org.key_project.logic.Name;
+import org.key_project.logic.op.Function;
+import org.key_project.util.ExtList;
+
+public final class SetLDT extends ParametricLDT {
+    public static final Name NAME = new Name("Set");
+
+    private final ParametricFunctionDecl sEmpty;
+    private final ParametricFunctionDecl sSingleton;
+    private final ParametricFunctionDecl sUnion;
+    private final ParametricFunctionDecl sIntersect;
+    private final ParametricFunctionDecl sSetMinus;
+    private final ParametricFunctionDecl sInfiniteUnion;
+    private final ParametricFunctionDecl sCard;
+
+
+    public SetLDT(TermServices services) {
+        super(NAME, services);
+
+        sEmpty = addParametricFunction(services,"sEmpty");
+        sSingleton = addParametricFunction(services,"sSingleton");
+        sUnion = addParametricFunction(services,"sUnion");
+        sIntersect = addParametricFunction(services,"sIntersect");
+        sSetMinus = addParametricFunction(services,"sSetMinus");
+        sInfiniteUnion = addParametricFunction(services,"sInfiniteUnion");
+        sCard = addParametricFunction(services,"sCard");
+    }
+
+    @Override
+    public boolean isResponsible(Operator op, JTerm[] subs, Services services, ExecutionContext ec) {
+        return false;
+    }
+
+    @Override
+    public boolean isResponsible(Operator op, JTerm left, JTerm right, Services services, ExecutionContext ec) {
+        return false;
+    }
+
+    @Override
+    public boolean isResponsible(Operator op, JTerm sub, TermServices services, ExecutionContext ec) {
+        return false;
+    }
+
+    @Override
+    public JTerm translateLiteral(Literal lit, Services services) {
+        return null;
+    }
+
+    @Override
+    public @Nullable Function getFunctionFor(Operator op, Services services, ExecutionContext ec) {
+        return null;
+    }
+
+    @Override
+    public boolean hasLiteralFunction(Function f) {
+        return false;
+    }
+
+    @Override
+    public Expression translateTerm(JTerm t, ExtList children, Services services) {
+        return null;
+    }
+}
