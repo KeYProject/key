3e5e375a5961f774f564a01b1e406905665f8f19
Drodt <daniel.drodt@tu-darmstadt.de>
2026-08-14 11:43:01 +0200

    Fix taclet oracle and some rules

--- key.core/src/test/java/de/uka/ilkd/key/speclang/ContractFactoryTest.java
+++ key.core/src/test/java/de/uka/ilkd/key/speclang/ContractFactoryTest.java
@@ -144,9 +144,10 @@ public class ContractFactoryTest {
                 @  signals_only RuntimeException;
                 @*/""";
         JTerm woLabels = calculateCombinedModifiableWOLabels(contract);
-        assertEquals("intersect<Pair<java.lang.Object,Field>>(if-then-else(equals(a,Z(5(#))),empty<Pair<java.lang.Object,Field>>,allLocs),"
-            + "if-then-else(not(equals(a,Z(5(#)))),singleton<Pair<java.lang.Object,Field>>(pair<java.lang.Object,Field>(self,testPackage.TestClass::#l)),"
-            + "allLocs))",
+        assertEquals(
+            "intersect<Pair<java.lang.Object,Field>>(if-then-else(equals(a,Z(5(#))),empty<Pair<java.lang.Object,Field>>,allLocs),"
+                + "if-then-else(not(equals(a,Z(5(#)))),singleton<Pair<java.lang.Object,Field>>(pair<java.lang.Object,Field>(self,testPackage.TestClass::#l)),"
+                + "allLocs))",
             woLabels.toString());
     }
 
