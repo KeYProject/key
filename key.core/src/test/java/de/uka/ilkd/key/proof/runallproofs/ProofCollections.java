67fa0613c2023846381662c8744e652e90557475
Drodt <daniel.drodt@tu-darmstadt.de>
2026-08-14 16:00:00 +0200

    Fix set example

--- key.core/src/test/java/de/uka/ilkd/key/proof/runallproofs/ProofCollections.java
+++ key.core/src/test/java/de/uka/ilkd/key/proof/runallproofs/ProofCollections.java
@@ -1205,7 +1205,7 @@ public class ProofCollections {
 
         g = c.group("PolymorphicSorts");
         g.loadable("standard_key/polymorphic/pseq.key");
-        g.provable("standard_key/polymorphic/setExample.key");
+        g.loadable("standard_key/polymorphic/setExample.proof");
 
         g = c.group("JavaFeatures");
         g.loadable("Java/TextBlockLiterals/project.key");
